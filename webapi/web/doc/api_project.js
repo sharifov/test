cb76574fe29f268e5276bcf6f083a1b499a98648
Alexandr <ac@zeit.style>
2018-08-15 16:02:24 +0300

    SL-2 Update docApi

--- webapi/web/doc/api_project.js
+++ webapi/web/doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-08-15T09:38:39.546Z",
+    "time": "2018-08-15T13:01:22.956Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
