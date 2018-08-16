784e4e0cf48ab780c193aa2beb3a269a391bd779
trumpfort <trumpfort@gmail.com>
2018-08-16 13:51:09 +0300

    View settings only Admin and Supervision

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=sale',
+            'username' => 'sale',
+            'password' => 'SalePasswd1! ',
             'charset' => 'utf8',
         ],
         'mailer' => [
