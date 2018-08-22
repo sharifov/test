9756d8ac81eb2ea3831004e61acf9279bd6debfd
Alexandr <ac@zeit.style>
2018-08-22 08:31:21 +0300

    Fix Quote afterSave

--- common/models/Quote.php
+++ common/models/Quote.php
@@ -341,24 +341,6 @@ class Quote extends \yii\db\ActiveRecord
         return false;
     }
 
-
-    public function afterSave($insert, $changedAttributes)
-    {
-        parent::afterSave($insert, $changedAttributes);
-
-        if (!$insert) {
-            //Add logs after changed model attributes
-            $leadLog = new LeadLog(new LeadLogMessage());
-            $leadLog->logMessage->oldParams = $changedAttributes;
-            $leadLog->logMessage->newParams = array_intersect_key($this->attributes, $changedAttributes);
-            $leadLog->logMessage->title = $insert ? 'Create' : 'Update';
-            $leadLog->logMessage->model = sprintf('%s (%s)', $this->formName(), $this->uid);
-            $leadLog->addLog([
-                'lead_id' => $this->lead_id,
-            ]);
-        }
-    }
-
     public static function parseDump($string, $validation = true, &$itinerary = [])
     {
 
@@ -520,15 +502,15 @@ class Quote extends \yii\db\ActiveRecord
 
         if (!$insert) {
             //Add logs after changed model attributes
-            $leadLog = new LeadLog((new LeadLogMessage()));
+            $leadLog = new LeadLog(new LeadLogMessage());
             $leadLog->logMessage->oldParams = $changedAttributes;
             $leadLog->logMessage->newParams = array_intersect_key($this->attributes, $changedAttributes);
-            $leadLog->logMessage->title = ($insert)
-                ? 'Create' : 'Update';
+            $leadLog->logMessage->title = $insert ? 'Create' : 'Update';
             $leadLog->logMessage->model = sprintf('%s (%s)', $this->formName(), $this->uid);
             $leadLog->addLog([
                 'lead_id' => $this->lead_id,
             ]);
+
 
             if ($this->lead->called_expert &&
                 $changedAttributes['status'] != $this->status &&
