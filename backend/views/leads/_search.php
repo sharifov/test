538539498089d3c339ee0f5186f7791bbbd1f871
trumpfort <trumpfort@gmail.com>
2018-08-30 17:24:30 +0300

    BOCL-42

--- backend/views/leads/_search.php
+++ backend/views/leads/_search.php
@@ -23,10 +23,10 @@ use yii\widgets\ActiveForm;
 
             <div class="row">
                 <div class="col-md-4">
-                    <?= $form->field($model, 'id')->input('number', ['min' => 1]) ?>
+                    <?= $form->field($model, 'id')->input('number', ['min' => 1])->label('Lead Id') ?>
                 </div>
                 <div class="col-md-4">
-                    <?= $form->field($model, 'uid') ?>
+                    <?= $form->field($model, 'uid')->label('Lead UID') ?>
                 </div>
                 <div class="col-md-4">
                     <?= $form->field($model, 'discount_id')->input('number', ['min' => 1]) ?>
@@ -34,23 +34,21 @@ use yii\widgets\ActiveForm;
             </div>
 
             <div class="row">
-                <div class="col-md-4">
-                    <?= $form->field($model, 'client_id')->input('number', ['min' => 1]) ?>
-                </div>
-                <div class="col-md-8">
-                    <?= $form->field($model, 'client_name') ?>
-                </div>
-            </div>
-
-            <div class="row">
                 <div class="col-md-6">
-                    <?= $form->field($model, 'client_email')//->input('email') ?>
+                    <?= $form->field($model, 'client_email') ?>
                 </div>
                 <div class="col-md-6">
-                    <?= $form->field($model, 'client_phone') ?>
+                    <?= $form->field($model, 'client_phone')
+                        ->widget(\borales\extensions\phoneInput\PhoneInput::className(), [
+                            'options' => [
+                                'class' => 'form-control'
+                            ],
+                            'jsOptions' => [
+                                'preferredCountries' => ['us'],
+                            ]
+                        ]) ?>
                 </div>
             </div>
-
 
         </div>
 
@@ -58,120 +56,94 @@ use yii\widgets\ActiveForm;
 
             <div class="row">
                 <div class="col-md-6">
-                    <?php  echo $form->field($model, 'trip_type')->dropDownList(\common\models\Lead::TRIP_TYPE_LIST, ['prompt' => '-']) ?>
+                    <?php echo $form->field($model, 'trip_type')->dropDownList(\common\models\Lead::TRIP_TYPE_LIST, ['prompt' => '-']) ?>
                 </div>
                 <div class="col-md-6">
-                    <?php  echo $form->field($model, 'cabin')->dropDownList(\common\models\Lead::CABIN_LIST, ['prompt' => '-']) ?>
+                    <?php echo $form->field($model, 'cabin')->dropDownList(\common\models\Lead::CABIN_LIST, ['prompt' => '-']) ?>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <?//= $form->field($model, 'status')->dropDownList(\common\models\Lead::STATUS_LIST, ['prompt' => '-']) ?>
-                    <?php
-                        echo $form->field($model, 'statuses')->widget(\kartik\select2\Select2::class, [
-                            'data' => \common\models\Lead::STATUS_LIST,
-                            'size' => \kartik\select2\Select2::SMALL,
-                            'options' => ['placeholder' => 'Select status', 'multiple' => true],
-                            'pluginOptions' => ['allowClear' => true],
-                        ]);
-                    ?>
-
+            <?php if (Yii::$app->user->identity->role != 'agent') : ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <?= $form->field($model, 'source')
+                            ->widget(\kartik\select2\Select2::classname(), [
+                                'data' => \common\models\Source::getGroupList(),
+                                'options' => ['placeholder' => 'All'],
+                                'pluginOptions' => ['multiple' => true, 'allowClear' => true],
+                            ])
+                        ?>
+                    </div>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-4">
-                    <?php  echo $form->field($model, 'adults')->dropDownList(array_combine(range(0, 9), range(0, 9)), ['prompt' => '-']) ?>
-                </div>
-                <div class="col-md-4">
-                    <?php  echo $form->field($model, 'children')->dropDownList(array_combine(range(0, 9), range(0, 9)), ['prompt' => '-']) ?>
-                </div>
-                <div class="col-md-4">
-                    <?php  echo $form->field($model, 'infants')->dropDownList(array_combine(range(0, 9), range(0, 9)), ['prompt' => '-']) ?>
-                </div>
-            </div>
-
-
-
+            <?php endif; ?>
         </div>
 
         <div class="col-md-3">
 
+            <?= $form->field($model, 'statuses')
+                ->widget(\kartik\select2\Select2::class, [
+                    'data' => \common\models\Lead::STATUS_LIST,
+                    'size' => \kartik\select2\Select2::SMALL,
+                    'options' => ['placeholder' => 'Select status', 'multiple' => true],
+                    'pluginOptions' => ['allowClear' => true],
+                ])
+            ?>
 
-            <?php  echo $form->field($model, 'project_id')->dropDownList(\common\models\Project::getList(), ['prompt' => '-']) ?>
+            <?php if (Yii::$app->user->identity->role != 'agent') : ?>
+                <div class="row">
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'created_date_from')->widget(
+                            \dosamigos\datepicker\DatePicker::class, [
+                            'inline' => false,
+                            'clientOptions' => [
+                                'autoclose' => true,
+                                'format' => 'dd-M-yyyy',
+                                'todayBtn' => true
+                            ]
+                        ]); ?>
 
-            <?php  echo $form->field($model, 'source_id')->dropDownList(\common\models\Source::getList(), ['prompt' => '-']) ?>
+                    </div>
 
-
-            <div class="row">
-                <div class="col-md-6">
-                    <?//php  echo $form->field($model, 'created_date_from') ?>
-
-                    <?= $form->field($model, 'created_date_from')->widget(
-                        \dosamigos\datepicker\DatePicker::class, [
-                        'inline' => false,
-                        //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
-                        'clientOptions' => [
-                            'autoclose' => true,
-                            'format' => 'dd-M-yyyy',
-                            'todayBtn' => true
-                        ]
-                    ]);?>
-
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'created_date_to')->widget(
+                            \dosamigos\datepicker\DatePicker::class, [
+                            'inline' => false,
+                            //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+                            'clientOptions' => [
+                                'autoclose' => true,
+                                'format' => 'dd-M-yyyy',
+                                'todayBtn' => true
+                            ]
+                        ]); ?>
+                    </div>
                 </div>
-
-                <div class="col-md-6">
-                    <?//php  echo $form->field($model, 'created_date_to') ?>
-                    <?= $form->field($model, 'created_date_to')->widget(
-                        \dosamigos\datepicker\DatePicker::class, [
-                        'inline' => false,
-                        //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
-                        'clientOptions' => [
-                            'autoclose' => true,
-                            'format' => 'dd-M-yyyy',
-                            'todayBtn' => true
-                        ]
-                    ]);?>
-                </div>
-            </div>
-
-            <?//= $form->field($model, 'employee_id') ?>
-
-
+            <?php endif; ?>
         </div>
 
         <div class="col-md-3">
-        <?php  // echo $form->field($model, 'request_ip_detail') ?>
-
-        <?php  // echo $form->field($model, 'offset_gmt') ?>
-
-        <?//php  echo $form->field($model, 'snooze_for') ?>
-
             <div class="row">
                 <div class="col-md-6">
-                    <?php  echo $form->field($model, 'rating')->dropDownList(array_combine(range(1, 3), range(1, 3)), ['prompt' => '-']) ?>
+                    <?php echo $form->field($model, 'rating')->dropDownList(array_combine(range(1, 3), range(1, 3)), ['prompt' => '-']) ?>
                 </div>
 
-                <?//php  echo $form->field($model, 'called_expert') ?>
                 <div class="col-md-6">
-                    <?php  echo $form->field($model, 'employee_id')->dropDownList(\common\models\Employee::getList(), ['prompt' => '-']) ?>
+                    <?php echo $form->field($model, 'bo_flight_id')->label('BO Sale ID') ?>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <?php  echo $form->field($model, 'bo_flight_id')->label('BO Sale ID') ?>
+            <?php if (Yii::$app->user->identity->role != 'agent') : ?>
+                <div class="row">
+                    <div class="col-md-6">
+                        <?php echo $form->field($model, 'employee_id')->dropDownList(\common\models\Employee::getList(), ['prompt' => '-']) ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'request_ip')
+                            ->widget(\yii\widgets\MaskedInput::class, [
+                                'clientOptions' => [
+                                    'alias' => 'ip'
+                                ],
+                            ]) ?>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <?php  echo $form->field($model, 'request_ip') ?>
-                </div>
-            </div>
-
-
-        <?php  echo $form->field($model, 'notes_for_experts') ?>
-
-        <?//php  echo $form->field($model, 'bo_flight_id') ?>
+            <?php endif; ?>
         </div>
     </div>
 
