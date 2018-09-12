7aeca59d927db8bddb58108a4c1b52eac4b09c87
Alexandr <ac@zeit.style>
2018-09-12 12:47:59 +0300

    SL-9 Fixed search form - action URL

--- frontend/views/leads/_search.php
+++ frontend/views/leads/_search.php
@@ -11,7 +11,7 @@ use yii\widgets\ActiveForm;
 <div class="lead-search">
 
     <?php $form = ActiveForm::begin([
-        'action' => ['index'],
+        //'action' => ['index'],
         'method' => 'get',
         'options' => [
             'data-pjax' => 1
