a93b2b42dfdca6af6d55fce434a8c62121da337a
Olga <om@zeit.style>
2018-10-01 19:08:14 +0300

    Changed Email template for offer (Arangrant Project)

--- common/views/mail/layouts/arangrant/_forOffer.php
+++ common/views/mail/layouts/arangrant/_forOffer.php
@@ -10,11 +10,10 @@
 
 <body style="-ms-text-size-adjust: 100%; -webkit-text-size-adjust: none; background-color: #EBEFF8; box-sizing: border-box; color: #333; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-size: 15px; font-weight: normal; line-height: 1.3; margin: 0; min-width: 100%; padding: 0; text-align: left; width: 100% !important;">
 <table width="100%" border="0" cellpadding="0" cellspacing="0" class="body"
-       style="background: #EBEFF8; border-collapse: collapse; border-spacing: 0; color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; height: 100%; line-height: 1.3; margin: 0; padding: 0; text-align: left; vertical-align: top; width: 100%;">
-    <tbody>
+       style="background: #eff2f9; background-color: #F2F6F7; border-collapse: collapse; border-spacing: 0; color: #0a0a0a; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-size: 14px; font-weight: normal; height: 100%; line-height: 1.3; margin: 0; padding: 0; text-align: left; vertical-align: top; width: 100%;">
     <tr style="padding: 0; text-align: left; vertical-align: top;">
         <td class="wrapper-inner" align="center" valign="top"
-            style="-moz-hyphens: auto; -webkit-hyphens: auto; border-collapse: collapse !important; color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; hyphens: auto; line-height: 1.3; margin: 0; max-width: 808px !important; min-width: 808px !important; padding: 110px 20px !important; text-align: left; vertical-align: top; width: 808px !important; word-wrap: break-word;">
+            style="-moz-hyphens: auto; -webkit-hyphens: auto; border-collapse: collapse !important; color: #0a0a0a; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; hyphens: auto; line-height: 1.3; margin: 0; max-width: 808px !important; min-width: 808px !important; padding: 40px 20px !important; text-align: left; vertical-align: top; width: 808px !important; word-wrap: break-word;">
             <table width="768" align="center" border="0" cellpadding="0" cellspacing="0"
                    class="container content-wrapper main-content"
                    style="background: #fefefe; background-color: #ffffff; border: 1px solid #DDDDDD; border-collapse: collapse; border-radius: 0; border-spacing: 0; margin: 0 auto; margin-bottom: 20px; max-width: 768px !important; min-width: 768px !important; padding: 0; text-align: inherit; vertical-align: top; width: 768px !important;">
@@ -26,30 +25,26 @@
                                style="border-collapse: collapse; border-spacing: 0; padding: 0; text-align: left; vertical-align: top; width: 100% !important;">
 
                             <!--Header-->
-                            <tbody>
+
                             <tr style="padding: 0; text-align: left; vertical-align: top;">
                                 <td class="header"
-                                    style="-moz-hyphens: auto; -webkit-hyphens: auto; border-bottom: 1px solid #F5F7FC; border-collapse: collapse !important; color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; hyphens: auto; line-height: 1.3; margin: 0; padding: 0; text-align: left; vertical-align: top; word-wrap: break-word;">
+                                    style="-moz-hyphens: auto; -webkit-hyphens: auto; border-bottom: 1px solid #F5F7FC; border-collapse: collapse !important; color: #0a0a0a; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; hyphens: auto; line-height: 1.3; margin: 0; padding: 0; text-align: left; vertical-align: top; word-wrap: break-word;">
                                     <table class="row"
                                            style="border-collapse: collapse; border-spacing: 0; display: table; padding: 0; position: relative; text-align: left; vertical-align: top; width: 100%;">
                                         <tr style="padding: 0; text-align: left; vertical-align: top;">
                                             <th class="small-6 large-6 columns header-logo" valign="middle"
-                                                align="center"
-                                                style="color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; height: 120px; line-height: 1.3; margin: 0 auto; min-height: 120px; padding: 0 40px !important; padding-bottom: 16px; padding-left: 24px; padding-right: 8px; text-align: left; vertical-align: middle !important; width: 50%;">
-                                                <img src="<?= $project->link ?>/images/logo-colored.png"
-                                                     alt="<?= $project->name ?>"
-                                                     width="179"
-                                                     height="30" class="logo float-center" align="center"
-                                                     style="-ms-interpolation-mode: bicubic; clear: both; display: block; float: none; height: 30px; margin: 0 auto; max-width: 100%; outline: none; text-align: center; text-decoration: none; width: 179px;">
+                                                style="background-color: #212133; color: #0a0a0a; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; height: 70px; line-height: 1.3; margin: 0 auto; min-height: 70px; padding: 0 32px !important; padding-bottom: 16px; padding-left: 24px; padding-right: 8px; text-align: left; vertical-align: middle !important; width: 50%;">
+                                                <img src="<?= $project->link ?>/images/logo.png"
+                                                     alt="<?= $project->name ?>" width="83" height="30" class="logo"
+                                                     align="center"
+                                                     style="-ms-interpolation-mode: bicubic; clear: both; display: block; height: 30px; max-width: 100%; outline: none; text-decoration: none; width: 83px;">
                                             </th>
                                             <th class="small-6 large-6 columns header-agent" valign="middle"
-                                                style="background-color: #F5F7FC; color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; height: 120px; line-height: 1.3; margin: 0 auto; min-height: 120px; padding: 0 40px !important; padding-bottom: 16px; padding-left: 24px; padding-right: 8px; text-align: left; vertical-align: middle !important; width: 50%;">
+                                                style="background-color: #212133; color: #0a0a0a; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; height: 70px; line-height: 1.3; margin: 0 auto; min-height: 70px; padding: 0 32px !important; padding-bottom: 16px; padding-left: 24px; padding-right: 8px; text-align: right; vertical-align: middle !important; width: 50%;">
                                                 <div class="agent-name"
-                                                     style="color: #556383; font-size: 14px; height: 16px; line-height: 20px; padding-top: 8px;">
-                                                    <?= $agentName ?>
-                                                </div>
+                                                     style="color: #fff; font-size: 14px; height: 16px; line-height: 20px; padding-bottom: 4px;"><?= $agentName ?></div>
                                                 <div class="agent-phone"
-                                                     style="color: #1C2F59; font-size: 24px; font-weight: bold; line-height: 1.2em; text-decoration: none;">
+                                                     style=";text-decoration: none; color: #FEB562; font-size: 20px; font-weight: bold; line-height: 1.2em;">
                                                     <?php if ($sellerContactInfo !== null && !empty($sellerContactInfo->direct_line)) {
                                                         echo $sellerContactInfo->direct_line;
                                                     } else {
@@ -58,7 +53,7 @@
                                                 </div>
                                             </th>
                                             <th class="expander"
-                                                style="color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left;"></th>
+                                                style="color: #0a0a0a; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left;"></th>
                                         </tr>
                                     </table>
                                 </td>
@@ -67,7 +62,6 @@
                             <!-- Body  -->
                             <?= $body ?>
 
-                            </tbody>
                         </table>
                     </td>
                 </tr>
@@ -167,6 +161,5 @@
 
         </td>
     </tr>
-    </tbody>
 </table>
 </body>
