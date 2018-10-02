a7605cefcd2f095efc1384d3d38a1c4725e41403
Olga <om@zeit.style>
2018-10-02 10:45:34 +0300

    offer email improvements

--- common/views/mail/layouts/arangrant/_forOffer.php
+++ common/views/mail/layouts/arangrant/_forOffer.php
@@ -93,12 +93,12 @@
                                                 <?php if ($sellerContactInfo !== null) : ?>
                                                     E-Mail: <a href="mailto:<?= $sellerContactInfo->email_user ?>"
                                                                class="footer-email"
-                                                               style="color: #4ECCC4; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left; text-decoration: none;"><?= $sellerContactInfo->email_user ?></a>
+                                                               style="color: #3C63BC; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left; text-decoration: none;"><?= $sellerContactInfo->email_user ?></a>
                                                     <br>
                                                 <?php else : ?>
                                                     E-Mail: <a href="mailto:<?= $employee->email ?>"
                                                                class="footer-email"
-                                                               style="color: #4ECCC4; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left; text-decoration: none;"><?= $employee->email ?></a>
+                                                               style="color: #3C63BC; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left; text-decoration: none;"><?= $employee->email ?></a>
                                                     <br>
                                                 <?php endif; ?>
                                                 <?php if ($sellerContactInfo !== null && !empty($sellerContactInfo->direct_line)) {
@@ -116,17 +116,6 @@
                                     <table class="row"
                                            style="border-collapse: collapse; border-spacing: 0; display: table; padding: 0; position: relative; text-align: left; vertical-align: top; width: 100%;">
                                         <tr style="padding: 0; text-align: left; vertical-align: top;">
-                                            <th class="small-12 large-12 columns first last" align="center"
-                                                style="color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0 auto; padding: 0; padding-bottom: 16px; padding-left: 24px; padding-right: 24px; text-align: left; width: 100%;">
-                                                <img src="<?= $project->link ?>/images/logo-colored.png" alt="Logo"
-                                                     width="139" height="24"
-                                                     class="footer-logo float-center"
-                                                     style="-ms-interpolation-mode: bicubic; clear: both; display: block; float: none; margin: 0 auto; margin-bottom: 15px; max-width: 100%; outline: none; text-align: center; text-decoration: none; width: auto;">
-                                            </th>
-                                            <th class="expander"
-                                                style="color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left;"></th>
-                                        </tr>
-                                        <tr style="padding: 0; text-align: left; vertical-align: top;">
                                             <th class="small-12 large-12 columns first last footer-text text-center"
                                                 align="center"
                                                 style="color: #556383; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.4em; margin: 0 auto; padding: 0; padding-bottom: 16px; padding-left: 24px; padding-right: 24px; text-align: center; width: 100%;">
@@ -136,17 +125,6 @@
                                                 <div class="footer-copyright" style="color: #556383;">
                                                     © 2018 The Travel Outlet of Virginia, LLC. All rights reserved.
                                                 </div>
-                                            </th>
-                                            <th class="expander"
-                                                style="color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left;"></th>
-                                        </tr>
-                                        <tr style="padding: 0; text-align: left; vertical-align: top;">
-                                            <th class="small-12 large-12 columns first last" align="center"
-                                                style="color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0 auto; padding: 0; padding-bottom: 16px; padding-left: 24px; padding-right: 24px; text-align: left; width: 100%;">
-                                                <img src="<?= $project->link ?>/images/partners.png" alt="Partners"
-                                                     width="281" height="40"
-                                                     class="footer-partners float-center"
-                                                     style="-ms-interpolation-mode: bicubic; clear: both; display: block; float: none; margin: 20px auto 0; max-width: 100%; outline: none; text-align: center; text-decoration: none; width: auto;">
                                             </th>
                                             <th class="expander"
                                                 style="color: #1C2F59; font-family: 'Roboto', Arial, Helvetica, sans-serif; font-weight: normal; line-height: 1.3; margin: 0; padding: 0; text-align: left;"></th>
