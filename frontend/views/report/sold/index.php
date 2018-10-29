26a29dbc142987b2e1af32057e5489b7ddbeb7e0
Alexandr <ac@zeit.style>
2018-10-29 17:00:12 +0200

    Fix: Added modal dialog for Reports Sold

--- frontend/views/report/sold/index.php
+++ frontend/views/report/sold/index.php
@@ -186,3 +186,18 @@ $this->registerJs($js);
         </div>
     </div>
 </div>
+
+
+<div class="modal modal-events fade" id="modal-report-info" style="display: none;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                Sold Leads
+                <button type="button" class="close" data-dismiss="modal">
+                    <i class="fa fa-times"></i>
+                </button>
+            </div>
+            <div class="modal-body"></div>
+        </div>
+    </div>
+</div>
