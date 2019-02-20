5cd1b395d14e2146c9b355af3742fefefcb1790c
senea <senea777@gmail.com>
2019-02-20 11:21:18 +0200

    feat: Browser Phone on sale. COM-63

--- common/components/CommunicationService.php
+++ common/components/CommunicationService.php
@@ -75,7 +75,6 @@ class CommunicationService extends Component
         $url = $this->url . $action;
 
         //$options = ['RETURNTRANSFER' => 1];
-        \Yii::error(VarDumper::dumpAsString($url, 10), 'CommunicationService::sendRequest:url');
 
         $this->request->setMethod($method)
             ->setUrl($url)
