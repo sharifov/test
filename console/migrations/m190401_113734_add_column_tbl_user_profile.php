763c7fcc7520d3e4dfc8f2e39dbaeb361efb70ce
Alexandr <ac@zeit.style>
2019-04-05 14:21:36 +0300

    feat: update migrate (SL-257)

--- console/migrations/m190401_113734_add_column_tbl_user_profile.php
+++ console/migrations/m190401_113734_add_column_tbl_user_profile.php
@@ -18,11 +18,9 @@ class m190401_113734_add_column_tbl_user_profile extends Migration
         $this->addColumn('{{%user_profile}}', 'up_skill', $this->tinyInteger()->defaultValue(0));
 
         $this->addColumn('{{%leads}}', 'l_call_status_id', $this->tinyInteger()->defaultValue(0));
-        //$this->addColumn('{{%leads}}', 'l_call_rating', $this->smallInteger()->defaultValue(0));
         $this->addColumn('{{%leads}}', 'l_pending_delay_dt', $this->dateTime());
 
         $this->createIndex('IND-leads_l_call_status_id', '{{%leads}}', ['l_call_status_id']);
-        //$this->createIndex('IND-leads_l_call_rating', '{{%leads}}', ['l_call_rating']);
         $this->createIndex('IND-leads_l_pending_delay_dt', '{{%leads}}', ['l_pending_delay_dt']);
     }
 
@@ -36,8 +34,7 @@ class m190401_113734_add_column_tbl_user_profile extends Migration
         $this->dropColumn('{{%user_profile}}', 'up_skill');
 
         $this->dropColumn('{{%leads}}', 'l_call_status_id');
-        $this->dropColumn('{{%leads}}', 'l_call_rating');
-        //$this->dropColumn('{{%leads}}', 'l_pending_delay_dt');
+        $this->dropColumn('{{%leads}}', 'l_pending_delay_dt');
     }
 
 }
