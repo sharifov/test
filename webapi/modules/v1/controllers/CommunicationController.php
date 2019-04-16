9c479ae53a8a994bc38cab4378ec5db763abaca6
senea <senea777@gmail.com>
2019-04-16 13:13:23 +0300

    feat: (SL-244) Phone calls callback fix: redirect direct call to agent

--- webapi/modules/v1/controllers/CommunicationController.php
+++ webapi/modules/v1/controllers/CommunicationController.php
@@ -1103,7 +1103,7 @@ class CommunicationController extends ApiBaseController
                     }
 
                     if($call->c_call_status && isset($post['callData']['status'])) {
-                        if(!in_array($call->c_call_status, [Call::CALL_STATUS_CANCELED, Call::CALL_STATUS_NO_ANSWER, Call::CALL_STATUS_FAILED])) {
+                        if(!in_array($call->c_call_status, [Call::CALL_STATUS_CANCELED, Call::CALL_STATUS_BUSY, Call::CALL_STATUS_NO_ANSWER, Call::CALL_STATUS_FAILED])) {
                             $call->c_call_status = $post['callData']['status'];
                         }
                     }
