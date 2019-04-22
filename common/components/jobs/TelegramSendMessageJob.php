59fa69830c51fdfa12c83a70700f664dcca8cfbd
Alexandr <ac@zeit.style>
2019-04-22 20:11:30 +0300

    fix: disable debug info (SL-282)

--- common/components/jobs/TelegramSendMessageJob.php
+++ common/components/jobs/TelegramSendMessageJob.php
@@ -38,7 +38,7 @@ class TelegramSendMessageJob extends BaseObject implements JobInterface
             if($this->user_id) {
                 $profile = UserProfile::find()->where(['up_user_id' => $this->user_id])->limit(1)->one();
 
-                Yii::info($this->user_id, 'info\TelegramJob:execute:info');
+                //Yii::info($this->user_id, 'info\TelegramJob:execute:info');
 
                 if ($profile && $profile->up_telegram && $profile->up_telegram_enable) {
 
