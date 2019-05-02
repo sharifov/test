4fce816197717e2981bec9a761567987a23aa3d5
Alexandr <ac@zeit.style>
2019-05-02 15:35:36 +0300

    feat: add migration lead_call_expert (SL-304)

---
+++ console/migrations/m190502_120134_create_tbl_lead_call_expert.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190502_120134_create_tbl_lead_call_expert
+ */
+class m190502_120134_create_tbl_lead_call_expert extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+
+    }
+
+
+}
