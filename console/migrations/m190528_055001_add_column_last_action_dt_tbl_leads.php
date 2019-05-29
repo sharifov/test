4ea5d453757fe688a7da245cbb88b8d5167c687c
Alexandr <ac@zeit.style>
2019-05-29 11:37:05 +0300

    feat: update migration - autocomplete field last_acton_dt (SL-361)

--- console/migrations/m190528_055001_add_column_last_action_dt_tbl_leads.php
+++ console/migrations/m190528_055001_add_column_last_action_dt_tbl_leads.php
@@ -14,6 +14,13 @@ class m190528_055001_add_column_last_action_dt_tbl_leads extends Migration
     {
         $this->addColumn('{{%leads}}', 'l_last_action_dt', $this->dateTime());
         $this->createIndex('IND-leads_l_last_action_dt', '{{%leads}}', ['l_last_action_dt']);
+
+        if (Yii::$app->cache) {
+            Yii::$app->cache->flush();
+        }
+
+        Yii::$app->db->getSchema()->refreshTableSchema('{{%leads}}');
+        $this->execute('UPDATE leads SET l_last_action_dt = updated');
     }
 
     /**
@@ -23,5 +30,11 @@ class m190528_055001_add_column_last_action_dt_tbl_leads extends Migration
     {
         $this->dropIndex('IND-leads_l_last_action_dt', '{{%leads}}');
         $this->dropColumn('{{%leads}}', 'l_last_action_dt');
+
+        if (Yii::$app->cache) {
+            Yii::$app->cache->flush();
+        }
+
+        Yii::$app->db->getSchema()->refreshTableSchema('{{%leads}}');
     }
 }
