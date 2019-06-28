e74a7fb5686f53940455f3f746818449d51a57c3
Alexandr <ac@zeit.style>
2019-06-28 11:49:05 +0300

    fix: update migration - add roles (SL-442)

--- console/migrations/m190628_075842_add_role_action_permission.php
+++ console/migrations/m190628_075842_add_role_action_permission.php
@@ -14,15 +14,14 @@ class m190628_075842_add_role_action_permission extends Migration
     {
         $auth = Yii::$app->authManager;
 
-        $admin = $auth->getRole('admin');
-        $agent = $auth->getRole('agent');
-        $supervision = $auth->getRole('supervision');
 
         $permission = $auth->createPermission('/notifications/pjax-notify');
         $auth->add($permission);
-        $auth->addChild($admin, $permission);
-        $auth->addChild($agent, $permission);
-        $auth->addChild($supervision, $permission);
+        $auth->addChild($auth->getRole('admin'), $permission);
+        $auth->addChild($auth->getRole('agent'), $permission);
+        $auth->addChild($auth->getRole('supervision'), $permission);
+        $auth->addChild($auth->getRole('qa'), $permission);
+        $auth->addChild($auth->getRole('userManager'), $permission);
 
         if (Yii::$app->cache) {
             Yii::$app->cache->flush();
