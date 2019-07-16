47c57c65899985c19f190d658c0ff96bc711548a
serge.murphy <serge.murphy@techork.com>
2019-07-16 12:04:35 +0300

    feat: added deferredeventdispatcher on transactionmanager (SL-411)

--- sales/services/TransactionManager.php
+++ sales/services/TransactionManager.php
@@ -2,19 +2,32 @@
 
 namespace sales\services;
 
+use sales\dispatchers\DeferredEventDispatcher;
+
 class TransactionManager
 {
+    private $dispatcher;
+
+    public function __construct(DeferredEventDispatcher $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
     public function wrap(callable $function)
     {
         $result = null;
         $transaction = \Yii::$app->db->beginTransaction();
         try {
+            $this->dispatcher->defer();
             $result = $function();
             $transaction->commit();
+            $this->dispatcher->release();
         } catch (\Exception $e) {
             $transaction->rollBack();
+            $this->dispatcher->clean();
             throw $e;
         }
         return $result;
     }
+
 }
