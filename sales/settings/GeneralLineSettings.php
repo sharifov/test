787620acd7424b2f68c4df8b9f3274f39eab7a23
serge.murphy <serge.murphy@techork.com>
2019-07-16 11:43:13 +0300

    feat: created class generalsettings (SL-411)

--- sales/settings/GeneralLineSettings.php
+++ sales/settings/GeneralLineSettings.php
@@ -1,7 +1,40 @@
 <?php
 
+namespace sales\settings;
 
-class GeneralLineSettings
+use Yii;
+use yii\base\BaseObject;
+
+/**
+ * Class GeneralLineSettings
+ * @property  $use_general_line_distribution
+ * @property  $general_line_leads_limit
+ * @property  $general_line_role_priority
+ * @property  $general_line_last_hours
+ * @property  $general_line_user_limit
+ * @property  $direct_agent_user_limit
+ */
+class GeneralLineSettings extends BaseObject
 {
+    public $use_general_line_distribution;
+    public $general_line_leads_limit;
+    public $general_line_role_priority;
+    public $general_line_last_hours;
+    public $general_line_user_limit;
+    public $direct_agent_user_limit;
 
+    public function __construct($config = [])
+    {
+        $settings = Yii::$app->params['settings'];
+        $general_line_call_distribution = Yii::$app->params['general_line_call_distribution'];
+
+        $this->use_general_line_distribution = $settings['use_general_line_distribution'] ?? $general_line_call_distribution['use_general_line_distribution'];
+        $this->general_line_leads_limit = $settings['general_line_leads_limit'] ?? $general_line_call_distribution['general_line_leads_limit'];
+        $this->general_line_role_priority = (int)($settings['general_line_role_priority'] ?? $general_line_call_distribution['general_line_role_priority']);
+        $this->general_line_last_hours = $settings['general_line_last_hours'] ?? $general_line_call_distribution['general_line_last_hours'];
+        $this->general_line_user_limit = $settings['general_line_user_limit'] ?? $general_line_call_distribution['general_line_user_limit'];
+        $this->direct_agent_user_limit = $settings['direct_agent_user_limit'] ?? $general_line_call_distribution['direct_agent_user_limit'];
+
+        parent::__construct($config);
+    }
 }
