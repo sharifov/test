401dc190b98e3ccc267882854dea9d5630274c5b
serge.murphy <serge.murphy@techork.com>
2019-07-17 16:41:10 +0300

    feat: added methods find, findbyphone for userprojectrepository (SL-411)

--- sales/repositories/user/UserProjectParamsRepository.php
+++ sales/repositories/user/UserProjectParamsRepository.php
@@ -1,10 +1,52 @@
 <?php
 
-
 namespace sales\repositories\user;
 
+use common\models\UserProjectParams;
 
 class UserProjectParamsRepository
 {
+
+    /**
+     * @param string $phone
+     * @return UserProjectParams|null
+     */
+    public function findByPhone(string $phone): ?UserProjectParams
+    {
+        return UserProjectParams::find()
+            ->where(['upp_phone_number' => $phone])
+            ->orWhere(['upp_tw_phone_number' => $phone])
+            ->limit(1)
+            ->one();
+    }
+
+    /**
+     * @param int|null $projectId
+     * @param string|null $from
+     * @param string|null $to
+     * @return UserProjectParams|null
+     */
+    public function find(?int $projectId, ?string $from, ?string $to): ?UserProjectParams
+    {
+        $upp = null;
+
+        if ($projectId && $from) {
+            $agentId = (int)str_replace('client:seller', '', $from);
+            if ($agentId) {
+                $upp = UserProjectParams::find()->where(['upp_user_id' => $agentId, 'upp_project_id' => $projectId])->one();
+            }
+        }
+
+        if (!$upp) {
+            $upp = $this->findByPhone($from);
+        }
+
+        if (!$upp) {
+            $upp = $this->findByPhone($to);
+        }
+
+        return $upp;
+
+    }
 
 }
