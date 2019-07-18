07923cbe35a051e3c7bb44f60553526ba7a323b8
serge.murphy <serge.murphy@techork.com>
2019-07-18 11:19:21 +0300

    feat: changed attributes name (SL-411)

--- sales/events/lead/LeadStatusChangedEvent.php
+++ sales/events/lead/LeadStatusChangedEvent.php
@@ -7,29 +7,29 @@ use common\models\Lead;
 /**
  * Class LeadStatusChangedEvent
  * @property Lead $lead
- * @property $oldStatus
+ * @property int|null $oldStatus
  * @property int $newStatus
- * @property $employeeId
+ * @property int|null $ownerId
  */
 class LeadStatusChangedEvent
 {
     public $lead;
     public $oldStatus;
     public $newStatus;
-    public $employeeId;
+    public $ownerId;
 
     /**
      * LeadStatusChangedEvent constructor.
      * @param Lead $lead
-     * @param $oldStatus
+     * @param int|null $oldStatus
      * @param int $newStatus
-     * @param $employeeId
+     * @param int|null $ownerId
      */
-    public function __construct(Lead $lead, $oldStatus, int $newStatus, $employeeId)
+    public function __construct(Lead $lead, ?int $oldStatus, int $newStatus, ?int $ownerId)
     {
         $this->lead = $lead;
         $this->oldStatus = $oldStatus;
         $this->newStatus = $newStatus;
-        $this->employeeId = $employeeId;
+        $this->ownerId = $ownerId;
     }
 }
