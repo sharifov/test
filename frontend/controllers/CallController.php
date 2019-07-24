6075bac7460571dd8f44de1a7146010b4bd40706
Alexandr <ac@zeit.style>
2019-07-24 12:58:55 +0300

    feat: added filter by time DateRangePicker (SL-469) feat: update template Call List search

--- frontend/controllers/CallController.php
+++ frontend/controllers/CallController.php
@@ -51,8 +51,8 @@ class CallController extends FController
             $params['CallSearch']['supervision_id'] = Yii::$app->user->id;
         }
 
-        $searchModel->datetime_start = date('Y-m-d', strtotime('-0 day'));
-        $searchModel->datetime_end = date('Y-m-d');
+        //$searchModel->datetime_start = date('Y-m-d', strtotime('-0 day'));
+        //$searchModel->datetime_end = date('Y-m-d');
 
         $dataProvider = $searchModel->search($params);
 
