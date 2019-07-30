f9cfdd003733c1c6d46831cb85c6b265cf673d61
Alexandr <ac@zeit.style>
2019-07-30 09:13:22 +0300

    fix: updated migration - default limit 10 (SL-480)

--- console/migrations/m190729_093920_add_column_tbl_user_params.php
+++ console/migrations/m190729_093920_add_column_tbl_user_params.php
@@ -12,11 +12,13 @@ class m190729_093920_add_column_tbl_user_params extends Migration
      */
     public function safeUp()
     {
-        $this->addColumn('{{%user_params}}', 'up_call_expert_limit', $this->smallInteger()->defaultValue(0));
+        $this->addColumn('{{%user_params}}', 'up_call_expert_limit', $this->smallInteger()->defaultValue(-1));
         Yii::$app->db->getSchema()->refreshTableSchema('{{%user_params}}');
         if (Yii::$app->cache) {
             Yii::$app->cache->flush();
         }
+
+        \common\models\UserParams::updateAll(['up_call_expert_limit' => 10]);
     }
 
     /**
