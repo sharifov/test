8b696e3f4c5a70f01b891b241d417c15980c7ac2
serge.murphy <serge.murphy@techork.com>
2019-08-07 13:57:25 +0300

    feat: added show count updated and deleted phones after migration (SL-466)

--- console/migrations/m190807_104601_update_phones_on_db.php
+++ console/migrations/m190807_104601_update_phones_on_db.php
@@ -13,19 +13,25 @@ class m190807_104601_update_phones_on_db extends Migration
      */
     public function safeUp()
     {
+        $countUpdated = 0;
+        $countDeleted = 0;
         foreach (ClientPhone::find()->select(['id', 'phone'])->asArray()->batch() as $phones) {
             foreach ($phones as $phone) {
                 $lastPhone = preg_replace('~[\D]~', '', $phone['phone']);
                 if (!$lastPhone) {
                     ClientPhone::deleteAll(['id' => $phone['id']]);
+                    $countDeleted++;
                     continue;
                 }
                 $newPhone = '+' . $lastPhone;
                 if ($newPhone !== $phone['phone']) {
                     ClientPhone::updateAll(['phone' => $newPhone], ['id' => $phone['id']]);
+                    $countUpdated++;
                 }
             }
         }
+        echo 'updated: ' . $countUpdated . ' phones' . PHP_EOL;
+        echo 'deleted: ' . $countDeleted . ' phones' . PHP_EOL;
     }
 
     /**
@@ -34,6 +40,6 @@ class m190807_104601_update_phones_on_db extends Migration
     public function safeDown()
     {
         echo "m190807_104601_update_phones_on_db cannot be reverted.\n";
-        return false;
+        return true;
     }
 }
