60cbb8300562298dbcdb33bc9296b1aa39d8b56b
serge.murphy <serge.murphy@techork.com>
2019-10-02 16:46:07 +0300

    feat: added leadquery, added active method for lead (SL-615)

--- common/models/LeadQuery.php
+++ common/models/LeadQuery.php
@@ -1,10 +1,23 @@
 <?php
 
-
 namespace common\models;
 
+use yii\db\ActiveQuery;
 
-class LeadQuery
+/**
+ * Class LeadQuery
+ */
+class LeadQuery extends ActiveQuery
 {
+
+    /**
+     * @return $this
+     */
+    public function active(): self
+    {
+        return $this->andWhere(['status' => [
+            Lead::STATUS_ON_HOLD, Lead::STATUS_PROCESSING, Lead::STATUS_SNOOZE, Lead::STATUS_FOLLOW_UP
+        ]]);
+    }
 
 }
