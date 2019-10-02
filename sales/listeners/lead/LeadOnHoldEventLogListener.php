23b13370447ab0e84468f3f1c4ca6f27ccb9107f
serge.murphy <serge.murphy@techork.com>
2019-10-02 16:56:18 +0300

    feat: add lead listener (SL-615)

--- sales/listeners/lead/LeadOnHoldEventLogListener.php
+++ sales/listeners/lead/LeadOnHoldEventLogListener.php
@@ -1,10 +1,47 @@
 <?php
 
-
 namespace sales\listeners\lead;
 
+use sales\helpers\user\UserFinder;
+use Yii;
+use sales\events\lead\LeadOnHoldEvent;
+use common\models\Lead;
+use sales\services\lead\LeadFlowLogService;
 
+/**
+ * Class LeadOnHoldEventLogListener
+ *
+ * @property LeadFlowLogService $leadFlowLogService
+ */
 class LeadOnHoldEventLogListener
 {
+    private $leadFlowLogService;
 
+    /**
+     * @param LeadFlowLogService $leadFlowLogService
+     */
+    public function __construct(LeadFlowLogService $leadFlowLogService)
+    {
+        $this->leadFlowLogService = $leadFlowLogService;
+    }
+
+    /**
+     * @param LeadOnHoldEvent $event
+     */
+    public function handle(LeadOnHoldEvent $event): void
+    {
+        try {
+            $this->leadFlowLogService->log(
+                $event->lead->id,
+                Lead::STATUS_ON_HOLD,
+                $event->oldStatus,
+                $event->ownerId,
+                UserFinder::getCurrentUserId(),
+                $event->description,
+                $event->created
+            );
+        } catch (\Throwable $e) {
+            Yii::error($e, 'Listeners:LeadOnHoldEventLogListener');
+        }
+    }
 }
