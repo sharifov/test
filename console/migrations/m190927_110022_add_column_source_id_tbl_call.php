c48662b4fd483165b87b8a0bef1d1f4ce93ae767
AlexConnor <alex.connor@techork.com>
2019-10-03 12:52:38 +0300

    fix: update migration m190927_110022_add_column_source_id_tbl_call - CASCADE DELETE (SL-608)

--- console/migrations/m190927_110022_add_column_source_id_tbl_call.php
+++ console/migrations/m190927_110022_add_column_source_id_tbl_call.php
@@ -13,7 +13,7 @@ class m190927_110022_add_column_source_id_tbl_call extends Migration
     public function safeUp()
     {
 		$this->addColumn('{{%call}}', 'c_source_id', $this->integer());
-		$this->addForeignKey('FK-call_c_source_id', '{{%call}}', ['c_source_id'], '{{%sources}}', ['id']);
+		$this->addForeignKey('FK-call_c_source_id', '{{%call}}', ['c_source_id'], '{{%sources}}', ['id'], 'SET NULL', 'CASCADE');
 
 		Yii::$app->db->getSchema()->refreshTableSchema('{{%call}}');
 
