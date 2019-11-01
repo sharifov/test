0d71e6fd02abc24647eb8ed93fdf7e67a87c48aa
AlexConnor <alex.connor@techork.com>
2019-11-01 16:39:40 +0200

    feat: updated lead view - client info (SL-714)

--- frontend/views/lead/client-info/_client_manage_name.php
+++ frontend/views/lead/client-info/_client_manage_name.php
@@ -13,15 +13,18 @@ use yii\widgets\Pjax;
 
 <table class="table table-bordered table-condensed">
 	<tr>
-		<td style="width: 40%; background-color: #eef3f9"><?= $client->getAttributeLabel('firstName') ?></td>
+        <td style="width: 32px; background-color: #eef3f9"><i class="fa fa-user"></i></td>
+		<td style="width: 100px; background-color: #eef3f9"><?= $client->getAttributeLabel('firstName') ?></td>
 		<td><?= \yii\helpers\Html::encode($client->first_name) ?></td>
 	</tr>
 	<tr>
+        <td style="background-color: #eef3f9"><i class="fa fa-user"></i></td>
 		<td style="background-color: #eef3f9"><?= $client->getAttributeLabel('last_name') ?></td>
 		<td><?= \yii\helpers\Html::encode($client->last_name) ?></td>
 	</tr>
 	<?php if(!empty($client->middle_name)): ?>
 		<tr>
+            <td style="background-color: #eef3f9"><i class="fa fa-user"></i></td>
 			<td style="background-color: #eef3f9"><?= $client->getAttributeLabel('middle_name') ?></td>
 			<td><?= \yii\helpers\Html::encode($client->middle_name) ?></td>
 		</tr>
