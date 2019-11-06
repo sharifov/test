051f1d9807bc200f7114b02e9acd1dc77ea4365d
AlexConnor <alex.connor@techork.com>
2019-11-06 13:20:05 +0200

    fix: added old lead logs button (SL-745)

--- frontend/views/lead/partial/_actions.php
+++ frontend/views/lead/partial/_actions.php
@@ -211,15 +211,9 @@ if($project){
 
         <?php
 
-//            if(!$leadModel->isNewRecord && $user->canRoles(['admin', 'supervision'])) {
-//                $countLogs = \common\models\LeadLog::find()->where(['lead_id' => $leadModel->id])->count();
-//                echo Html::a('Logs' . ($countLogs ? ' ('.$countLogs.')' : '' ), null,
-//                    [
-//                        'id' => 'btn-lead-logs',
-//                        'class' => 'btn btn-default',
-//                        'data-url' => Url::to(['leads/ajax-activity-logs', 'id' => $leadModel->id])
-//                ]);
-//            }
+            if(!$leadModel->isNewRecord && $user->canRoles(['admin', 'supervision'])) {
+
+            }
 
         ?>
 
@@ -229,6 +223,16 @@ if($project){
                 'class' => 'btn btn-default',
                 'title' => 'Status Logs LeadID #' . $leadForm->lead->id
             ]) ?>
+
+            <?php
+                $countLogs = \common\models\LeadLog::find()->where(['lead_id' => $leadModel->id])->count();
+                echo Html::a('Old Data Logs' . ($countLogs ? ' ('.$countLogs.')' : '' ), null,
+                    [
+                        'id' => 'btn-lead-logs',
+                        'class' => 'btn btn-default',
+                        'data-url' => Url::to(['leads/ajax-activity-logs', 'id' => $leadModel->id])
+                    ]);
+            ?>
 
             <?= Html::a('<i class="fa fa-list"></i> Data Logs', null, [
                 'id' => 'btn-general-lead-log',
