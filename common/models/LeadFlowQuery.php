ddc98681fd4931d6e2b8307a3df040bd0c2d4a12
serge.murphy <serge.murphy@techork.com>
2019-11-12 09:47:35 +0200

    feat: modify method for search last taken lead (SL-752)

--- common/models/LeadFlowQuery.php
+++ common/models/LeadFlowQuery.php
@@ -15,6 +15,7 @@ class LeadFlowQuery extends ActiveQuery
     {
         return $this->andWhere([
             'employee_id' => $userId,
+            'lf_owner_id' => $userId,
             'lf_from_status_id' => Lead::STATUS_PENDING,
             'status' => Lead::STATUS_PROCESSING,
         ])->asArray()->orderBy(['created' => SORT_DESC])->limit(1);
