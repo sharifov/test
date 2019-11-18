a903e1b0a67c899f14fe9f2bd0fe8c9f2842c0ae
VBarnes <vincent.barnes@techork.com>
2019-11-18 08:41:25 +0200

    feat:(SL-754) Add new page Leads Report

---
+++ frontend/views/report/_search_leads_report.php
@@ -0,0 +1,81 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use sales\access\EmployeeDepartmentAccess;
+?>
+
+<div class="calls-search">
+    <?php $form = ActiveForm::begin([
+        'action' => ['report/calls-report'],
+        'method' => 'get',
+    ]); ?>
+
+    <div class="row">
+        <div class="col-md-3">
+            <div class="row">
+                <div class="col-md-12">
+                    <?/*= $form->field($model, 'createTimeRange', [
+                        'options' => ['class' => 'form-group']
+                    ])->widget(\kartik\daterange\DateRangePicker::class, [
+                        'presetDropdown' => false,
+                        'hideInput' => true,
+                        'convertFormat' => true,
+                        'pluginOptions' => [
+                            'timePicker' => true,
+                            'timePickerIncrement' => 1,
+                            'timePicker24Hour' => true,
+                            'locale' => [
+                                'format' => 'Y-m-d H:i',
+                                'separator' => ' - '
+                            ]
+                        ]
+                    ])->label('Calls Created Date');
+                    */?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <?/*= $form->field($model, 'c_created_user_id')->dropDownList($list->getEmployees(), ['prompt' => '-'])->label('Username') */?>
+                </div>
+                <div class="col-md-6">
+                    <?/*= $form->field($model, 'callDepId')->dropDownList(EmployeeDepartmentAccess::getDepartments(), ['prompt' => '-'])->label('Department') */?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <?/*= $form->field($model, 'c_project_id')->dropDownList(\common\models\Project::getList(), ['prompt' => '-'])->label('Project') */?>
+                </div>
+                <div class="col-md-6">
+                    <?/*= $form->field($model, 'userGroupId')->dropDownList(\common\models\UserGroup::getList(), ['prompt' => '-'])->label('User Group') */?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <?/*= $form->field($model, 'call_duration_from')->input('number', ['min' => 0]) */?>
+                </div>
+                <div class="col-md-6">
+                    <?/*= $form->field($model, 'call_duration_to')->input('number', ['min' => 0]) */?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+            <div class="form-group text-center">
+                <?= Html::submitButton('<i class="fa fa-search"></i> Search', ['name' => 'search', 'class' => 'btn btn-primary']) ?>
+                <?= Html::a('<i class="glyphicon glyphicon-repeat"></i> Reset form', ['report/calls-report'], ['class' => 'btn btn-warning']) ?>
+            </div>
+        </div>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
--- frontend/views/report/leads-report.php
+++ frontend/views/report/leads-report.php
@@ -1,3 +1,31 @@
 <?php
+use yii\helpers\Html;
+use kartik\grid\GridView;
+use sales\access\ListsAccess;
 
-echo "leads flow";
+$this->title = 'Leads Report';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="calls-report-index">
+    <h1><i class=""></i> <?= Html::encode($this->title) ?></h1>
+
+    <div class="">
+        <div class="x_panel">
+            <div class="x_title">
+                <h2><i class="fa fa-search"></i> Search</h2>
+                <ul class="nav navbar-right panel_toolbox">
+                    <li>
+                        <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="x_content" style="display: block">
+                <?= $this->render('_search_leads_report' /*, ['model' => $searchModel, 'list' => $list]*/);  ?>
+            </div>
+        </div>
+    </div>
+
+    content
+
+</div>
