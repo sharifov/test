3b7a91dc88c3b31e07f42a359c33578dba1f2f9a
VBarnes <vincent.barnes@techork.com>
2019-11-21 13:24:15 +0200

    fix/SL-765 Agents Rating profit incorrect value #adjust grid to bootstrap 4

--- frontend/views/stats/ratings/_list_item_ratings.php
+++ frontend/views/stats/ratings/_list_item_ratings.php
@@ -11,14 +11,14 @@ $class = 'text-success';
 
 <div class="col-md-12" style="margin-bottom: 5px">
     <div class="list-striped">
-        <div class="col-md-1">
+        <div class="col-md-2">
             <?=($index + 1)?>.
         </div>
-        <div class="col-md-4">
+        <div class="col-md-6">
             <?/*=Html::tag('i', '', ['class' => 'fa ' . $iconClass . ' fa-lg ' . $class, 'title' => $model['username']])*/?>
             <?=Html::encode($model['username'])?>
         </div>
-        <div class="col-md-5">
+        <div class="col-md-3">
             <?php if (isset($model['finalProfit']) && $model['finalProfit'] > 0) : ?>
                 <?= '$ ' . number_format($model['finalProfit']) ?>
             <?php elseif (isset($model['soldLeads'])  && $model['soldLeads'] > 0 ) :?>
