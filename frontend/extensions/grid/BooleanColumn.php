b49030547391164b73a77f3da401da93488f0f04
serge.murphy <serge.murphy@techork.com>
2019-12-06 16:41:18 +0200

    feat: added custom columns for grid view (SL-870)

--- frontend/extensions/grid/BooleanColumn.php
+++ frontend/extensions/grid/BooleanColumn.php
@@ -1,10 +1,14 @@
 <?php
 
-
 namespace frontend\extensions\grid;
 
+use yii\grid\DataColumn;
 
-class BooleanColumn
+class BooleanColumn extends DataColumn
 {
-
+    public $format = 'booleanByLabel';
+    public $filter = [
+        1 => 'Yes',
+        0 => 'No'
+    ];
 }
