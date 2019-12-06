3af48b295b16c32d45786c25eba8a4d360a81e16
AlexConnor <alex.connor@techork.com>
2019-12-06 17:28:19 +0200

    feat: update product hotel view (SL-867)

--- modules/hotel/views/hotel/partial/_view_search.php
+++ modules/hotel/views/hotel/partial/_view_search.php
@@ -21,7 +21,7 @@ $dataProviderQuotes = $searchModel->searchProduct($params);
 ?>
 <div class="hotel-view-search">
 
-    <h2>Hotel Request ID: <?= Html::encode($model->ph_id) ?></h2>
+<!--    <h2>Hotel Request ID: --><?//= Html::encode($model->ph_id) ?><!--</h2>-->
 
 
     <div class="row">
@@ -32,7 +32,7 @@ $dataProviderQuotes = $searchModel->searchProduct($params);
         <?= DetailView::widget([
             'model' => $model,
             'attributes' => [
-                //'ph_id',
+                'ph_id',
                 //'ph_product_id',
                 'ph_check_in_date:date',
                 'ph_check_out_date:date',
@@ -52,49 +52,76 @@ $dataProviderQuotes = $searchModel->searchProduct($params);
 
         <?php if ($model->hotelRooms):?>
             <?php foreach ($model->hotelRooms as $rk => $room): ?>
-                <div class="col-md-12">
-                    <h4><?=($rk + 1)?>. <span title="RoomID: <?=Html::encode($room->hr_id)?>">Room</span> <?=$room->hr_room_name ? ': ' . Html::encode($room->hr_room_name) : ''?>
-                        <?= \yii\bootstrap4\Html::a('<i class="fa fa-edit"></i> Update', null, [
-                            'data-url' => \yii\helpers\Url::to(['/hotel/hotel-room/update-ajax', 'id' => $room->hr_id]),
-                            'class' => 'btn btn-outline-warning btn-sm btn-update-hotel-room'
-                        ])?>
 
-                        <?//=\yii\bootstrap4\Html::a('<i class="fa fa-remove"></i>', ['hotel-room/delete-ajax', 'id' => $room->hr_id], ['class' => 'btn btn-danger btn-sm'])?>
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>
+<!--                        <i class="fa fa-check-square-o"></i> -->
+                        <?=($rk + 1)?>. <span title="RoomID: <?=Html::encode($room->hr_id)?>">Room</span><?=$room->hr_room_name ? ': ' . Html::encode($room->hr_room_name) : ''?>
+                    </h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                        <li>
+                            <?= \yii\bootstrap4\Html::a('<i class="fa fa-edit warning"></i> Update', null, [
+                                'data-url' => \yii\helpers\Url::to(['/hotel/hotel-room/update-ajax', 'id' => $room->hr_id]),
+                                'class' => 'btn-update-hotel-room'
+                            ])?>
+                            <?//=\yii\bootstrap4\Html::a('<i class="fa fa-remove"></i>', ['hotel-room/delete-ajax', 'id' => $room->hr_id], ['class' => 'btn btn-danger btn-sm'])?>
+                        </li>
 
-                        <?= \yii\bootstrap4\Html::a('<i class="fa fa-remove"></i> Delete', null, [
-                                'data-url' => \yii\helpers\Url::to(['/hotel/hotel-room/delete-ajax']),
-                                'data-room-id' => $room->hr_id,
-                                'data-hotel-id' => $model->ph_id,
-                                'class' => 'btn btn-outline-danger btn-sm btn-delete-hotel-room'
-                        ]) ?>
+                        <?//php if ($is_manager) : ?>
+                        <!--                    <li>-->
+                        <!--                        --><?//=Html::a('<i class="fa fa-plus-circle success"></i> Add Quote', null, ['class' => 'add-clone-alt-quote', 'data-uid' => 0, 'data-url' => Url::to(['quote/create', 'leadId' => $leadForm->getLead()->id, 'qId' => 0])])?>
+                        <!--                    </li>-->
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-cog"></i></a>
+                            <div class="dropdown-menu" role="menu">
 
-                    </h4>
-                    <hr>
-                    <?php if ($room->hotelRoomPaxes):?>
-                        <table class="table table-bordered">
-                            <thead>
-                            <tr class=" bg-info">
-                                <th>Nr.</th>
-                                <th>Type</th>
-                                <th>Age</th>
-                                <th>Name</th>
-                                <th>Date of Birth</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php foreach ($room->hotelRoomPaxes as $nr => $pax): ?>
-                            <tr>
-                                <td title="Pax Id: <?=Html::encode($pax->hrp_id)?>"><?=($nr + 1)?>. Pax</td>
-                                <td><b><?=Html::encode($pax->getPaxTypeName())?></b></td>
-                                <td><?=$pax->hrp_age ?: '-'?></td>
-                                <td><?=Html::encode($pax->hrp_first_name)?> <?=Html::encode($pax->hrp_last_name)?></td>
-                                <td><?=$pax->hrp_dob ? date('Y-M-d', strtotime($pax->hrp_dob)) : '-'?></td>
-                            </tr>
-                            <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    <?php endif; ?>
+                                <?= \yii\bootstrap4\Html::a('<i class="fa fa-remove"></i> Delete room', null, [
+                                    'data-url' => \yii\helpers\Url::to(['/hotel/hotel-room/delete-ajax']),
+                                    'data-room-id' => $room->hr_id,
+                                    'data-hotel-id' => $model->ph_id,
+                                    'class' => 'dropdown-item text-danger btn-delete-hotel-room'
+                                ]) ?>
+
+
+                            </div>
+                        </li>
+                        <?//php endif; ?>
+                        <li>
+                            <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                        </li>
+                    </ul>
+                    <div class="clearfix"></div>
                 </div>
+                <div class="x_content" style="display: block">
+                    <div class="col-md-12">
+                        <?php if ($room->hotelRoomPaxes):?>
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr class=" bg-info">
+                                    <th>Nr.</th>
+                                    <th>Type</th>
+                                    <th>Age</th>
+                                    <th>Name</th>
+                                    <th>Date of Birth</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php foreach ($room->hotelRoomPaxes as $nr => $pax): ?>
+                                <tr>
+                                    <td title="Pax Id: <?=Html::encode($pax->hrp_id)?>"><?=($nr + 1)?>. Pax</td>
+                                    <td><b><?=Html::encode($pax->getPaxTypeName())?></b></td>
+                                    <td><?=$pax->hrp_age ?: '-'?></td>
+                                    <td><?=Html::encode($pax->hrp_first_name)?> <?=Html::encode($pax->hrp_last_name)?></td>
+                                    <td><?=$pax->hrp_dob ? date('Y-M-d', strtotime($pax->hrp_dob)) : '-'?></td>
+                                </tr>
+                                <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
             <?php endforeach; ?>
         <?php endif; ?>
 
