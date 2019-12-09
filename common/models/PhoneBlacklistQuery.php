8449ae87a27e46e89360ff946f59c120cae63b3c
serge.murphy <serge.murphy@techork.com>
2019-12-09 09:27:02 +0200

    feat: added method isexists on phoneblacklist query (SL-871)

--- common/models/PhoneBlacklistQuery.php
+++ common/models/PhoneBlacklistQuery.php
@@ -13,5 +13,15 @@ class PhoneBlacklistQuery extends \yii\db\ActiveQuery
     public function active(): PhoneBlacklistQuery
     {
         return $this->andWhere(['pbl_enabled' => true]);
-   }
+    }
+
+    /**
+     * @param string $phone
+     * @return bool
+     */
+    public function isExists(string $phone): bool
+    {
+        $list = $this->select(['pbl_phone'])->active()->column();
+        return in_array($phone, $list, true);
+    }
 }
