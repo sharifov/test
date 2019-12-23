4b3eb6906c4b344a0b4fd94ab08bef0a31be6304
serge.murphy <serge.murphy@techork.com>
2019-12-23 17:25:56 +0200

    feat: added leadform for create by api (SL-902)

---
+++ sales/model/lead/useCase/api/create/LeadForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace sales\model\lead;
+
+class LeadForm
+{
+    public $sub_sources_code;
+
+    public $status;
+    public $uid;
+    public $project_id;
+    public $source_id;
+
+    public $trip_type;
+
+    public $cabin;
+    public $adults;
+    public $children;
+    public $infants;
+
+    public $request_ip;
+    public $request_ip_detail;
+
+    public $discount_id;
+
+}
