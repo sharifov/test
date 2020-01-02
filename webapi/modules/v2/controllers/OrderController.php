563b75cf63e1584922e123001f094d307758f470
serge.murphy <serge.murphy@techork.com>
2020-01-02 15:10:50 +0200

    feat: added api order controller (SL-921)

--- webapi/modules/v2/controllers/OrderController.php
+++ webapi/modules/v2/controllers/OrderController.php
@@ -1,10 +1,27 @@
 <?php
 
-
 namespace webapi\modules\v2\controllers;
 
+use sales\model\order\OrderCodeException;
+use webapi\src\Messages;
+use webapi\src\response\ErrorResponse;
+use Yii;
 
-class OrderController
+/**
+ * Class OrderController
+ */
+class OrderController extends BaseController
 {
+    public function actionCreate()
+    {
+        if (!$requestData = Yii::$app->request->post()) {
+            return new ErrorResponse([
+                'statusCode' => 400,
+                'message' => Messages::LOAD_DATA_ERROR,
+                'errors' => ['Not found Order data on POST request'],
+                'code' => OrderCodeException::API_ORDER_NOT_FOUND_DATA_ON_REQUEST,
+            ]);
+        }
 
+    }
 }
