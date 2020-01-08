1f8de4dae52804f2a8f5f1216caebdc5b3f2f929
serge.murphy <serge.murphy@techork.com>
2020-01-08 09:55:15 +0200

    feat: added the ability to add technical data to api logger (SL-921)

--- webapi/src/logger/TechnicalData.php
+++ webapi/src/logger/TechnicalData.php
@@ -2,7 +2,30 @@
 
 namespace webapi\src\logger;
 
+/**
+ * Class TechnicalData
+ *
+ * @property $key
+ * @property $data
+ */
 class TechnicalData
 {
+    private $key;
+    private $data;
 
+    public function __construct(string $key, $data)
+    {
+        $this->key = $key;
+        $this->data = $data;
+    }
+
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
 }
