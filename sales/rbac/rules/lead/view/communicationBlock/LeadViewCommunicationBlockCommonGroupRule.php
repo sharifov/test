790103c662727e80b486d646b17446d821f00c63
serge.murphy <serge.murphy@techork.com>
2020-01-20 16:29:56 +0200

    fix: returned rules (SL-966)

--- sales/rbac/rules/lead/view/communicationBlock/LeadViewCommunicationBlockCommonGroupRule.php
+++ sales/rbac/rules/lead/view/communicationBlock/LeadViewCommunicationBlockCommonGroupRule.php
@@ -2,7 +2,25 @@
 
 namespace sales\rbac\rules\lead\view\communicationBlock;
 
-class LeadViewCommunicationBlockCommonGroupRule
-{
+use common\models\Lead;
+use sales\access\EmployeeGroupAccess;
+use yii\rbac\Rule;
 
+class LeadViewCommunicationBlockCommonGroupRule extends Rule
+{
+    public function execute($userId, $item, $params): bool
+    {
+        if (!isset($params['lead']) || !$params['lead'] instanceof Lead) {
+            return false;
+        }
+
+        /** @var Lead $lead */
+        $lead = $params['lead'];
+
+        if ($leadOwnerId = $lead->employee_id) {
+            return EmployeeGroupAccess::isUserInCommonGroup($userId, $leadOwnerId);
+        }
+
+        return false;
+    }
 }
