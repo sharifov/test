0eae94f90c6e664c739ec87a93ddd6b24f373507
serge.murphy <serge.murphy@techork.com>
2020-01-24 16:40:27 +0200

    feat: added product column (SL-960)

--- modules/product/src/grid/columns/ProductColumn.php
+++ modules/product/src/grid/columns/ProductColumn.php
@@ -2,7 +2,38 @@
 
 namespace modules\product\src\grid\columns;
 
-class ProductColumn
-{
+use yii\grid\DataColumn;
 
+/**
+ * Class LeadColumn
+ *
+ * @property $relation
+ *
+ *  Ex.
+        [
+            'class' => modules\product\src\grid\columns\ProductColumn::class,
+            'attribute' => 'product_id',
+            'relation' => 'product',
+        ],
+ *
+ */
+class ProductColumn extends DataColumn
+{
+    public $relation;
+
+    public function init(): void
+    {
+        parent::init();
+        if (!$this->relation) {
+            throw new \InvalidArgumentException('relation must be set.');
+        }
+    }
+
+    protected function renderDataCellContent($model, $key, $index): string
+    {
+        if ($model->{$this->attribute}) {
+            return $this->grid->formatter->format($model->{$this->relation}, 'lead');
+        }
+        return $this->grid->formatter->format(null, $this->format);
+    }
 }
