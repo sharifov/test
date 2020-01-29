860ebb044f8e5e1657de0e04064d035192b2c918
serge.murphy <serge.murphy@techork.com>
2020-01-29 09:53:43 +0200

    feat: changed method find on repositories (SL-960)

--- modules/order/src/entities/orderProduct/OrderProductRepository.php
+++ modules/order/src/entities/orderProduct/OrderProductRepository.php
@@ -20,21 +20,20 @@ class OrderProductRepository
         $this->eventDispatcher = $eventDispatcher;
     }
 
-    public function find(int $id): OrderProduct
+    public function find(int $orderId, int $productQuoteId): OrderProduct
     {
-        if ($orderProduct = OrderProduct::findOne($id)) {
+        if ($orderProduct = OrderProduct::findOne(['orp_order_id' => $orderId, 'orp_product_quote_id' => $productQuoteId])) {
             return $orderProduct;
         }
         throw new NotFoundException('Order product is not found', OrderCodeException::ORDER_PRODUCT_NOT_FOUND);
     }
 
-    public function save(OrderProduct $orderProduct): int
+    public function save(OrderProduct $orderProduct): void
     {
         if (!$orderProduct->save(false)) {
             throw new \RuntimeException('Saving error', OrderCodeException::ORDER_PRODUCT_SAVE);
         }
         $this->eventDispatcher->dispatchAll($orderProduct->releaseEvents());
-        return $orderProduct->orp_order_id;
     }
 
     public function remove(OrderProduct $order): void
