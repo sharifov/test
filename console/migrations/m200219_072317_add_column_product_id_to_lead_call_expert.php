eac85ca98c91d2ea41228b8a21b60956a2f5b9e7
andrew.snake <andrew.snake@techork.com>
2020-02-19 10:04:32 +0200

    feat: added migration (SL-979)

---
+++ console/migrations/m200219_072317_add_column_product_id_to_lead_call_expert.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200219_072317_add_column_poduct_to_lead_call_expert
+ */
+class m200219_072317_add_column_poduct_to_lead_call_expert extends Migration
+{
+    /**
+     * @var string
+     */
+    public $tableName = 'lead_call_expert';
+    /**
+     * @var string
+     */
+    public $columnName = 'lce_product_id';
+
+    public $table;
+
+    public function init()
+    {
+        parent::init();
+        $this->table = '{{%'. $this->tableName .'}}';
+    }
+
+    /**
+     * @return bool|void
+     */
+    public function safeUp()
+    {
+        $this->addColumn($this->table, $this->columnName, $this->integer());
+
+        $this->addForeignKey(
+            'FK-' . $this->tableName . '-' . $this->columnName, $this->table, [$this->columnName],
+            '{{%product}}', ['pr_id'], 'SET NULL', 'CASCADE');
+    }
+
+    /**
+     * @return bool|void
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('FK-' . $this->tableName . '-' . $this->columnName, $this->table);
+
+        $this->dropColumn($this->table, $this->columnName);
+    }
+}
