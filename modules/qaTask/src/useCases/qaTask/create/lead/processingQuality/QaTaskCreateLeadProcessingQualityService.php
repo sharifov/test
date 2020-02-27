a1ed6547634fff88842be15d9efba5a476ae2f15
serge.murphy <serge.murphy@techork.com>
2020-02-27 11:42:31 +0200

    fix: fixed key name (SL-1003)

--- modules/qaTask/src/useCases/qaTask/create/lead/processingQuality/QaTaskCreateLeadProcessingQualityService.php
+++ modules/qaTask/src/useCases/qaTask/create/lead/processingQuality/QaTaskCreateLeadProcessingQualityService.php
@@ -20,7 +20,7 @@ use sales\dispatchers\EventDispatcher;
  */
 class QaTaskCreateLeadProcessingQualityService
 {
-    public const CATEGORY_KEY = 'lead_processing_quality';
+    public const CATEGORY_KEY = 'qa_lead_processing_quality';
 
     private $qaTaskRepository;
     private $eventDispatcher;
