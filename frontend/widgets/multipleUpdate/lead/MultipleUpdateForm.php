ec1010d45d974ba7cd8c269d9b238e2ff152a1ca
serge.murphy <serge.murphy@techork.com>
2020-03-09 17:52:04 +0200

    feat: added new reasons for trash status (SL-1225)

--- frontend/widgets/multipleUpdate/lead/MultipleUpdateForm.php
+++ frontend/widgets/multipleUpdate/lead/MultipleUpdateForm.php
@@ -30,6 +30,9 @@ class MultipleUpdateForm extends Model
             4 => 'Invalid phone number',
             5 => 'Canceled trip',
             6 => 'Test',
+            7 => 'Transfer to Customer Care',
+            8 => 'Transfer to Exchange',
+            9 => 'Transfer to Schedule Change',
             0 => 'Other'
         ],
         Lead::STATUS_REJECT => [
