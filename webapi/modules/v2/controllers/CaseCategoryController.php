bdb6ba63b1c14b472a29f18c1a4ff953a2bc73bb
andrew.snake <andrew.snake@techork.com>
2020-03-16 13:36:01 +0200

    feat: init-commit (SL-1264)

--- webapi/modules/v2/controllers/CaseCategoryController.php
+++ webapi/modules/v2/controllers/CaseCategoryController.php
@@ -2,29 +2,38 @@
 
 namespace webapi\modules\v2\controllers;
 
-use common\models\UserGroup;
+use sales\entities\cases\CasesCategory;
 use webapi\src\response\messages\DataMessage;
 use webapi\src\response\messages\Message;
 use webapi\src\response\SuccessResponse;
+use yii\helpers\ArrayHelper;
+use yii\filters\HttpCache;
 
 /**
- * Class UserGroupController
+ * Class CaseCategoryController
  */
-class UserGroupController extends BaseController
+class CaseCategoryController extends BaseController
 {
+    /**
+     * @return array
+     */
     public function behaviors(): array
     {
-        $behaviors = parent::behaviors();
-        $behaviors['HttpCache'] = [
-            'class' => 'yii\filters\HttpCache',
-            'only' => ['list'],
-            'lastModified' => static function ($action, $params) {
-                return strtotime(UserGroup::find()->max('ug_updated_dt'));
-            },
-        ];
-        return $behaviors;
+        $behaviors = [
+			'HttpCache' => [
+				'class' => HttpCache::class,
+                'only' => ['list'],
+                'lastModified' => static function () {
+                    return strtotime(CasesCategory::find()->max('cc_updated_dt'));
+                },
+			],
+		];
+        return ArrayHelper::merge(parent::behaviors(), $behaviors);
     }
 
+    /**
+     * @return array
+     */
     protected function verbs(): array
     {
         $verbs = parent::verbs();
@@ -33,10 +42,10 @@ class UserGroupController extends BaseController
     }
 
     /**
-     * @api {get} /v2/user-group/list Get User Groups
+     * @api {get} /v2/case-category/list Get User Groups
      * @apiVersion 0.2.0
-     * @apiName UserGroupList
-     * @apiGroup UserGroup
+     * @apiName CaseCategoryList
+     * @apiGroup CaseCategory
      * @apiPermission Authorized User
      *
      * @apiHeader {string} Authorization Credentials <code>base64_encode(Username:Password)</code>
@@ -58,44 +67,38 @@ class UserGroupController extends BaseController
      * @apiSuccessExample {json} Success-Response:
      *
      * HTTP/1.1 200 OK
-     *   {
-     *       "status": 200,
-     *       "message": "OK",
-     *       "data": {
-     *           "user-group": [
-     *               {
-     *                   "ug_id": 1,
-     *                   "ug_key": "ug1",
-     *                   "ug_name": "Bucuresti Team",
-     *                  "ug_disable": 0,
-     *                   "ug_updated_dt": "2018-12-18 09:17:45"
-     *               },
-     *               {
-     *                   "ug_id": 2,
-     *                   "ug_key": "ug2",
-     *                   "ug_name": "100J Team",
-     *                  "ug_disable": 0,
-     *                   "ug_updated_dt": "2018-12-18 09:17:59"
-     *               },
-     *               {
-     *                   "ug_id": 3,
-     *                   "ug_key": "ug3",
-     *                   "ug_name": "Pro Team",
-     *                   "ug_disable": 1,
-     *                   "ug_updated_dt": "2018-12-18 09:18:10"
-     *               },
-     *           ]
-     *       },
-     *       "technical": {
-     *           "action": "v2/user-group/list",
-     *           "response_id": 8080269,
-     *           "request_dt": "2020-02-27 15:00:43",
-     *           "response_dt": "2020-02-27 15:00:43",
-     *           "execution_time": 0.006,
-     *           "memory_usage": 189944
-     *       },
-     *       "request": []
-     *   }
+     * {
+     *      "status": 200,
+     *      "message": "OK",
+     *      "data": {
+     *          "case-category": [
+     *              {
+     *                  "cc_key": "add_infant",
+     *                  "cc_name": "Add infant",
+     *                  "cc_dep_id": 3,
+     *                  "cc_system": 0,
+     *                  "cc_updated_dt": null
+     *              },
+     *              {
+     *                  "cc_key": "add_insurance",
+     *                  "cc_name": "Insurance Add/Remove",
+     *                  "cc_dep_id": 3,
+     *                  "cc_system": 0,
+     *                  "cc_updated_dt": "2019-09-26 15:14:01"
+     *              }
+     *          ]
+     *      },
+     *      "technical": {
+     *          "action": "v2/case-category/list",
+     *          "response_id": 11926631,
+     *          "request_dt": "2020-03-16 11:26:34",
+     *          "response_dt": "2020-03-16 11:26:34",
+     *          "execution_time": 0.076,
+     *          "memory_usage": 506728
+     *      },
+     *      "request": []
+     *  }
+     *
      *
      * @apiSuccessExample {json} Not Modified-Response (304):
      *
@@ -115,17 +118,17 @@ class UserGroupController extends BaseController
      *   }
      *
      */
+
     public function actionList(): SuccessResponse
     {
-        $userGroup = UserGroup::find()
-            ->select(['ug_id', 'ug_key', 'ug_name', 'ug_disable', 'ug_updated_dt'])
-            //->where(['ug_disable' => false])
-            ->orderBy(['ug_id' => SORT_ASC])
+        $casesCategory = CasesCategory::find()
+            ->select(['cc_key', 'cc_name', 'cc_dep_id', 'cc_system', 'cc_updated_dt'])
+            ->orderBy(['cc_key' => SORT_ASC])
             ->all();
 
         return new SuccessResponse(
             new DataMessage(
-                new Message('user-group', $userGroup)
+                new Message('case-category', $casesCategory)
             )
         );
     }
