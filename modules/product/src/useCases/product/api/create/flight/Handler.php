4986a330a6349d05b5a1396cbc7eed252513609d
serge.murphy <serge.murphy@techork.com>
2020-03-17 20:46:12 +0200

    feat: added functional for create product flight +migration (SL-1249)

--- modules/product/src/useCases/product/api/create/flight/Handler.php
+++ modules/product/src/useCases/product/api/create/flight/Handler.php
@@ -2,53 +2,75 @@
 
 namespace modules\product\src\useCases\product\api\create\flight;
 
+use modules\flight\models\Flight;
+use modules\flight\models\FlightSegment;
+use modules\flight\src\dto\flightSegment\SegmentDTO;
+use modules\flight\src\repositories\flight\FlightRepository;
+use modules\flight\src\repositories\flightSegment\FlightSegmentRepository;
+use modules\flight\src\services\flight\calculator\FlightTripTypeCalculator;
 use modules\product\src\entities\product\dto\CreateDto;
 use modules\product\src\entities\product\Product;
 use modules\product\src\entities\product\ProductRepository;
 use modules\product\src\entities\productType\ProductType;
-use modules\product\src\repositories\ProductableRepository;
-use modules\product\src\services\ProductFactory;
 use sales\services\TransactionManager;
-use webapi\models\ApiLead;
 
 /**
  * Class Handler
  *
  * @property TransactionManager $transactionManager
  * @property ProductRepository $productRepository
- * @property ProductFactory $factory
- * @property ProductableRepository $productableRepository
+ * @property FlightRepository $flightRepository
+ * @property FlightSegmentRepository $flightSegmentRepository
  */
 class Handler
 {
     private $transactionManager;
     private $productRepository;
-    private $factory;
-    private $productableRepository;
+    private $flightRepository;
+    private $flightSegmentRepository;
 
     public function __construct(
         TransactionManager $transactionManager,
         ProductRepository $productRepository,
-        ProductFactory $factory,
-        ProductableRepository $productableRepository
+        FlightRepository $flightRepository,
+        FlightSegmentRepository $flightSegmentRepository
     )
     {
         $this->transactionManager = $transactionManager;
         $this->productRepository = $productRepository;
-        $this->factory = $factory;
-        $this->productableRepository = $productableRepository;
+        $this->flightRepository = $flightRepository;
+        $this->flightSegmentRepository = $flightSegmentRepository;
     }
 
-    public function handle(int $leadId, ApiLead $request): void
+    public function handle(
+        int $leadId,
+        ?string $cabinClass,
+        int $adults,
+        int $children,
+        int $infants,
+        SegmentDTO ...$segments
+    ): void
     {
-        $this->transactionManager->wrap(function () use ($leadId, $request) {
+        $this->transactionManager->wrap(function () use ($leadId, $cabinClass, $adults, $children, $infants, $segments) {
 
             $product = Product::create(new CreateDto($leadId, ProductType::PRODUCT_FLIGHT, null, null));
             $this->productRepository->save($product);
 
-            $productItem = $this->factory->create(ProductType::PRODUCT_FLIGHT, $product->pr_id);
-            $this->productableRepository->save(ProductType::PRODUCT_FLIGHT, $productItem);
+            $flight = Flight::createByApi(
+                $product->pr_id,
+                FlightTripTypeCalculator::calculate(...$segments),
+                $cabinClass,
+                $adults,
+                $children,
+                $infants
+            );
+            $this->flightRepository->save($flight);
 
+            foreach ($segments as $segment) {
+                $segment->flightId = $flight->fl_id;
+                $flightSegment = FlightSegment::create($segment);
+                $this->flightSegmentRepository->save($flightSegment);
+            }
 
             return $product->pr_id;
 
