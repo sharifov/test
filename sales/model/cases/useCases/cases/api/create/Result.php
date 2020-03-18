4cf679140ee5e38560bfe449b95b36de15b66356
serge.murphy <serge.murphy@techork.com>
2020-03-18 16:36:14 +0200

    fix: added null arg to result constructor (SL-1267)

--- sales/model/cases/useCases/cases/api/create/Result.php
+++ sales/model/cases/useCases/cases/api/create/Result.php
@@ -6,16 +6,16 @@ namespace sales\model\cases\useCases\cases\api\create;
  * Class Result
  *
  * @property string $caseGid
- * @property string $clientUuid
+ * @property string|null $clientUuid
  */
 class Result
 {
     public $caseGid;
     public $clientUuid;
 
-    public function __construct(string $caseGid, string $clientUuid)
+    public function __construct(string $caseGid, ?string $clientUuid)
     {
         $this->caseGid = $caseGid;
-        $this->clientUuid = $clientUuid;
+        $this->clientUuid = $clientUuid ?: '';
     }
 }
