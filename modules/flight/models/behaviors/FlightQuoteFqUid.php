acde232a0b4ef5bc1c0e30d48ad330400b3b1856
andrew.snake <andrew.snake@techork.com>
2020-03-20 13:32:35 +0200

    feat: pre-commit (SL-1274)

--- modules/flight/models/behaviors/FlightQuoteFqUid.php
+++ modules/flight/models/behaviors/FlightQuoteFqUid.php
@@ -2,7 +2,6 @@
 
 namespace modules\flight\models\behaviors;
 
-use modules\flight\models\FlightQuote;
 use yii\base\Behavior;
 use yii\db\ActiveRecord;
 
@@ -12,43 +11,19 @@ use yii\db\ActiveRecord;
  */
 class FlightQuoteFqUid extends Behavior
 {
-
-    /* TODO::
-    При создании flight_quote в beforeSave генерируем уникальный string (uniqueid), если он не пустой.
-    При Update, проверяем, если поле пустое, то генерим fq_uid и сохраняем, если не передается. (текущая логика уже есть в примерах моделей)
-     */
-
     /**
      * @return array
      */
     public function events(): array
     {
         return [
-            ActiveRecord::EVENT_BEFORE_INSERT => 'beforeInsert',
-            ActiveRecord::EVENT_BEFORE_UPDATE => 'beforeUpdate',
-            //ActiveRecord::EVENT_AFTER_UPDATE => 'customAfterSave',
-            //ActiveRecord::EVENT_AFTER_INSERT => 'customAfterSave',
+            ActiveRecord::EVENT_BEFORE_INSERT => 'generateUid',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'generateUid',
         ];
     }
 
-    /**
-     * @param $event
-     */
-    public function customAfterSave($event): void
+    public function generateUid(): void
     {
-        if (array_key_exists('pq_profit_amount', $event->changedAttributes) ) {
-
-        }
-    }
-
-    public function beforeInsert(): void
-    {
-        $this->owner->fq_uid = 0.00; // if need
-        $this->uid = empty($this->owner->fq_uid) ? uniqid('fq_uid', true) : $this->owner->fq_uid;
-    }
-
-    public function beforeUpdate(): void
-    {
-        // example
+        $this->owner->fq_uid = empty($this->owner->fq_uid) ? uniqid('', true) : $this->owner->fq_uid;
     }
 }   
