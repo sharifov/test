7ce143b2e10bc771d79dd012588da18adf94ff89
Alex Grub <alex.grub@checkork.com>
2020-05-07 15:32:10 +0300

    feat: updated coupons view (SL-1531)

--- frontend/views/cases/coupons/view.php
+++ frontend/views/cases/coupons/view.php
@@ -16,7 +16,27 @@
 		<div class="clearfix"></div>
 	</div>
 	<div class="x_content" style="display: none; margin-top: -10px;">
-
+        <table class="table table-bordered table-hover table-condensed">
+            <thead>
+            <tr>
+                <th style="width: 50px">#</th>
+                <th style="width: 30px"> </th>
+                <th class="text-center" style="width: 130px">Code</th>
+                <th class="text-center" style="width: 130px">Amount</th>
+                <th class="text-center" style="width: 130px">Currency Code</th>
+                <th class="text-center" style="width: 130px">Percent</th>
+                <th class="text-center" style="width: 130px">Exp Date</th>
+                <th class="text-center" style="width: 130px">Start Date</th>
+                <th class="text-center" style="width: 130px">Status</th>
+                <th class="text-center">Notes</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th colspan="10" style="text-align: center">Not Found Data</th>
+                </tr>
+            </tbody>
+        </table>
 	</div>
 </div>
 <?php \yii\widgets\Pjax::end()?>
