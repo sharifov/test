59fec6bc4b9a4082dccc8cb73e18f50626bf7fef
AlexConnor <alex.connor@techork.com>
2020-05-08 23:30:57 +0300

    feat: update user online code (SL-1535)

--- common/models/UserOnline.php
+++ common/models/UserOnline.php
@@ -74,19 +74,4 @@ class UserOnline extends ActiveRecord
     {
         return $this->hasOne(Employee::class, ['id' => 'uo_user_id']);
     }
-
-//    /**
-//     * @param bool $insert
-//     * @param array $changedAttributes
-//     */
-//    public function afterSave($insert, $changedAttributes)
-//    {
-//        parent::afterSave($insert, $changedAttributes);
-//
-//        if ($insert) {
-//            Yii::warning(VarDumper::dumpAsString($insert), 'info\UserOnline:afterSave:insert');
-//        } else {
-//            Yii::warning(VarDumper::dumpAsString($insert), 'info\UserOnline:afterSave:update');
-//        }
-//    }
 }
