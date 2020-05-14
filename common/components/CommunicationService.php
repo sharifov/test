3bce5b2d90df45ce2ef35bd4cbe9cdeb3d9d9242
andrew.snake <andrew.snake@techork.com>
2020-05-14 17:25:18 +0300

    hotfix: disable pause request to COM

--- common/components/CommunicationService.php
+++ common/components/CommunicationService.php
@@ -586,8 +586,8 @@ class CommunicationService extends Component implements CommunicationServiceInte
         if ($recordingSid) {
             $data['recording_sid'] = $recordingSid;
         }
-
-        $response = $this->sendRequest('voice/update-recording-status', $data);
+        /* TODO::  */
+        /*$response = $this->sendRequest('voice/update-recording-status', $data);
 
         if ($response->isOk) {
             if(isset($response->data['data']['response'])) {
@@ -607,7 +607,7 @@ class CommunicationService extends Component implements CommunicationServiceInte
             $out['error'] = $response->content;
             \Yii::error(VarDumper::dumpAsString($out['error']),
             'CommunicationService::updateRecordingStatus:failRequest');
-        }
+        }*/
 
         return $out;
     }
