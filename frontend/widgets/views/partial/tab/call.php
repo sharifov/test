bc4e68606d6ae9e45a74711d55bf712a7ee96b90
Alex Grub <alex.grub@checkork.com>
2020-05-15 14:19:45 +0300

    feat: updated icon of mute microphone (SL-1550)

--- frontend/widgets/views/partial/tab/call.php
+++ frontend/widgets/views/partial/tab/call.php
@@ -170,7 +170,7 @@
 
         <div class="call-pane__call-btns">
             <button class="call-pane__mute" id="call-pane__mute" data-is-muted="false">
-                <i class="fa fa-volume-mute"></i>
+                <i class="fas fa-microphone"></i>
             </button>
             <button class="call-pane__start-call calling-state-block">
                 <div class="call-in-action">
