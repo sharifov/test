b7acd5e6f62a0154d394a802810c55d45f977867
AlexConnor <alex.connor@techork.com>
2020-05-21 15:13:13 +0300

    feat: updated PROD ENV config files (Sl-1525)

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,32 +1,57 @@
 <?php
 return [
+
+    'serviceName' => '{{ common.config.params.serviceName:str }}',
+    'serviceVersion' => '{{ common.config.params.serviceVersion:str }}',
+    'appName' => '{{ common.config.params.appName:str }}',
     'appInstance' => '{{ common.config.params.appInstance:str }}',
+
     'url_address'      => '{{ common.config.params.url_address:str }}',
-    'email_from' => [
-        'sales' => '{{ common.config.params.email_from.sales:str }}',
-    ],
-    'email_to' => [
-        'bcc_sales' => '{{ common.config.params.email_to.bcc_sales:str }}'
+    'url_api_address'  => '{{ common.config.params.url_api_address:str }}',
+
+    'syncAirlineClasses' => '{{ common.config.params.syncAirlineClasses:str }}',
+    'getAirportUrl' => '{{ common.config.params.getAirportUrl:str }}',
+
+    'search' => [
+        'host' => '{{ common.config.params.search.host:str }}',
+        'api_cid' => '{{ common.config.params.search.api_cid:str }}',
+        'api_key' => '{{ common.config.params.search.api_key:str }}',
     ],
 
-    'getAirportUrl' => '{{ common.config.params.getAirportUrl:str }}',
-    'sync' => [
-        'ver' => '1.0.0',
-        'apiKey' => '{{ common.config.params.sync.apiKey:str }}',
-        'serverUrl' => '{{ common.config.params.sync.serverUrl:str }}'
-    ],
-    'AWS_MAILER' => [
-        'host' => '{{ common.config.params.AWS_MAILER.host:str }}',
-        'port' => '587',
-        'security' => 'tls',
-        'username' => '{{ common.config.params.AWS_MAILER.username:str }}',
-        'password' => '{{ common.config.params.AWS_MAILER.password:str }}'
-    ],
     'searchApiUrl' => '{{ common.config.params.searchApiUrl:str }}',
+    'ipinfodb_key' => '{{ common.config.params.ipinfodb_key:str }}',
+
+    'backOffice' => [
+        'ver' => '{{ common.config.params.backOffice.ver:str }}',
+        'apiKey' => '{{ common.config.params.backOffice.apiKey:str }}',
+        'serverUrl' => '{{ common.config.params.backOffice.serverUrl:str }}',
+        'webHookEndpoint' => 'webhook/ping',
+    ],
+
+    'telegram' => [
+        'bot_username'  => '{{ common.config.params.telegram.bot_username:str }}',
+        'token'         => '{{ common.config.params.telegram.token:str }}',
+        'webhook_url'   => '{{ common.config.params.telegram.webhook_url:str }}',
+    ],
 
     'crypt' => [
         'method'    => 'aes-256-cbc',
         'password'  => '{{ common.config.params.crypt.password:str }}',
         'iv'        => '{{ common.config.params.crypt.iv:str }}',
-    ]
+    ],
+
+    'email_from' => [
+        'sales' => '{{ common.config.params.email_from.sales:str }}',
+    ],
+
+    'email_to' => [
+        'bcc_sales' => '{{ common.config.params.email_from.bcc_sales:str }}',
+    ],
+
+    'lead' => [
+        'call2DelayTime' => '{{ common.config.params.lead.call2DelayTime:int }}',
+    ],
+    'processing_fee' => '{{ common.config.params.processing_fee:int }}',
+    'global_phone' => '{{ common.config.params.global_phone:str }}',
+
 ];
