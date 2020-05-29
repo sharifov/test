70b5f5201460becebb4bd7d99f653f218cb1015f
andrew.snake <andrew.snake@techork.com>
2020-05-29 11:15:13 +0300

    feat: pre-commit (SL-1667)

--- console/migrations/m200529_073740_add_captcha_setting.php
+++ console/migrations/m200529_073740_add_captcha_setting.php
@@ -2,6 +2,7 @@
 
 use common\models\SettingCategory;
 use yii\db\Migration;
+use yii\helpers\Inflector;
 
 /**
  * Class m200529_073740_add_captcha_setting
@@ -17,16 +18,33 @@ class m200529_073740_add_captcha_setting extends Migration
             $category->sc_name = 'Authentication';
             $category->save();
         }
-
+        $name = 'Captcha Login Enable';
         $this->insert('{{%setting}}', [
-            's_key' => 'enable_request_to_bo_sale',
-            's_name' => 'Enable requests to BO sale',
+            's_key' => Inflector::slug($name, '_'),
+            's_name' => $name,
             's_type' => \common\models\Setting::TYPE_BOOL,
-            's_value' => 1,
+            's_value' => false,
             's_updated_dt' => date('Y-m-d H:i:s'),
             's_category_id' => $category ? $category->sc_id : null,
         ]);
-
+        $name = 'Captcha Login Attemps';
+        $this->insert('{{%setting}}', [
+            's_key' => Inflector::slug($name, '_'),
+            's_name' =>$name,
+            's_type' => \common\models\Setting::TYPE_INT,
+            's_value' => 0,
+            's_updated_dt' => date('Y-m-d H:i:s'),
+            's_category_id' => $category ? $category->sc_id : null,
+        ]);
+        $name = 'User Notify failed login attemps';
+        $this->insert('{{%setting}}', [
+            's_key' => Inflector::slug($name, '_'),
+            's_name' => $name,
+            's_type' => \common\models\Setting::TYPE_INT,
+            's_value' => 0,
+            's_updated_dt' => date('Y-m-d H:i:s'),
+            's_category_id' => $category ? $category->sc_id : null,
+        ]);
 
 
         if (Yii::$app->cache) {
