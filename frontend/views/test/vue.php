09bfa0e314cda07e77471627ce226362b2986d1c
AlexConnor <alex.connor@techork.com>
2020-06-17 09:20:08 +0300

    feat: updated test vue script (SL-1724)

--- frontend/views/test/vue.php
+++ frontend/views/test/vue.php
@@ -3,7 +3,7 @@ use yii\helpers\Html;
 \frontend\assets\VueAsset::register($this); // register VueAsset
 
 $this->registerJsFile('/js/vue/call-widget.js', [
-    'position' => \yii\web\View::POS_READY,
+    'position' => \yii\web\View::POS_END,
     'depends' => [
         \frontend\assets\VueAsset::class
     ]
@@ -29,58 +29,48 @@ $this->registerJsFile('/js/vue/call-widget.js', [
 
 
 
-<script type="text/x-template" id="checkbox-template">
-    <div class="checkbox-wrapper" @click="check">
+<script type="text/x-template" id="call-widget-template">
+    <div class="checkbox-wrapper" >
         <div :class="{ checkbox: true, checked: checked }"></div>
         <div class="title">{{ title }}</div>
     </div>
 </script>
 
 
-<script type="text/x-template" id="test2">
-    <div>
-        <p>Этот шаблон будет скомпилирован в области видимости компонента-потомка.</p>
-        <p>Доступа к данным родителя нет.</p>
-    </div>
-</script>
-
 
 <div id="app"> <!-- the root Vue element -->
-    <my-checkbox></my-checkbox> <!-- your component -->
+    <call-widget></call-widget> <!-- your component -->
     <p>{{ message }}</p>
     <button v-on:click="reverseMessage">Reverse Message</button>
-    <my-test2></my-test2>
 </div>
 
 
 <script>
+    //Vue.config('debug', true);
 
     // this is the JS file, eg app.js
+    // Vue.component('call-widget', {
+    //     template: '#call-widget-template',
+    //     data() {
+    //         return { checked: false, title: 'Check me' }
+    //     },
+    //     methods: {
+    //         check() { this.checked = !this.checked; }
+    //     }
+    // });
 
-
-    Vue.component('my-test2', {
-        //template: '#test2',
-        template: require('./js/vue/1.html'),
-        data() {
-            return { checked: false, title: 'Check me' }
-        },
-        methods: {
-            check() { this.checked = !this.checked; }
-        }
-    });
+    // Vue.component('my-test2', {
+    //     template: '#test2',
+    //     data() {
+    //         return { checked: false, title: 'Check me' }
+    //     },
+    //     methods: {
+    //         check() { this.checked = !this.checked; }
+    //     }
+    // });
 
     // new Vue({el:'#app'})
 
 
-    var app = new Vue({
-        el: '#app',
-        data: {
-            message: 'Hello Vue.js!'
-        },
-        methods: {
-            reverseMessage: function () {
-                this.message = this.message.split('').reverse().join('')
-            }
-        }
-    })
+
 </script>
--- frontend/web/js/vue/call-widget.js
+++ frontend/web/js/vue/call-widget.js
@@ -1,9 +1,42 @@
-Vue.component('my-checkbox', {
-    template: '#checkbox-template',
+// var cwComponent = Vue.component('call-widget', {
+//     template: '#call-widget-template',
+//     data() {
+//         return { checked: false, title: 'Check me' }
+//     },
+//     methods: {
+//         check() { this.checked = !this.checked; },
+//         alert: function () {
+//             alert(123);
+//         }
+//     }
+// });
+
+
+var ComponentA = {
+    template: '#call-widget-template',
     data() {
         return { checked: false, title: 'Check me' }
     },
     methods: {
-        check() { this.checked = !this.checked; }
+        //check() { this.checked = !this.checked; },
+        alertic: function () {
+            alert(123);
+        }
+    }
+}
+
+
+var app = new Vue({
+    el: '#app',
+    components: {
+        'call-widget': ComponentA
+    },
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
 });
