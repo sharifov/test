203082797c3237a36d1c85450ff81a5533897be8
AlexConnor <alex.connor@techork.com>
2020-06-19 18:47:59 +0300

    feat: added react ajax (SL-1724)

--- frontend/web/js/call-widget/history-tab.jsx
+++ frontend/web/js/call-widget/history-tab.jsx
@@ -2,21 +2,58 @@
 class HistoryTab extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {
+            date: new Date(),
+            error: null,
+            isLoaded: false,
+            items: []
+        };
     }
 
     componentDidMount() {
+        fetch("/call/react-init-call-widget")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.calls
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
     }
 
     componentWillUnmount() {
     }
 
     render() {
-        return (
-            <div>
-                <h1>History Tab init!</h1>
-                <h2>Now {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
+        const { error, isLoaded, items } = this.state;
+
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    <h3>Last 3 Calls</h3>
+                <ul>
+                    {items.map(item => (
+                        <li key={item.c_id}>
+                            {item.c_id} {item.c_from}
+                        </li>
+                    ))}
+                </ul>
+                </div>
+            );
+        }
+
+
     }
 }
