8eb7ac55df6af680cb9553d994161d2242eb23ef
andrew.snake <andrew.snake@techork.com>
2020-07-03 09:30:14 +0300

    hotfix: Unsubscribe - if exist, set status to 200 (SL-1892)

--- sales/forms/api/clientEmail/SubscribeForm.php
+++ sales/forms/api/clientEmail/SubscribeForm.php
@@ -41,13 +41,6 @@ class SubscribeForm extends Model
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 160],
-
-            ['email', function ($attribute) {
-                if (!(new EmailUnsubscribeRepository())->find($this->email, $this->project_id)) {
-                    $this->addError($attribute,
-                    'Database entry (email : ' . $this->email . ', project : ' . $this->project_id . ') not exists');
-                }
-            }],
         ];
     }
 }
