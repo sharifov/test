30c5bfbdd3b163087b49a9e05cae527a1fb635bd
serge.murphy <serge.murphy@techork.com>
2020-07-08 16:26:48 +0300

    feat: added actie queue css (SL-1920)

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -288,9 +288,112 @@ class TestController extends FController
 //            'queue' => 'hold',
             'queue' => 'inProgress',
             'duration' => 0,
+            'conference' => [
+                    'sid' => 'conf' . $tmp,
+                    'duration' => 0,
+                    'participants' => [
+                        [
+                            'callSid' => 'callSid1',
+                            'avatar' => 'N',
+                            'name' => 'Name 1',
+                            'phone' => '+373 1',
+                            'type' => 'coaching',
+                            'duration' => 1
+                        ],
+                        [
+                            'callSid' => 'callSid2',
+                            'avatar' => 'N',
+                            'name' => 'Name 2',
+                            'phone' => '+373 2',
+                            'type' => '',
+                            'duration' => 2
+                        ],
+                        [
+                            'callSid' => 'callSid3',
+                            'avatar' => 'N',
+                            'name' => 'Name 3',
+                            'phone' => '+373 3',
+                            'type' => '',
+                            'duration' => 3
+                        ],
+                        [
+                            'callSid' => 'callSid4',
+                            'avatar' => 'N',
+                            'name' => 'Name 1',
+                            'phone' => '+373 1',
+                            'type' => 'coaching',
+                            'duration' => 4
+                        ],
+                        [
+                            'callSid' => 'callSid5',
+                            'avatar' => 'N',
+                            'name' => 'Name 2',
+                            'phone' => '+373 2',
+                            'type' => '',
+                            'duration' => 5
+                        ],
+                        [
+                            'callSid' => 'callSid6',
+                            'avatar' => 'N',
+                            'name' => 'Name 3',
+                            'phone' => '+373 3',
+                            'type' => '',
+                            'duration' => 6
+                        ],
+                        [
+                            'callSid' => 'callSid7',
+                            'avatar' => 'N',
+                            'name' => 'Name 1',
+                            'phone' => '+373 1',
+                            'type' => 'coaching',
+                            'duration' => 7
+                        ],
+                        [
+                            'callSid' => 'callSid8',
+                            'avatar' => 'N',
+                            'name' => 'Name 2',
+                            'phone' => '+373 2',
+                            'type' => '',
+                            'duration' => 8
+                        ],
+                        [
+                            'callSid' => 'callSid9',
+                            'avatar' => 'N',
+                            'name' => 'Name 3',
+                            'phone' => '+373 3',
+                            'type' => '',
+                            'duration' => 9
+                        ],
+                        [
+                            'callSid' => 'callSid10',
+                            'avatar' => 'N',
+                            'name' => 'Name 1',
+                            'phone' => '+373 1',
+                            'type' => 'coaching',
+                            'duration' => 10
+                        ],
+                        [
+                            'callSid' => 'callSid11',
+                            'avatar' => 'N',
+                            'name' => 'Name 2',
+                            'phone' => '+373 2',
+                            'type' => '',
+                            'duration' => 11
+                        ],
+                        [
+                            'callSid' => 'callSid12',
+                            'avatar' => 'N',
+                            'name' => 'Name 3',
+                            'phone' => '+373 3',
+                            'type' => '',
+                            'duration' => 12
+                        ],
+                ],
+            ]
         ];
 //        Notifications::publish('updateIncomingCall', ['user_id' => 295], $callInfo);
-        Notifications::publish('callUpdate', ['user_id' => 295], $callInfo);
+//        Notifications::publish('callUpdate', ['user_id' => 295], $callInfo);
+        Notifications::publish('callUpdate', ['user_id' => 167], $callInfo);
         die;
 
         $confData = [
