4728717bf6b2254c3db82481d834f3295f110539
andrew.snake <andrew.snake@techork.com>
2020-07-08 17:26:40 +0300

    feat: added to case form (SL-1943)

--- frontend/models/CasePreviewEmailForm.php
+++ frontend/models/CasePreviewEmailForm.php
@@ -80,7 +80,7 @@ class CasePreviewEmailForm extends Model
             [['e_email_to', 'e_email_from'], 'email'],
             [['e_email_tpl_id', 'e_case_id'], 'integer'],
             [['e_email_message', 'e_quote_list', 'coupon_list'], 'string'],
-            [['e_email_subject'], 'string', 'max' => 80, 'min' => 5],
+            [['e_email_subject'], 'string', 'max' => 200, 'min' => 5],
             [['e_email_from_name', 'e_email_to_name'], 'string', 'max' => 50],
             [['e_language_id'], 'string', 'max' => 5],
             [['e_user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Employee::class, 'targetAttribute' => ['e_user_id' => 'id']],
