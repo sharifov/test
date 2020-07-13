af34c647318418aee1fda8c76c3f556d67a6afb3
serge.murphy <serge.murphy@techork.com>
2020-07-13 23:56:42 +0300

    feat: fixed time last message (SL-1951)

--- sales/model/clientChat/entity/ClientChat.php
+++ sales/model/clientChat/entity/ClientChat.php
@@ -251,9 +251,9 @@ class ClientChat extends \yii\db\ActiveRecord
 		$this->cch_owner_user_id = null;
 	}
 
-    public function getLastMessageByOwner(): ?ClientChatMessage
+    public function getLastMessage(): ?ClientChatMessage
     {
-        return ClientChatMessage::find()->andWhere(['ccm_cch_id' => $this->cch_id, 'ccm_user_id' => $this->cch_owner_user_id])->orderBy(['ccm_id' => SORT_DESC])->limit(1)->one();
+        return ClientChatMessage::find()->andWhere(['ccm_cch_id' => $this->cch_id])->orderBy(['ccm_id' => SORT_DESC])->limit(1)->one();
     }
 
     public function attributeLabels(): array
