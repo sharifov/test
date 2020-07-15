00ec99d640298b11a485ac89c7599817e4089e0b
serge.murphy <serge.murphy@techork.com>
2020-07-15 11:12:28 +0300

    fix: fixed quote capture size (SL-1951)

--- frontend/controllers/ClientChatController.php
+++ frontend/controllers/ClientChatController.php
@@ -555,7 +555,7 @@ class ClientChatController extends FController
                 $content_data,
                 Yii::$app->language ?: 'en-US',
                 [
-                    'img_width' => 800,
+                    'img_width' => 270,
                     'img_height' => 60,
                     'img_format' => 'png',
                     'img_update' => 1
