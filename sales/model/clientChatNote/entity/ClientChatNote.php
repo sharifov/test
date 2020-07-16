9ab7d5a82c6771a5fb9f87ba8679c8d5a16010bf
andrew.snake <andrew.snake@techork.com>
2020-07-16 16:59:36 +0300

    feat: added model (SL-1997)

---
+++ sales/model/clientChatNote/entity/ClientChatNote.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace sales\model\clientChatNote\entity;
+
+use common\models\Employee;
+use sales\model\clientChat\entity\ClientChat;
+use Yii;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "client_chat_note".
+ *
+ * @property int $ccn_id
+ * @property int|null $ccn_chat_id
+ * @property int|null $ccn_user_id
+ * @property string|null $ccn_note
+ * @property int|null $ccn_deleted
+ * @property string|null $ccn_created_dt
+ * @property string|null $ccn_updated_dt
+ *
+ * @property ClientChat $chat
+ * @property Employee $user
+ */
+class ClientChatNote extends ActiveRecord
+{
+
+    public static function tableName(): string
+    {
+        return 'client_chat_note';
+    }
+
+    public function rules(): array
+    {
+        return [
+            [['ccn_chat_id', 'ccn_user_id', 'ccn_deleted'], 'integer'],
+            [['ccn_note'], 'string'],
+            [['ccn_created_dt', 'ccn_updated_dt'], 'safe'],
+            [['ccn_chat_id'], 'exist', 'skipOnError' => true, 'targetClass' => ClientChat::class, 'targetAttribute' => ['ccn_chat_id' => 'cch_id']],
+            [['ccn_user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Employee::class, 'targetAttribute' => ['ccn_user_id' => 'id']],
+        ];
+    }
+
+    public function attributeLabels(): array
+    {
+        return [
+            'ccn_id' => 'Ccn ID',
+            'ccn_chat_id' => 'Ccn Chat ID',
+            'ccn_user_id' => 'Ccn User ID',
+            'ccn_note' => 'Ccn Note',
+            'ccn_deleted' => 'Ccn Deleted',
+            'ccn_created_dt' => 'Ccn Created Dt',
+            'ccn_updated_dt' => 'Ccn Updated Dt',
+        ];
+    }
+
+    public function getChat(): ActiveQuery
+    {
+        return $this->hasOne(ClientChat::class, ['cch_id' => 'ccn_chat_id']);
+    }
+
+    public function getUser(): ActiveQuery
+    {
+        return $this->hasOne(Employee::class, ['id' => 'ccn_user_id']);
+    }
+
+    public static function find(): clientChatNoteScopes
+    {
+        return new clientChatNoteScopes(static::class);
+    }
+}
