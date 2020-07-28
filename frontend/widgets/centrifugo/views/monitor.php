3e185a8b340930212a0fc1db2f83df09c808057c
VBarnes <vincent.barnes@techork.com>
2020-07-28 16:11:17 +0300

    feat:(SL-2049) Client Chat Real Time Monitoring  #add param client_chat_real_time_monitoring  #New Migration

--- frontend/widgets/centrifugo/views/monitor.php
+++ frontend/widgets/centrifugo/views/monitor.php
@@ -1,5 +1,5 @@
 <?php
-
+use sales\helpers\setting\SettingHelper;
 /**
  * @var $centrifugoUrl
  * @var $token
@@ -7,9 +7,12 @@
  * @var $this yii\web\View
  */
 
+$realtimeMonitorEnable = SettingHelper::isClientChatRealTimeMonitoringEnabled();
+
 $passChannelsToJs ='["' . implode('", "', $channels) . '"]';
 
 $js = <<<JS
+let enableLiveUpdate = '$realtimeMonitorEnable'
 let channels = $passChannelsToJs;
 var centrifuge = new Centrifuge('$centrifugoUrl');
 centrifuge.setToken('$token');
@@ -21,12 +24,12 @@ function channelConnector(chName)
     centrifuge.subscribe(chName, function(message) {           
         let messageObj = JSON.parse(message.data.message);        
         if(messageObj.chatsData){
-            //console.log(messageObj.chatsData)
+            console.log(messageObj.chatsData)
             //$("#card-live-chat").text('');
             messageObj.chatsData.forEach(function (chat, index) {
                 $("#card-live-chat").prepend(renderChat(chat));                              
             });
-            getLastChatsUpdate()
+            getLastChatsUpdate(enableLiveUpdate)
             updateLocalTime()
         }
         
@@ -64,17 +67,17 @@ function contentUpdate(chatsFromDateTime) {
         type: 'POST',
         data: {"formDate": chatsFromDateTime},
         success: function(data) { 
-            //console.info('Request data on connect');            
-            //$("#page-updated-time").text('').text(data.updatedTime); 
-            //startTimers();                
+            //console.info('Request data on connect'); 
         }
     });
 }
 
-function getLastChatsUpdate() {
-    setTimeout(function(){
+function getLastChatsUpdate() {    
+    if(enableLiveUpdate){
+        setTimeout(function(){
          contentUpdate(getLastCreatedChatDate())  
     }, 10000);
+    }    
 }
 
 function getLastCreatedChatDate()
@@ -138,37 +141,31 @@ function renderChat(chat) {
     return '<div id="ch-'+ chat.cch_id +'" class="col-md-12" style="margin-bottom:2px">' +
                 '<table class="table table-condensed table-client-chat-monitor">' +
                     '<tbody id="chat-'+ chat.cch_id +'">' +
-                    '<tr class="warning">' +
-                        '<td class="text-center" style="width:150px">' + 
-                            renderGeneralInfo(chat.cch_id, chat.cch_created_dt) +
-                        '</td>' +
-                        '<td class="text-left" style="width:250px">' +
-                            renderAgentInfo(chat.cch_id, chat.cch_owner_user_id, chat.username, chat.outMsg) +
-                        '</td>' +
-                        '<td class="text-left" style="width:250px">' +
-                            renderClientInfo(chat.cch_id, chat.cch_client_id, chat.clientName, chat.inMsg) +
-                        '</td>' +
-                        '<td class="text-center" style="width:130px">' +
-                            renderProjectInfo(chat.project, chat.department, chat.channel) +
-                        '</td>' +
-                        
-                        '<td class="text-left" style="width:450px">' +                        
-                            renderAgentMessage(chat.cch_id, chat.cch_owner_user_id, chat.username) +
-                        '</td>' +
-                        '<td class="text-left" style="width:450px">' +                            
-                            renderClientMessage(chat.cch_id, chat.cch_client_id, chat.clientName) +
-                        '</td>' +
-                        
-                        /*'<td class="text-left"><i>l:<a href="/lead/view/0fb7b8b6cb49f458be2cc5fb5b4f4aa1" target="_blank">503669</a></i><br></td>' +*/
-                        /*'<td class="text-center"><i class="fa fa-pause text-success"></i> Delay<br><span class="badge badge-warning timer" data-sec="10965160" data-control="start" data-format="%M:%S">59:50</span></td>' +*/
-                        /*'<td class="text-center" style="width:160px"><i class="fa fa-clock-o"></i> 11:26:49<br></td>' +*/
-                        /*'<td class="text-left" style="width:160px"><i class="fa fa-fax fa-1x fa-border"></i> +18552068194<i class="fa fa-user fa-1x fa-border"></i> Linda</td>' +*/
-                    '</tr>' +
+                        '<tr class="warning">' +
+                            '<td class="text-center" style="width:150px">' + 
+                                renderGeneralInfo(chat.cch_id, chat.cch_created_dt) +
+                            '</td>' +
+                            '<td class="text-left" style="width:250px">' +
+                                renderAgentInfo(chat.cch_id, chat.cch_owner_user_id, chat.username, chat.outMsg) +
+                            '</td>' +
+                            '<td class="text-left" style="width:250px">' +
+                                renderClientInfo(chat.cch_id, chat.cch_client_id, chat.clientName, chat.inMsg) +
+                            '</td>' +
+                            '<td class="text-center" style="width:130px">' +
+                                renderProjectInfo(chat.project, chat.department, chat.channel) +
+                            '</td>' +
+                            
+                            '<td class="text-left" style="width:450px">' +                        
+                                renderAgentMessage(chat.cch_id, chat.cch_owner_user_id, chat.username) +
+                            '</td>' +
+                            '<td class="text-left" style="width:450px">' +                            
+                                renderClientMessage(chat.cch_id, chat.cch_client_id, chat.clientName) +
+                            '</td>' +
+                        '</tr>' +
                     '</tbody>' +
                 '</table>' +
-            '</div>';
+           '</div>';
 }
-
 
 function renderAgentMessage(chatID, agentID, agentName){
     let html = '';
@@ -303,8 +300,8 @@ function renderClientInfo(chatID, clientID, clientName, inMsgCount){
               '<div class="media-body">' +
                     '<a class="title" href="/client/view?id='+ clientID +'" target="_blank">'+ clientName +'</a>' +
                     '<p><i id="icn-'+ countLocator +'" class="fa fa-arrow-down red"></i> Sent Messages: <strong id="count-'+ countLocator +'">'+ inMsgCount +'</strong> </p>' +
-                    /*'<p> <small>12 Sales Today</small>' +*/
-                    '</p>' +
+                    /*'<p> <small>12 Sales Today</small>' +
+                    '</p>' + */
               '</div>' +
           '</div>';
     return html; 
@@ -350,8 +347,8 @@ function renderGeneralInfo(id, chatCreateDate) {
 
 function removePulse() {
   setTimeout(function(){
-            $("i").removeClass('icon-pulse');                        
-    }, 10000);
+      $("i").removeClass('icon-pulse');                        
+  }, 10000);
 }
 
 function pushChatOnTop(chatID) {    
