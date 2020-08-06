6f0338cc86ef6cd2d82e1d76ee1321a1eac92f05
andrew.snake <andrew.snake@techork.com>
2020-08-06 14:35:15 +0300

    feat: added widthProject param (SL-2256)

--- frontend/views/user-project-params/create_ajax.php
+++ frontend/views/user-project-params/create_ajax.php
@@ -49,6 +49,7 @@ use borales\extensions\phoneInput\PhoneInput;
         'data' => $model->upp_email_list_id ? [
             $model->upp_email_list_id => $model->emailList->el_email
         ] : [],
+        'withProject' => true,
     ]) ?>
 
     <?php //= $form->field($model, 'upp_tw_phone_number')->textInput(['maxlength' => true]) ?>
