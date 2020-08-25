d4e8efe69bd109d419e5fe4b24e6c9a1320d5db2
Alex Grub <alex.grub@techork.com>
2020-08-25 11:50:41 +0300

    feat: removed that increase php memory limit to 512MB for yii debug (SL-2445)

--- frontend/controllers/ClientChatController.php
+++ frontend/controllers/ClientChatController.php
@@ -523,7 +523,6 @@ class ClientChatController extends FController
 
 		try {
 			if ($form->load(Yii::$app->request->post()) && !$form->pjaxReload && $form->validate()) {
-				ini_set('memory_limit', '512M');
 				$newDepartment = $this->clientChatService->transfer($form);
 				return '<script>$("#modal-sm").modal("hide"); refreshChatPage('.$form->cchId.', '.ClientChat::TAB_ACTIVE.'); createNotify("Success", "Chat successfully transferred to '.$newDepartment->dep_name.' department. ", "success")</script>';
 			}
@@ -722,7 +721,6 @@ class ClientChatController extends FController
 		];
 
 		try {
-			ini_set('memory_limit', '512M');
 
 			$chat = $this->clientChatRepository->findById($cchId);
 
