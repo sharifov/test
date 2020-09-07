21d8edf0405fd635794d1957d71bf008d7547ccc
Alex Grub <alex.grub@techork.com>
2020-09-07 13:51:49 +0300

    feat: display users who are online in the form of a chat transfer (SL-2554)

--- sales/model/clientChatUserChannel/entity/Scopes.php
+++ sales/model/clientChatUserChannel/entity/Scopes.php
@@ -2,6 +2,11 @@
 
 namespace sales\model\clientChatUserChannel\entity;
 
+use common\models\Employee;
+use common\models\UserOnline;
+use common\models\UserProfile;
+use yii\db\Expression;
+
 /**
  * @see ClientChatUserChannel
  */
@@ -15,5 +20,24 @@ class Scopes extends \yii\db\ActiveQuery
 	public function byUserId(int $id): Scopes
 	{
 		return $this->andWhere(['ccuc_user_id' => $id]);
+	}
+
+	public function joinUser(): Scopes
+	{
+		return $this->join('INNER JOIN', Employee::tableName(), 'ccuc_user_id = id');
+	}
+
+	public function hasRcProfile(): Scopes
+	{
+		return $this->join(
+			'INNER JOIN',
+			UserProfile::tableName(),
+			new Expression('ccuc_user_id = up_user_id and (up_rc_user_id <> \' \' or up_rc_user_id is not null)')
+		);
+	}
+
+	public function onlineUsers(): Scopes
+	{
+		return $this->join('INNER JOIN', UserOnline::tableName(), 'ccuc_user_id = uo_user_id');
 	}
 }
