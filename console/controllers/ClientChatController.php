fb7098e3eeab4acfa98e2464447cf3d7fda810da
Alex Grub <alex.grub@techork.com>
2020-09-24 16:04:43 +0300

    feat: changed registration/deletion console command for rc

--- console/controllers/ClientChatController.php
+++ console/controllers/ClientChatController.php
@@ -168,7 +168,7 @@ class ClientChatController extends Controller
 
 			$userProfile = UserProfile::findOne(['up_user_id' => $user['id']]);
 
-            $result = $rocketChat->deleteUser($userProfile->up_rc_user_id ?? null, $user['nickname_client_chat'] ?: $user['username'], $deleteByUsername);
+            $result = $rocketChat->deleteUser($userProfile->up_rc_user_id ?? null, $user['username'], $deleteByUsername);
 
             echo "\n-- " . $user['username'] . ' ('.$user['id'].') --' . PHP_EOL;
 
