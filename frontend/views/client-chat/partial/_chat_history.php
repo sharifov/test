fc9b57cbbb62dd3d26a0c0963938840667571ba2
andrew.snake <andrew.snake@techork.com>
2020-10-23 16:50:36 +0300

    feat: fix readonly logic (SL-3234)

--- frontend/views/client-chat/partial/_chat_history.php
+++ frontend/views/client-chat/partial/_chat_history.php
@@ -11,14 +11,14 @@ $iDate = null;
 $rcUrl = Yii::$app->rchat->host  . '/home';
 $userRcAuthToken = Auth::user()->userProfile ? Auth::user()->userProfile->up_rc_auth_token : '';
 $readOnly = (!$clientChat->isOwner(Auth::id()) || ($clientChat->isArchive() || $clientChat->isClosed())) ? '&readonly=true' : '';
-$goto = urlencode('/live/' . $clientChat->cch_rid . '?layout=embedded' . $readOnly);
 $randInt = random_int(1, 99999);
+$goto = urlencode('/live/' . $clientChat->cch_rid . '?layout=embedded' . $readOnly . '&rnd=' . $randInt);
 ?>
 
 <?php if ($clientChat): ?>
     <iframe class="_rc-iframe"
         onload="removeCcLoadFromIframe()"
-        src="<?php echo $rcUrl ?>?&layout=embedded<?= $readOnly ?>&resumeToken=<?= $userRcAuthToken ?>&goto=<?= $goto ?>&rnd=<?php echo $randInt ?>"
+        src="<?php echo $rcUrl ?>?&layout=embedded<?= $readOnly ?>&resumeToken=<?= $userRcAuthToken ?>&rnd=<?php echo $randInt ?>&goto=<?= $goto ?>"
         id="_rc-<?php echo $clientChat->cch_id ?>"
         style="border: none; width: 100%; height: 100%;"
         name="_<?php echo $randInt ?>_<?php echo $clientChat->cch_status_id ?>"></iframe>
