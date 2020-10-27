1a523e7e24475700549a00bbe2e461f7e338b5c6
Alex Grub <alex.grub@techork.com>
2020-10-27 09:19:36 +0200

    feat: uncommented creation tables in migration (SL-2235)

--- console/migrations/m201022_110324_create_tbls_for_canned_response.php
+++ console/migrations/m201022_110324_create_tbls_for_canned_response.php
@@ -46,33 +46,33 @@ class m201022_110324_create_tbls_for_canned_response extends Migration
 //            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
 //        }
 //
-//        $this->createTable('{{%client_chat_canned_response_category}}', [
-//            'crc_id' => $this->primaryKey(),
-//            'crc_name' => $this->string(50)->notNull(),
-//            'crc_enabled' => $this->boolean(),
-//            'crc_created_dt' => $this->dateTime(),
-//            'crc_updated_dt' => $this->dateTime(),
-//            'crc_created_user_id' => $this->integer(),
-//            'crc_updated_user_id' => $this->integer()
-//        ]);
+        $this->createTable('{{%client_chat_canned_response_category}}', [
+            'crc_id' => $this->primaryKey(),
+            'crc_name' => $this->string(50)->notNull(),
+            'crc_enabled' => $this->boolean(),
+            'crc_created_dt' => $this->dateTime(),
+            'crc_updated_dt' => $this->dateTime(),
+            'crc_created_user_id' => $this->integer(),
+            'crc_updated_user_id' => $this->integer()
+        ]);
 
 //        $this->addForeignKey('FK-crc_created_user_id', '{{%client_chat_canned_response_category}}', ['crc_created_user_id'], '{{%employees}}', ['id'], 'SET NULL', 'CASCADE');
 //        $this->addForeignKey('FK-crc_updated_user_id', '{{%client_chat_canned_response_category}}', ['crc_updated_user_id'], '{{%employees}}', ['id'], 'SET NULL', 'CASCADE');
 
-//        $this->createTable('{{%client_chat_canned_response}}', [
-//            'cr_id' => $this->primaryKey(),
-//            'cr_project_id' => $this->integer()->defaultValue(null),
-//            'cr_category_id' => $this->integer()->defaultValue(null),
-//            'cr_language_id' => $this->string(5)->defaultValue(null),
-//            'cr_user_id' => $this->integer()->defaultValue(null),
-//            'cr_sort_order' => $this->smallInteger(),
-//            'cr_message' => $this->text(),
-//            'cr_created_dt' => $this->dateTime(),
-//            'cr_updated_dt' => $this->dateTime()
-//        ]);
+        $this->createTable('{{%client_chat_canned_response}}', [
+            'cr_id' => $this->primaryKey(),
+            'cr_project_id' => $this->integer()->defaultValue(null),
+            'cr_category_id' => $this->integer()->defaultValue(null),
+            'cr_language_id' => $this->string(5)->defaultValue(null),
+            'cr_user_id' => $this->integer()->defaultValue(null),
+            'cr_sort_order' => $this->smallInteger(),
+            'cr_message' => $this->text(),
+            'cr_created_dt' => $this->dateTime(),
+            'cr_updated_dt' => $this->dateTime()
+        ]);
 
 //        $this->addForeignKey('FK-cr_project_id', '{{%client_chat_canned_response}}', ['cr_project_id'], '{{%projects}}', ['id'], 'SET NULL', 'CASCADE');
-//        $this->addForeignKey('FK-cr_category_id', '{{%client_chat_canned_response}}', ['cr_category_id'], '{{%client_chat_canned_response_category}}', ['crc_id'], 'SET NULL', 'CASCADE');
+        $this->addForeignKey('FK-cr_category_id', '{{%client_chat_canned_response}}', ['cr_category_id'], '{{%client_chat_canned_response_category}}', ['crc_id'], 'SET NULL', 'CASCADE');
 //        $this->addForeignKey('FK-cr_language_id', '{{%client_chat_canned_response}}', ['cr_language_id'], '{{%language}}', ['language_id'], 'SET NULL', 'CASCADE');
 //        $this->addForeignKey('FK-cr_user_id', '{{%client_chat_canned_response}}', ['cr_user_id'], '{{%employees}}', ['id'], 'SET NULL', 'CASCADE');
 
@@ -91,8 +91,8 @@ class m201022_110324_create_tbls_for_canned_response extends Migration
      */
     public function safeDown()
     {
-//        $this->dropTable('{{%client_chat_canned_response}}');
-//        $this->dropTable('{{%client_chat_canned_response_category}}');
+        $this->dropTable('{{%client_chat_canned_response}}');
+        $this->dropTable('{{%client_chat_canned_response_category}}');
 
         $auth = Yii::$app->authManager;
         $permission = $auth->getPermission('client-chat/canned-response');
