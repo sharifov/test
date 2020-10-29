2d1c3ac848db3116bede592c061b71d6b7d59139
andrew.snake <andrew.snake@techork.com>
2020-10-29 18:40:00 +0200

    feat: in transfer - set status to acrhive (SL-3341)

--- sales/services/clientChatService/ClientChatService.php
+++ sales/services/clientChatService/ClientChatService.php
@@ -418,7 +418,7 @@ class ClientChatService
             }
             $lastMessage = $this->clientChatLastMessageRepository->getByChatId($clientChat->cch_id);
 
-            $clientChat->close($chatUserAccess->ccua_user_id, ClientChatStatusLog::ACTION_ACCEPT_TRANSFER);
+            $clientChat->archive($chatUserAccess->ccua_user_id, ClientChatStatusLog::ACTION_ACCEPT_TRANSFER);
             $this->clientChatRepository->save($clientChat);
 
             $dto = ClientChatCloneDto::feelInOnTransfer($clientChat);
