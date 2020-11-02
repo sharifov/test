0b570b6c2fe5bc55d41aa12d68610a17a15881fe
Alex Grub <alex.grub@techork.com>
2020-11-02 13:55:54 +0200

    feat: added new validation on chat api form (SL-3353)

--- sales/model/clientChatRequest/useCase/api/create/ClientChatRequestApiForm.php
+++ sales/model/clientChatRequest/useCase/api/create/ClientChatRequestApiForm.php
@@ -16,49 +16,66 @@ use yii\helpers\ArrayHelper;
  */
 class ClientChatRequestApiForm extends Model
 {
-	public string $event;
-	public ?int $eventId;
-	public array $data;
-	public ?string $rid;
+    public string $event;
+    public ?int $eventId;
+    public array $data;
+    public ?string $rid;
 
-	private const SCENARIO_ROOM_CONNECTED = 'ROOM_CONNECTED';
+    private const SCENARIO_ROOM_CONNECTED = 'ROOM_CONNECTED';
 
-	private const SCENARIO_LIST = [
-		self::SCENARIO_ROOM_CONNECTED
-	];
+    private const SCENARIO_LIST = [
+        self::SCENARIO_ROOM_CONNECTED
+    ];
 
-	public function rules(): array
-	{
-		return [
-			[['event', 'rid'], 'string'],
-			[['event'], 'in', 'range' => ClientChatRequest::getEventList()],
+    public function rules(): array
+    {
+        return [
+            [['event', 'rid'], 'string'],
+            [['event'], 'in', 'range' => ClientChatRequest::getEventList()],
 
-			['data', 'safe'],
-			['data', 'validateTsParam'],
+            ['data', 'safe'],
+            ['data', 'validateTsParam'],
+            ['data', 'validateProjectParam', 'on' => self::SCENARIO_ROOM_CONNECTED],
+            ['data', 'validateMessageFromBot'],
 
-			[['event'], 'required'],
-			[['rid'], 'required', 'on' => self::SCENARIO_ROOM_CONNECTED],
+            [['event'], 'required'],
+            [['rid'], 'required', 'on' => self::SCENARIO_ROOM_CONNECTED],
 
-			[['eventId'], 'integer'],
-			[['eventId'], 'in', 'range' => ClientChatRequest::getEventIdList()],
+            [['eventId'], 'integer'],
+            [['eventId'], 'in', 'range' => ClientChatRequest::getEventIdList()],
 
-		];
-	}
+        ];
+    }
 
-	public function fillIn(string $event, array $data): self
-	{
-		$this->event = $event;
-		$this->data = $data;
-		$this->eventId = ClientChatRequest::getEventIdByName($event);
-		$this->rid = $data['rid'] ?? null;
-		$this->scenario = in_array($this->event, self::SCENARIO_LIST) ? $this->event : 'default';
-		return $this;
-	}
+    public function fillIn(string $event, array $data): self
+    {
+        $this->event = $event;
+        $this->data = $data;
+        $this->eventId = ClientChatRequest::getEventIdByName($event);
+        $this->rid = $data['rid'] ?? null;
+        $this->scenario = in_array($this->event, self::SCENARIO_LIST) ? $this->event : 'default';
+        return $this;
+    }
 
-	public function validateTsParam($attributes): void
-	{
-		if (ClientChatRequest::isMessage($this->eventId) && !isset($this->data['timestamp'])) {
-			$this->addError('data', 'Undefined index: timestamp in data request');
-		}
-	}
+    public function validateTsParam($attributes): void
+    {
+        if (ClientChatRequest::isMessage($this->eventId) && !isset($this->data['timestamp'])) {
+            $this->addError('data', 'Undefined index: timestamp in data request');
+        }
+    }
+
+    public function validateProjectParam($attribute): void
+    {
+        if (empty($this->data['visitor']['project'])) {
+            $this->addError('data', 'Project parameter is not found');
+        }
+    }
+
+    public function validateMessageFromBot()
+    {
+        if (ClientChatRequest::isMessage($this->eventId)
+            && !empty($this->data['agent']['username']) && $this->data['agent']['username'] === 'bot') {
+            $this->addError('data', 'Messages from the bot are not processed');
+        }
+    }
 }
