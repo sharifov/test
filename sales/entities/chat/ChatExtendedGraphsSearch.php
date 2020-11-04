5716f777bf09a7c979f21fef64cba2fbb93b726b
VBarnes <vincent.barnes@techork.com>
2020-11-04 10:25:33 +0200

    feat:(SL-3373) Statistics update  # add filter by source type in subquery for accepted chats counting

--- sales/entities/chat/ChatExtendedGraphsSearch.php
+++ sales/entities/chat/ChatExtendedGraphsSearch.php
@@ -102,6 +102,7 @@ class ChatExtendedGraphsSearch extends ClientChatSearch
 
         $ccTblSubQuery = new Query();
         $ccTblSubQuery->select('*')->from(ClientChat::tableName())->where('' . $this->setGroupingParam() . ' = date');
+        $ccTblSubQuery->andWhere(['cch_source_type_id' => [ClientChat::SOURCE_TYPE_CLIENT, ClientChat::SOURCE_TYPE_AGENT]]);
 
         $ccuaTblSubQuery = new Query();
         $ccuaTblSubQuery->select('*')->from(ClientChatUserAccess::tableName())->where(['ccua_status_id' => ClientChatUserAccess::STATUS_ACCEPT]);
