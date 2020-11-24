64d4913c38791cc9e9b3419c65e03eafc98f4a87
AlexConnor <alex.connor@techork.com>
2020-11-24 11:42:24 +0200

    fix: updated default source frontend language

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -149,7 +149,7 @@ return [
                 '*' => [
                     'class' => 'yii\i18n\DbMessageSource',
                     'db' => 'db',
-                    'sourceLanguage' => 'en-US', // Developer language
+                    'sourceLanguage' => 'xx', // 'en-US' Developer language
                     'sourceMessageTable' => '{{%language_source}}',
                     'messageTable' => '{{%language_translate}}',
                     'cachingDuration' => 3600,
