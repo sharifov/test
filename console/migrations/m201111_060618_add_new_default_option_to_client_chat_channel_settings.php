926dbaf1d0f86a89ef191cfa40056792c8d43001
AlexConnor <alex.connor@techork.com>
2020-12-01 12:45:52 +0200

    fix: update old migration - channel->ccc_settings (SL-3353)

--- console/migrations/m201111_060618_add_new_default_option_to_client_chat_channel_settings.php
+++ console/migrations/m201111_060618_add_new_default_option_to_client_chat_channel_settings.php
@@ -12,18 +12,18 @@ class m201111_060618_add_new_default_option_to_client_chat_channel_settings exte
      */
     public function safeUp()
     {
-        $chatChannels = \sales\model\clientChatChannel\entity\ClientChatChannel::find()->all();
-
-        /** @var $channel ClientChatChannel */
-        foreach ($chatChannels as $channel) {
-            $settings = \yii\helpers\Json::decode($channel->ccc_settings);
-            if (!isset($settings['system']['allowRealtime'])) {
-                $settings['system']['allowRealtime'] = true;
-            }
-
-            $channel->ccc_settings = \yii\helpers\Json::encode($settings);
-            $channel->save();
-        }
+//        $chatChannels = \sales\model\clientChatChannel\entity\ClientChatChannel::find()->all();
+//
+//        /** @var $channel ClientChatChannel */
+//        foreach ($chatChannels as $channel) {
+//            $settings = \yii\helpers\Json::decode($channel->ccc_settings);
+//            if (!isset($settings['system']['allowRealtime'])) {
+//                $settings['system']['allowRealtime'] = true;
+//            }
+//
+//            $channel->ccc_settings = \yii\helpers\Json::encode($settings);
+//            $channel->save();
+//        }
     }
 
     /**
