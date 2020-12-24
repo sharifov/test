f475d70a0a68954b1f6c2ace20a3fd6204fea4b1
andrew.snake <andrew.snake@techork.com>
2020-12-24 10:54:34 +0200

    Finish SL-4163_Error_TelegramJob_execute_catch

--- common/components/jobs/TelegramSendMessageJob.php
+++ common/components/jobs/TelegramSendMessageJob.php
@@ -9,11 +9,13 @@
 namespace common\components\jobs;
 
 use common\models\UserProfile;
+use sales\services\telegram\TelegramService;
 use yii\base\BaseObject;
 use yii\helpers\VarDumper;
 use yii\queue\JobInterface;
 use Yii;
 use yii\queue\Queue;
+use yii\queue\RetryableJobInterface;
 
 /**
  * This is the model class for "TelegramSendMessage".
@@ -22,7 +24,7 @@ use yii\queue\Queue;
  * @property string $text
  */
 
-class TelegramSendMessageJob extends BaseObject implements JobInterface
+class TelegramSendMessageJob implements RetryableJobInterface
 {
     public $user_id;
     public $text;
@@ -33,39 +35,52 @@ class TelegramSendMessageJob extends BaseObject implements JobInterface
      */
     public function execute($queue): bool
     {
-
         try {
-            if ($this->user_id) {
-                $profile = UserProfile::find()->where(['up_user_id' => $this->user_id])->limit(1)->one();
+            if ($this->user_id && $telegramChatId = TelegramService::getTelegramChatIdByUserId($this->user_id)) {
+                $tgm = Yii::$app->telegram;
+                $tgm->sendMessage([
+                    'chat_id' => $telegramChatId,
+                    'text' => strip_tags($this->text),
+                ]);
 
-                //Yii::info($this->user_id, 'info\TelegramJob:execute:info');
-
-                if ($profile && $profile->up_telegram && $profile->up_telegram_enable) {
-                    $tgm = Yii::$app->telegram;
-
-                    $tgm->sendMessage([
-                        'chat_id' => $profile->up_telegram,
-                        'text' => strip_tags($this->text),
-                    ]);
-
-                    unset($tgm, $profile);
-                    return true;
-                }
+                unset($tgm);
+                return true;
             }
-        } catch (\Throwable $e) {
-            Yii::error(VarDumper::dumpAsString($e->getMessage()), 'TelegramJob:execute:catch');
-        }
+        } catch (\Throwable $throwable) {
+            $errorMessage = VarDumper::dumpAsString($throwable->getMessage());
 
+            if (TelegramService::isBotBlockedByUser($errorMessage)) {
+                Yii::info(VarDumper::dumpAsString([
+                    'message' => $errorMessage,
+                    'userId' => $this->user_id,
+                ]), 'info\TelegramJob:execute:catch');
+
+                UserProfile::disableTelegramByUserId((int) $this->user_id);
+            } else {
+                Yii::error(VarDumper::dumpAsString([
+                    'message' => $errorMessage,
+                    'userId' => $this->user_id,
+                ]), 'TelegramJob:execute:catch');
+            }
+        }
         return false;
     }
 
-    public function getTtr()
+    /**
+     * @return int time to reserve in seconds
+     */
+    public function getTtr(): int
     {
-        return 1 * 5;
+        return 30;
     }
 
-    /*public function canRetry($attempt, $error)
+    /**
+     * @param int $attempt number
+     * @param \Exception|\Throwable $error from last execute of the job
+     * @return bool
+     */
+    public function canRetry($attempt, $error): bool
     {
-        return ($attempt < 5) && ($error instanceof TemporaryException);
-    }*/
+        return ($attempt < 3);
+    }
 }
---
+++ sales/services/telegram/TelegramService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace sales\services\telegram;
+
+use common\models\UserProfile;
+use frontend\helpers\JsonHelper;
+use sales\helpers\app\AppHelper;
+use Yii;
+
+/**
+ * Class TelegramService
+ */
+class TelegramService
+{
+    public const DESCRIPTION_BOT_BLOCKED_BY_USER = 'Forbidden: bot was blocked by the user';
+
+    /**
+     * @param string $responseBody
+     * @return array|null
+     */
+    public static function getResponseFromStringBody(string $responseBody): ?array
+    {
+        try {
+            if (($beginPos = strpos($responseBody, '{')) !== false && ($endPos = strpos($responseBody, '}')) !== false) {
+                $rawJson = substr($responseBody, $beginPos, ($endPos - $beginPos) + 1);
+                $rawJson = stripslashes($rawJson);
+                return JsonHelper::decode($rawJson);
+            }
+        } catch (\Throwable $throwable) {
+            Yii::warning(
+                AppHelper::throwableFormatter($throwable),
+                'TelegramService:getResponseFromStringBody'
+            );
+        }
+        return null;
+    }
+
+    /**
+     * @param string $responseBody
+     * @return bool
+     */
+    public static function isBotBlockedByUser(string $responseBody): bool
+    {
+        $response = self::getResponseFromStringBody($responseBody);
+        return !empty($response['description']) && $response['description'] === self::DESCRIPTION_BOT_BLOCKED_BY_USER;
+    }
+
+    /**
+     * @param int $userId
+     * @return null|string
+     */
+    public static function getTelegramChatIdByUserId(?int $userId): ?string
+    {
+        $profile = UserProfile::findOne(['up_user_id' => (int) $userId]);
+        if ($profile && $profile->up_telegram && $profile->up_telegram_enable) {
+            return $profile->up_telegram;
+        }
+        return null;
+    }
+}
