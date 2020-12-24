802e1776da4ecd7bcc7b4ecc78a7a705de5048b4
Alex Grub <alex.grub@techork.com>
2020-12-24 12:21:38 +0200

    feat: included bootstrap progressbar asset to minified assets (SL-4157)

--- frontend/assets/AppCrudAsset.php
+++ frontend/assets/AppCrudAsset.php
@@ -5,6 +5,7 @@ namespace frontend\assets;
 
 use frontend\assets\groups\AllSharedAsset;
 use frontend\assets\groups\BootstrapGroupAsset;
+use frontend\themes\gentelella_v2\assets\BootstrapProgressbar;
 use frontend\themes\gentelella_v2\assets\groups\GentelellaCrudAsset;
 use yii\web\JqueryAsset;
 
@@ -13,6 +14,7 @@ class AppCrudAsset extends \yii\web\AssetBundle
     public $depends = [
         JqueryAsset::class,
         BootstrapGroupAsset::class,
+        BootstrapProgressbar::class,
         AllSharedAsset::class,
         PageLoaderAsset::class,
         GentelellaCrudAsset::class,
