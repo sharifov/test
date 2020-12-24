6c4d7c049da964098903838dd63f6d100a3c7604
AlexConnor <alex.connor@techork.com>
2020-12-24 12:56:31 +0200

    feat: updated airports page (SL-4165)

--- frontend/views/airports/index.php
+++ frontend/views/airports/index.php
@@ -2,6 +2,8 @@
 
 use common\components\grid\DateTimeColumn;
 use common\components\grid\UserSelect2Column;
+use common\models\Airports;
+use kartik\select2\Select2;
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
@@ -15,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="airports-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><i class="fa fa-plane"></i> <?= Html::encode($this->title) ?></h1>
 
     <p>
         <?= Html::a('<i class="fa fa-plus"></i> Create Airports', ['create'], ['class' => 'btn btn-success']) ?>
@@ -39,29 +41,116 @@ $this->params['breadcrumbs'][] = $this->title;
     </p>
 
     <?php Pjax::begin(); ?>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]);?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+        'tableOptions' => ['class' => 'table table-bordered table-condensed table-hover'],
+        'rowOptions' => static function (Airports $model) {
+            if ($model->a_close) {
+                return [
+                    'class' => 'danger'
+                ];
+            }
+
+            if ($model->a_disabled) {
+                return [
+                    'class' => 'danger'
+                ];
+            }
+        },
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-            'iata',
+            [
+                'attribute' => 'iata',
+                'value' => static function (Airports $model) {
+                    return '<span class="badge badge-info">' . Html::encode($model->iata) . '</span>';
+                },
+                'format' => 'raw'
+            ],
             'a_icao',
             'name',
+
+            //'a_city_code',
+            [
+                'attribute' => 'a_city_code',
+                'value' => static function (Airports $model) {
+                    return $model->a_city_code ? '<span class="badge badge-light">' . Html::encode($model->a_city_code) . '</span>' : '-';
+                },
+                'format' => 'raw'
+            ],
             'city',
-            'a_city_code',
-            'country',
-            'a_country_code',
-            'a_state',
+
+            //'a_country_code',
 
             [
-                    'attribute' => 'timezone',
-                'filter' => \common\models\Employee::timezoneList(true)
+                //'label' => 'Type Name',
+                'attribute' => 'a_country_code',
+                'contentOptions' => ['style' => 'width: 240px'],
+                'value' => static function (Airports $model) {
+                    return $model->a_country_code ? '<span class="badge badge-success">' . Html::encode($model->a_country_code) . '</span>' : '-';
+                },
+                'format' => 'raw',
+                'filter' => Select2::widget([
+                    //'model' => \common\models\search\ProjectLocaleSearch::class,
+                    //'attribute' => 'pl_language_id',
+
+                    'name' => 'AirportsSearch[a_country_code]',
+                    'data' => \common\models\Language::getCountryNames(),
+
+                    //'theme' => Select2::THEME_DEFAULT,
+                    'size' => Select2::SMALL,
+                    'value' => empty(Yii::$app->request->get('AirportsSearch')['a_country_code']) ? null : Yii::$app->request->get('AirportsSearch')['a_country_code'],
+                    //'hideSearch' => false,
+                    'options' => [
+                        'placeholder' => '',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true,
+                    ]
+                ]),
             ],
+            'country',
+
+            //'a_state',
+
+
+            [
+                //'label' => 'Type Name',
+                'attribute' => 'timezone',
+                'contentOptions' => ['style' => 'width: 240px'],
+                'value' => static function (Airports $model) {
+                    return $model->timezone ? Html::encode($model->timezone) : '-';
+                },
+                'format' => 'raw',
+                'filter' => Select2::widget([
+                    //'model' => \common\models\search\ProjectLocaleSearch::class,
+                    //'attribute' => 'pl_language_id',
+
+                    'name' => 'AirportsSearch[timezone]',
+                    'data' => \common\models\Employee::timezoneList(true),
+
+                    //'theme' => Select2::THEME_DEFAULT,
+                    'size' => Select2::SMALL,
+                    'value' => empty(Yii::$app->request->get('AirportsSearch')['timezone']) ? null : Yii::$app->request->get('AirportsSearch')['timezone'],
+                    //'hideSearch' => false,
+                    'options' => [
+                        'placeholder' => '',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true,
+                    ]
+                ]),
+            ],
+
+//            [
+//                    'attribute' => 'timezone',
+//                'filter' => \common\models\Employee::timezoneList(true)
+//            ],
             'dst',
-            'a_rank',
-            'a_multicity:boolean',
+            //'a_rank',
+            //'a_multicity:boolean',
             'a_close:boolean',
             'a_disabled:boolean',
             'latitude',
