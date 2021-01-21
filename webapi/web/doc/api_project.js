5b90c14ae853020615fd4537bbadaeeb523bcb56
andrew.snake <andrew.snake@techork.com>
2021-01-21 15:59:31 +0200

    feat: refresh api doc (SL-4434)

--- webapi/web/doc/api_project.js
+++ webapi/web/doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-21T12:04:11.405Z",
+    "time": "2021-01-21T13:53:34.543Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
