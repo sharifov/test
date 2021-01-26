97a0d4a863e2a2dd7a78add343c8edb4e5950f80
AlexConnor <alex.connor@techork.com>
2021-01-26 16:00:31 +0200

    feat: updated js - realtime map (SL-4486)

--- frontend/web/js/vue/call-realtime-map/realtime-map.js
+++ frontend/web/js/vue/call-realtime-map/realtime-map.js
@@ -476,7 +476,7 @@ var callMapApp = Vue.createApp({
                 if (item.us_is_on_call) {
                     iconClass = 'fa fa-phone'
                 } else if (item.us_call_phone_status) {
-                    iconClass = 'fa fa-lock'
+                    iconClass = 'fa fa-tty'
                 } else if (item.us_has_call_access) {
                     iconClass = 'fa fa-random'
                 }
