3dc694d626df3938f555f13ef6163c2c990af081
VBarnes <vincent.barnes@techork.com>
2021-02-04 13:54:17 +0200

    ref:(SL-4589) Refactoring logs report

--- common/models/search/ApiLogSearch.php
+++ common/models/search/ApiLogSearch.php
@@ -8,6 +8,7 @@ use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use common\models\ApiLog;
+use yii\db\Expression;
 
 /**
  * ApiLogSearch represents the model behind the search form of `common\models\ApiLog`.
@@ -29,7 +30,18 @@ class ApiLogSearch extends ApiLog
     {
         return [
             [['al_id', 'al_user_id', 'al_memory_usage', 'al_db_query_count'], 'integer'],
-            [['al_request_data', 'al_response_data', 'al_response_dt', 'al_ip_address', 'al_action', 'al_execution_time', 'al_db_execution_time'], 'safe'],
+            [
+                [
+                    'al_request_data',
+                    'al_response_data',
+                    'al_response_dt',
+                    'al_ip_address',
+                    'al_action',
+                    'al_execution_time',
+                    'al_db_execution_time'
+                ],
+                'safe'
+            ],
             ['createTimeRange', 'convertDateTimeRange']
         ];
     }
@@ -102,5 +114,64 @@ class ApiLogSearch extends ApiLog
             ->andFilterWhere(['like', 'al_ip_address', $this->al_ip_address]);
 
         return $dataProvider;
+    }
+
+    /**
+     * @param string $fromDate
+     * @param string $todate
+     * @param string $range
+     * @param string $apiUserId
+     * @param string $selectedAction
+     * @return array
+     */
+    public static function getApiLogStats(
+        string $fromDate,
+        string $todate,
+        string $range,
+        string $apiUserId,
+        string $selectedAction
+    ): array {
+        if ($range == 'H') {
+            $queryDateFormat = 'HH24:00';
+        } elseif ($range == 'D') {
+            $queryDateFormat = 'YYYY-MM-DD';
+        } elseif ($range == 'M') {
+            $queryDateFormat = 'YYYY-MON';
+        } elseif ($range == 'HD') {
+            $queryDateFormat = 'YYYY-MM-DD HH24:00';
+        }
+
+        $actionList = self::getActionsList();
+
+        $apiStatsQuery = self::find();
+        $apiStatsQuery->select(["al_action, AVG(al_execution_time) AS execution_time, AVG(al_memory_usage) AS memUsage, DATE(al_request_dt) as create_date, to_char(al_request_dt, '$queryDateFormat' ) as timeLine, COUNT(*) AS cnt"]);
+        $apiStatsQuery->from('api_log');
+        $apiStatsQuery->where(['between', 'DATE(al_request_dt)', $fromDate, $todate]);
+        $apiStatsQuery->andWhere('al_execution_time IS NOT NULL');
+        if ($apiUserId != '') {
+            $apiStatsQuery->andWhere(['=', 'al_user_id', $apiUserId]);
+        }
+        if ($selectedAction != '') {
+            $apiStatsQuery->andWhere(['=', 'al_action', $selectedAction]);
+        }
+        $apiStatsQuery->groupBy(["al_action, create_date, to_char(al_request_dt, '$queryDateFormat')"]);
+        $apiStatsQuery->orderBy(new Expression('create_date ASC, execution_time DESC, timeLine ASC'));
+        $result = $apiStatsQuery->asArray()->all();
+        $apiStats = [];
+
+        foreach ($actionList as $actionKey => $action) {
+            foreach ($result as $key => $item) {
+                if ($action['al_action'] == $item['al_action']) {
+                    $apiStats[$item['timeline']]['action' . $actionKey] = $item['al_action'];
+                    $apiStats[$item['timeline']]['exetime' . $actionKey] = $item['execution_time'];
+                    $apiStats[$item['timeline']]['memusage' . $actionKey] = $item['memusage'];
+                    $apiStats[$item['timeline']]['cnt' . $actionKey] = $item['cnt'];
+                    $apiStats[$item['timeline']]['timeline'] = $item['timeline'];
+                }
+            }
+        }
+
+        ksort($apiStats);
+        return $apiStats;
     }
 }
