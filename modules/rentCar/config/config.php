2e0c3963d443937b5084165a944296c90d1e0ad6
andrew.snake <andrew.snake@techork.com>
2021-02-11 18:00:06 +0200

    fix: fix config (SL-4662)

--- modules/rentCar/config/config.php
+++ modules/rentCar/config/config.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'apiService' => [
             'class' => \modules\rentCar\components\ApiRentCarService::class,
-            'url' => 'https://stage-communication-api.travel-dev.com',
+            'url' => 'https://stage-communication-api.travel-dev.com/v1/',
             'username' => 'sales',
             'password' => 'Sales2018!',
         ],
