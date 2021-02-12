a8b13beab16be54a5497d15f934c8b6706406f95
andrew.snake <andrew.snake@techork.com>
2021-02-12 11:57:16 +0200

    Finish SL-4718_RentCar_Cant_add_quote

--- modules/rentCar/src/helpers/RentCarQuoteHelper.php
+++ modules/rentCar/src/helpers/RentCarQuoteHelper.php
@@ -3,6 +3,7 @@
 namespace modules\rentCar\src\helpers;
 
 use modules\rentCar\src\entity\rentCar\RentCar;
+use yii\helpers\StringHelper;
 
 /**
  * Class RentCarQuoteHelper
@@ -15,10 +16,9 @@ class RentCarQuoteHelper
      * @param string $separator
      * @return string
      */
-    public static function nameGenerator(RentCar $rentCar, array $data, string $separator = ' '): string
+    public static function nameGenerator(RentCar $rentCar, array $data, string $separator = ' | '): string
     {
-        return $rentCar->prc_pick_up_code . $separator .
-            $rentCar->prc_pick_up_date . $separator .
-            RentCarDataParser::getModelName($data);
+        $name = $rentCar->prc_pick_up_code  . $separator . RentCarDataParser::getModelName($data);
+        return StringHelper::truncate($name, 37);
     }
 }
