bcf80149907c3416a74f110c40a1edbe03f32294
VBarnes <vincent.barnes@techork.com>
2021-02-12 17:57:06 +0200

    fix:(SL-4692) Client phone and email displayed  # fix email masking function

--- sales/helpers/email/MaskEmailHelper.php
+++ sales/helpers/email/MaskEmailHelper.php
@@ -9,11 +9,13 @@ class MaskEmailHelper
     public static function masking(string $email): string
     {
         if (filter_var($email, FILTER_VALIDATE_EMAIL) && SettingHelper::clientDataPrivacyEnable()) {
-            list($first, $last) = explode('@', $email);
-            $first = str_replace(substr($first, '3'), str_repeat('*', strlen($first) - 3), $first);
-            $last = explode('.', $last);
-            $last_domain = str_replace(substr($last['0'], '0', strlen($last['0']) - 2), str_repeat('*', strlen($last['0']) - 2), $last['0']);
-            return $first . '@' . $last_domain . '.' . $last['1'];
+            list($user, $domains) = explode('@', $email);
+
+            $domains = explode('.', $domains);
+            $user = strlen($user) >= 3 ? substr($user, 0, 3) : $user;
+            $domainName = substr($domains['0'], -2);
+
+            return $user . str_repeat('*', 5) . '@' . str_repeat('*', 2) . $domainName . '.' . $domains['1'];
         }
 
         return $email;
