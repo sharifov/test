4a31f426da227923701ac87b124bea18755701c2
AlexConnor <alex.connor@techork.com>
2021-02-15 11:53:00 +0200

    feat: updated fas fa-archway icon (SL-4744)

--- console/migrations/m210215_090147_update_tbl_product_type_icon_css.php
+++ console/migrations/m210215_090147_update_tbl_product_type_icon_css.php
@@ -12,7 +12,7 @@ class m210215_090147_update_tbl_product_type_icon_css extends Migration
       2 => ['pt_sort_order' => 2, 'pt_icon_class' => 'fa fa-hotel'],
       3 => ['pt_sort_order' => 3, 'pt_icon_class' => 'fa fa-car'],
       4 => ['pt_sort_order' => 4, 'pt_icon_class' => 'fa fa-ship'],
-      5 => ['pt_sort_order' => 5, 'pt_icon_class' => 'fa fa-university'],
+      5 => ['pt_sort_order' => 5, 'pt_icon_class' => 'fas fa-archway'],
     ];
 
     /**
