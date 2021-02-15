257a29d20496fd3e91a609f71dbf5588ffd5920b
serge.murphy <serge.murphy@techork.com>
2021-02-15 14:52:42 +0200

    feat: added default pay status (SL-4747)

--- modules/order/src/services/CreateOrderDTO.php
+++ modules/order/src/services/CreateOrderDTO.php
@@ -2,16 +2,20 @@
 
 namespace modules\order\src\services;
 
+use modules\order\src\entities\order\OrderPayStatus;
 use modules\order\src\entities\order\OrderStatus;
 
 class CreateOrderDTO
 {
+    public $payStatus;
+
     public $status;
 
     public $leadId;
 
     public function __construct(int $leadId)
     {
+        $this->payStatus = OrderPayStatus::NOT_PAID;
         $this->status = OrderStatus::PENDING;
         $this->leadId = $leadId;
     }
