459e1c90ab5fedb4558ea7df42472d738694558e
andrew.snake <andrew.snake@techork.com>
2021-02-15 14:06:20 +0200

    feat: fix reload offers (SL-4762)

--- modules/rentCar/views/rent-car/partial/_quote_prices.php
+++ modules/rentCar/views/rent-car/partial/_quote_prices.php
@@ -60,7 +60,14 @@ $productQuote = $rentCarQuote->rcqProductQuote;
                         'inputType' => Editable::INPUT_TEXT,
                         'buttonsTemplate' => '{submit}',
                         'pluginEvents' => [
-                            'editableSuccess' => "function(event, val, form, data) { $.pjax.reload({container: '#pjax-quote_prices-{$rentCarQuote->getId()}', async: false}); $('#quote_profit_{$rentCarQuote->getId()}').popover('hide').popover('dispose');$('#quote_profit_{$rentCarQuote->getId()}').popover();$.pjax.reload({container: '#pjax-product-quote-{$productQuote->pq_id}', async: false});pjaxReload({container: '#pjax-lead-orders'})}",
+                            'editableSuccess' => "function(event, val, form, data) { 
+                                $.pjax.reload({container: '#pjax-quote_prices-{$rentCarQuote->getId()}', async: false}); 
+                                $('#quote_profit_{$rentCarQuote->getId()}').popover('hide').popover('dispose');
+                                $('#quote_profit_{$rentCarQuote->getId()}').popover();
+                                $.pjax.reload({container: '#pjax-product-quote-{$productQuote->pq_id}', async: false});
+                                pjaxReload({container: '#pjax-lead-orders'});
+                                pjaxReload({container: '#pjax-lead-offers'});
+                            }",
                         ],
                         'inlineSettings' => [
                             'templateBefore' => '<div class="editable-pannel">{loading}',
