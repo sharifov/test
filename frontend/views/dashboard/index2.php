a335cc870ed3275a214b1daef290e76591ebe204
VBarnes <vincent.barnes@techork.com>
2021-02-16 14:59:09 +0200

    feat:(SL-4653) Admin Dashboard refactor

--- frontend/views/dashboard/index2.php
+++ frontend/views/dashboard/index2.php
@@ -1711,7 +1711,7 @@
                         <div class="x_content">
                             <div class="dashboard-widget-content">
                                 <div class="col-md-4 hidden-small">
-                                    <h2 class="line_30">3297 Leads from 60 countries</h2>
+                                    <h2 class="line_30">3297 Leads from 171 countries</h2>
                                     <table class="countries_list">
                                         <tbody>
                                         <tr>
