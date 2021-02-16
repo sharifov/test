8f1a7935d71b04a0b8f1c8b217c6789e95efaefd
Alex Grub <alex.grub@techork.com>
2021-02-16 14:50:28 +0200

    feat: display correctly btns for search hotel quotes (SL-4783)

--- modules/hotel/views/hotel-quote/search/_list_hotel_room_quotes.php
+++ modules/hotel/views/hotel-quote/search/_list_hotel_room_quotes.php
@@ -25,12 +25,21 @@ $quoteExist = $hotelSearch->quoteExist($dataRoom['groupKey']);
       <th>Board</th>
       <th>Guests</th>
       <th>Price</th>
-      <th class="text-right"></th>
+      <th class="text-right">
+          <?php if ($quoteExist) :?>
+            <span class="badge badge-white">Added</span>
+          <?php else : ?>
+              <?= \yii\bootstrap4\Html::a('<i class="fa fa-plus"></i> add Quote', null, ['data-url' => \yii\helpers\Url::to(['/hotel/hotel-quote/add-ajax', 'ph_id' => $hotelSearch->ph_id]),
+                  'data-hotel-code' => $dataHotel['code'],
+                  'data-quote-key' => $dataRoom['groupKey'],
+                  'class' => 'btn btn-success btn-add-hotel-quote']) ?>
+          <?php endif; ?>
+      </th>
     </tr>
   </thead>
   <tbody>
     <?php foreach ($quoteRooms as $quoteRoomKey => $quoteRoom) : ?>
-    <tr id="tr-hotel-quote-<?=($dataRoom['groupKey'])?>" <?= $quoteExist ? 'class="bg-success"' : '' ?>>
+    <tr id="tr-hotel-quote-<?=($dataRoom['groupKey'])?>" class="tr-hotel-quote-<?=($dataRoom['groupKey'])?> <?= $quoteExist ? 'bg-success' : '' ?>">
       <th><?= $quoteRoomKey + 1 ?></th>
       <td>
         <div><?= Html::encode($quoteRoom['name']) ?></div>
@@ -41,17 +50,7 @@ $quoteExist = $hotelSearch->quoteExist($dataRoom['groupKey']);
         <span class="ml-2"><i class="fa fa-user"></i> <?=(Html::encode($quoteRoom['adults'] ?? 0))?></span>
         <span class="ml-2"><i class="fa fa-child"></i> <?=(Html::encode($quoteRoom['children'] ?? 0))?></span>
       </td>
-      <td>$<?=number_format(Html::encode($quoteRoom['amount'] - ($quoteRoom['markup'] ?? 0)), 2)?></td>
-      <td>
-          <?php if ($quoteExist) :?>
-            <span class="badge badge-white">Added</span>
-          <?php else : ?>
-              <?= \yii\bootstrap4\Html::a('<i class="fa fa-plus"></i> add Quote', null, ['data-url' => \yii\helpers\Url::to(['/hotel/hotel-quote/add-ajax', 'ph_id' => $hotelSearch->ph_id]),
-                  'data-hotel-code' => $dataHotel['code'],
-                  'data-quote-key' => $dataRoom['groupKey'],
-                  'class' => 'btn btn-success btn-add-hotel-quote']) ?>
-          <?php endif; ?>
-      </td>
+      <td colspan="2">$<?=number_format(Html::encode($quoteRoom['amount'] - ($quoteRoom['markup'] ?? 0)), 2)?></td>
     </tr>
     <?php endforeach; ?>
   </tbody>
