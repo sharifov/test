66c3e20210e0ae6c73ce1eebf94be4a0d55d3fcb
VBarnes <vincent.barnes@techork.com>
2021-02-18 12:10:55 +0200

    feat:(SL-4673) Search Leads - divide filters into blocks  # divided filters horizontal position

--- frontend/views/leads/_search.php
+++ frontend/views/leads/_search.php
@@ -28,42 +28,42 @@ use frontend\extensions\DatePicker;
     ]); ?>
 
     <div class="row">
-        <div class="col-md-3 col-sm-3  profile_details">
+        <div class="col-md-12 col-sm-12 profile_details">
             <div class="well profile_view">
                 <div class="col-sm-12">
-                    <h4 class="brief"><i>Lead</i></h4><br>
+                    <h4 class="brief"><i>Lead</i></h4>
                     <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'id')->input('number', ['min' => 1]) ?>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'uid') ?>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'gid')->textInput(['maxlength' => true]) ?>
                         </div>
                         <?php /*<div class="col-md-4">
                     <?= $form->field($model, 'discount_id')->input('number', ['min' => 1]) ?>
                 </div>*/ ?>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4">
+                   <!-- </div>
+                    <div class="row">-->
+                        <div class="col-md-1">
                             <?= $form->field($model, 'client_id')->input('number', ['min' => 1]) ?>
                         </div>
-                        <div class="col-md-8">
+                        <div class="col-md-2">
                             <?= $form->field($model, 'client_name') ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-2">
                             <?= $form->field($model, 'client_email')//->input('email')?>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-2">
                             <?= $form->field($model, 'client_phone') ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-2">
                             <?= $form->field($model, 'createdType')->dropDownList(Lead::TYPE_CREATE_LIST, ['prompt' => '-']) ?>
                         </div>
                     </div>
@@ -73,12 +73,12 @@ use frontend\extensions\DatePicker;
             </div>
         </div>
 
-        <div class="col-md-3 col-sm-3  profile_details">
+        <div class="col-md-12 col-sm-12  profile_details">
             <div class="well profile_view">
                 <div class="col-sm-12">
-                    <h4 class="brief"><i>Common</i></h4><br>
+                    <h4 class="brief"><i>Common</i></h4>
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-1">
                             <?php //= $form->field($model, 'status')->dropDownList(Lead::STATUS_LIST, ['prompt' => '-'])?>
                             <?php
                             echo $form->field($model, 'statuses')->widget(Select2::class, [
@@ -90,13 +90,13 @@ use frontend\extensions\DatePicker;
                             ?>
 
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6"><?php echo $form->field($model, 'projectId')->dropDownList($lists->getProjects(), ['prompt' => '-']) ?></div>
-                        <div class="col-md-6"><?php echo $form->field($model, 'source_id')->dropDownList($lists->getSources(true), ['prompt' => '-']) ?></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-1"><?php echo $form->field($model, 'projectId')->dropDownList($lists->getProjects(), ['prompt' => '-']) ?></div>
+                        <div class="col-md-1"><?php echo $form->field($model, 'source_id')->dropDownList($lists->getSources(true), ['prompt' => '-']) ?></div>
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-1">
                             <?php echo $form->field($model, 'employee_id')->widget(Select2::class, [
                                 'data' => $lists->getEmployees(true),
                                 'size' => Select2::SMALL,
@@ -104,27 +104,43 @@ use frontend\extensions\DatePicker;
                                 'pluginOptions' => ['allowClear' => true],
                             ]); ?>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'bo_flight_id')->input('number', ['min' => 0])->label('Sale ID') ?>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-1">
                             <?= $form->field(
                                 $model,
                                 'hybrid_uid'
                             )->textInput(['title' => 'Hybrid UID'])->label('Booking ID') ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4"><?php echo $form->field($model, 'quote_pnr')->label('PNR') ?></div>
-                        <div class="col-md-4">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-1"><?php echo $form->field($model, 'quote_pnr')->label('PNR') ?></div>
+                        <div class="col-md-1">
                             <?php echo $form->field($model, 'rating')->dropDownList(array_combine(range(1, 3), range(1, 3)), ['prompt' => '-']) ?>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'l_answered')->dropDownList([1 => 'Yes', 0 => 'No'], ['prompt' => '-']) ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
+                        <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-1">
+                            <?= $form->field($model, 'discount_id')->input('number', ['min' => 1]) ?>
+                        </div>
+                        <div class="col-md-1">
+                            <?php echo $form->field($model, 'request_ip') ?>
+                        </div>
+                        <!--</div>
+                        <div class="row">-->
+                        <div class="col-md-1">
+                            <?= $form->field($model, 'quoteTypeId')->dropDownList(Quote::TYPE_LIST, ['prompt' => '-']) ?>
+                        </div>
+                        <div class="col-md-1">
+                            <?= $form->field($model, 'l_is_test')->dropDownList([0 => 'False', 1 => 'True'], ['prompt' => '-'])->label('Is Test') ?>
+                        </div>
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-2">
                             <?= $form->field($model, 'createdRangeTime', [
                                 'options' => ['class' => 'form-group']
                             ])->widget(\kartik\daterange\DateRangePicker::class, [
@@ -143,9 +159,9 @@ use frontend\extensions\DatePicker;
                             ])->label('Created Date From / To');
 ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-2">
                             <?= $form->field($model, 'statusRangeTime', [
                                 'options' => ['class' => 'form-group']
                             ])->widget(\kartik\daterange\DateRangePicker::class, [
@@ -164,9 +180,9 @@ use frontend\extensions\DatePicker;
                             ])->label('Status Date From / To');
 ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-2">
                             <?= $form->field($model, 'updatedRangeTime', [
                                 'options' => ['class' => 'form-group']
                             ])->widget(\kartik\daterange\DateRangePicker::class, [
@@ -185,9 +201,9 @@ use frontend\extensions\DatePicker;
                             ])->label('Updated Date From / To');
 ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
+                   <!-- </div>
+                    <div class="row">-->
+                        <div class="col-md-2">
                             <?= $form->field($model, 'lastActionRangeTime', [
                                 'options' => ['class' => 'form-group']
                             ])->widget(\kartik\daterange\DateRangePicker::class, [
@@ -207,69 +223,53 @@ use frontend\extensions\DatePicker;
 ?>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <?= $form->field($model, 'discount_id')->input('number', ['min' => 1]) ?>
-                        </div>
-                        <div class="col-md-6">
-                            <?php echo $form->field($model, 'request_ip') ?>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <?= $form->field($model, 'quoteTypeId')->dropDownList(Quote::TYPE_LIST, ['prompt' => '-']) ?>
-                        </div>
-                        <div class="col-md-6">
-                            <?= $form->field($model, 'l_is_test')->dropDownList([0 => 'False', 1 => 'True'], ['prompt' => '-'])->label('Is Test') ?>
-                        </div>
-                    </div>
                 </div>
                 <div class=" profile-bottom text-center">
                 </div>
             </div>
         </div>
 
-        <div class="col-md-3 col-sm-3  profile_details">
+        <div class="col-md-12 col-sm-12  profile_details">
             <div class="well profile_view">
                 <div class="col-sm-12">
-                    <h4 class="brief"><i>Trip</i></h4><br>
+                    <h4 class="brief"><i>Trip</i></h4>
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-1">
                             <?php echo $form->field($model, 'trip_type')->dropDownList(Lead::TRIP_TYPE_LIST, ['prompt' => '-']) ?>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-1">
                             <?php echo $form->field($model, 'cabin')->dropDownList(Lead::CABIN_LIST, ['prompt' => '-']) ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-1">
                             <?php echo $form->field($model, 'adults')->dropDownList(array_combine(range(0, 9), range(0, 9)), ['prompt' => '-']) ?>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-1">
                             <?php echo $form->field($model, 'children')->dropDownList(array_combine(range(0, 9), range(0, 9)), ['prompt' => '-']) ?>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-1">
                             <?php echo $form->field($model, 'infants')->dropDownList(array_combine(range(0, 9), range(0, 9)), ['prompt' => '-']) ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-2">
                             <?php echo $form->field($model, 'origin_airport') ?>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-2">
                             <?php echo $form->field($model, 'destination_airport') ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-2">
                             <?php echo $form->field($model, 'origin_country') ?>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-2">
                             <?php echo $form->field($model, 'destination_country') ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-2">
                             <?= $form->field($model, 'departRangeTime', ['options' => ['class' => 'form-group']])->widget(\kartik\daterange\DateRangePicker::class, [
                                 'presetDropdown' => false,
                                 'hideInput' => true,
@@ -314,39 +314,39 @@ use frontend\extensions\DatePicker;
             </div>
         </div>
 
-        <div class="col-md-3 col-sm-3  profile_details">
+        <div class="col-md-12 col-sm-12  profile_details">
             <div class="well profile_view">
                 <div class="col-sm-12">
-                    <h4 class="brief"><i>Communication</i></h4><br>
+                    <h4 class="brief"><i>Communication</i></h4>
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'callsQtyFrom')->textInput() ?>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'callsQtyTo')->textInput() ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-1">
                             <?= $form->field($model, 'smsQtyFrom')->textInput() ?>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'smsQtyTo')->textInput() ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-1">
                             <?= $form->field($model, 'emailsQtyFrom')->textInput() ?>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'emailsQtyTo')->textInput() ?>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
+                    <!--</div>
+                    <div class="row">-->
+                        <div class="col-md-1">
                             <?= $form->field($model, 'chatsQtyFrom')->textInput() ?>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-1">
                             <?= $form->field($model, 'chatsQtyTo')->textInput() ?>
                         </div>
                     </div>
