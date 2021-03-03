257f11580d718dba87bc40c79917d4078065b13f
serge.murphy <serge.murphy@techork.com>
2021-03-03 12:22:59 +0200

    feat: removed product quote status validate (SL-5033)

--- modules/order/src/entities/order/OrderStatus.php
+++ modules/order/src/entities/order/OrderStatus.php
@@ -117,7 +117,6 @@ class OrderStatus
 
     public static function guard(?int $fromStatus, int $toStatus): void
     {
-        return;
         if (!isset(self::ROUTE_RULES[$fromStatus])) {
             throw new \DomainException('Disallow transfer from ' . self::getName($fromStatus));
         }
