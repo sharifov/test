b9bafd3f74d879357da5be3e8a19517ea114300c
serge.murphy <serge.murphy@techork.com>
2021-03-04 16:28:20 +0200

    feat: added attraction quote to pdf request (SL-5057)

---
+++ modules/attraction/src/services/RequestPdfDataGenerator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace modules\attraction\src\services;
+
+use modules\attraction\models\AttractionQuote;
+
+class RequestPdfDataGenerator
+{
+    public function generate(AttractionQuote $quote): array
+    {
+        $order = $quote->atnqProductQuote->pqOrder ?? null;
+        if (!$order) {
+            throw new \DomainException('Not found relation Order. AttractionQuoteId: ' . $quote->atnq_id);
+        }
+
+        $projectData = [];
+
+        if ($order->or_lead_id) {
+            if ($order->orLead->project_id) {
+                $project = $order->orLead->project;
+                $projectContactInfo = [];
+                if ($project->contact_info) {
+                    $projectContactInfo = @json_decode($project->contact_info, true);
+                }
+                $projectData = [
+                    'name' => $project->name,
+                    'url' => $project->link,
+                    'address' => $projectContactInfo['address'] ?? '',
+                    'phone' => $projectContactInfo['phone'] ?? '',
+                    'email' => $projectContactInfo['email'] ?? '',
+                ];
+            }
+        }
+
+        return [
+            'project' => $projectData,
+            'project_key' => $project->project_key ?? '',
+            'attraction' => $quote->atnqProductQuote->serialize(),
+            'order' => $order->serialize(),
+            'content' => '',
+            'subject' => '',
+        ];
+    }
+}
