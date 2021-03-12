00d95af6747150c135195c6d47072d904275776d
serge.murphy <serge.murphy@techork.com>
2021-03-12 11:35:03 +0200

    feat: changed show offer total price to app total (SL-5131)

--- frontend/views/lead/offers/_list_item.php
+++ frontend/views/lead/offers/_list_item.php
@@ -182,7 +182,7 @@ use yii\bootstrap4\Html;
                 <?php $clientCurrency = $offer->of_client_currency ?: 'USD' ?>
                 <?php $clientTotal = ', Client Total: <b>' . number_format($offer->of_client_total, 2) . ' ' . Html::encode($clientCurrency) . '</b>' ?>
             <?php endif ?>
-            <h4>Total: <b><?=number_format($offer->offerTotalCalcSum, 2)?> USD</b><?php echo $clientTotal ?></h4>
+            <h4>Total: <b><?=number_format($offer->of_app_total, 2)?> USD</b><?php echo $clientTotal ?></h4>
         </div>
 
 
