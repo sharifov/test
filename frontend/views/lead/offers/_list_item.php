5d22744ca23bd70eec065d3ad10d7e8efae8b4c4
serge.murphy <serge.murphy@techork.com>
2021-03-12 11:53:26 +0200

    feat: changed show offer total price to app total (SL-5131)

--- frontend/views/lead/offers/_list_item.php
+++ frontend/views/lead/offers/_list_item.php
@@ -164,7 +164,8 @@ use yii\bootstrap4\Html;
                 <tr>
                     <th class="text-right" colspan="5">Total: </th>
                     <td class="text-right" colspan="2">(price + opt)</td>
-                    <th class="text-right"><?=number_format($originTotalPrice + $optionTotalPrice, 2)?></th>
+                    <?php /* <th class="text-right"><?=number_format($originTotalPrice + $optionTotalPrice, 2)?></th> */ ?>
+                    <th class="text-right"><?=number_format($offer->of_app_total, 2)?></th>
                     <th class="text-right"><?=number_format($offer->of_client_total, 2)?> <?=Html::encode($offer->of_client_currency)?></th>
                     <th></th>
                 </tr>
