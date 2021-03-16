f6c15a35f9e2dfa18e71160bf8459b049ec1b417
andrew.snake <andrew.snake@techork.com>
2021-03-16 16:08:50 +0200

    Finish SL-5166_Rent_Car_change_config

--- modules/rentCar/config/config.php
+++ modules/rentCar/config/config.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'apiService' => [
             'class' => \modules\rentCar\components\ApiRentCarService::class,
-            'url' => 'https://api-sandbox.rezserver.com/api/car/', /* TODO:: for presentation */
-            'refid' => '8965',
-            'api_key' => '69348609cb746791fe82aab86634c3e6',
+            'url' => 'https://api-sandbox.rezserver.com/api/car/',
+            'refid' => '',
+            'api_key' => '',
         ],
     ],
 ];
