25c08d040078ee00c922466d7ddd12ab6d513f8f
VBarnes <vincent.barnes@techork.com>
2021-04-12 16:47:05 +0300

    feat(SL-5325): View - product list block  #dispaly flights quotes

--- modules/order/views/order/_partial/product.php
+++ modules/order/views/order/_partial/product.php
@@ -1,17 +1,34 @@
 <?php
 
+use yii\helpers\Html;
 use modules\order\src\entities\order\Order;
 
 /**
  * @var Order $order
  */
+
+$orderRelatedQuotes = $order->productQuotes;
+$orderRelatedProducts = [];
+$filterProducts = [];
+
+$flightProductQuotes = [];
+$hotelProductQuotes = [];
+
+
+foreach ($orderRelatedQuotes as $productQuote) {
+    if (!in_array($productQuote->pq_product_id, $filterProducts)) {
+        $orderRelatedProducts[] = $productQuote->pqProduct;
+        $filterProducts[] = $productQuote->pq_product_id;
+    }
+}
+
 ?>
 
 <div class="order-view-product-box">
     <div id="pjax-order-invoice-18" data-pjax-container="" data-pjax-timeout="10000">
         <div class="x_panel x_panel_product">
             <div class="x_title">
-                <h2><i class="fas fa-file-invoice-dollar"></i> Product List</h2>
+                <h2><i class="fas fa-file-contract"></i> Product List</h2>
                 <ul class="nav navbar-right panel_toolbox">
                     <li>
                         <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
@@ -20,7 +37,57 @@ use modules\order\src\entities\order\Order;
                 <div class="clearfix"></div>
             </div>
             <div class="x_content" style="display: block">
-                <?php \yii\helpers\VarDumper::dump($order->productQuotes); ?>
+                <?php foreach ($orderRelatedProducts as $product) : ?>
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>
+                            <a class="collapse-link">
+                                <i class="<?= Html::encode($product->getIconClass()) ?>" title="ID: <?=$product->pr_id?>"></i> <?=Html::encode($product->prType->pt_name)?> <?=$product->pr_name ? ' - ' . Html::encode($product->pr_name) : ''?>
+                            </a>
+                            <sup title="Number of quotes">(<?=count($product->productQuotes)?>)</sup>
+
+                            <?php if ($product->pr_description) :?>
+                                <a  id="product_description_<?=$product->pr_id ?>"
+                                    class="popover-class fa fa-info-circle text-info"
+                                    data-toggle="popover" data-html="true" data-trigger="hover" data-placement="top"
+                                    data-container="body" title="<?=Html::encode($product->pr_name)?>"
+                                    data-content='<?=Html::encode($product->pr_description)?>'
+                                ></a>
+                            <?php endif; ?>
+                        </h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li>
+                                <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content" style="display: block">
+                        <?php foreach ($orderRelatedQuotes as $productQuote) : ?>
+                            <?php if ($productQuote->isFlight() && $productQuote->pq_product_id == $product->pr_id) :?>
+                                <?php $flightProductQuotes[] = $productQuote; ?>
+                            <?php endif; ?>
+
+                            <?php if ($productQuote->isHotel() && $productQuote->pq_product_id == $product->pr_id) :?>
+                                <?php $hotelProductQuotes[] = $productQuote; ?>
+                            <?php endif; ?>
+
+                        <?php endforeach; ?>
+
+                        <?php if ($product->isFlight()) :?>
+                            <?= $this->render('product_flight_quotes', [
+                                    'data' => $flightProductQuotes,
+                                    'productId' => $product->pr_id
+                            ]) ?>
+                        <?php endif; ?>
+
+                        <?php if ($product->isHotel()) :?>
+                            <? //= $this->render('product_flight_quotes', ['data' => $hotelProductQuotes]) ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                    <?php $flightProductQuotes = []; ?>
+                <?php endforeach; ?>
             </div>
         </div>
 
---
+++ modules/order/views/order/_partial/product_flight_quotes.php
@@ -0,0 +1,56 @@
+<?php
+
+use yii\widgets\ListView;
+use yii\data\ArrayDataProvider;
+use yii\widgets\Pjax;
+
+/**
+ * @var $data array
+ * @var $productId int
+ */
+
+$dataProvider = new ArrayDataProvider([
+    'allModels' => $data,
+    'pagination' => [
+        'pageSize' => 1,
+    ],
+]);
+
+?>
+<?php Pjax::begin(['id' => 'pjax-product-quote-list-' . $productId, 'timeout' => 5000, 'enablePushState' => false, 'enableReplaceState' => false]); ?>
+<div class="x_panel">
+    <div class="x_title">
+        <h2><i class="fa fa-folder-o"></i> Flight Quotes
+            <?php if ($dataProvider->totalCount) : ?>
+                <sup>(<?=$dataProvider->totalCount?>)</sup>
+            <?php endif; ?>
+        </h2>
+        <ul class="nav navbar-right panel_toolbox">
+            <li>
+                <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+            </li>
+        </ul>
+        <div class="clearfix"></div>
+    </div>
+    <div class="x_content" style="display: block">
+
+        <?= ListView::widget([
+            'dataProvider' => $dataProvider,
+            'itemView' => 'product_flight_quote_item',
+            'emptyText' => '<div class="text-center">Not found quotes</div><br>',
+            //'layout' => "\n{items}<div class=\"text-center\">{pager}</div>\n", // {summary}\n<div class="text-center">{pager}</div>
+            'viewParams' => [
+//                'appliedQuote' => $lead->getAppliedAlternativeQuotes(),
+//                'leadId' => $lead->id,
+//                'leadForm' => $leadForm,
+//                'isManager' => $is_manager,
+            ],
+            'itemOptions' => [
+                //'class' => 'item',
+                'tag' => false,
+            ],
+        ]);?>
+
+    </div>
+</div>
+<?php Pjax::end() ?>
