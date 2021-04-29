e210b90d117ce5ab25661e3ba670c34dddec97e7
andrew.snake <andrew.snake@techork.com>
2021-04-29 13:45:42 +0300

    feat: fix refresh transactions block (SL-5480)

--- frontend/views/cases/orders/case_orders.php
+++ frontend/views/cases/orders/case_orders.php
@@ -637,9 +637,11 @@ $js = <<<JS
                         text: data.message,
                         hide: true
                     });
-                  if ($('#pjax-order-transaction-' + orderId).length) {
-                      pjaxReload({container: '#pjax-order-transaction-' + orderId, async: true});
-                  }
+                    $('#pjax-order-payment-' + orderId).on('pjax:end', function (data, xhr) {
+                        if ($('#pjax-order-transaction-' + orderId).length) {
+                            pjaxReload({container: '#pjax-order-transaction-' + orderId, async: true, replace: true});
+                        }
+                    });
               })
             .fail(function( jqXHR, textStatus ) {
                 alert( "Request failed: " + textStatus );
