db77473fa35326cb2a830f7aa3975705bc1e4c30
AlexConnor <alex.connor@techork.com>
2021-05-20 11:30:06 +0300

    feat: added assets + new cs-fixer config (SL-5474)

---
+++ frontend/assets/JqueryAsset.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * This asset bundle provides the [jQuery](http://jquery.com/) JavaScript library.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class JqueryAsset extends AssetBundle
+{
+    //public $sourcePath = '@bower/jquery/dist';
+    public $sourcePath = null;
+//    public $js = [
+//        'jquery.js',
+//    ];
+
+    public $js = [
+        //'https://code.jquery.com/jquery-3.4.1.min.js',
+        'https://code.jquery.com/jquery-3.6.0.min.js',
+    ];
+}
