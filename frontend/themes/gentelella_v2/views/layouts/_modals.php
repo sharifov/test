c4fef3fff8cf6e838e0203f02de95aadb0b19424
andrew.snake <andrew.snake@techork.com>
2021-05-25 11:17:56 +0300

    feat: fix select2 modal (SL-5600)

--- frontend/themes/gentelella_v2/views/layouts/_modals.php
+++ frontend/themes/gentelella_v2/views/layouts/_modals.php
@@ -10,21 +10,30 @@ use yii\bootstrap4\Modal;
     'id' => 'modal-lg',
     'title' => '',
     //'footer' => '<a href="#" class="btn btn-primary" data-dismiss="modal">Close</a>',
-    'size' => Modal::SIZE_LARGE
+    'size' => Modal::SIZE_LARGE,
+    'options' => [
+        'tabindex' => false // important for Select2 to work properly
+    ],
 ]); ?>
 
 <?php echo Modal::widget([
     'id' => 'modal-sm',
     'title' => '',
     //'footer' => '<a href="#" class="btn btn-primary" data-dismiss="modal">Close</a>',
-    'size' => Modal::SIZE_SMALL
+    'size' => Modal::SIZE_SMALL,
+    'options' => [
+        'tabindex' => false // important for Select2 to work properly
+    ],
 ]); ?>
 
 <?php echo Modal::widget([
     'id' => 'modal-df',
     'title' => '',
     //'footer' => '<a href="#" class="btn btn-primary" data-dismiss="modal">Close</a>',
-    'size' => Modal::SIZE_DEFAULT
+    'size' => Modal::SIZE_DEFAULT,
+    'options' => [
+        'tabindex' => false // important for Select2 to work properly
+    ],
 ]); ?>
 
 <?php echo Modal::widget([
