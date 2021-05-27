f76b1600d718389d2fc274f3ed24d2446ff30ce5
andrew.snake <andrew.snake@techork.com>
2021-05-27 14:44:13 +0300

    feat: remove CRUD btns from view (SL-5617)

--- modules/abac/views/abac-doc/view.php
+++ modules/abac/views/abac-doc/view.php
@@ -17,17 +17,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="col-md-4">
 
-        <p>
-            <?= Html::a('Update', ['update', 'id' => $model->ad_id], ['class' => 'btn btn-primary']) ?>
-            <?= Html::a('Delete', ['delete', 'id' => $model->ad_id], [
-                'class' => 'btn btn-danger',
-                'data' => [
-                    'confirm' => 'Are you sure you want to delete this item?',
-                    'method' => 'post',
-                ],
-            ]) ?>
-        </p>
-
         <?= DetailView::widget([
             'model' => $model,
             'attributes' => [
