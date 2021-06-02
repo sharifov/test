b26e3e260cdc40f121fad67dc75f950906c2911b
andrew.snake <andrew.snake@techork.com>
2021-06-02 12:31:32 +0300

    feat: added base (SL-5660)

---
+++ frontend/widgets/frontendWidgetList/louassist/LouAssistWidget.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace frontend\widgets\frontendWidgetList\louassist;
+
+use common\models\UserConnection;
+use sales\auth\Auth;
+use sales\helpers\setting\SettingHelper;
+use Yii;
+use yii\helpers\ArrayHelper;
+
+/**
+ * LouAssistWidget
+ *
+ * @property bool|null $enabled
+ * @property array|null $params
+ * @property array|null $routes
+ */
+class LouAssistWidget extends \yii\bootstrap\Widget
+{
+    public $enabled;
+    public $params;
+    public $routes;
+
+    public function init()
+    {
+        parent::init();
+
+        /* TODO:: add if properties empty */
+        if ($this->enabled === null) {
+            
+        }
+
+    }
+
+    public function run()
+    {
+        if (!$this->enabled) {
+            return '';
+        }
+        if (!$this->checkRoute()) {
+            return '';
+        }
+        if (!$identify = ArrayHelper::remove($this->params, 'identify')) {
+            throw new \RuntimeException('"identify" is required in LouAssistWidget params');
+        }
+
+        return $this->render('view', [
+            'params' => $this->params,
+            'identify' => $identify,
+            'userId' => Auth::id(),
+        ]);
+    }
+
+    private function checkRoute(): bool
+    {
+        if (ArrayHelper::isIn('*', $this->routes)) {
+            return true;
+        }
+        if (ArrayHelper::isIn(Yii::$app->controller->action->uniqueId, $this->routes)) {
+            return true;
+        }
+        if (ArrayHelper::isIn(Yii::$app->controller->uniqueId . '/*', $this->routes)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ frontend/widgets/frontendWidgetList/louassist/views/view.php
@@ -0,0 +1,24 @@
+<?php
+
+use frontend\helpers\JsonHelper;
+use yii\web\View;
+
+/** @var array $params */
+/** @var int $userId */
+/** @var View $this */
+/** @var string $identify */
+?>
+
+<?php
+
+$paramsEncoded = JsonHelper::encode($params);
+
+$this->registerJsFile('//run.louassist.com/v2.5.1-m?id=' . $userId, [
+    'position' => \yii\web\View::POS_HEAD,
+]);
+
+$js = <<<JS
+    LOU.identify('{$identify}', {$paramsEncoded})
+JS;
+$this->registerJs($js);
+?>
