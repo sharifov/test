bbd4e89ca3920c836b373e65aac7f9332a30d266
andrew.snake <andrew.snake@techork.com>
2021-06-03 11:45:49 +0300

    feat: added reset cache logic (SL-5651)

--- frontend/controllers/SalesController.php
+++ frontend/controllers/SalesController.php
@@ -12,18 +12,21 @@ use Yii;
  */
 class SalesController extends FController
 {
+    private const CACHE_DURATION = 60 * 30;
+
     public function actionIndex()
     {
         set_time_limit(30);
+        $cacheDuration = Yii::$app->request->get('debug') ? -1 : self::CACHE_DURATION;
 
         $searchModel = new SalesSearch(Auth::user());
-        $dataProvider = $searchModel->searchByUser(Yii::$app->request->queryParams);
+        $dataProvider = $searchModel->searchByUser(Yii::$app->request->queryParams, $cacheDuration);
         $query = clone $dataProvider->query;
 
         $totalCount = $dataProvider->totalCount;
         $sumGrossProfit = array_sum(array_column($query->all(), 'gross_profit'));
 
-        $qualifiedLeadsTakenQuery = $searchModel->qualifiedLeadsTakenQuery(Yii::$app->request->queryParams);
+        $qualifiedLeadsTakenQuery = $searchModel->qualifiedLeadsTakenQuery(Yii::$app->request->queryParams, $cacheDuration);
 
         return $this->render('index', [
             'searchModel' => $searchModel,
