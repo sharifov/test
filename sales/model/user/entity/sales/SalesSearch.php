e12de85a101fd51885164546854d70834250e39a
andrew.snake <andrew.snake@techork.com>
2021-06-03 14:53:00 +0300

    feat: added chat logic (SL-5651)

--- sales/model/user/entity/sales/SalesSearch.php
+++ sales/model/user/entity/sales/SalesSearch.php
@@ -15,6 +15,7 @@ use sales\model\callLog\entity\callLog\CallLogStatus;
 use sales\model\callLog\entity\callLog\CallLogType;
 use sales\model\callLog\entity\callLogLead\CallLogLead;
 use sales\model\callLog\entity\callLogRecord\CallLogRecord;
+use sales\model\clientChatLead\entity\ClientChatLead;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use yii\db\Expression;
@@ -253,11 +254,19 @@ class SalesSearch extends Model
                 ->cache($cacheDuration)
         ], Lead::tableName() . '.id = calls.cll_lead_id');
 
+        $query->leftJoin([
+            'chat' => ClientChatLead::find()
+                ->select(['ccl_lead_id'])
+                ->groupBy(['ccl_lead_id'])
+                ->cache($cacheDuration)
+        ], Lead::tableName() . '.id = chat.ccl_lead_id');
+
         $query->andWhere([
             'OR',
                 ['IS NOT', 'emails.e_lead_id', null],
                 ['IS NOT', 'sms.s_lead_id', null],
                 ['IS NOT', 'calls.cll_lead_id', null],
+                ['IS NOT', 'chat.ccl_lead_id', null],
         ]);
 
         $query->cache($cacheDuration);
