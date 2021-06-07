a4730a0ad8d482d849861f60548497f3ee3620f5
andrew.snake <andrew.snake@techork.com>
2021-06-07 13:42:39 +0300

    feat: remove test data (SL-5690)

--- console/migrations/m210607_104101_remove_test_data_example_system_key.php
+++ console/migrations/m210607_104101_remove_test_data_example_system_key.php
@@ -12,7 +12,9 @@ class m210607_104101_remove_test_data_example_system_key extends Migration
      */
     public function safeUp()
     {
-
+        $this->delete('{{%lead_data_key}}', ['IN', 'ldk_key', [
+            'example_system_key'
+        ]]);
     }
 
     /**
@@ -21,22 +23,5 @@ class m210607_104101_remove_test_data_example_system_key extends Migration
     public function safeDown()
     {
         echo "m210607_104101_remove_test_data_example_system_key cannot be reverted.\n";
-
-        return false;
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m210607_104101_remove_test_data_example_system_key cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
