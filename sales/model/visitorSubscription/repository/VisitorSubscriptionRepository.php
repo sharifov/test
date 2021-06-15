eb78a1f61c0a73338ee08417d7790af6a6fc28c2
Alex Grub <alex.grub@techork.com>
2021-06-15 11:49:50 +0300

    feat: created api for chat unsubscribe (SL-5713)

--- sales/model/visitorSubscription/repository/VisitorSubscriptionRepository.php
+++ sales/model/visitorSubscription/repository/VisitorSubscriptionRepository.php
@@ -3,6 +3,7 @@
 namespace sales\model\visitorSubscription\repository;
 
 use sales\model\visitorSubscription\entity\VisitorSubscription;
+use sales\repositories\NotFoundException;
 
 class VisitorSubscriptionRepository
 {
@@ -12,5 +13,13 @@ class VisitorSubscriptionRepository
             return $subscription->vs_id;
         }
         throw new \RuntimeException('Visitor subscription saving error: ' . $subscription->getErrorSummary(true)[0]);
+    }
+
+    public function findByUid(string $uid): VisitorSubscription
+    {
+        if ($subscription = VisitorSubscription::findOne(['vs_subscription_uid' => $uid])) {
+            return $subscription;
+        }
+        throw new NotFoundException('Subscription not found by uid: ' . $uid);
     }
 }
