f4dd59032a95ab2bdc5fbd8093ae9803d0b22c9a
alex-elliot <alex.elliot@techork.com>
2021-06-23 15:39:29 +0300

    feat: [API] Create App check list endpoint (SL-5749)

--- webapi/modules/v1/controllers/CheckHealthController.php
+++ webapi/modules/v1/controllers/CheckHealthController.php
@@ -3,7 +3,6 @@
 namespace webapi\modules\v1\controllers;
 
 use webapi\behaviors\HttpBasicAuthCheckHealth;
-use webapi\models\ApiCheckHealth;
 use yii\rest\Controller;
 use yii\helpers\VarDumper;
 
@@ -31,6 +30,47 @@ class CheckHealthController extends Controller
 
     public function actionQuick()
     {
-        return ApiCheckHealth::quickCheckHealth();
+        $response = [];
+        try {
+            $test_row = \Yii::$app->db->createCommand('SELECT id FROM employees LIMIT 1')->queryOne();
+            $response['mysql'] = boolval($test_row > 0);
+        } catch (\Throwable $e) {
+            $response['mysql'] = false;
+        }
+
+        try {
+            $test_row = \Yii::$app->db_postgres->createCommand('SELECT id from log LIMIT 1')->queryAll();
+            $response['postgresql'] = boolval($test_row > 0);
+        } catch (\Throwable $e) {
+            $response['postgresql'] = false;
+        }
+
+        try {
+            $redis = \Yii::$app->redis;
+            $key = 'health_check';
+            $redis->set($key, 'test_passed');
+            if ($redis->get($key) == 'test_passed') {
+                $response['redis'] = true;
+            } else {
+                $response['redis'] = false;
+            }
+        } catch (\Throwable $e) {
+            $response['redis'] = false;
+        }
+
+        try {
+            $communication = \Yii::$app->communication;
+            $comRequest = $communication->phoneNumberList(1, 1);
+            if (!$comRequest || !empty($comRequest['error'])) {
+                $response['communication'] = false;
+            } else {
+                $response['communication'] = true;
+            }
+        } catch (\Throwable $e) {
+            $response['communication'] = false;
+        }
+
+
+        return $response;
     }
 }
