c6e4b93a97c2e1132ac655a4b412ee0f405b26a2
andrew.snake <andrew.snake@techork.com>
2021-06-28 15:03:39 +0300

    feat: added migrate (SL-5832)

---
+++ console/migrations/m210628_105718_add_param_to_department_phone_project.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210628_105718_add_param_to_department_phone_project
+ */
+class m210628_105718_add_param_to_department_phone_project extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210628_105718_add_param_to_department_phone_project cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210628_105718_add_param_to_department_phone_project cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
