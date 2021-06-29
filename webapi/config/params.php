479faa3184503432e82575b1ef516bc50f5a6912
alex-elliot <alex.elliot@techork.com>
2021-06-29 14:31:19 +0300

    feat: [API] Create App check list endpoint (SL-5749)

--- webapi/config/params.php
+++ webapi/config/params.php
@@ -4,7 +4,7 @@ return [
     'client.passwordResetTokenExpire'  => 3600,
     'serviceName' => 'sales-api',
     'host' => 'communication.api.travelinsides.com',
-    'apiCheckHealth' => [
+    'apiHealthCheck' => [
         'user' => '',
         'password' => ''
     ]
