55e12a2dd33bd9f7e3b92692eb549272c6ab813d
andrew.snake <andrew.snake@techork.com>
2021-06-29 17:19:25 +0300

    feat: remove clean table (SL-5800)

--- console/controllers/OneTimeController.php
+++ console/controllers/OneTimeController.php
@@ -1318,7 +1318,7 @@ class OneTimeController extends Controller
         echo Console::renderColoredString('%g --- Start %w[' . date('Y-m-d H:i:s') . '] %g' .
             self::class . ':' . __FUNCTION__ . ' %n'), PHP_EOL;
 
-        Yii::$app->db->createCommand()->truncateTable(ContactPhoneData::tableName())->execute();
+        //Yii::$app->db->createCommand()->truncateTable(ContactPhoneData::tableName())->execute();
 
         $time_start = microtime(true);
 
