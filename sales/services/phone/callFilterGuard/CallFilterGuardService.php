11bad7e5d73735fd610c0f65516d58d36be11bd8
andrew.snake <andrew.snake@techork.com>
2021-06-29 16:21:05 +0300

    feat: remove debug (SL-5800)

--- sales/services/phone/callFilterGuard/CallFilterGuardService.php
+++ sales/services/phone/callFilterGuard/CallFilterGuardService.php
@@ -94,11 +94,6 @@ class CallFilterGuardService
     public function isTrusted(): bool
     {
         if ($this->checkByContactPhoneData()) {
-            \Yii::info(
-                'checkByContactPhoneData - isTrust',
-                'info\Debug:' . self::class . ':' . __FUNCTION__
-            );
-            /* TODO: FOR DEBUG:: must by remove */
             return true;
         }
         return ($this->getTrustPercent() >= $this->departmentPhoneProjectParamsService->getCallFilterGuardPercent());
