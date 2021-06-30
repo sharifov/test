6fb14cb79e56b2a1c3edf2c8938af7c1fd7adccd
VBarnes <vincent.barnes@techork.com>
2021-06-30 11:45:10 +0300

    fix:(SL-4894) In the notifications displayed client phone number

--- common/models/Call.php
+++ common/models/Call.php
@@ -1174,10 +1174,14 @@ class Call extends \yii\db\ActiveRecord
             if ($userListNotifications) {
                 //$from = PhoneFormatter::getPhoneOrNickname($this->c_from);
                 //$to = PhoneFormatter::getPhoneOrNickname($this->c_to);
+                $msgPart = '';
+                if ($this->c_source_type_id != self::SOURCE_DIRECT_CALL) {
+                    $msgPart = 'Queued ';
+                }
 
                 $holdMessage = $changedAttributes['c_status_id'] === self::STATUS_HOLD ? ' Hold' : '';
                 $title = 'Missed' . $holdMessage . ' Call (' . $this->getSourceName() . ')';
-                $message = 'Missed ' . $this->c_source_type_id != self::SOURCE_DIRECT_CALL ? 'Queued ' : '' . $holdMessage . 'Call (Id: ' . Purifier::createCallShortLink($this) . ')  from ';
+                $message = 'Missed ' . $msgPart . $holdMessage . 'Call (Id: ' . Purifier::createCallShortLink($this) . ')  from ';
                 if ($this->c_lead_id && $this->cLead) {
                     $message .= $this->cLead->client ? $this->cLead->client->getFullName() : '';
                     $message .= '<br> Lead (Id: ' . Purifier::createLeadShortLink($this->cLead) . ')';
