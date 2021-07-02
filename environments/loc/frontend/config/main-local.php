f6d12b3b9e805b9d5d8ca51838ad42a393f4c1c9
Alex Grub <alex.grub@techork.com>
2021-07-02 14:56:23 +0300

    feat: updated default environments configurations (SL-5842)

--- environments/loc/frontend/config/main-local.php
+++ environments/loc/frontend/config/main-local.php
@@ -6,39 +6,6 @@ $config = [
         'request' => [
             'cookieValidationKey' => env('frontend.config.main.components.request.cookieValidationKey'),
         ],
-        'log' => [
-            'targets' => [
-                [
-                    'class' => \kivork\mattermostLogTarget\Target::class,
-                    'logVars' => [],
-                    'levels' => ['error'],
-                    'prefix' => static function () {
-                        $userID = isset(Yii::$app->user) ? (Yii::$app->user->isGuest ? '-' : Yii::$app->user->id) : '';
-                        $ip = $_SERVER['REMOTE_ADDR'] ?? '';
-                        return "[frontend][$ip][$userID]";
-                    },
-                    'except' => [
-                        'yii\web\HttpException:404',
-                        'yii\web\HttpException:403'
-                    ],
-                    'filters' => [
-                        'yii\web\BadRequestHttpException: Unable to verify your data submission'
-                    ],
-                    'containerSettings' =>  [
-                        'driver' => [
-                            'scheme' => 'https',
-                            'basePath' => '/api/v4',
-                            'url' => env('common.config.params.mattermostLogTarget.containerSettings.driver.url'),
-                            'login_id' => env('common.config.params.mattermostLogTarget.containerSettings.driver.login_id'),
-                            'password' => env('common.config.params.mattermostLogTarget.containerSettings.driver.password'),
-                        ],
-                    ],
-                    'chanelId' => env('common.config.params.mattermostLogTarget.chanelId'),
-                    'handlerClassName' => \common\components\logger\MattermostJobHandler::class,
-                    'appPrefix' => 'DEV - CRM - Frontend',
-                ],
-            ]
-        ],
     ],
 ];
 
