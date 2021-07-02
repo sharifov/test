681e4d1b8a247948ad443cba94e6beb5b8ce7677
Alex Grub <alex.grub@techork.com>
2021-07-02 09:24:21 +0300

    feat: migrated app configuration in dotenv file (SL-5842)

--- environments/prod/modules/attraction/config/config-local.php
+++ environments/prod/modules/attraction/config/config-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'apiService' => [
             'class' => \modules\attraction\components\ApiAttractionService::class,
-            'url' => '{{ modules.attraction.config.components.apiService.url:str }}',
-            'apiKey' => '{{ modules.attraction.config.components.apiService.apiKey:str }}',
-            'secret' => '{{ modules.attraction.config.components.apiService.secret:str }}',
+            'url' => env('modules.attraction.config.config.components.apiService.url'),
+            'apiKey' => env('modules.attraction.config.config.components.apiService.apiKey'),
+            'secret' => env('modules.attraction.config.config.components.apiService.secret'),
         ],
     ],
 ];
