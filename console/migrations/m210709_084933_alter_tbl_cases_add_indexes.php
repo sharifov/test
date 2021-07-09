4b245db6adadce93bcadc672d7b77eb6d10bd686
Alex Grub <alex.grub@techork.com>
2021-07-09 12:59:38 +0300

    feat: modified migrations that add indexes to cases && case_sale tables (SL-5897)

--- console/migrations/m210709_084933_alter_tbl_cases_add_indexes.php
+++ console/migrations/m210709_084933_alter_tbl_cases_add_indexes.php
@@ -12,17 +12,17 @@ class m210709_084933_alter_tbl_cases_add_indexes extends Migration
      */
     public function safeUp()
     {
-        $this->createIndex('IND_cases_category_id', '{{%cases}}', 'cs_category_id');
-        $this->createIndex('IND_cases_status', '{{%cases}}', 'cs_status');
-        $this->createIndex('IND_cases_user_id', '{{%cases}}', 'cs_user_id');
-        $this->createIndex('IND_cases_dep_id', '{{%cases}}', 'cs_dep_id');
-        $this->createIndex('IND_cases_project_id', '{{%cases}}', 'cs_project_id');
-        $this->createIndex('IND_cases_client_id', '{{%cases}}', 'cs_client_id');
-        $this->createIndex('IND_cases_last_action_dt', '{{%cases}}', 'cs_last_action_dt');
-        $this->createIndex('IND_cases_order_uid', '{{%cases}}', 'cs_order_uid');
-        $this->createIndex('IND_cases_cs_created_dt', '{{%cases}}', 'cs_created_dt');
-        $this->createIndex('IND_cases_cs_updated_dt', '{{%cases}}', 'cs_updated_dt');
-        $this->createIndex('IND_cases_cs_need_action', '{{%cases}}', 'cs_need_action');
+        $this->createIndex('IND-cases-cs_category_id', '{{%cases}}', 'cs_category_id');
+        $this->createIndex('IND-cases-cs_status', '{{%cases}}', 'cs_status');
+        $this->createIndex('IND-cases-cs_user_id', '{{%cases}}', 'cs_user_id');
+        $this->createIndex('IND-cases-cs_dep_id', '{{%cases}}', 'cs_dep_id');
+        $this->createIndex('IND-cases-cs_project_id', '{{%cases}}', 'cs_project_id');
+        $this->createIndex('IND-cases-cs_client_id', '{{%cases}}', 'cs_client_id');
+        $this->createIndex('IND-cases-cs_last_action_dt', '{{%cases}}', 'cs_last_action_dt');
+        $this->createIndex('IND-cases-cs_order_uid', '{{%cases}}', 'cs_order_uid');
+        $this->createIndex('IND-cases-cs_created_dt', '{{%cases}}', 'cs_created_dt');
+        $this->createIndex('IND-cases-cs_updated_dt', '{{%cases}}', 'cs_updated_dt');
+        $this->createIndex('IND-cases-cs_need_action', '{{%cases}}', 'cs_need_action');
     }
 
     /**
@@ -30,16 +30,16 @@ class m210709_084933_alter_tbl_cases_add_indexes extends Migration
      */
     public function safeDown()
     {
-        $this->dropIndex('IND_cases_category_id', '{{%cases}}');
-        $this->dropIndex('IND_cases_status', '{{%cases}}');
-        $this->dropIndex('IND_cases_user_id', '{{%cases}}');
-        $this->dropIndex('IND_cases_dep_id', '{{%cases}}');
-        $this->dropIndex('IND_cases_project_id', '{{%cases}}');
-        $this->dropIndex('IND_cases_client_id', '{{%cases}}');
-        $this->dropIndex('IND_cases_last_action_dt', '{{%cases}}');
-        $this->dropIndex('IND_cases_order_uid', '{{%cases}}');
-        $this->dropIndex('IND_cases_cs_created_dt', '{{%cases}}');
-        $this->dropIndex('IND_cases_cs_updated_dt', '{{%cases}}');
-        $this->dropIndex('IND_cases_cs_need_action', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_category_id', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_status', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_user_id', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_dep_id', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_project_id', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_client_id', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_last_action_dt', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_order_uid', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_created_dt', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_updated_dt', '{{%cases}}');
+        $this->dropIndex('IND-cases-cs_need_action', '{{%cases}}');
     }
 }
