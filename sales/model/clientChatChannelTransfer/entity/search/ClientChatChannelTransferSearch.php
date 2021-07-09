736fc5b8b70d52a9591a538146090ab9b842879d
Alex Grub <alex.grub@techork.com>
2021-07-09 14:14:19 +0300

    feat: added project fields on chat channel transfer index page (SL-5901)

--- sales/model/clientChatChannelTransfer/entity/search/ClientChatChannelTransferSearch.php
+++ sales/model/clientChatChannelTransfer/entity/search/ClientChatChannelTransferSearch.php
@@ -3,17 +3,33 @@
 namespace sales\model\clientChatChannelTransfer\entity\search;
 
 use common\models\Employee;
+use common\models\Project;
+use sales\model\clientChatChannel\entity\ClientChatChannel;
 use yii\data\ActiveDataProvider;
 use sales\model\clientChatChannelTransfer\entity\ClientChatChannelTransfer;
+use yii\helpers\ArrayHelper;
 
+/**
+ * Class ClientChatChannelTransferSearch
+ * @package sales\model\clientChatChannelTransfer\entity\search
+ *
+ * @property int|null $fromChannelProjectId
+ * @property int|null $toChannelProjectId
+ */
 class ClientChatChannelTransferSearch extends ClientChatChannelTransfer
 {
+    public $fromChannelProjectId;
+
+    public $toChannelProjectId;
+
     public function rules(): array
     {
         return [
             ['cctr_created_dt', 'date', 'format' => 'php:Y-m-d'],
 
             ['cctr_created_user_id', 'integer'],
+
+            [['fromChannelProjectId', 'toChannelProjectId'], 'integer'],
 
             ['cctr_from_ccc_id', 'integer'],
 
@@ -40,6 +56,18 @@ class ClientChatChannelTransferSearch extends ClientChatChannelTransfer
             \sales\helpers\query\QueryHelper::dayEqualByUserTZ($query, 'cctr_created_dt', $this->cctr_created_dt, $user->timezone);
         }
 
+        if ($this->fromChannelProjectId) {
+            $query->innerJoin(ClientChatChannel::tableName(), 'cctr_from_ccc_id = ccc_id and ccc_project_id = :fromProjectId', [
+                'fromProjectId' => $this->fromChannelProjectId
+            ]);
+        }
+
+        if ($this->toChannelProjectId) {
+            $query->innerJoin(ClientChatChannel::tableName(), 'cctr_to_ccc_id = ccc_id and ccc_project_id = :toProjectId', [
+                'toProjectId' => $this->toChannelProjectId
+            ]);
+        }
+
         $query->andFilterWhere([
             'cctr_from_ccc_id' => $this->cctr_from_ccc_id,
             'cctr_to_ccc_id' => $this->cctr_to_ccc_id,
@@ -47,5 +75,13 @@ class ClientChatChannelTransferSearch extends ClientChatChannelTransfer
         ]);
 
         return $dataProvider;
+    }
+
+    public function attributeLabels(): array
+    {
+        return ArrayHelper::merge(parent::attributeLabels(), [
+           'fromChannelProjectId' => 'From Channel Project',
+           'toChannelProjectId' => 'To Channel Project',
+        ]);
     }
 }
