a5db1f06b946ab88eb0b64b65031b9e7efbcbf3c
VBarnes <vincent.barnes@techork.com>
2021-08-03 15:36:17 +0300

    feat: (SL-6069) Api bo_wh  #regenerate api doc after fix conflicts

--- webapi/web/doc/api_project.js
+++ webapi/web/doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-08-03T11:46:42.362Z",
+    "time": "2021-08-03T12:34:22.331Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
