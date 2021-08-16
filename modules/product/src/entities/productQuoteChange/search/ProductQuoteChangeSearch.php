215da5f84a0ebceb3eff3916f93d2d7afbd827e4
AlexConnor <alex.connor@techork.com>
2021-08-16 12:12:43 +0300

    feat: update product Quote Change CRUD

--- modules/product/src/entities/productQuoteChange/search/ProductQuoteChangeSearch.php
+++ modules/product/src/entities/productQuoteChange/search/ProductQuoteChangeSearch.php
@@ -46,6 +46,10 @@ class ProductQuoteChangeSearch extends ProductQuoteChange
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => ['defaultOrder' => ['pqc_id' => SORT_DESC]],
+            'pagination' => [
+                'pageSize' => 30,
+            ],
         ]);
 
         $this->load($params);
