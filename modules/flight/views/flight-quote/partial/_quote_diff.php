b191510da4854467a2c785279bc46975cf8f8a1d
Alex Grub <alex.grub@techork.com>
2021-08-18 11:39:27 +0300

    feat: display difference between original and reprotection quotes (SL-6178)

--- modules/flight/views/flight-quote/partial/_quote_diff.php
+++ modules/flight/views/flight-quote/partial/_quote_diff.php
@@ -9,11 +9,15 @@
 <div class="row">
   <div class="col-md-6">
       <h4>Origin Quote</h4>
-      <?= $originQuoteHtml ?>
+      <div style="margin-left: -20px">
+        <?= $originQuoteHtml ?>
+      </div>
   </div>
 
   <div class="col-md-6">
     <h4>Reprotection Quote</h4>
-    <?= $reprotectionQuoteHtml ?>
+    <div style="margin-left: -20px">
+      <?= $reprotectionQuoteHtml ?>
+    </div>
   </div>
 </div>
