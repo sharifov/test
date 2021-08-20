9e29a3e35a5595a4f1620da9f7f51ed54d3590a3
Alex Grub <alex.grub@techork.com>
2021-08-20 14:13:19 +0300

    feat: added new option in project params (SL-6152)

--- sales/model/project/entity/params/ObjectParams.php
+++ sales/model/project/entity/params/ObjectParams.php
@@ -6,20 +6,24 @@ namespace sales\model\project\entity\params;
  * Class ObjectParams
  *
  * @property CaseParams $case
+ * @property QuoteParams $quote
  */
 class ObjectParams
 {
     public CaseParams $case;
+    public QuoteParams $quote;
 
     public function __construct(array $params)
     {
         $this->case = new CaseParams($params['case'] ?? self::default()['case']);
+        $this->quote = new QuoteParams($params['quote'] ?? self::default()['quote']);
     }
 
     public static function default(): array
     {
         return [
             'case' => CaseParams::default(),
+            'quote' => QuoteParams::default()
         ];
     }
 }
