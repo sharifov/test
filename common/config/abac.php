af1d4642d01bb2cc666efe23f70e54e8bd0047e0
VBarnes <vincent.barnes@techork.com>
2021-08-20 14:16:29 +0300

    feat: (SL-6159) Actions - Add Assign and Multiple Assign actions [ABAC]

--- common/config/abac.php
+++ common/config/abac.php
@@ -9,7 +9,8 @@ return [
         'order' => \modules\order\src\abac\OrderAbacObject::class,
         'case' => \modules\cases\src\abac\CasesAbacObject::class,
         'lead' => \modules\lead\src\abac\LeadAbacObject::class,
-        'email' => \modules\email\src\abac\EmailAbacObject::class
+        'email' => \modules\email\src\abac\EmailAbacObject::class,
+        'qa-task' => \modules\qaTask\src\abac\QaTaskAbacObject::class
     ],
     'scanDirs' => [
         '/modules/',
