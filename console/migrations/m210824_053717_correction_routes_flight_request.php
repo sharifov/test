48e27e6661c64080ac1c3738ff9db18ef612c68d
andrew.snake <andrew.snake@techork.com>
2021-08-24 08:43:09 +0300

    fix: correction crud routes

---
+++ console/migrations/m210824_053717_correction_routes_flight_request.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210824_053717_correction_routes_flight_request
+ */
+class m210824_053717_correction_routes_flight_request extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210824_053717_correction_routes_flight_request cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210824_053717_correction_routes_flight_request cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
