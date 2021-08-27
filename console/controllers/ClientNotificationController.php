c34d17901361c10409bd55e9020742f6645a4b56
serge.murphy <serge.murphy@techork.com>
2021-08-27 18:22:53 +0300

    feat: added client notification clear script (SL-6204)

--- console/controllers/ClientNotificationController.php
+++ console/controllers/ClientNotificationController.php
@@ -3,11 +3,17 @@
 namespace console\controllers;
 
 use sales\helpers\app\AppHelper;
+use sales\model\client\notifications\client\entity\ClientNotification;
+use sales\model\client\notifications\client\entity\CommunicationType;
 use sales\model\client\notifications\phone\ClientNotificationPhoneExecutor;
 use sales\model\client\notifications\phone\entity\ClientNotificationPhoneList;
+use sales\model\client\notifications\phone\entity\Status as PhoneStatus;
+use sales\model\client\notifications\sms\entity\Status as SmsStatus;
 use sales\model\client\notifications\sms\ClientNotificationSmsExecutor;
 use sales\model\client\notifications\sms\entity\ClientNotificationSmsList;
+use sales\services\TransactionManager;
 use yii\console\Controller;
+use yii\console\ExitCode;
 use yii\console\widgets\Table;
 use yii\helpers\Console;
 
@@ -16,28 +22,64 @@ use yii\helpers\Console;
  *
  * @property ClientNotificationPhoneExecutor $clientNotificationPhoneExecutor
  * @property ClientNotificationSmsExecutor $clientNotificationSmsExecutor
+ * @property TransactionManager $transactionManager
  */
 class ClientNotificationController extends Controller
 {
     private ClientNotificationPhoneExecutor $clientNotificationPhoneExecutor;
     private ClientNotificationSmsExecutor $clientNotificationSmsExecutor;
+    private TransactionManager $transactionManager;
 
     public function __construct(
         $id,
         $module,
         ClientNotificationPhoneExecutor $clientNotificationPhoneExecutor,
         ClientNotificationSmsExecutor $clientNotificationSmsExecutor,
+        TransactionManager $transactionManager,
         $config = []
     ) {
         parent::__construct($id, $module, $config);
         $this->clientNotificationPhoneExecutor = $clientNotificationPhoneExecutor;
         $this->clientNotificationSmsExecutor = $clientNotificationSmsExecutor;
+        $this->transactionManager = $transactionManager;
+    }
+
+    public function actionClear()
+    {
+        $date = (new \DateTimeImmutable('-30 day'))->format('Y-m-d H:i:s');
+
+        $smsNotifications = ClientNotificationSmsList::find()
+            ->select(['cnsl_id'])
+            ->andWhere(['<>', 'cnsl_status_id', SmsStatus::NEW])
+            ->andWhere(['<', 'cnsl_created_dt', $date])
+            ->column();
+        foreach ($smsNotifications as $notificationId) {
+            $this->transactionManager->wrap(function () use ($notificationId) {
+                ClientNotificationSmsList::deleteAll(['cnsl_id' => $notificationId]);
+                ClientNotification::deleteAll(['cn_communication_object_id' => $notificationId, 'cn_communication_type_id' => CommunicationType::SMS]);
+            });
+        }
+
+        $phoneNotifications = ClientNotificationPhoneList::find()
+            ->select(['cnfl_id'])
+            ->andWhere(['<>', 'cnfl_status_id', PhoneStatus::NEW])
+            ->andWhere(['<', 'cnfl_created_dt', $date])
+            ->column();
+        foreach ($phoneNotifications as $notificationId) {
+            $this->transactionManager->wrap(function () use ($notificationId) {
+                ClientNotificationPhoneList::deleteAll(['cnfl_id' => $notificationId]);
+                ClientNotification::deleteAll(['cn_communication_object_id' => $notificationId, 'cn_communication_type_id' => CommunicationType::PHONE]);
+            });
+        }
+
+        return ExitCode::OK;
     }
 
     public function actionNotify()
     {
         $this->callNotify();
         $this->smsNotify();
+        return ExitCode::OK;
     }
 
     private function callNotify(): void
