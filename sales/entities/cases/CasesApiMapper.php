b7c7df172a60f07e78bab8cc4953aa909967e22b
alex-elliot <alex.elliot@techork.com>
2021-09-09 12:24:02 +0300

    feat: [API] Get Case by contact data (SL-6034)

--- sales/entities/cases/CasesApiMapper.php
+++ sales/entities/cases/CasesApiMapper.php
@@ -13,9 +13,15 @@ class CasesApiMapper extends Cases
             'id' => 'cs_id',
             'gid' => 'cs_gid',
             'created_dt' => 'cs_created_dt',
-            'updated_dt' => 'cs_updated_dt',
-            'last_action_dt' => 'cs_last_action_dt',
-            'order_uid' => 'cs_order_uid',
+            'updated_dt' => function ($model) {
+                return $model->cs_updated_dt ?? '';
+            },
+            'last_action_dt' => function ($model) {
+                return $model->cs_last_action_dt ?? '';
+            },
+            'order_uid' => function ($model) {
+                return $model->cs_order_uid ?? '';
+            },
             'project_name' => 'name',
             'status_name' => function ($model) {
                 return CasesStatus::getName($model->cs_status);
@@ -23,7 +29,9 @@ class CasesApiMapper extends Cases
             'category_key' => function ($model) {
                 return CaseCategory::getKey($model->cs_category_id) ?: '' ;
             },
-            'next_flight'
+            'next_flight' => function ($model) {
+                return $model->next_flight ?? '';
+            },
         ];
     }
 }
