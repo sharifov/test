19ad1b59a399cbe1cf21c65046081d7aab5cb6aa
Alex Grub <alex.grub@techork.com>
2021-09-21 15:06:37 +0300

    feat: update migration

--- console/migrations/m210921_092910_alter_tbl_profit_split_modify_foreign_key.php
+++ console/migrations/m210921_092910_alter_tbl_profit_split_modify_foreign_key.php
@@ -12,8 +12,9 @@ class m210921_092910_alter_tbl_profit_split_modify_foreign_key extends Migration
      */
     public function safeUp()
     {
+        $this->execute('DELETE FROM `profit_split` WHERE ps_lead_id NOT IN (SELECT id FROM leads)');
         $this->dropForeignKey('fk-ps-lead', '{{%profit_split}}');
-        $this->addForeignKey('fk-ps-lead', '{{%profit_split}}', 'ps_lead_id', 'leads', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk-ps-lead', '{{%profit_split}}', 'ps_lead_id', '{{%leads}}', 'id', 'CASCADE', 'CASCADE');
     }
 
     /**
@@ -22,6 +23,6 @@ class m210921_092910_alter_tbl_profit_split_modify_foreign_key extends Migration
     public function safeDown()
     {
         $this->dropForeignKey('fk-ps-lead', '{{%profit_split}}');
-        $this->addForeignKey('fk-ps-lead', '{{%profit_split}}', 'ps_lead_id', 'leads', 'id');
+        $this->addForeignKey('fk-ps-lead', '{{%profit_split}}', 'ps_lead_id', '{{%leads}}', 'id');
     }
 }
