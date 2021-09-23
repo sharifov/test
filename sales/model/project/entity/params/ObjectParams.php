1fa290fe7ae8b0e10131e22ba8e569e598bdd662
andrew.snake <andrew.snake@techork.com>
2021-09-23 11:15:55 +0300

    Finish SL-6298_Leads_Allow_to_create_lead_on_call_by_project

--- sales/model/project/entity/params/ObjectParams.php
+++ sales/model/project/entity/params/ObjectParams.php
@@ -7,23 +7,27 @@ namespace sales\model\project\entity\params;
  *
  * @property CaseParams $case
  * @property QuoteParams $quote
+ * @property LeadParams $lead
  */
 class ObjectParams
 {
     public CaseParams $case;
     public QuoteParams $quote;
+    public LeadParams $lead;
 
     public function __construct(array $params)
     {
         $this->case = new CaseParams($params['case'] ?? self::default()['case']);
         $this->quote = new QuoteParams($params['quote'] ?? self::default()['quote']);
+        $this->lead = new LeadParams($params['lead'] ?? self::default()['lead']);
     }
 
     public static function default(): array
     {
         return [
             'case' => CaseParams::default(),
-            'quote' => QuoteParams::default()
+            'quote' => QuoteParams::default(),
+            'lead' => LeadParams::default(),
         ];
     }
 }
