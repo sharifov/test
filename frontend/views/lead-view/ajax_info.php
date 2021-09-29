ba709ea610a21fd861e82f41403b4619d234cb0e
VBarnes <vincent.barnes@techork.com>
2021-09-29 13:52:49 +0300

    fix: (SL-6399) 404 error when trying to navigate to another page of cases/leads at the Client Details

--- frontend/views/lead-view/ajax_info.php
+++ frontend/views/lead-view/ajax_info.php
@@ -16,11 +16,11 @@ use yii\widgets\Pjax;
     <?= $this->render('_client_info', ['model' => $model, 'disableMasking' => $disableMasking]) ?>
 
     <?php if ($model->isClient()) : ?>
-            <?php Pjax::begin(['id' => 'client_leads_info', 'timeout' => 2000, 'enablePushState' => false]); ?>
+            <?php Pjax::begin(['id' => 'client_leads_info', 'timeout' => 10000, 'enablePushState' => false]); ?>
                 <?= $this->render('_leads_info', ['dataProvider' => $leadsDataProvider]) ?>
             <?php Pjax::end() ?>
 
-            <?php Pjax::begin(['id' => 'client_cases_info', 'timeout' => 2000, 'enablePushState' => false]); ?>
+            <?php Pjax::begin(['id' => 'client_cases_info', 'timeout' => 10000, 'enablePushState' => false]); ?>
                 <?= $this->render('_cases_info', ['dataProvider' => $casesDataProvider]) ?>
             <?php Pjax::end() ?>
     <?php endif;?>
