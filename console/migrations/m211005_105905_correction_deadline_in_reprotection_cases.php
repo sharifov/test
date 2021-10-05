ffbfa7f78d645324b958a1afe29c77a043eba713
andrew.snake <andrew.snake@techork.com>
2021-10-05 14:21:51 +0300

    feat: added correction migrate (SL-6435)

---
+++ console/migrations/m211005_105905_correction_deadline_in_reprotection_cases.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211005_105905_correction_deadline_in_reprotection_cases
+ */
+class m211005_105905_correction_deadline_in_reprotection_cases extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m211005_105905_correction_deadline_in_reprotection_cases cannot be reverted.\n";
+
+        return false;
+    }
+}
