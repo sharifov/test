466a36bda365dd61597efe42bbef15230a88e74b
serge.murphy <serge.murphy@techork.com>
2021-10-18 18:02:05 +0300

    feat: debug lead search

--- common/models/search/LeadSearch.php
+++ common/models/search/LeadSearch.php
@@ -378,6 +378,13 @@ class LeadSearch extends Lead
             $departmentIds = array_keys(EmployeeDepartmentAccess::getDepartments($user));
             $query->andWhere(['IN', Lead::tableName() . '.project_id', $projectIds]);
             $query->andWhere(['IN', Lead::tableName() . '.l_dep_id', $departmentIds]);
+            Yii::info([
+                'userId' => $user->id,
+                'username' => $user->username,
+                'projectAccess' => $projectIds,
+                'departmentAccess' => $departmentIds,
+
+            ], 'info\LeadSearchDebug');
         }
 
         $dataProvider = new ActiveDataProvider([
