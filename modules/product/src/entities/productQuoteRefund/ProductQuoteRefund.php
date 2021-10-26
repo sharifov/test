1bee47af9b43d301cf11611259bb9dd57f1f9e86
andrew.snake <andrew.snake@techork.com>
2021-10-26 13:36:26 +0300

    feat: fixed rules (SL-6552)

--- modules/product/src/entities/productQuoteRefund/ProductQuoteRefund.php
+++ modules/product/src/entities/productQuoteRefund/ProductQuoteRefund.php
@@ -227,6 +227,9 @@ class ProductQuoteRefund extends \yii\db\ActiveRecord
             ['pqr_data_json', 'safe'],
             ['pqr_data_json', 'trim'],
             ['pqr_data_json', CheckJsonValidator::class],
+
+            [['pqr_type_id'], 'integer'],
+            [['pqr_type_id'], 'in', 'range' => array_keys(self::TYPE_LIST)],
         ];
     }
 
@@ -254,6 +257,7 @@ class ProductQuoteRefund extends \yii\db\ActiveRecord
             'pqr_updated_dt' => 'Updated Dt',
             'pqr_case_id' => 'Case ID',
             'pqr_data_json' => 'Data',
+            'pqr_type_id' => 'Type',
         ];
     }
 
