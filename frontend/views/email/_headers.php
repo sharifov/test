1ba8f7de8b116e88dd8dd19241438a2bcda84450
alex-elliot <alex.elliot@techork.com>
2021-11-10 14:25:54 +0200

    [UX] Cases email details: printer-friendly PDF version (SL-6509)

--- frontend/views/email/_headers.php
+++ frontend/views/email/_headers.php
@@ -19,8 +19,8 @@ use sales\helpers\email\MaskEmailHelper;
                 : '<b>Email from:</b> ' . ($mail->eCreatedUser ? Html::encode($mail->eCreatedUser->username) : '-') . ', (' . Html::encode($mail->e_email_from_name) . ' &lt;' . Html::encode($mail->e_email_from) . '&gt;)<br>';
 echo $mail->isInbox()
 ? '<b>Email To:</b> ' . Html::encode($mail->e_email_to_name) . ' &lt;' . Html::encode($mail->e_email_to) . '&gt;<br>'
-: '<b>Email To:</b> ' . Html::encode($mail->e_email_to_name) . ' &lt;' . Html::encode(MaskEmailHelper::masking($mail->e_email_to)) . '&gt;'.'<br>';
+: '<b>Email To:</b> ' . Html::encode($mail->e_email_to_name) . ' &lt;' . Html::encode(MaskEmailHelper::masking($mail->e_email_to)) . '&gt;' . '<br>';
 echo '<b>Date:</b> ' . Yii::$app->formatter->asDatetime(strtotime($mail->e_created_dt));
- ?>
+?>
 <br>
 <br>
