89df9956b4e2d42f2d317997476b9bf439fc3b0c
Alex Grub <alex.grub@techork.com>
2021-11-10 17:24:24 +0200

    fix: updated error handler when bo refund endpoint is wrong (SL-6673)

--- webapi/modules/v1/controllers/FlightQuoteRefundController.php
+++ webapi/modules/v1/controllers/FlightQuoteRefundController.php
@@ -829,10 +829,6 @@ class FlightQuoteRefundController extends ApiBaseController
 
             $boDataRequest = BoRequestDataHelper::getDataForVoluntaryRefundConfirm($project->api_key, $voluntaryRefundConfirmForm, $productQuoteRefund);
             $result = BackOffice::voluntaryRefund($boDataRequest, $boRequestEndpoint);
-            \Yii::info([
-                'sentData' => $boDataRequest,
-                'response' => $result
-            ], 'info\BoConfirmRequest');
             if (mb_strtolower($result['status']) === 'failed') {
                 $productQuoteRefund->error();
                 $this->productQuoteRefundRepository->save($productQuoteRefund);
