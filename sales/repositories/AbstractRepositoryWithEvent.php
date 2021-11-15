2b8781a420c4705a9027a1285627dbeead2a9bbb
andrew.snake <andrew.snake@techork.com>
2021-11-15 17:26:25 +0200

    feat: added AbstractRepositoryWithEvent

---
+++ sales/repositories/AbstractRepositoryWithEvent.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace sales\repositories;
+
+use sales\dispatchers\EventDispatcher;
+use sales\helpers\ErrorsToStringHelper;
+use Yii;
+
+/**
+ * Class AbstractRepositoryWithEvent
+ *
+ * @property EventDispatcher|null $eventDispatcher
+ */
+abstract class AbstractRepositoryWithEvent extends AbstractBaseRepository
+{
+    private ?EventDispatcher $eventDispatcher = null;
+
+    public function setEventDispatcher(EventDispatcher $eventDispatcher): void
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    public function getEventDispatcher(): EventDispatcher
+    {
+        return $this->eventDispatcher ?? ($this->eventDispatcher = Yii::createObject(
+            EventDispatcher::class
+        ));
+    }
+
+    public function save(bool $runValidation = false): AbstractBaseRepository
+    {
+        if (!$this->model->save($runValidation)) {
+            throw new \RuntimeException(ErrorsToStringHelper::extractFromModel($this->model));
+        }
+        if (method_exists($this->model, 'releaseEvents')) {
+            $this->getEventDispatcher()->dispatchAll($this->model->releaseEvents());
+        }
+        return $this;
+    }
+}
