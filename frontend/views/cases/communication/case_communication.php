f28c6fd121a49539233c63335da1dd498e18e92f
alex-elliot <alex.elliot@techork.com>
2021-11-17 14:49:40 +0200

    [UX] Cases email details: printer-friendly PDF version (SL-6509)

--- frontend/views/cases/communication/case_communication.php
+++ frontend/views/cases/communication/case_communication.php
@@ -806,15 +806,15 @@ $js = <<<JS
     
     $('body').on('click', '#print_button', function () {
         let w = window.open();
-        let js_timer = document.createElement("script");
-        js_timer.innerHTML = 'setTimeout( "window.print(); window.close();", 3000);'; 
-        w.document.body.append(js_timer);
         $(w.document.body).html($('#object-email-view').contents()[0].body.innerHTML);
-        // window.document.addEventListener('DOMContentLoaded', function () { window.print(); window.close(); }, false);
         w.document.head.append('<style>@media print { body background-color:#FFFFFF; background-image:none; color:#000000 }  }</style>');
         let mail_headers = document.createElement("div");
         mail_headers.innerHTML = $('#email_info').html();
         w.document.body.prepend(mail_headers);
+        let js_timer = document.createElement("script");
+        js_timer.innerHTML = 'setTimeout( "window.print(); window.close();", 3000);'; 
+        w.document.head.append(js_timer);
+        // window.document.addEventListener('DOMContentLoaded', function () { window.print(); window.close(); }, false);
     });
 
     $('body').on('change', '.quotes-uid', function() {
