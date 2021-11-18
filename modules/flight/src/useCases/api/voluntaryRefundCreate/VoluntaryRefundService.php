4dc0f065029570c279d48993b5e5cb20ae054353
Alex Grub <alex.grub@techork.com>
2021-11-18 12:23:59 +0200

    feat: uncommented block (SL-6734)

--- modules/flight/src/useCases/api/voluntaryRefundCreate/VoluntaryRefundService.php
+++ modules/flight/src/useCases/api/voluntaryRefundCreate/VoluntaryRefundService.php
@@ -231,13 +231,12 @@ class VoluntaryRefundService
             throw new \RuntimeException('BO endpoint is not set', VoluntaryRefundCodeException::BO_REQUEST_IS_NO_SEND);
         }
 
-//        $boDataRequest = BoRequestDataHelper::getDataForVoluntaryCreateByForm($project->api_key, $voluntaryRefundCreateForm);
-//        $result = BackOffice::voluntaryRefund($boDataRequest, $boRequestEndpoint);
-//        if (mb_strtolower($result['status']) === 'failed') {
-//            $this->errorHandler($case, null, 'BO returns an error', null);
-//            throw new BoResponseException($result['message'] ?? '', VoluntaryRefundCodeException::BO_REQUEST_FAILED);
-//        }
-        $result = [];
+        $boDataRequest = BoRequestDataHelper::getDataForVoluntaryCreateByForm($project->api_key, $voluntaryRefundCreateForm);
+        $result = BackOffice::voluntaryRefund($boDataRequest, $boRequestEndpoint);
+        if (mb_strtolower($result['status']) === 'failed') {
+            $this->errorHandler($case, null, 'BO returns an error', null);
+            throw new BoResponseException($result['message'] ?? '', VoluntaryRefundCodeException::BO_REQUEST_FAILED);
+        }
 
         try {
             $orderRefund = OrderRefund::createByVoluntaryRefund(
