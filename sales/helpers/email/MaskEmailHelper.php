f38ecb3f4f82a51b38546f9960d14dacdb444ad9
alex-elliot <alex.elliot@techork.com>
2021-12-09 17:49:27 +0200

    [Logs] Mask (obfuscate) emails in "email_list" field (SL-6863)

--- sales/helpers/email/MaskEmailHelper.php
+++ sales/helpers/email/MaskEmailHelper.php
@@ -8,7 +8,7 @@ class MaskEmailHelper
 {
     public static function masking(?string $email, $forceShow = false): ?string
     {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL) && SettingHelper::clientDataPrivacyEnable() && !$forceShow) {
+        if (SettingHelper::clientDataPrivacyEnable() && !$forceShow) {
             return self::maskingPartial($email);
         }
 
@@ -17,12 +17,16 @@ class MaskEmailHelper
 
     public static function maskingPartial(?string $email): ?string
     {
-        list($user, $domains) = explode('@', $email);
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            list($user, $domains) = explode('@', $email);
 
-        $domains = explode('.', $domains);
-        $user = strlen($user) >= 3 ? substr($user, 0, 3) : $user;
-        $domainName = substr($domains['0'], -2);
+            $domains = explode('.', $domains);
+            $user = strlen($user) >= 3 ? substr($user, 0, 3) : $user;
+            $domainName = substr($domains['0'], -2);
 
-        return $user . str_repeat('*', 5) . '@' . str_repeat('*', 2) . $domainName . '.' . $domains['1'];
+            return $user . str_repeat('*', 5) . '@' . str_repeat('*', 2) . $domainName . '.' . $domains['1'];
+        } else {
+            return $email;
+        }
     }
 }
