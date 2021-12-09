e2c8ab6d1b227c5836ba5039bab02530fa048a00
andrew.snake <andrew.snake@techork.com>
2021-12-09 08:51:44 +0200

    fix: added lead data (SL-6869)

---
+++ modules/webEngage/src/service/webEngageEventData/call/eventData/FirstCallNotPickedEventData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace modules\webEngage\src\service\webEngageEventData\call\eventData;
+
+use modules\webEngage\src\service\webEngageEventData\lead\eventData\AbstractLeadEventData;
+
+/**
+ * Class FirstCallNotPickedEventData
+ *
+ */
+class FirstCallNotPickedEventData extends AbstractLeadEventData
+{
+}
