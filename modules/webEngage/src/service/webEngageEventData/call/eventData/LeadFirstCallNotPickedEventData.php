50898d13289853693554e7b5ae9f67622c062e05
andrew.snake <andrew.snake@techork.com>
2021-12-21 10:23:07 +0200

    feat: added FirstCallNotPickedLead logic (SL-6961)

---
+++ modules/webEngage/src/service/webEngageEventData/call/eventData/LeadFirstCallNotPickedEventData.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace modules\webEngage\src\service\webEngageEventData\call\eventData;
+
+use modules\webEngage\src\service\webEngageEventData\lead\eventData\AbstractLeadEventData;
+
+/**
+ * Class LeadFirstCallNotPickedEventData
+ */
+class LeadFirstCallNotPickedEventData extends AbstractLeadEventData
+{
+}
