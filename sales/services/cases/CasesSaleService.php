805102e266224b9e364f1a2a2d0501fd4bc47031
alex-elliot <alex.elliot@techork.com>
2021-12-24 12:04:57 +0200

    [Log Tracking] check log category - CasesSaleService:createOrderStructureFromSale:Throwable (SL-6937)

--- sales/services/cases/CasesSaleService.php
+++ sales/services/cases/CasesSaleService.php
@@ -660,7 +660,7 @@ class CasesSaleService
                             $transaction->rollBack();
                             $message = AppHelper::throwableLog($throwable, true);
                             $message['saleData'] = $refreshSaleData;
-                            Yii::error($message, 'CasesSaleService:createSale:Throwable');
+                            Yii::error($message, 'CasesSaleService:createSale:OrderManageService:Throwable');
                         }
                     }
                 } else {
