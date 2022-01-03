c5671999d79724fdbe3d821f9b494bccce17d90a
serge.murphy <serge.murphy@techork.com>
2022-01-03 09:13:44 +0200

    feat: added index to phone device tbl +migration

---
+++ console/migrations/m220103_070849_add_index_to_phone_device_tbl.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220103_070849_add_index_to_phone_device_tbl
+ */
+class m220103_070849_add_index_to_phone_device_tbl extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createIndex('IDX-phone_device_group', '{{%phone_device}}', ['pd_id', 'pd_user_id', 'pd_connection_id', 'pd_user_agent']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropIndex('IDX-phone_device_group', '{{%phone_device}}');
+    }
+}
