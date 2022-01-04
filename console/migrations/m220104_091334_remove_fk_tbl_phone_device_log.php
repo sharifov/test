8ca9d8edadbfcf34c6ac07e9c78c4ee160d5836d
serge.murphy <serge.murphy@techork.com>
2022-01-04 13:27:05 +0200

    fix: fixed revert device log migration error (SL-7051)

--- console/migrations/m220104_091334_remove_fk_tbl_phone_device_log.php
+++ console/migrations/m220104_091334_remove_fk_tbl_phone_device_log.php
@@ -12,9 +12,12 @@ class m220104_091334_remove_fk_tbl_phone_device_log extends Migration
      */
     public function safeUp()
     {
-        $this->dropForeignKey('FK-phone_device_log_device', '{{%phone_device_log}}');
-        if (Yii::$app->cache) {
-            Yii::$app->cache->flush();
+        try {
+            $this->dropForeignKey('FK-phone_device_log_device', '{{%phone_device_log}}');
+            if (Yii::$app->cache) {
+                Yii::$app->cache->flush();
+            }
+        } catch (Throwable $e) {
         }
     }
 
