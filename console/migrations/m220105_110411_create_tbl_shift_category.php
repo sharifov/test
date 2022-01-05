6cd4cfa794efe692e92a03b25355ffe039ba1b54
Alex Grub <alex.grub@techork.com>
2022-01-05 15:53:41 +0200

    feat: created table shift_category + crud + migration (SL-7071)

---
+++ console/migrations/m220105_110411_create_tbl_shift_category.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220105_110411_create_tbl_shift_category
+ */
+class m220105_110411_create_tbl_shift_category extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%shift_category}}', [
+            'sc_id' => $this->primaryKey(),
+            'sc_name' => $this->string(50)->notNull(),
+            'sc_created_user_id' => $this->integer(),
+            'sc_updated_user_id' => $this->integer(),
+            'sc_created_dt' => $this->dateTime(),
+            'sc_updated_dt' => $this->dateTime()
+        ], $tableOptions);
+
+        $this->addForeignKey('FK-shift_category-sc_created_user_id', '{{%shift_category}}', 'sc_created_user_id', '{{%employees}}', 'id', 'SET NULL', 'SET NULL');
+        $this->addForeignKey('FK-shift_category-sc_updated_user_id', '{{%shift_category}}', 'sc_updated_user_id', '{{%employees}}', 'id', 'SET NULL', 'SET NULL');
+
+        if (Yii::$app->cache) {
+            Yii::$app->cache->flush();
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%shift_category}}');
+
+        if (Yii::$app->cache) {
+            Yii::$app->cache->flush();
+        }
+    }
+}
---
+++ frontend/views/shift-category-crud/index.php
@@ -0,0 +1,63 @@
+<?php
+
+use common\components\grid\DateTimeColumn;
+use common\components\grid\UserSelect2Column;
+use sales\model\shiftSchedule\entity\shiftCategory\ShiftCategory;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+
+/* @var $this yii\web\View */
+/* @var $searchModel sales\model\shiftSchedule\entity\shiftCategory\search\ShiftCategorySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Shift Categories';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="shift-category-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Shift Category', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            'sc_id',
+            'sc_name',
+            [
+                'class' => UserSelect2Column::class,
+                'attribute' => 'sc_created_user_id',
+                'relation' => 'createdUser',
+                'format' => 'username',
+                'placeholder' => 'Select User'
+            ],
+            [
+                'class' => UserSelect2Column::class,
+                'attribute' => 'sc_updated_user_id',
+                'relation' => 'updatedUser',
+                'format' => 'username',
+                'placeholder' => 'Select User'
+            ],
+            ['class' => DateTimeColumn::class, 'attribute' => 'sc_created_dt'],
+            ['class' => DateTimeColumn::class, 'attribute' => 'sc_updated_dt'],
+            [
+                'class' => ActionColumn::class,
+                'urlCreator' => function ($action, ShiftCategory $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'sc_id' => $model->sc_id]);
+                }
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
