c7dd09d645664b58fe6efc9b6bbf2ead9cb61921
andrew.snake <andrew.snake@techork.com>
2022-01-12 16:24:34 +0200

    feat: added truncate pathInfo (SL-7095)

--- frontend/components/UserSiteActivityLog.php
+++ frontend/components/UserSiteActivityLog.php
@@ -3,8 +3,12 @@
 namespace frontend\components;
 
 use frontend\models\UserSiteActivity;
+use src\helpers\app\AppHelper;
+use src\helpers\ErrorsToStringHelper;
 use Yii;
 use yii\base\Behavior;
+use yii\helpers\ArrayHelper;
+use yii\helpers\StringHelper;
 use yii\helpers\VarDumper;
 use yii\web\Application;
 use yii\web\ForbiddenHttpException;
@@ -48,22 +52,29 @@ class UserSiteActivityLog extends Behavior
                     ->count();
 
                 if ($requestCount > $settings['user_site_activity_count']) {
-                    // Yii::warning(VarDumper::dumpAsString(['usa_user_id' => Yii::$app->user->id, 'usa_request_url' => $request_url]), 'UserSiteActivityLog:block');
-//                    throw new NotAcceptableHttpException('Many requests for this url. With frequent requests, the system may block you. Please wait any time ...', 111);
                     throw new ForbiddenHttpException('You\'ve made too many requests recently. Please wait and try your request again later.', 111);
                 }
             }
 
-            $activity = new UserSiteActivity();
-            $activity->usa_user_id      = Yii::$app->user->id;
-            $activity->usa_ip           = Yii::$app->request->getUserIP();
-            $activity->usa_request_url  = $request_url;
-            $activity->usa_page_url     = Yii::$app->request->pathInfo;
-            $activity->usa_request_type = Yii::$app->request->isPost ? UserSiteActivity::REQUEST_TYPE_POST : UserSiteActivity::REQUEST_TYPE_GET;
-            $activity->usa_request_get  = Yii::$app->request->get() ? json_encode(Yii::$app->request->get()) : null;
-            // $activity->usa_request_post = Yii::$app->request->post() ? json_encode(Yii::$app->request->post()) : null;
-            if (!$activity->save()) {
-                Yii::error(VarDumper::dumpAsString($activity->errors), 'UserSiteActivityLog:UserSiteActivity:save');
+            try {
+                $activity = new UserSiteActivity();
+                $activity->usa_user_id      = Yii::$app->user->id;
+                $activity->usa_ip           = Yii::$app->request->getUserIP();
+                $activity->usa_request_url  = $request_url;
+                $activity->usa_page_url     = StringHelper::truncate(Yii::$app->request->pathInfo, 250);
+                $activity->usa_request_type = Yii::$app->request->isPost ? UserSiteActivity::REQUEST_TYPE_POST : UserSiteActivity::REQUEST_TYPE_GET;
+                $activity->usa_request_get  = Yii::$app->request->get() ? json_encode(Yii::$app->request->get()) : null;
+
+                if (!$activity->save()) {
+                    throw new \RuntimeException(ErrorsToStringHelper::extractFromModel($activity));
+                }
+                if (strlen(Yii::$app->request->pathInfo) > 255) {
+                    throw new \RuntimeException('Len pathInfo over limit (255). ' . Yii::$app->request->pathInfo);
+                }
+            } catch (\RuntimeException | \DomainException $throwable) {
+                \Yii::warning(AppHelper::throwableLog($throwable), 'UserSiteActivityLog:UserSiteActivity:Exception');
+            } catch (\Throwable $throwable) {
+                \Yii::error(AppHelper::throwableLog($throwable), 'UserSiteActivityLog:UserSiteActivity:Throwable');
             }
         }
     }
