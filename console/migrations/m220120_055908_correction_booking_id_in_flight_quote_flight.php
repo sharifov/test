74191f7d031dda4b67c2ba85731d7fd2de594892
andrew.snake <andrew.snake@techork.com>
2022-01-20 08:06:19 +0200

    fix: added correction migrate - bookingId in FQF

--- console/migrations/m220120_055908_correction_booking_id_in_flight_quote_flight.php
+++ console/migrations/m220120_055908_correction_booking_id_in_flight_quote_flight.php
@@ -1,6 +1,8 @@
 <?php
 
+use modules\flight\models\FlightQuoteFlight;
 use yii\db\Migration;
+use yii\helpers\Console;
 
 /**
  * Class m220120_055908_correction_booking_id_in_flight_quote_flight
@@ -12,7 +14,25 @@ class m220120_055908_correction_booking_id_in_flight_quote_flight extends Migrat
      */
     public function safeUp()
     {
+        $query = FlightQuoteFlight::find()
+            ->where(['fqf_booking_id' => null])
+            ->andWhere(['IS NOT', 'fqf_child_booking_id', null])
+            ->andWhere(['>=', 'DATE(fqf_created_dt)', '2022-01-19'])
+            ->all();
 
+        echo PHP_EOL;
+        $total = count($query);
+        $processed = 0;
+        Console::startProgress($processed, $total);
+
+        foreach ($query as $flightQuoteFlight) {
+            $flightQuoteFlight->fqf_booking_id = $flightQuoteFlight->fqf_child_booking_id;
+            $flightQuoteFlight->save();
+            $processed++;
+            Console::updateProgress($processed, $total);
+        }
+        Console::endProgress(false);
+        echo PHP_EOL;
     }
 
     /**
@@ -21,22 +41,5 @@ class m220120_055908_correction_booking_id_in_flight_quote_flight extends Migrat
     public function safeDown()
     {
         echo "m220120_055908_correction_booking_id_in_flight_quote_flight cannot be reverted.\n";
-
-        return false;
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m220120_055908_correction_booking_id_in_flight_quote_flight cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
