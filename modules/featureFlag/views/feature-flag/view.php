8369f19929b238e10c964e29ad95783d6badb86e
Alex Connor <alex.connor@techork.com>
2022-01-26 09:52:14 +0200

    feat: added feture-flag model + migration (P2BPT-72)

--- modules/featureFlag/views/feature-flag/view.php
+++ modules/featureFlag/views/feature-flag/view.php
@@ -66,7 +66,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     },
                     'format' => 'raw'
                 ],
-                    
+
 
 
                 //'ff_type',
