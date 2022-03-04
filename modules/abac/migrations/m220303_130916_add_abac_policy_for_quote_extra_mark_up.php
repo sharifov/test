7b15784ac989f205ed5db067f9c8e816b490ba64
skincaid <san.kincaid@techork.com>
2022-03-04 15:16:07 +0600

    P2BPT-321 delete view  in abac migration

--- modules/abac/migrations/m220303_130916_add_abac_policy_for_quote_extra_mark_up.php
+++ modules/abac/migrations/m220303_130916_add_abac_policy_for_quote_extra_mark_up.php
@@ -21,7 +21,7 @@ class m220303_130916_add_abac_policy_for_quote_extra_mark_up extends Migration
             'ap_subject_json' => '{"condition":"AND","rules":[{"id":"lead-view/quote_status__id","field":"quote_status_id","type":"integer","input":"select","operator":"==","value":[1]},{"id":"lead-view/is_owner","field":"is_owner","type":"boolean","input":"select","operator":"==","value":true},{"id":"lead-view/lead_status_id","field":"lead_status_id","type":"integer","input":"select","operator":"==","value":[2]}],"valid":true}',
             'ap_object' => 'lead-view/action-ajax-edit-lead-quote-extra-mark-up-modal-content',
             'ap_action' => '(edit)',
-            'ap_action_json' => "[\"view\",\"edit\"]",
+            'ap_action_json' => "[\"edit\"]",
             'ap_effect' => 1,
             'ap_title' => '',
             'ap_sort_order' => 50,
