083de9a422244845539f6527b5acd3ce06cce030
andrew.snake <andrew.snake@techork.com>
2022-03-07 15:53:52 +0200

    feat: fix chat send quote list (P2BPT-343)

--- frontend/views/client-chat/partial/_quote_prices.php
+++ frontend/views/client-chat/partial/_quote_prices.php
@@ -1,19 +1,50 @@
 <?php
+
 /**
  * @var $this \yii\web\View
  * @var $quote \common\models\Quote
  */
+
+use common\models\Currency;
+use common\models\query\CurrencyQuery;
+use src\helpers\app\AppHelper;
+use src\services\quote\quotePriceService\ClientQuotePriceService;
+use yii\helpers\ArrayHelper;
+
+$currency = empty($quote->q_client_currency) ? Currency::getDefaultCurrencyCode() : $quote->q_client_currency;
+try {
+    if (!$currencySymbol = CurrencyQuery::getCurrencySymbolByCode($currency)) {
+        throw new \RuntimeException('Currency Symbol not found');
+    }
+} catch (\Throwable $throwable) {
+    $message = ArrayHelper::merge(AppHelper::throwableLog($throwable), ['quoteId' => $quote->id, 'currency' => $currency]);
+    \Yii::error($message, 'Views_ClientChat_partial_quote_prices:currencySymbol:Throwable');
+    $currencySymbol = $currency;
+}
+
+if ($quote->isClientCurrencyDefault()) {
+    $priceData = $quote->getPricesData();
+} else {
+    try {
+        $priceData = (new ClientQuotePriceService($quote))->getClientPricesData();
+    } catch (\Throwable $throwable) {
+        $message = ArrayHelper::merge(AppHelper::throwableLog($throwable), ['quoteId' => $quote->id]);
+        \Yii::error($message, 'Views_ClientChat_partial_quote_prices:PriceData:Throwable');
+        $priceData = $quote->getPricesData();
+        $currency = Currency::getDefaultCurrencyCode();
+    }
+}
 ?>
-<?php $priceData = $quote->getPricesData();?>
+
 <table class="table table-striped table-prices" id="quote-prices-<?= $quote->id?>">
     <thead>
         <tr>
             <th>Pax</th>
             <th>Q</th>
-            <th>NP, $</th>
-            <th>Mkp, $</th>
-            <th>Ex Mkp, $</th>
-            <th>SP, $</th>
+            <th>NP, <?php echo $currencySymbol ?></th>
+            <th>Mkp, <?php echo $currencySymbol ?></th>
+            <th>Ex Mkp, <?php echo $currencySymbol ?></th>
+            <th>SP, <?php echo $currencySymbol ?></th>
         </tr>
     </thead>
     <tbody>
