45ba6c12f834423cf5bc9af43bb8bfc7cab04103
San Kincaid <san.kincaid@techork.com>
2022-03-09 12:57:00 +0000

    feat: Automate sync currency rates virtual cron (P2BPT-364)

---
+++ console/migrations/m220309_122504_add_automate_sync_currency_virtual_cron.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220309_122504_add_automate_sync_currency_virtual_cron
+ */
+class m220309_122504_add_automate_sync_currency_virtual_cron extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->delete('{{%cron_scheduler}}', ['IN', 'cs_cron_command', [
+            'service/update-currency',
+        ]]);
+        $cs = new \kivork\VirtualCron\Models\CronScheduler();
+        $cs->cs_cron_expression = '1 * * * *';
+        $cs->cs_cron_command = 'service/update-currency';
+        $cs->cs_description = 'Update Currency Rates from Currency Service';
+        $cs->cs_hash = \kivork\VirtualCron\Services\Hasher::generate(
+            $cs->cs_cron_expression,
+            $cs->cs_cron_command,
+            $cs->cs_cron_params
+        );
+        $cs->cs_enabled = false;
+        if (!$cs->save()) {
+            echo $cs->getErrorSummary(true)[0];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete('{{%cron_scheduler}}', ['IN', 'cs_cron_command', [
+            'service/update-currency',
+        ]]);
+    }
+}
