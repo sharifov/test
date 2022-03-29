66429312b25873884883dfbe485cccf0985023cd
skincaid <san.kincaid@techork.com>
2022-03-29 14:19:34 +0600

    feat: PRT fix on Justfly settings 2(P2BPT-497)

--- console/migrations/m220329_073203_fix_price_research_tool_justfly_name_settings.php
+++ console/migrations/m220329_073203_fix_price_research_tool_justfly_name_settings.php
@@ -13,126 +13,20 @@ class m220329_073203_fix_price_research_tool_justfly_name_settings extends Migra
     public function safeUp()
     {
         try {
-            $this->delete('{{%setting}}', [
-                'IN',
-                's_key',
-                [
-                    'price_research_links',
-                ]
-            ]);
-            $settingCategory = \common\models\SettingCategory::getOrCreateByName('Price link Search');
-
-            $this->insert(
-                '{{%setting}}',
-                [
-                    's_key'         => 'price_research_links',
-                    's_name'        => 'Price Search Links',
-                    's_type'        => \common\models\Setting::TYPE_ARRAY,
-                    's_value'       => json_encode([
-                        'sky_scanner' => [
-                            'name'                             => 'SkyScanner',
-                            'enabled'                          => true,
-                            'url'                              => 'https://www.skyscanner.net/transport/',
-                            'types'                            => [
-                                'oneTrip'   => 'flights/{%origin%}/{%destination%}/{%departure_date%}/?adults={%adults_count%}&adultsv2={%adults_count%}&cabinclass={%cabin_type%}{%children_sub_query%}&inboundaltsenabled=false&outboundaltsenabled=false&preferdirects=false&ref=home&rtn=0',
-                                'roundTrip' => 'flights/{%origin%}/{%destination%}/{%first_departure_date%}/{%second_departure_date%}/?adults={%adults_count%}&adultsv2={%adults_count%}&cabinclass={%cabin_type%}{%children_sub_query%}&inboundaltsenabled=false&outboundaltsenabled=false&preferdirects=false&ref=home&rtn=0',
-                                'multiCity' => 'd/{%itinerary_part%}?adults={%adults_count%}&adultsv2={%adults_count%}&cabinclass={%cabin_type%}{%children_sub_query%}&inboundaltsenabled=false&outboundaltsenabled=false&preferdirects=false&ref=home&rtn=0',
-                            ],
-                            'multiCityItineraryPattern'        => '{%origin%}/{%departure_date%}/{%destination%}/',
-                            'dateFormat'                       => 'yyMMdd',
-                            'multiCityDateFormat'              => 'YYYY-MM-dd',
-                            'cabinClassMappings'               => [
-                                'E' => 'economy',
-                                'P' => 'premiumeconomy',
-                                'B' => 'business',
-                                'F' => 'first',
-                            ],
-                            'childrenParameterType'            => 'enumerable',
-                            'childrenParameterSeparator'       => '|',
-                            'childrenSubQueryPart'             => '&childrenv2={%children_and_infants_count_part%}',
-                            'childPaxTypeEnumerableParameter'  => '10',
-                            'infantPaxTypeEnumerableParameter' => '0',
-                        ],
-                        'kayak'       =>
-                            [
-                                'name'    => 'Kayak',
-                                'enabled' => true,
-                                'url'     => 'https://www.kayak.com/flights/',
-                                'types'   => [
-                                    'oneTrip'   => '{%origin%}-{%destination%}/{%departure_date%}/{%cabin_type%}{%adults_count%}adults{%children_sub_query%}?sort=price_a',
-                                    'roundTrip' => '{%origin%}-{%destination%}/{%first_departure_date%}/{%second_departure_date%}/{%cabin_type%}{%adults_count%}adults{%children_sub_query%}?fs=cabin={%cabin_type%}&sort=bestflight_a',
-                                    'multiCity' => '{%itinerary_part%}{%cabin_type%}{%adults_count%}adults{%children_sub_query%}?fs=cabin={%cabin_type%}&sort=bestflight_a',
-                                ],
-
-                                'multiCityItineraryPattern'        => '{%origin%}-{%destination%}/{%departure_date%}-h/',
-                                'dateFormat'                       => 'YYYY-MM-dd',
-                                'multiCityDateFormat'              => 'YYYY-MM-dd',
-                                'cabinClassMappings'               => [
-                                    'E' => '',
-                                    'P' => 'premium/',
-                                    'B' => 'business/',
-                                    'F' => 'first/',
-                                ],
-                                'childrenParameterType'            => 'enumerable',
-                                'childrenParameterSeparator'       => '-',
-                                'childrenSubQueryPart'             => '/children-{%children_and_infants_count_part%}',
-                                'childPaxTypeEnumerableParameter'  => '11',
-                                'infantPaxTypeEnumerableParameter' => '1S',
-                            ],
-                        'momondo'     =>
-                            [
-                                'name'    => 'Momondo',
-                                'enabled' => true,
-                                'url'     => 'https://www.momondo.com/flight-search/',
-                                'types'   => [
-                                    'oneTrip'   => '{%origin%}-{%destination%}/{%departure_date%}/{%cabin_type%}{%adults_count%}adults{%children_sub_query%}?sort=price_a',
-                                    'roundTrip' => '{%origin%}-{%destination%}/{%first_departure_date%}/{%second_departure_date%}{%cabin_type%}{%adults_count%}adults{%children_sub_query%}?sort=price_a',
-                                    'multiCity' => '{%itinerary_part%}{%cabin_type%}{%adults_count%}adults{%children_sub_query%}?sort=price_a',
-                                ],
-
-                                'multiCityItineraryPattern'        => '{%origin%}-{%destination%}/{%departure_date%}-h/',
-                                'dateFormat'                       => 'YYYY-MM-dd',
-                                'multiCityDateFormat'              => 'YYYY-MM-dd',
-                                'cabinClassMappings'               => [
-                                    'E' => '',
-                                    'P' => 'premium/',
-                                    'B' => 'business/',
-                                    'F' => 'first/',
-                                ],
-                                'childrenParameterType'            => 'enumerable',
-                                'childrenParameterSeparator'       => '-',
-                                'childrenSubQueryPart'             => '/children-{%children_and_infants_count_part%}',
-                                'childPaxTypeEnumerableParameter'  => '11',
-                                'infantPaxTypeEnumerableParameter' => '1S',
-                            ],
-                        'justify_com' =>
-                            [
-                                'name'    => 'Justfly.com',
-                                'enabled' => true,
-                                'url'     => 'https://www.justfly.com/flight/search?',
-                                'types'   => [
-                                    'oneTrip'   => 'num_adults={%adults_count%}{%children_sub_query%}&seat_class={%cabin_type%}&seg0_date={%departure_date%}&seg0_from={%origin%}&seg0_to={%destination%}&type=oneway',
-                                    'roundTrip' => 'num_adults={%adults_count%}{%children_sub_query%}&seat_class={%cabin_type%}&seg0_date={%first_departure_date%}&seg0_from={%origin%}&seg0_to={%destination%}&seg1_date={%second_departure_date%}&seg1_from={%destination%}&seg1_to={%origin%}&type=roundtrip',
-                                    'multiCity' => 'num_adults={%adults_count%}{%children_sub_query%}&seat_class={%cabin_type%}{%itinerary_part%}&type=multi',
-                                ],
-
-                                'multiCityItineraryPattern' => '&seg{%segment_index%}_date={%departure_date%}&seg{%segment_index%}_from={%origin%}&seg{%segment_index%}_to={%destination%}',
-                                'dateFormat'                => 'YYYY-MM-dd',
-                                'multiCityDateFormat'       => 'YYYY-MM-dd',
-                                'cabinClassMappings'        => [
-                                    'E' => 'Economy',
-                                    'P' => 'EconomyPremium',
-                                    'B' => 'Business',
-                                    'F' => 'First',
-                                ],
-                                'childrenParameterType'     => 'quantitative',
-                                'childrenSubQueryPart'      => '&num_children={%children_count%}&num_infants={%infants_count%}&num_infants_lap=0',
-                            ],
-                    ]),
-                    's_updated_dt'  => date('Y-m-d H:i:s'),
-                    's_category_id' => $settingCategory->sc_id,
-                ]
-            );
+            $setting = \common\models\Setting::find()->where(['s_key' => 'price_research_links'])->one();
+            if (empty($setting)) {
+                return;
+            }
+            $settingValue    = json_decode($setting->s_value, true);
+            $justflySettings = \yii\helpers\ArrayHelper::getValue($settingValue, 'justify_com');
+            if (empty($justflySettings)) {
+                return;
+            }
+            $justflySettings['name'] = 'Justfly.com';
+            unset($settingValue['justify_com']);
+            $settingValue['justfly_com'] = $justflySettings;
+            $setting->s_value            = json_encode($settingValue, true);
+            $setting->save();
             if (Yii::$app->cache) {
                 Yii::$app->cache->flush();
             }
@@ -152,13 +46,20 @@ class m220329_073203_fix_price_research_tool_justfly_name_settings extends Migra
     public function safeDown()
     {
         try {
-            $this->delete('{{%setting}}', [
-                'IN',
-                's_key',
-                [
-                    'price_research_links',
-                ]
-            ]);
+            $setting = \common\models\Setting::find()->where(['s_key' => 'price_research_links'])->one();
+            if (empty($setting)) {
+                return;
+            }
+            $settingValue    = json_decode($setting->s_value, true);
+            $justflySettings = \yii\helpers\ArrayHelper::getValue($setting, 'justfly_com');
+            if (empty($justflySettings)) {
+                return;
+            }
+            $justflySettings['name'] = 'Justify.com';
+            unset($settingValue['justfly_com']);
+            $settingValue['justify_com'] = $justflySettings;
+            $setting->s_value            = json_encode($settingValue, true);
+            $setting->save();
             if (Yii::$app->cache) {
                 Yii::$app->cache->flush();
             }
