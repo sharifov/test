e15f2dacadbec448ba09989a592ee01ec7b2255e
Rain Saburo <rain.saburo@techork.com>
2022-03-31 18:12:08 +0600

    fix add check old Status

--- src/listeners/cases/CasesSwitchStatusAwaitingtoErrorListener.php
+++ src/listeners/cases/CasesSwitchStatusAwaitingtoErrorListener.php
@@ -6,6 +6,7 @@ use src\entities\cases\events\CasesErrorStatusEvent;
 use common\models\Notifications;
 use frontend\widgets\notification\NotificationMessage;
 use common\components\purifier\Purifier;
+use src\entities\cases\CasesStatus;
 use Yii;
 
 class CasesSwitchStatusAwaitingtoErrorListener
@@ -13,7 +14,15 @@ class CasesSwitchStatusAwaitingtoErrorListener
     public function handle(CasesErrorStatusEvent $event): void
     {
         try {
-            if ($ntf = Notifications::create($event->ownerId, 'Case Error', 'Case (' . Purifier::createCaseShortLink($event->case) . ') has been moved Awaiting to Error', Notifications::TYPE_WARNING, true)) {
+            $subject = Yii::t('email', 'Case Error');
+            $body = Yii::t(
+                'email',
+                "Case ({case_link}) has been moved from Awaiting to Error.",
+                [
+                    'case_link' => Purifier::createCaseShortLink($event->case)
+                ]
+            );
+            if ($event->oldStatus == CasesStatus::STATUS_AWAITING && $ntf = Notifications::create($event->ownerId, $subject, $body, Notifications::TYPE_WARNING, true)) {
                 $dataNotification = (Yii::$app->params['settings']['notification_web_socket']) ? NotificationMessage::add($ntf) : [];
                 Notifications::publish('getNewNotification', ['user_id' => $event->ownerId], $dataNotification);
             }
--- src/listeners/cases/CasesSwitchStatusAwaitingtoSolvedListener.php
+++ src/listeners/cases/CasesSwitchStatusAwaitingtoSolvedListener.php
@@ -6,6 +6,7 @@ use src\entities\cases\events\CasesSolvedStatusEvent;
 use common\models\Notifications;
 use frontend\widgets\notification\NotificationMessage;
 use common\components\purifier\Purifier;
+use src\entities\cases\CasesStatus;
 use Yii;
 
 class CasesSwitchStatusAwaitingtoSolvedListener
@@ -13,7 +14,15 @@ class CasesSwitchStatusAwaitingtoSolvedListener
     public function handle(CasesSolvedStatusEvent $event): void
     {
         try {
-            if ($ntf = Notifications::create($event->ownerId, 'Case Solved', 'Case (' . Purifier::createCaseShortLink($event->case) . ') has been moved Awaiting to Solved', Notifications::TYPE_SUCCESS, true)) {
+            $subject = Yii::t('email', 'Case Solved');
+            $body = Yii::t(
+                'email',
+                "Case ({case_link}) has been moved from Awaiting to Solved.",
+                [
+                    'case_link' => Purifier::createCaseShortLink($event->case)
+                ]
+            );
+            if ($event->oldStatus == CasesStatus::STATUS_AWAITING && $ntf = Notifications::create($event->ownerId, $subject, $body, Notifications::TYPE_SUCCESS, true)) {
                 $dataNotification = (Yii::$app->params['settings']['notification_web_socket']) ? NotificationMessage::add($ntf) : [];
                 Notifications::publish('getNewNotification', ['user_id' => $event->ownerId], $dataNotification);
             }
