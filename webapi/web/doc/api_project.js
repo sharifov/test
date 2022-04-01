4e6ada7d86f89e3ae392ea87995954f805561f1f
andrew.snake <andrew.snake@techork.com>
2022-04-01 10:48:27 +0300

    feat: added baseTaxes (P2BPT-538)

--- webapi/web/doc/api_project.js
+++ webapi/web/doc/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2022-03-04T13:03:28.765Z",
+    "time": "2022-04-01T07:41:26.700Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
