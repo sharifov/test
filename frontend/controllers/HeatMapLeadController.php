8656c452f9a9c550dda79bf61b35391bdc6ac433
andrew.snake <andrew.snake@techork.com>
2022-04-05 12:42:33 +0300

    feat: added abac for heat map lead (P2BPT-512)

--- frontend/controllers/HeatMapLeadController.php
+++ frontend/controllers/HeatMapLeadController.php
@@ -4,8 +4,13 @@ namespace frontend\controllers;
 
 use common\models\Project;
 use common\models\Sources;
+use modules\lead\src\abac\dto\LeadAbacDto;
+use modules\lead\src\abac\LeadAbacObject;
+use src\auth\Auth;
 use src\model\lead\reports\HeatMapLeadSearch;
 use src\model\lead\reports\HeatMapLeadService;
+use yii\helpers\ArrayHelper;
+use yii\web\ForbiddenHttpException;
 
 /**
  * Class HeatMapLeadController
@@ -18,8 +23,28 @@ class HeatMapLeadController extends FController
         $this->layoutCrud();
     }
 
+    public function behaviors(): array
+    {
+        $behaviors = [
+            'access' => [
+                'allowActions' => [
+                    'index',
+                ],
+            ],
+        ];
+        return ArrayHelper::merge(parent::behaviors(), $behaviors);
+    }
+
     public function actionIndex(): string
     {
+        $leadAbacDto = new LeadAbacDto(null, (int) Auth::id());
+        /** @abac $leadAbacDto, LeadAbacObject::OBJ_HEAT_MAP_LEAD, LeadAbacObject::ACTION_ACCESS, access to actionIndex */
+        $canView = \Yii::$app->abac->can($leadAbacDto, LeadAbacObject::OBJ_EXTRA_QUEUE, LeadAbacObject::ACTION_ACCESS);
+
+        if (!$canView) {
+            throw new ForbiddenHttpException('Access denied.');
+        }
+
         $searchModel = new HeatMapLeadSearch(\Yii::$app->user->identity->timezone);
         $resultHeatMap = $searchModel->leadCountHeatMap(\Yii::$app->request->queryParams);
         $mappedResult = HeatMapLeadService::mapResult($resultHeatMap, $searchModel->getFromDT(), $searchModel->getToDT());
