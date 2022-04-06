dcd7a94fc76a2b30d4beb4c0598180e45f7038eb
Alex Connor <alex.connor@techork.com>
2022-04-06 17:44:50 +0300

    feat: update task list FullCalendar (P2BPT-563)

--- frontend/views/task/list.php
+++ frontend/views/task/list.php
@@ -62,27 +62,105 @@ $js = <<<JS
 
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      headerToolbar: {
-        center: 'dayGridMonth,timeGridFourDay' // buttons for switching between views
-      },
-      views: {
-        timeGridFourDay: {
-          type: 'timeGrid',
-          duration: { days: 7 },
-          buttonText: '7 day'
+        //initialView: 'dayGridWeek',
+        initialView: 'dayGridMonth',
+        height: 700,
+        navLinks: true,
+        
+        // initialDate: '2022-04-05',
+        // eventColor: 'green',
+
+      // eventDidMount: function(info) {
+      //   var tooltip = new Tooltip(info.el, {
+      //     title: info.event.extendedProps.description,
+      //     placement: 'top',
+      //     trigger: 'hover',
+      //     container: 'body'
+      //   });
+      // },
+
+        
+        // navLinkDayClick: function(date, jsEvent) {
+        //     console.log('day', date.toISOString());
+        //     console.log('coords', jsEvent.pageX, jsEvent.pageY);
+        //   },
+        
+        navLinkWeekClick: function(weekStart, jsEvent) {
+            console.log('week start', weekStart.toISOString());
+            console.log('coords', jsEvent.pageX, jsEvent.pageY);
+          },
+        // themeSystem: 'bootstrap',
+        // customTimeGridDay
+        firstDay: 1,
+        headerToolbar: {
+            center: 'dayGridMonth,customDayGridWeek,timeGridWeek,timeGridDay' // buttons for switching between views
+        },
+     
+      
+        footerToolbar: {
+            left: 'custom1,custom2',
+            center: '',
+            right: 'listDay,listWeek,listMonth'
+        },
+      customButtons: {
+        custom1: {
+          text: 'custom 1',
+          click: function() {
+            alert('clicked custom button 1!');
+          }
         }
       },
-      visibleRange: function(currentDate) {
-    // Generate a new date for manipulating in the next step
-    var startDate = new Date(currentDate.valueOf());
-    var endDate = new Date(currentDate.valueOf());
+      
+      views: {
+        // customTimeGridDay: {
+        //   type: 'timeGrid',
+        //   duration: { days: 7 },
+        //   buttonText: '7 days'
+        // },
+        customDayGridWeek: {
+          type: 'dayGridWeek',
+          buttonText: 'WeekDay'
+        },
+        
+        listDay: { buttonText: 'list day' },
+        listWeek: { buttonText: 'list week' },
+        listMonth: { buttonText: 'list month' }
+      },
+      
+      //plugins: [ 'dayGridPlugin' ],
+        timeZone: 'local',
+        locale: 'en',
+        dayMaxEvents: true, // allow "more" link when too many events
+        events: 'https://fullcalendar.io/api/demo-feeds/events.json?overload-day',
+        
+          //events: 'https://fullcalendar.io/api/demo-feeds/events.json',
+          editable: false,
+          selectable: true,
+        
+        //events: 'https://fullcalendar.io/api/demo-feeds/events.json?with-resources=2',
+        // events: [
+        //     { start: '2022-04-06T12:30:00Z' }, // will be shifted to local
+        //     { start: '2022-04-07T12:30:00' }, // already same offset as local, so won't shift
+        //     { start: '2022-04-08T12:30:00' } // will be parsed as if it were '2018-09-01T12:30:00+XX:XX'
+        //   ],
+          dateClick: function(arg) {
+            
+            // console.log('Clicked on: ' + arg.dateStr);
+            // console.log('Coordinates: ' + arg.jsEvent.pageX + ',' + arg.jsEvent.pageY);
+            // console.log('Current view: ' + arg.view.type);
+            //console.log('Resource ID: ' + arg.resource.id);
+            console.log(arg);
+            // change the day's background color just for fun
+            // arg.dayEl.style.backgroundColor = 'red';
+            
+            //console.log(arg.date.toString()); // use *local* methods on the native Date Object
+            // will output something like 'Sat Sep 01 2018 00:00:00 GMT-XX:XX (Eastern Daylight Time)'
+          },
+          select: function(info) {
+            console.log(info);
+            //console.log('selected ' + info.startStr + ' to ' + info.endStr);
+          }
 
-    // Adjust the start & end dates, respectively
-    startDate.setDate(startDate.getDate() - 1); // One day in the past
-    endDate.setDate(endDate.getDate() + 2); // Two days into the future
-
-    return { start: startDate, end: endDate };
-  }
     });
 
     // var calendar = new FullCalendar.Calendar(calendarEl, {
