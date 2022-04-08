89b5178bd3281cbfa4bb9e812225dc3ec6aa3e06
shakarim <safi.shain@techork.com>
2022-04-08 23:15:56 +0600

    Feat: The class `Repo` has been added for `quoteCommunication`. This class implements logging logic

---
+++ src/quoteCommunication/Repo.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace src\quoteCommunication;
+
+use common\models\Email;
+use common\models\QuoteCommunication;
+use frontend\models\CommunicationForm;
+
+/**
+ * Class Repo
+ * @package src\quoteCommunication
+ */
+class Repo
+{
+    /**
+     * @param Email $email
+     * @param int $quoteId
+     * @return bool
+     */
+    public static function createForEmail(Email $email, int $quoteId): bool
+    {
+        $model = new QuoteCommunication();
+        $model->setAttributes([
+            'qc_communication_type' => CommunicationForm::TYPE_EMAIL,
+            'qc_communication_id' => $email->e_id,
+            'qc_quote_id' => $quoteId
+        ]);
+        return $model->save();
+    }
+}
