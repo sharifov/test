164e021cf2b59908cdcb3c3151b68118ee8be72d
Lana Bates <lana.bates@techork.com>
2022-04-09 16:27:42 +0300

    TMS-281 ref:ProductQuoteStatusChangeEvent implements ProductQuoteStatusChangeInterface

--- modules/product/src/entities/productQuote/events/ProductQuoteStatusChangeEvent.php
+++ modules/product/src/entities/productQuote/events/ProductQuoteStatusChangeEvent.php
@@ -2,7 +2,7 @@
 
 namespace modules\product\src\entities\productQuote\events;
 
-class ProductQuoteStatusChangeEvent
+class ProductQuoteStatusChangeEvent implements ProductQuoteStatusChangeInterface
 {
     public $productQuoteId;
     public $startStatusId;
@@ -21,5 +21,40 @@ class ProductQuoteStatusChangeEvent
         $this->actionId = $actionId;
         $this->ownerId = $ownerId;
         $this->creatorId = $creatorId;
+    }
+
+    public function getId(): int
+    {
+        return $this->productQuoteId;
+    }
+
+    public function getStartStatus(): ?int
+    {
+        return $this->startStatusId;
+    }
+
+    public function getEndStatus(): int
+    {
+        return $this->endStatusId;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function getActionId(): ?int
+    {
+        return $this->actionId;
+    }
+
+    public function getOwnerId(): ?int
+    {
+        return $this->ownerId;
+    }
+
+    public function getCreatorId(): ?int
+    {
+        return $this->creatorId;
     }
 }
