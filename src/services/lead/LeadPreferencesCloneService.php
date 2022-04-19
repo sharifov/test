62fa90e3434041b30a94ff8729e4d6c0fddeaf03
andrew.snake <andrew.snake@techork.com>
2022-04-19 08:09:47 +0300

    fix: fix lead clone createOnlyCurrency (P2BPT-617)

--- src/services/lead/LeadPreferencesCloneService.php
+++ src/services/lead/LeadPreferencesCloneService.php
@@ -2,55 +2,31 @@
 
 namespace src\services\lead;
 
+use common\models\Currency;
 use common\models\LeadPreferences;
 use src\repositories\lead\LeadPreferencesRepository;
-use src\services\ServiceFinder;
-use src\services\TransactionManager;
-use yii\web\NotFoundHttpException;
 
 /**
  * Class LeadAssignService
  * @property LeadPreferencesRepository $leadPreferencesRepository
- * @property TransactionManager $transactionManager
- * @property EventDispatcher $eventDispatcher
- * @property ServiceFinder $serviceFinder
  */
 class LeadPreferencesCloneService
 {
-    private $leadPreferencesRepository;
-    private $transactionManager;
-    private $eventDispatcher;
-    private $serviceFinder;
+    private LeadPreferencesRepository $leadPreferencesRepository;
 
     public function __construct(
-        LeadPreferencesRepository $leadPreferencesRepository,
-        TransactionManager $transactionManager,
-        ServiceFinder $serviceFinder
+        LeadPreferencesRepository $leadPreferencesRepository
     ) {
         $this->leadPreferencesRepository = $leadPreferencesRepository;
-        $this->transactionManager = $transactionManager;
-        $this->serviceFinder = $serviceFinder;
     }
 
-    /**
-     * @param int $leadId
-     * @param int $cloneLeadId
-     * @return LeadPreferences
-     * @throws \Throwable
-     */
-    public function cloneLeadPreferences($leadId, $cloneLeadId): LeadPreferences
+    public function cloneLeadPreferenceCurrency(int $leadId, int $cloneLeadId): LeadPreferences
     {
-        $lead = $this->serviceFinder->leadFind($cloneLeadId);
         $leadPreferences = LeadPreferences::find()->where(['lead_id' => $leadId])->one();
+        $currency = $leadPreferences->pref_currency ?? Currency::getDefaultCurrencyCode();
 
-        $clone = $this->transactionManager->wrap(function () use ($lead, $leadPreferences) {
-            $currency = $leadPreferences->pref_currency ?? null;
-            $clone = $leadPreferences->createOnlyCurrency($lead->id, $currency);
-            $this->leadPreferencesRepository->save($clone);
-
-            return $clone;
-        });
-
-        return $clone;
+        $cloneLeadPreferences = LeadPreferences::createOnlyCurrency($cloneLeadId, $currency);
+        $this->leadPreferencesRepository->save($cloneLeadPreferences);
+        return $cloneLeadPreferences;
     }
 }
