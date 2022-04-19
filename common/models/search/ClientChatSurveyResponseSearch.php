03ad399c67bf08d5a98bac90bb531dd1917c22b7
shakarim <safi.shain@techork.com>
2022-04-19 17:12:04 +0600

    feat: the `ccsr_created_dt` is available filtering condition for `common/models/search/ClientChatSurveyResponseSearch.php`

--- common/models/search/ClientChatSurveyResponseSearch.php
+++ common/models/search/ClientChatSurveyResponseSearch.php
@@ -19,8 +19,8 @@ class ClientChatSurveyResponseSearch extends ClientChatSurveyResponse
     {
         return [
             [['ccsr_id', 'ccsr_client_chat_survey_id'], 'integer'],
-            [['ccsr_question', 'ccsr_response'], 'string'],
-            [['ccsr_id', 'ccsr_client_chat_survey_id', 'ccsr_question', 'ccsr_response'], 'safe']
+            [['ccsr_question', 'ccsr_response', 'ccsr_created_dt'], 'string'],
+            [['ccsr_id', 'ccsr_client_chat_survey_id', 'ccsr_question', 'ccsr_response', 'ccsr_created_dt'], 'safe']
         ];
     }
 
@@ -59,6 +59,20 @@ class ClientChatSurveyResponseSearch extends ClientChatSurveyResponse
         $query
             ->andFilterWhere(['like', 'ccsr_question', $this->ccsr_question])
             ->andFilterWhere(['like', 'ccsr_response', $this->ccsr_response]);
+
+        /*
+         * If `ccsr_created_dt` is not empty - we add filtering by this field.
+         *
+         * I could use something like `$query->andFilterWhere(['DATE(ccsr_created_dt)' => $this->ccsr_created_dt])`
+         * but didn't, because in MySQL the 'DATE/1' function calculates date for every single record in database
+         * (including values that not included in the selection), as result - uses more system resources.
+         */
+        if ($this->ccsr_created_dt !== null && $this->ccsr_created_dt !== "") {
+            $createdDateTime = \DateTime::createFromFormat('Y-m-d', $this->ccsr_created_dt);
+            $fromCreatedDateTime = "{$createdDateTime->format('Y-m-d')} 00:00:00";
+            $toCreatedDateTime = "{$createdDateTime->format('Y-m-d')} 23:59:59";
+            $query->andFilterWhere(['BETWEEN', 'ccsr_created_dt', $fromCreatedDateTime, $toCreatedDateTime]);
+        }
 
         return $dataProvider;
     }
