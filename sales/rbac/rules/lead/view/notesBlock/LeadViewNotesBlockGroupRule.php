955e3e59bb27e0a222e858c932735671a2c05022
Esto Boston <esto.boston@techork.com>
2022-05-04 16:58:21 +0600

    feat: change abac policy - communication block access only for lead owner (P2BPT-662)

--- sales/rbac/rules/lead/view/notesBlock/LeadViewNotesBlockGroupRule.php
+++ sales/rbac/rules/lead/view/notesBlock/LeadViewNotesBlockGroupRule.php
@@ -3,7 +3,7 @@
 namespace sales\rbac\rules\lead\view\notesBlock;
 
 use common\models\Lead;
-use sales\access\EmployeeGroupAccess;
+use src\access\EmployeeGroupAccess;
 use yii\rbac\Rule;
 
 class LeadViewNotesBlockGroupRule extends Rule
