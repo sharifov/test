48198df9f787833426da5bcd78b6669745ad811d
Serge Renshou <serge.renshou@techork.com>
2022-05-05 00:06:56 +0300

    update dev zone directly from the child

---
+++ devops/terraform/dev/parrent-route53.tf
@@ -0,0 +1,26 @@
+# Import environment infra tf state
+data "terraform_remote_state" "env_parent_zone" {
+  backend = "s3"
+  config = {
+    bucket  = "terraform-${var.ENV}-traveldev"
+    key     = "${var.ENV}"
+    region  = "${var.REGION}"
+    profile = "aws-${var.ENV}-infra"
+  }
+}
+
+# Create NS for this project
+resource "aws_route53_record" "project_in_parent" {
+  allow_overwrite = true
+  name            = var.DOMAIN
+  ttl             = 172800
+  type            = "NS"
+  zone_id         = data.terraform_remote_state.env_parent_zone.outputs.route53_public_zone_id
+
+  records = [
+    aws_route53_zone.public.name_servers[0],
+    aws_route53_zone.public.name_servers[1],
+    aws_route53_zone.public.name_servers[2],
+    aws_route53_zone.public.name_servers[3],
+  ]
+}
