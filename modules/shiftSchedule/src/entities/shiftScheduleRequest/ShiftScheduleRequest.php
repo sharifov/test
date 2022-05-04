62d9c2752e02b877d46cb3e204e7cc7847f9c40c
mike.kane <mike.kane@techork.com>
2022-05-04 12:42:48 +0300

    feat: update entity shift schedule request (P2BPT-578)

--- modules/shiftSchedule/src/entities/shiftScheduleRequest/ShiftScheduleRequest.php
+++ modules/shiftSchedule/src/entities/shiftScheduleRequest/ShiftScheduleRequest.php
@@ -20,6 +20,8 @@ use yii\db\BaseActiveRecord;
  * @property int|null $srh_sst_id
  * @property int $srh_status_id
  * @property string|null $srh_description
+ * @property string|null $srh_start_utc_dt
+ * @property string|null $srh_end_utc_dt
  * @property string|null $srh_created_dt
  * @property string|null $srh_update_dt
  * @property int|null $srh_created_user_id
@@ -27,6 +29,11 @@ use yii\db\BaseActiveRecord;
  *
  * @property Employee $srhCreatedUser
  * @property ShiftScheduleType $srhSst
+ * @property-read string $statusNameColor
+ * @property-read string $statusName
+ * @property-read string $scheduleTypeKey
+ * @property-read string $scheduleTypeTitle
+ * @property-read int $duration
  * @property UserShiftSchedule $srhUss
  */
 class ShiftScheduleRequest extends ActiveRecord
@@ -44,7 +51,7 @@ class ShiftScheduleRequest extends ActiveRecord
     ];
 
     public const STATUS_LIST_COLOR = [
-        self::STATUS_PENDING => 'info',
+        self::STATUS_PENDING => 'warning',
         self::STATUS_APPROVE => 'success',
         self::STATUS_DECLINED => 'danger',
         self::STATUS_REMOVED => 'secondary',
@@ -90,7 +97,7 @@ class ShiftScheduleRequest extends ActiveRecord
         return [
             [['srh_uss_id', 'srh_sst_id', 'srh_status_id', 'srh_created_user_id', 'srh_updated_user_id'], 'integer'],
             [['srh_status_id'], 'required'],
-            [['srh_created_dt', 'srh_update_dt'], 'safe'],
+            [['srh_start_utc_dt', 'srh_end_utc_dt', 'srh_created_dt', 'srh_update_dt'], 'safe'],
             [['srh_description'], 'string', 'max' => 1000],
             [['srh_created_user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Employee::class, 'targetAttribute' => ['srh_created_user_id' => 'id']],
             [['srh_sst_id'], 'exist', 'skipOnError' => true, 'targetClass' => ShiftScheduleType::class, 'targetAttribute' => ['srh_sst_id' => 'sst_id']],
@@ -109,6 +116,8 @@ class ShiftScheduleRequest extends ActiveRecord
             'srh_sst_id' => Yii::t('app', 'Shift Schedule Type ID'),
             'srh_status_id' => Yii::t('app', 'Status ID'),
             'srh_description' => Yii::t('app', 'Description'),
+            'srh_start_utc_dt' => Yii::t('app', 'Start Dt'),
+            'srh_end_utc_dt' => Yii::t('app', 'End Dt'),
             'srh_created_dt' => Yii::t('app', 'Created Dt'),
             'srh_update_dt' => Yii::t('app', 'Update Dt'),
             'srh_created_user_id' => Yii::t('app', 'Created User ID'),
@@ -163,10 +172,59 @@ class ShiftScheduleRequest extends ActiveRecord
     }
 
     /**
+     * @param int $statusId
+     * @return string
+     */
+    public static function getStatusNameColorById(int $statusId): string
+    {
+        return self::STATUS_LIST_COLOR[$statusId] ?? '';
+    }
+
+    /**
      * @return array
      */
     public static function getList(): array
     {
         return self::STATUS_LIST;
+    }
+
+    /**
+     * @return string
+     */
+    public function getScheduleTypeKey(): string
+    {
+        return $this->srhSst ? $this->srhSst->sst_key : '-';
+    }
+
+    /**
+     * @return string
+     */
+    public function getScheduleTypeTitle(): string
+    {
+        return (!$this->srhSst || empty($this->srhSst->sst_title)) ? '-' :
+            $this->srhSst->sst_title;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDuration(): int
+    {
+        return round((strtotime($this->srh_end_utc_dt) - strtotime($this->srh_start_utc_dt)) / (60 * 60 * 24));
+    }
+
+    /**
+     * Get compatible status with UserShiftSchedule statuses
+     * @param int $statusId
+     * @return int
+     */
+    public static function getCompatibleStatus(int $statusId): int
+    {
+        return [
+            self::STATUS_PENDING => UserShiftSchedule::STATUS_PENDING,
+            self::STATUS_APPROVE => UserShiftSchedule::STATUS_APPROVED,
+            self::STATUS_DECLINED => UserShiftSchedule::STATUS_CANCELED,
+            self::STATUS_REMOVED => UserShiftSchedule::STATUS_DELETED,
+        ][$statusId] ?? UserShiftSchedule::STATUS_PENDING;
     }
 }
