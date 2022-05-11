5c0c995ee11aa18655f000b8013c62511c51a92a
mikhael.celso <mikhael.celso@techork.com>
2022-05-11 09:42:26 +0300

    fix: checkbox label UI in lead/search

--- frontend/views/leads/_search.php
+++ frontend/views/leads/_search.php
@@ -75,7 +75,7 @@ use frontend\extensions\DatePicker;
                             <?= $form->field($model, 'excludeExtraQueue')->checkbox([])->label('Exclude Extra Queue'); ?>
                         </div>
                         <div class="col-md-1">
-                            <?= $form->field($model, 'excludeBonusQueue')->checkbox([['label' => 'Exclude Bonus Queue / Follow up Queue']])->label('Exclude Bonus Queue / Follow up Queue'); ?>
+                            <?= $form->field($model, 'excludeBonusQueue')->checkbox(['label' => 'Exclude Bonus Queue / Follow up Queue'])->label('Exclude Bonus Queue / Follow up Queue'); ?>
                         </div>
                     </div>
                 </div>
