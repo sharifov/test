daf7f568703dfd777fb5dcec9a22550aec280ed0
Den Dexo <den.dexo@techork.com>
2022-05-13 10:02:31 +0300

    AOP-2 added $pq_expiration_dt property in ProductQuoteRefund

--- modules/product/src/entities/productQuoteRefund/ProductQuoteRefund.php
+++ modules/product/src/entities/productQuoteRefund/ProductQuoteRefund.php
@@ -39,6 +39,7 @@ use yii\helpers\Html;
  * @property int|null $pqr_updated_user_id
  * @property string|null $pqr_created_dt
  * @property string|null $pqr_updated_dt
+ * @property string|null $pqr_expiration_dt
  * @property int|null $pqr_case_id
  * @property string $pqr_type_id [tinyint unsigned]
  *
@@ -261,7 +262,7 @@ class ProductQuoteRefund extends \yii\db\ActiveRecord implements Serializable
                 'pqr_client_currency_rate', 'pqr_client_selling_price', 'pqr_client_refund_amount',
                 'pqr_client_penalty_amount', 'pqr_client_processing_fee_amount', 'pqr_refund_cost',
                 'pqr_client_refund_cost'], 'number', 'min' => 0, 'max' => 999999.99],
-            [['pqr_created_dt', 'pqr_updated_dt'], 'safe'],
+            [['pqr_created_dt', 'pqr_updated_dt', 'pqr_expiration_dt'], 'safe'],
             [['pqr_client_currency'], 'string', 'max' => 3],
             [['pqr_client_currency'], 'default', 'value' => null],
 
