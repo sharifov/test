ae112117b9b30ae3eac3f0efd81903cc40f866b1
Ayan Ahmet <ayan.ahmet@techork.com>
2022-05-17 10:27:43 +0000

    feat: style change PHPdoc (P2BPT-771)

--- frontend/helpers/TimeConverterHelper.php
+++ frontend/helpers/TimeConverterHelper.php
@@ -5,10 +5,12 @@ namespace frontend\helpers;
 class TimeConverterHelper
 {
     /**
-     * @param $hours
+     * @param string $hours
+     * @format $hours - H:i
+     * @example $minutes = TimeConverterHelper::hoursToMinutes('01:42');
      * @return int
      */
-    public static function hoursToMinutes($hours): int
+    public static function hoursToMinutes(string $hours): int
     {
         $minutes = 0;
         if (strpos($hours, ':') !== false) {
@@ -18,22 +20,16 @@ class TimeConverterHelper
     }
 
     /**
-     * @param $minutes
-     * @return string
+     * @param int $minutes
+     * @return string|0
      */
-    public static function minutesToHours($minutes): string
+    public static function minutesToHours(int $minutes): string
     {
+        if ($minutes <= 0) {
+            return 0;
+        }
         $hours = (int)($minutes / 60);
         $minutes -= $hours * 60;
         return sprintf("%d:%02.0f", $hours, $minutes);
-    }
-
-    /**
-     * @param $minutes
-     * @return string|int
-     */
-    public static function getMinutesToHours($minutes)
-    {
-        return $minutes ? static::minutesToHours($minutes) : 0;
     }
 }
