3e3845d367e9b195c2e570b8c236170ebb9bcccc
Alex Grub <alex.grub@techork.com>
2022-05-18 11:05:31 +0300

    feat: create filter form for shift events on user shift calendar page (P2BPT-782)

--- modules/shiftSchedule/src/helpers/UserShiftScheduleHelper.php
+++ modules/shiftSchedule/src/helpers/UserShiftScheduleHelper.php
@@ -2,10 +2,14 @@
 
 namespace modules\shiftSchedule\src\helpers;
 
+use common\models\Employee;
+use common\models\UserGroup;
 use modules\shiftSchedule\src\abac\dto\ShiftAbacDto;
 use modules\shiftSchedule\src\abac\ShiftAbacObject;
 use modules\shiftSchedule\src\entities\shiftScheduleType\ShiftScheduleType;
 use modules\shiftSchedule\src\entities\userShiftSchedule\UserShiftSchedule;
+use yii\helpers\Html;
+use yii\helpers\Url;
 use Yii;
 
 class UserShiftScheduleHelper
@@ -71,5 +75,65 @@ class UserShiftScheduleHelper
             }
         }
         return $shiftScheduleTypeList;
+    }
+
+    /**
+     * @param UserGroup[] $userGroups
+     * @param array $usersIds
+     * @return array
+     */
+    public static function prepareResourcesForTimelineCalendar(array $userGroups, array $usersIds = []): array
+    {
+        $resourceList = [];
+        $groupIds = [];
+
+        foreach ($userGroups as $key => $group) {
+            $resource = [
+                'id' => 'ug-' . $group->ug_id,
+                'name' => '<i class="fa fa-users"></i> ' . $group->ug_name,
+                'color' => '#1dab2f',
+                'title' => $group->ug_key,
+                'collapsed' => $key !== 0,
+                'isGroup' => true,
+                'description' => '',
+                'icons' => []
+            ];
+
+            $users = Employee::find()
+                ->joinWith(['userGroupAssigns'])
+                ->where(['ugs_group_id' => $group->ug_id])
+                ->andWhere(['status' => Employee::STATUS_ACTIVE])
+                ->orderBy(['username' => SORT_ASC])
+                ->andFilterWhere(['id' => $usersIds])
+                ->all();
+            if ($users) {
+                $userList = [];
+                foreach ($users as $i => $user) {
+                    $userList[] = [
+                        'id' => 'us-' . $user->id,
+                        'name' => '#' . ($i + 1) . ' <i class="fa fa-user"></i> ' . $user->username,
+                        'color' => '#1dab2f',
+                        'title' => '(' . $user->id . ') ' . $user->email,
+                        'isGroup' => false,
+                        'icons' => [
+                            Html::a('<i class="fa fa-calendar"></i>', Url::to(['/shift-schedule/user', 'id' => $user->id]), [
+                                'title' => 'User Shift Calendar',
+                                'target' => '_blank'
+                            ]),
+                            Html::a('<i class="fa fa fa-user-plus">', Url::to(['/shift/user-shift-assign/index', 'UserShiftAssignListSearch[userId]' => $user->id]), [
+                                'title' => 'User Shift Assign',
+                                'target' => '_blank'
+                            ])
+                        ],
+                        'description' => ''
+                    ];
+                }
+                $resource['name'] .= ' <sup>' . count($userList) . '</sup>';
+                $resource['children'] = $userList;
+            }
+            $resourceList[] = $resource;
+            $groupIds[] = $group->ug_id;
+        }
+        return [$resourceList, $groupIds];
     }
 }
