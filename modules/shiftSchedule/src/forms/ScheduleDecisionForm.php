b865ba9661efffe499163e4cd5006e353beaba73
mike.kane <mike.kane@techork.com>
2022-05-25 12:25:25 +0300

    feat: remove saveDicision from decision form (P2BPT-869)

--- modules/shiftSchedule/src/forms/ScheduleDecisionForm.php
+++ modules/shiftSchedule/src/forms/ScheduleDecisionForm.php
@@ -2,10 +2,6 @@
 
 namespace modules\shiftSchedule\src\forms;
 
-use common\models\Employee;
-use modules\shiftSchedule\src\entities\shiftScheduleRequest\ShiftScheduleRequest;
-use modules\shiftSchedule\src\services\ShiftScheduleRequestService;
-use src\auth\Auth;
 use yii\base\Model;
 
 class ScheduleDecisionForm extends Model
@@ -41,37 +37,6 @@ class ScheduleDecisionForm extends Model
                 'max' => self::DESCRIPTION_MAX_LENGTH,
             ],
         ];
-    }
-
-    /**
-     * Save Decision request to Shift Schedule Request table
-     * @param ShiftScheduleRequest $model
-     * @return bool
-     */
-    public function saveDecision(ShiftScheduleRequest $model): bool
-    {
-        $scheduleRequest = new ShiftScheduleRequest();
-        $scheduleRequest->attributes = $model->attributes;
-        $scheduleRequest->ssr_status_id = $this->status;
-        $scheduleRequest->ssr_description = $this->description;
-        $scheduleRequest->ssr_updated_user_id = Auth::id();
-        if ($scheduleRequest->getIsCanEditPreviousDate()) {
-            if ($scheduleRequest->save()) {
-                ShiftScheduleRequestService::sendNotification(
-                    Employee::ROLE_AGENT,
-                    $scheduleRequest,
-                    ShiftScheduleRequestService::NOTIFICATION_TYPE_CREATE
-                );
-                ShiftScheduleRequestService::sendNotification(
-                    Employee::ROLE_SUPERVISION,
-                    $scheduleRequest,
-                    ShiftScheduleRequestService::NOTIFICATION_TYPE_UPDATE
-                );
-                return true;
-            }
-        }
-
-        return false;
     }
 
     /**
