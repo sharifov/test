8a0b9d6ef091e0a73d99b4797d4b005a4cb688e3
mikhael.celso <mikhael.celso@techork.com>
2022-06-08 13:04:52 +0300

    improvement: index [add link to quote], view [show related segments] (P2BPT-798)

--- frontend/views/quote-trip-crud/view.php
+++ frontend/views/quote-trip-crud/view.php
@@ -1,10 +1,17 @@
 <?php
 
+use common\models\QuoteSegment;
+use common\models\QuoteTrip;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\QuoteTrip */
+/* @var $searchModel common\models\search\QuoteSegmentSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = $model->qt_id;
 $this->params['breadcrumbs'][] = ['label' => 'Quote Trips', 'url' => ['index']];
@@ -26,14 +33,46 @@ $this->params['breadcrumbs'][] = $this->title;
 //        ]) ?>
 <!--    </p>-->
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'qt_id',
-            'qt_duration',
-            'qt_key',
-            'qt_quote_id',
-        ],
-    ]) ?>
+    <div class="row">
+        <div class="col-md-5">
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'qt_id',
+                    'qt_duration',
+                    'qt_key',
+                    'qt_quote_id',
+                ],
+            ]) ?>
+        </div>
+        <div class="col-md-7">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'layout' => "{errors}\n{items}\n{pager}",
+                'columns' => [
+                    [
+                        'attribute' => 'qs_id',
+                        'format' => 'raw',
+                        'value' => function (QuoteSegment $model) {
+                            return '<i class="fa fa-link"></i> ' . Html::a($model->qs_id, ['/quote-segment-crud/index', 'QuoteSegmentSearch[qs_id]' => $model->qs_id], ['title' => 'Show', 'target' => '_blank', 'data-pjax' => 0]);
+                        }
+                    ],
+                    'qs_departure_time',
+                    'qs_arrival_time',
+                    'qs_flight_number',
+                    'qs_departure_airport_code',
+                    'qs_arrival_airport_code',
+                    [
+                        'class' => ActionColumn::class,
+                        'urlCreator' => function ($action, QuoteSegment $model2, $key, $index, $column) {
+                            return Url::to(['quote-segment-crud/' . $action, 'qs_id' => $model2->qs_id]);
+                        },
+                        'template' => '{view}',
+                    ]
+                ]
+            ]); ?>
+        </div>
+    </div>
 
 </div>
