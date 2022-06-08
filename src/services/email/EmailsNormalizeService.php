fe0ec2df09b972600bd3fb037b84246f711b904d
Lana Bates <lana.bates@techork.com>
2022-06-08 23:09:01 +0300

    TMS-472 feat:EmailsNormalizeService

---
+++ src/services/email/EmailsNormalizeService.php
@@ -0,0 +1,234 @@
+<?php
+namespace src\services\email;
+
+use src\entities\email\EmailAddress;
+use src\entities\email\EmailBody;
+use src\entities\email\EmailBlob;
+use src\helpers\email\TextConvertingHelper;
+use src\entities\email\EmailParams;
+use src\entities\email\EmailContact;
+use src\entities\email\Email;
+use common\models\Client;
+use common\models\Email as EmailOld;
+use src\entities\cases\Cases;
+use common\models\Lead;
+use src\entities\email\EmailLog;
+
+class EmailsNormalizeService
+{
+
+    public $emailBody;
+    public $emailBlob;
+    public $emailContacts;
+    public $emailCases;
+    public $emailLeads;
+    public $emailClients;
+    /**
+     *
+     * @var Email
+     */
+    public $email;
+    public $emailLog;
+    public $emailParams;
+
+    public function createEmailFromOld(EmailOld $emailOld)
+    {
+        $this->email = Email::createFromEmailObject($emailOld);
+        $this->email->save();
+
+        if($this->email) {
+            echo 'Email created<br/>';
+            $this
+                ->fillEmailParams($emailOld->e_priority, $emailOld->e_template_type_id, $emailOld->e_language_id)
+                ->fillEmailBody($emailOld->e_email_subject, $emailOld->e_email_body_text, $emailOld->e_email_data)
+                ->fillEmailBlob($emailOld->e_email_body_blob)
+                ->fillEmailContacts(
+                    $emailOld->e_email_from,
+                    $emailOld->e_email_to,
+                    $emailOld->e_email_from_name,
+                    $emailOld->e_email_to_name,
+                    $emailOld->e_email_cc,
+                    $emailOld->e_email_bc)
+                /*->linkClient($emailOld->e_client_id)
+                ->linkCase($emailOld->e_case_id)
+                ->linkLead($emailOld->e_lead_id) */;
+        }
+
+        return $this;
+    }
+
+    public function fillEmailParams(int $priority, ?int $templateType, ?string $language)
+    {
+        $attributes = [
+            'ep_template_type_id' => $templateType,
+            'ep_language_id' => $language,
+            'ep_priority' => $priority,
+        ];
+
+        $this->emailParams = new EmailParams();
+        $this->emailParams->attributes = $attributes;
+        $this->emailParams->save();
+
+        $this->email->link('params', $this->emailParams);
+
+        return $this;
+    }
+
+    public function fillEmailLog(
+        int $communicationId,
+        string $messageId,
+        ?string $errorMessage,
+        ?string $statusDoneDt,
+        ?string $readDt,
+        ?string $refMessageId,
+        ?string $inboxCreatedDt,
+        ?int $inboxEmailId)
+    {
+        $attributes = [
+            'el_status_done_dt' => $statusDoneDt,
+            'el_read_dt' => $readDt,
+            'el_error_message' => $errorMessage,
+            'el_message_id' => $messageId,
+            'el_ref_message_id' => $refMessageId,
+            'el_inbox_created_dt' => $inboxCreatedDt,
+            'el_inbox_email_id' => $inboxEmailId,
+            'el_communication_id' => $communicationId,
+        ];
+
+        $this->emailLog = new EmailLog();
+        $this->emailLog->attributes = $attributes;
+        $this->emailLog->save();
+
+        $this->emailLog->link('email', $this->email);
+
+        return $this;
+    }
+
+    public function fillEmailBody(string $subject, string $body, $emailData)
+    {
+        $attributes = [
+            'embd_email_subject' => $subject,
+            'embd_email_body_text' => $body,
+            'embd_email_data' => $emailData,
+            'embd_hash' => hash('crc32b', join(' | ', [$subject, $body])),
+        ];
+
+        $this->emailBody = new EmailBody();
+        $this->emailBody->attributes = $attributes;
+        $this->emailBody->save();
+
+        $this->email->link('emailBody', $this->emailBody);
+
+        return $this;
+    }
+
+    public function fillEmailBlob(string $body)
+    {
+        $this->emailBlob = new EmailBlob();
+        $this->emailBlob->attributes = ['embb_email_body_blob' => TextConvertingHelper::compress($body)];
+        $this->emailBlob->save();
+
+        $this->emailBlob->link('emailBody', $this->emailBody);
+
+        return $this;
+    }
+
+    //call after fill Email
+
+
+    public function linkClient(?int $clientId)
+    {
+        if ($clientId !== null) {
+            $client = Client::findOne(['id' => $clientId]);
+            if ($client) {
+                $this->email->link('clients', $client);
+            }
+        }
+        return $this;
+    }
+
+    public function linkCase(?int $caseId)
+    {
+        if ($caseId !== null) {
+            $case = Cases::findOne(['cs_id' => $caseId]);
+            if ($caseId) {
+                $this->email->link('cases', $caseId);
+            }
+        }
+        return $this;
+    }
+
+    public function linkLead(?int $leadId)
+    {
+        if ($leadId !== null) {
+            $lead = Lead::findOne(['id' => $leadId]);
+            if ($leadId) {
+                $this->email->link('leads', $leadId);
+            }
+        }
+        return $this;
+    }
+
+    public function fillEmailContacts(string $emailFrom, string $emailTo, ?string $emailFromName, ?string $emailToName, ?string $emailCc, ?string $emailBcc)
+    {
+        $addressFrom = $this->getAddress($emailFrom, $emailFromName);
+        $contactFrom = new EmailContact();
+        $contactFrom->attributes = [
+            'ec_address_id' => $addressFrom->ea_id,
+            'ec_email_id' => $this->email->e_id,
+            'ec_type_id' => EmailContact::TYPE_FROM
+        ];
+        $contactFrom->save();
+        $this->emailContacts[] = $contactFrom;
+
+        $addressTo = $this->getAddress($emailTo, $emailToName);
+        $contactTo = new EmailContact();
+        $contactTo->attributes = [
+            'ec_address_id' => $addressTo->ea_id,
+            'ec_email_id' => $this->email->e_id,
+            'ec_type_id' => EmailContact::TYPE_TO
+        ];
+        $contactTo->save();
+        $this->emailContacts[] = $contactTo;
+
+        if (!empty($emailCc)) {
+            $addressCc = $this->getAddress($emailCc);
+            $contactCc = new EmailContact();
+            $contactCc->attributes = [
+                'ec_address_id' => $addressCc->ea_id,
+                'ec_email_id' => $this->email->e_id,
+                'ec_type_id' => EmailContact::TYPE_CC
+            ];
+            $contactCc->save();
+            $this->emailContacts[] = $contactCc;
+        }
+
+        if (!empty($emailBcc)) {
+            $addressBcc = $this->getAddress($emailBcc);
+            $contactBcc = new EmailContact();
+            $contactBcc->attributes = [
+                'ec_address_id' => $addressCc->ea_id,
+                'ec_email_id' => $this->email->e_id,
+                'ec_type_id' => EmailContact::TYPE_BCC
+            ];
+            $contactBcc->save();
+            $this->emailContacts[] = $contactBcc;
+        }
+
+        return $this;
+    }
+
+    public function getAddress(string $email,?string $name): EmailAddress
+    {
+        $attributes = [
+            'ea_email' => $email,
+            'ea_name' => preg_replace('~\"(.*)\"~iU', "$1", $name),
+        ];
+        $address = EmailAddress::findOrNew(['ea_email' => $email], $attributes);
+        if ($address->isNewRecord) {
+            $address->save();
+        }
+
+        return $address;
+    }
+}
