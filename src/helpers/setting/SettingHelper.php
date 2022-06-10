3d4e6cad3f65829f96e2bac302059f93be41fb12
serge.murphy <serge.murphy@techork.com>
2022-06-10 15:44:10 +0300

    feat: added possibility ovveride phne to number (TMS-658)

--- src/helpers/setting/SettingHelper.php
+++ src/helpers/setting/SettingHelper.php
@@ -1012,4 +1012,9 @@ class SettingHelper
     {
         return (bool) (Yii::$app->params['settings']['enable_agent_call_queue_job_after_change_call_status_ready'] ?? true);
     }
+
+    public static function isOverridePhoneToForwarderFrom(): bool
+    {
+        return (bool) (Yii::$app->params['settings']['call_is_override_phone_to_forwarded_from'] ?? false);
+    }
 }
