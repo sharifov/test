d46de4f6bf260f52db086c7bb8f82eb00dc64b99
Alex Grub <alex.grub@techork.com>
2022-06-14 10:51:32 +0300

    feat: downgrade chat distribution logic (P2BPT-1030)

--- src/services/clientChatService/ClientChatService.php
+++ src/services/clientChatService/ClientChatService.php
@@ -211,6 +211,18 @@ class ClientChatService
         $limit = $channel->getSystemUserLimit();
         $users = $employeeSearch->searchAvailableAgentsForChatRequests($clientChat, $limit, $channel->getSortParameters());
 
+        if (SettingHelper::isClientChatDebugEnable() && $clientChat->isTransfer()) {
+            \Yii::info([
+                'message' => 'Users will be assign to chat',
+                'chatId' => $clientChat->cch_id,
+                'chatStatus' => $clientChat->getStatusName(),
+                'users' => ArrayHelper::getColumn($users, 'id'),
+                'countUsers' => count($users),
+                'microTime' => microtime(true),
+                'date' => date('Y-m-d H:i:s'),
+            ], 'info\ClientChatDebug');
+        }
+
         $key = self::getRedisDistributionLogicKey($clientChat->cch_id);
         if ($users) {
             foreach ($users as $user) {
