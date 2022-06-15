38d52c3d650e1e1678d909974dd3e7f0d1c44446
Alex Grub <alex.grub@techork.com>
2022-06-15 17:56:35 +0300

    fix: call not static method statically

--- common/models/Quote.php
+++ common/models/Quote.php
@@ -602,7 +602,7 @@ class Quote extends \yii\db\ActiveRecord
         return $this->hasMany(QuoteTrip::class, ['qt_quote_id' => 'id']);
     }
 
-    public function getDataForProfit($quoteId)
+    public static function getDataForProfit($quoteId)
     {
         $query = new Query();
 
@@ -612,7 +612,7 @@ class Quote extends \yii\db\ActiveRecord
                 'fare_type' => 'q.fare_type',
                 'check_payment' => 'q.check_payment'
             ])
-            ->from(Quote::tableName() . ' q')
+            ->from(self::tableName() . ' q')
             ->leftJoin(QuotePrice::tableName() . ' qp', 'q.id = qp.quote_id')
             ->where(['q.id' => $quoteId]);
 
