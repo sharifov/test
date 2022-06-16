b5305e3542fcb52578fb0630af62817eaf139720
noor.alden <noor.alden@techork.com>
2022-06-16 17:18:31 +0600

    fix: fix button var rules(TMS-588)

--- common/models/search/CaseSaleSearch.php
+++ common/models/search/CaseSaleSearch.php
@@ -32,8 +32,8 @@ class CaseSaleSearch extends CaseSale
     public function rules()
     {
         return [
-            [['css_cs_id', 'css_sale_id', 'css_sale_pax', 'css_created_user_id', 'css_updated_user_id'], 'integer'],
-            [['css_sale_book_id', 'css_sale_pnr', 'css_sale_data','nextId','prevId','cursor'], 'safe'],
+            [['css_cs_id', 'css_sale_id', 'css_sale_pax', 'css_created_user_id', 'css_updated_user_id','nextId','prevId','cursor'], 'integer'],
+            [['css_sale_book_id', 'css_sale_pnr', 'css_sale_data'], 'safe'],
             [['css_sale_created_dt', 'css_created_dt', 'css_updated_dt'], 'date', 'format' => 'php:Y-m-d'],
 
             [['css_need_sync_bo'], 'boolean'],
