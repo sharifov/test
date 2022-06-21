b3bc8c8ef7651550cd9224036c4e47189da1b2e0
Lana Bates <lana.bates@techork.com>
2022-06-21 23:28:06 +0300

    TMS-472 feat:EmailNormalizedController created

---
+++ frontend/controllers/EmailNormalizedController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace frontend\controllers;
+
+use common\models\search\EmailSearch;
+use src\entities\email\Email;
+use src\entities\email\EmailSearch as EmailNormalizedSearch;
+use src\model\email\useCase\send\EmailSenderService;
+use src\services\email\EmailsNormalizeService;
+use Yii;
+use yii\filters\VerbFilter;
+use yii\helpers\ArrayHelper;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
+
+/**
+ * EmailNormalizedController implements the CRUD actions for Email model.
+ *
+ * @property EmailSenderService $emailSender
+ */
+class EmailNormalizedController extends FController
+{
+    private $emailSender;
+
+    public function __construct($id, $module, EmailSenderService $emailSender, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->emailSender = $emailSender;
+    }
+
+    public function behaviors()
+    {
+        $behaviors = [
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+            'access' => [
+                'allowActions' => [
+                    'view',
+                ],
+            ],
+        ];
+        return ArrayHelper::merge(parent::behaviors(), $behaviors);
+    }
+
+    /**
+     * Lists all Email models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new EmailNormalizedSearch();
+
+        $params = Yii::$app->request->queryParams;
+        if (Yii::$app->user->identity->canRole('supervision')) {
+            $params['EmailSearch']['supervision_id'] = Yii::$app->user->id;
+        }
+        $searchModel->date_range = null;
+
+        $dataProvider = $searchModel->search($params);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * @param $id
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException
+     * @throws \Throwable
+     * @throws \yii\db\StaleObjectException
+     */
+    public function actionDelete($id)//: \yii\web\Response
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Email model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Email the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id): Email
+    {
+        if (($model = Email::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
+    /**
+     * @param $id
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException
+     */
+    public function actionSoftDelete($id): \yii\web\Response
+    {
+        $model = $this->findModel($id);
+        $model->e_is_deleted = (int) ! $model->e_is_deleted;
+        $model->save();
+        return $this->redirect(Yii::$app->request->referrer);
+    }
+}
