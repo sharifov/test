7d48ae5300ae28bad4dc027b0c11381529bcf3e8
mike.kane <mike.kane@techork.com>
2022-06-22 11:44:56 +0300

    feat: migration alter table user_profile add columns (P2BPT-1082)

---
+++ console/migrations/m220622_083544_alter_table_userprofile_add_columns.php
@@ -0,0 +1,51 @@
+<?php
+
+use src\helpers\app\DBHelper;
+use yii\db\Migration;
+
+/**
+ * Class m220622_083544_alter_table_userprofile_add_columns
+ */
+class m220622_083544_alter_table_userprofile_add_columns extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $schemaRefresh = false;
+        if (!DBHelper::isColumnExist('{{%user_profile}}', 'up_otp_hashed_code')) {
+            $this->addColumn('{{%user_profile}}', 'up_otp_hashed_code', $this->string(32));
+            $schemaRefresh = true;
+        }
+
+        if (!DBHelper::isColumnExist('{{%user_profile}}', 'up_otp_expired_dt')) {
+            $this->addColumn('{{%user_profile}}', 'up_otp_expired_dt', $this->dateTime());
+        }
+
+        if ($schemaRefresh) {
+            Yii::$app->db->getSchema()->refreshTableSchema('{{%user_profile}}');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $schemaRefresh = false;
+        if (DBHelper::isColumnExist('{{%user_profile}}', 'up_otp_expired_dt')) {
+            $this->dropColumn('{{%user_profile}}', 'up_otp_hashed_code');
+            $schemaRefresh = true;
+        }
+
+        if (DBHelper::isColumnExist('{{%user_profile}}', 'up_otp_expired_dt')) {
+            $this->dropColumn('{{%user_profile}}', 'up_otp_expired_dt');
+            $schemaRefresh = true;
+        }
+
+        if ($schemaRefresh) {
+            Yii::$app->db->getSchema()->refreshTableSchema('{{%user_profile}}');
+        }
+    }
+}
