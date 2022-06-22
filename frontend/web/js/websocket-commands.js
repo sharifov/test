55d1df2c2d42e97777ad49949c0788143ba0a50d
Esto Boston <esto.boston@techork.com>
2022-06-22 13:30:16 +0600

    feat: added new socket cmd reloadSchiftScheduleRequest (P2BPT-1078)

--- frontend/web/js/websocket-commands.js
+++ frontend/web/js/websocket-commands.js
@@ -593,6 +593,9 @@ function wsInitConnect(wsUrl, reconnectInterval, userId, onlineObj, ccNotificati
                             windowObjectReference.focus();
                         }
                     }
+                    if (obj.cmd === 'reloadShitScheduleRequest') {
+                       
+                    }
                 }
                 // onlineObj.find('i').removeClass('danger').removeClass('warning').addClass('success');
             } catch (error) {
