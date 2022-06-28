40fc9e1f123bdf57873fd8f7f4ba8caf1caabb4a
Lana Bates <lana.bates@techork.com>
2022-06-28 22:05:46 +0300

    TMS-629 feat:EmailParamsForm created

---
+++ src/entities/email/form/EmailParamsForm.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace src\entities\email\form;
+
+use yii\base\Model;
+use src\entities\email\EmailParams;
+
+class EmailParamsForm extends Model
+{
+    public $id;
+    public $templateType;
+    public $language;
+    public $priority;
+
+    public function __construct(array $config = [])
+    {
+        parent::__construct($config);
+    }
+
+    public static function fromArray(array $data)
+    {
+        $instance = new static();
+        $instance->setAttributes($data);
+        return $instance;
+    }
+
+    public static function fromModel(EmailParams $param, $config = [])
+    {
+        $instance = new static($config);
+        $instance->templateType = $param->ep_template_type_id;
+        $instance->language = $param->ep_language_id;
+        $instance->priority = $param->ep_priority;
+        $instance->id = $param->ep_id;
+
+        return $instance;
+    }
+
+    public static function replyFromModel(EmailParams $param, $config = [])
+    {
+        $instance = new static($config);
+        $instance->templateType = $param->ep_template_type_id;
+        $instance->language = $param->ep_language_id;
+        $instance->priority = $param->ep_priority;
+
+        return $instance;
+    }
+
+    public function rules(): array
+    {
+        return [
+            ['language', 'string', 'max' => 5],
+            [['priority','templateType','id'], 'integer'],
+        ];
+    }
+}
