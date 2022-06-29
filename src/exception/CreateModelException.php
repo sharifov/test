477aa70dbbd424adb0ffe1ed66701460325451b4
Lana Bates <lana.bates@techork.com>
2022-06-29 22:31:20 +0300

    TMS-629 feat:created CreateModelException class

---
+++ src/exception/CreateModelException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src\exceptions;
+
+class CreateModelException extends \RuntimeException
+{
+    protected $errors = [];
+
+    public function __construct(string $className = '', $errors = [])
+    {
+        $this->errors = $errors;
+        $message = sprintf('%s create error.', empty($className) ? 'Model' : $className);
+        parent::__construct($message);
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+}
