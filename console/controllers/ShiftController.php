a9453eb466132c4611a833bd7093123f397db518
Ronson Gordy <ronson.gordy@kiv.dev>
2022-07-04 18:00:07 +0600

    feat: added automated decline (P2BPT-953)

--- console/controllers/ShiftController.php
+++ console/controllers/ShiftController.php
@@ -3,8 +3,18 @@
 namespace console\controllers;
 
 use common\models\Setting;
+use DomainException;
+use modules\shiftSchedule\src\entities\shiftScheduleRequest\ShiftScheduleRequest;
+use modules\shiftSchedule\src\entities\userShiftSchedule\UserShiftSchedule;
+use modules\shiftSchedule\src\entities\userShiftSchedule\UserShiftScheduleQuery;
+use modules\shiftSchedule\src\forms\ScheduleDecisionForm;
+use modules\shiftSchedule\src\services\ShiftScheduleRequestService;
 use modules\shiftSchedule\src\services\UserShiftScheduleService;
+use src\helpers\app\AppHelper;
 use src\helpers\setting\SettingHelper;
+use src\repositories\NotFoundException;
+use Throwable;
+use Yii;
 use yii\console\Controller;
 use yii\helpers\Console;
 
@@ -33,5 +43,39 @@ class ShiftController extends Controller
             ' s] %g'), PHP_EOL;
         echo Console::renderColoredString('%g --- End : %w[' . date('Y-m-d H:i:s') . '] %g' .
             self::class . ':' . __FUNCTION__ . ' %n'), PHP_EOL;
+    }
+
+    public function actionFindAndDeclineShiftsWithPendingStatusIfWtOrWtrExists()
+    {
+        $shifts = UserShiftScheduleQuery::getPendingListWithIntersectionByWTAndWTR();
+
+        if (empty($shifts)) {
+            return false;
+        }
+
+        foreach ($shifts as $shift) {
+            try {
+                $ssr = ShiftScheduleRequest::findOne(['ssr_uss_id' => $shift->uss_id]);
+
+                if (empty($ssr)) {
+                    throw new NotFoundException("Not exist this Shift Schedule Request ({$shift->uss_id})");
+                }
+
+                $decisionFormModel = new ScheduleDecisionForm();
+                $decisionFormModel->status = UserShiftSchedule::STATUS_CANCELED;
+                $decisionFormModel->description = 'Declined by system';
+
+                $shift->uss_status_id = $decisionFormModel->status;
+                $shift->uss_description = $decisionFormModel->description;
+
+                if ($shift->save()) {
+                    ShiftScheduleRequestService::saveDecision($ssr, $decisionFormModel, $shift->user);
+                }
+            } catch (DomainException $e) {
+                Yii::error(AppHelper::throwableLog($e), 'ShiftController:actionFindAndDeclineShiftsWithPendingStatusIfWtOrWtrExists:DomainException');
+            } catch (Throwable $e) {
+                Yii::error(AppHelper::throwableLog($e), 'ShiftController:actionFindAndDeclineShiftsWithPendingStatusIfWtOrWtrExists:Throwable');
+            }
+        }
     }
 }
