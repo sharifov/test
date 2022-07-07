5e4e0efa4172e70f7daa82bd97ac447f4afd47e0
Lana Bates <lana.bates@techork.com>
2022-07-07 22:00:54 +0300

    TMS-748 feat:EmailServiceInterface added declaration of createFromCase

--- src/services/email/EmailServiceInterface.php
+++ src/services/email/EmailServiceInterface.php
@@ -4,10 +4,14 @@ namespace src\services\email;
 
 use frontend\models\LeadPreviewEmailForm;
 use common\models\Lead;
+use src\entities\cases\Cases;
+use frontend\models\CasePreviewEmailForm;
 
 interface EmailServiceInterface
 {
     public function createFromLead(LeadPreviewEmailForm $previewEmailForm, Lead $lead, array $attachments = []);
+
+    public function createFromCase(CasePreviewEmailForm $previewEmailForm, Cases $case, array $attachments = []);
 
     public function sendMail($email, array $data = []);
 }
