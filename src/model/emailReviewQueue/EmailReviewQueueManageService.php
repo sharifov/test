185fbabd4db2877a4f09a9e2fb53ac5e8612796e
Lana Bates <lana.bates@techork.com>
2022-07-10 22:54:41 +0300

    TMS-758 feat:EmailReviewQueueManageService updated for using Email normalized

--- src/model/emailReviewQueue/EmailReviewQueueManageService.php
+++ src/model/emailReviewQueue/EmailReviewQueueManageService.php
@@ -5,6 +5,7 @@ namespace src\model\emailReviewQueue;
 use common\models\Email;
 use src\model\emailReviewQueue\entity\EmailReviewQueue;
 use src\model\emailReviewQueue\entity\EmailReviewQueueRepository;
+use \src\entities\email\Email as EmailNorm;
 
 /**
  * @property EmailReviewQueueRepository $repository
@@ -24,7 +25,8 @@ class EmailReviewQueueManageService
             $email->e_id,
             $email->e_project_id,
             $departmentId,
-            $email->e_created_user_id
+            $email->e_created_user_id,
+            ($email instanceof EmailNorm)
         );
         $model->statusToPending();
         $this->repository->save($model);
