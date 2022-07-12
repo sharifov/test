e468b43cb3c273a77a538df8cb2686cd94227b1b
Lana Bates <lana.bates@techork.com>
2022-07-12 23:32:51 +0300

    TMS-758 feat:email-normalized update form added contact id hidden fields

--- frontend/views/email-normalized/_new_mail.php
+++ frontend/views/email-normalized/_new_mail.php
@@ -41,6 +41,9 @@ use common\components\widgets\BaseForm;
         <?= $form->field($emailForm, 'type')->simpleHidden(); ?>
         <?= $form->field($emailForm, 'status')->simpleHidden(); ?>
 
+		<?= $form->field($emailForm->contacts['from'], '[from]id')->simpleHidden(); ?>
+		<?= $form->field($emailForm->contacts['to'], '[to]id')->simpleHidden(); ?>
+
         <div class="row">
             <div class="col-md-4">
                 <?php if ($action) :?>
