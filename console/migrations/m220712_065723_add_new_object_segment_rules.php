217af9234ffd1e7444e3998a15fcdd18a812628d
Alex Grub <alex.grub@checkork.com>
2022-07-12 10:33:09 +0300

    feat: create object segment date for client return (P2BPT-1199)

---
+++ console/migrations/m220712_065723_add_new_object_segment_rules.php
@@ -0,0 +1,47 @@
+<?php
+
+use modules\objectSegment\src\contracts\ObjectSegmentListContract;
+use yii\db\Migration;
+
+/**
+ * Class m220712_065723_add_new_object_segment_rules
+ */
+class m220712_065723_add_new_object_segment_rules extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $objectSegmentList = (new \yii\db\Query())->from('{{%object_segment_list}}')->select(['osl_id'])->where(['osl_key' => ObjectSegmentListContract::OBJECT_SEGMENT_LIST_KEY_CLIENT_RETURN])->limit(1)->one();
+        if ($objectSegmentList) {
+            $this->insert('{{%object_segment_rules}}', [
+                'osr_osl_id' => $objectSegmentList['osl_id'],
+                'osr_title' => 'Return Customer',
+                'osr_rule_condition' => '(r.sub.count_sold_leads >= 1)',
+                'osr_rule_condition_json' => '{"condition":"AND","rules":[{"id":"clientcount_sold_leads","field":"count_sold_leads","type":"integer","input":"number","operator":"greater_or_equal","value":1}],"valid":true}',
+                'osr_created_dt' => date('Y-m-d H:i:s'),
+                'osr_enabled' => 1
+            ]);
+            $this->insert('{{%object_segment_rules}}', [
+                'osr_osl_id' => $objectSegmentList['osl_id'],
+                'osr_title' => 'Diamond Return Customer',
+                'osr_rule_condition' => '(r.sub.count_sold_leads >= 10)',
+                'osr_rule_condition_json' => '{"condition":"AND","rules":[{"id":"clientcount_sold_leads","field":"count_sold_leads","type":"integer","input":"number","operator":"greater_or_equal","value":10}],"valid":true}',
+                'osr_created_dt' => date('Y-m-d H:i:s'),
+                'osr_enabled' => 1
+            ]);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $objectSegmentList = (new \yii\db\Query())->from('{{%object_segment_list}}')->select(['osl_id'])->where(['osl_key' => ObjectSegmentListContract::OBJECT_SEGMENT_LIST_KEY_CLIENT_RETURN])->limit(1)->one();
+        if ($objectSegmentList) {
+            $this->delete('{{%object_segment_rules}}', ['osr_osl_id' => $objectSegmentList['osl_id']]);
+        }
+    }
+}
--- modules/objectSegment/src/contracts/ObjectSegmentListContract.php
+++ modules/objectSegment/src/contracts/ObjectSegmentListContract.php
@@ -5,8 +5,10 @@ namespace modules\objectSegment\src\contracts;
 interface ObjectSegmentListContract
 {
     public const OBJECT_SEGMENT_LIST_KEY_LEAD_TYPE_BUSINESS = 'lead_business_type';
+    public const OBJECT_SEGMENT_LIST_KEY_CLIENT_RETURN = 'client_return';
 
     public const KEYS_LIST = [
-        self::OBJECT_SEGMENT_LIST_KEY_LEAD_TYPE_BUSINESS
+        self::OBJECT_SEGMENT_LIST_KEY_LEAD_TYPE_BUSINESS,
+        self::OBJECT_SEGMENT_LIST_KEY_CLIENT_RETURN
     ];
 }
