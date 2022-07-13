fca3f4a5d38c89d2ebc2e6088c4dd2e471038a49
Lana Bates <lana.bates@techork.com>
2022-07-13 15:38:57 +0300

    TMS-758 feat:EmailPreviewFromInterface added properties

--- frontend/models/EmailPreviewFromInterface.php
+++ frontend/models/EmailPreviewFromInterface.php
@@ -4,5 +4,14 @@ namespace frontend\models;
 
 interface EmailPreviewFromInterface
 {
+    public $e_email_from;
+    public $e_email_to;
+    public $e_email_from_name;
+    public $e_email_to_name;
+    public $e_email_subject;
+    public $e_email_message;
+    public $e_email_tpl_id;
+    public $e_language_id;
+
     public function countLettersInEmailMessage(): int;
 }
