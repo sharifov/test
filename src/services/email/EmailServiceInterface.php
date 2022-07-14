d2caaee4833afadb853dc2b1bdcd72787f07b95f
Lana Bates <lana.bates@techork.com>
2022-07-14 18:57:51 +0300

    TMS-793 ref:EmailServiceInterface createFrom param EmailPreviewFromInterface

--- src/services/email/EmailServiceInterface.php
+++ src/services/email/EmailServiceInterface.php
@@ -2,17 +2,16 @@
 
 namespace src\services\email;
 
-use frontend\models\LeadPreviewEmailForm;
 use common\models\Lead;
 use src\entities\cases\Cases;
-use frontend\models\CasePreviewEmailForm;
 use src\forms\emailReviewQueue\EmailReviewQueueForm;
+use frontend\models\EmailPreviewFromInterface;
 
 interface EmailServiceInterface
 {
-    public function createFromLead(LeadPreviewEmailForm $previewEmailForm, Lead $lead, array $attachments = []);
+    public function createFromLead(EmailPreviewFromInterface $previewEmailForm, Lead $lead, array $attachments = []);
 
-    public function createFromCase(CasePreviewEmailForm $previewEmailForm, Cases $case, array $attachments = []);
+    public function createFromCase(EmailPreviewFromInterface $previewEmailForm, Cases $case, array $attachments = []);
 
     public function sendMail($email, array $data = []);
 
