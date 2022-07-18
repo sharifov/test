e589373eeb8ea596521a0a4677c6cfeb3c3729fc
Alex Grub <alex.grub@checkork.com>
2022-07-18 10:53:31 +0300

    feat: create console script for indicate all clients return type (P2BPT-1201)

--- console/controllers/ClientController.php
+++ console/controllers/ClientController.php
@@ -7,6 +7,8 @@ use modules\objectSegment\src\contracts\ObjectSegmentKeyContract;
 use modules\objectSegment\src\object\dto\ClientSegmentObjectDto;
 use src\helpers\app\AppHelper;
 use src\model\clientData\entity\ClientData;
+use src\model\clientDataKey\entity\ClientDataKeyDictionary;
+use src\model\clientDataKey\service\ClientDataKeyService;
 use yii\console\Controller;
 use yii\helpers\Console;
 
@@ -14,13 +16,19 @@ class ClientController extends Controller
 {
     public function actionClientReturnIndication($limit = 1000)
     {
+        \Yii::$app->log->targets['db-info']->exportInterval = 1;
+
         echo Console::renderColoredString('%g --- Start %w[' . date('Y-m-d H:i:s') . '] %g' .
             self::class . ':' . __FUNCTION__ . ' %n'), PHP_EOL;
 
         $timeStart = microtime(true);
 
+        $keyId = ClientDataKeyService::getIdByKeyCache(ClientDataKeyDictionary::CLIENT_RETURN);
+
         $clients = Client::find()
-            ->join('LEFT OUTER JOIN', ClientData::tableName(), 'cd_client_id = id')
+            ->join('LEFT OUTER JOIN', ClientData::tableName(), 'cd_client_id = id and cd_key_id = :key', [
+                'key' => $keyId
+            ])
             ->where(['cd_client_id' => null])
             ->orderBy(['id' => SORT_DESC])
             ->limit($limit)
@@ -29,12 +37,17 @@ class ClientController extends Controller
         $processed = 0;
         $countItems = count($clients);
 
-        \Yii::info('Script Started', 'info\console:ClientController:actionClientReturnIndication');
         if (!$countItems) {
             echo Console::renderColoredString('%g --- End: All types of client returns detected %w') . PHP_EOL;
             \Yii::info('All types of client returns detected', 'info\console:ClientController:actionClientReturnIndication');
             return;
         }
+
+        \Yii::info([
+            'message' => 'Script Started',
+            'processed' =>  $processed,
+            'countItems' => $countItems,
+        ], 'info\console:ClientController:actionClientReturnIndication');
 
         Console::startProgress($processed, $countItems, 'Counting objects: ', false);
 
@@ -49,13 +62,15 @@ class ClientController extends Controller
             Console::updateProgress($processed, $countItems);
         }
         Console::endProgress(false);
-        \Yii::info([
-            'processed' =>  $processed,
-            'countItems' => $countItems
-        ], 'info\console:ClientController:actionClientReturnIndication');
 
         $timeEnd = microtime(true);
         $time = number_format(round($timeEnd - $timeStart, 2), 2);
         echo Console::renderColoredString('%g --- Execute Time: %w[' . $time . ' s] %g') . PHP_EOL;
+
+        \Yii::info([
+            'processed' =>  $processed,
+            'countItems' => $countItems,
+            'time' => $time
+        ], 'info\console:ClientController:actionClientReturnIndication');
     }
 }
