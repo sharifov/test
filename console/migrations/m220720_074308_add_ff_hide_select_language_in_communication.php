f104306b8b671204f7a2dbf6b494a0022779506f
Esto Boston <esto.boston@techork.com>
2022-07-21 15:00:37 +0600

    feat: revert

--- console/migrations/m220720_074308_add_ff_hide_select_language_in_communication.php
+++ console/migrations/m220720_074308_add_ff_hide_select_language_in_communication.php
@@ -7,9 +7,9 @@ use src\helpers\app\AppHelper;
 use yii\db\Migration;
 
 /**
- * Class m220720_074308_add_ff_hide_select_language_in_communication
+ * Class m220720_105943_remove_ff_hide_select_language_in_communication
  *
- * @description correction in m220720_105943_remove_ff_hide_select_language_in_communication
+ * @description correction in m220720_074308_add_ff_hide_select_language_in_communication
  */
 class m220720_074308_add_ff_hide_select_language_in_communication extends Migration
 {
