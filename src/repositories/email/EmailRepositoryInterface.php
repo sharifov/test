35f8eea3d1f6302aec52eddffbb09e30853b4129
Lana Bates <lana.bates@techork.com>
2022-07-22 15:40:56 +0300

    TMS-806 feat:EmailRepositoryInterface created

---
+++ src/repositories/email/EmailRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace src\repositories\email;
+
+use src\entities\email\Email;
+use src\entities\email\EmailInterface;
+
+interface EmailRepositoryInterface
+{
+    public function find(int $id);
+
+    public function save(EmailInterface $email);
+
+    public function read(EmailInterface $email): void;
+
+    public function delete(EmailInterface $email): int;
+
+    public function deleteByIds($id): array;
+}
