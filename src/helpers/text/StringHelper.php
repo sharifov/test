0cc5b9d7ff155721cf33489bcfccd7fb4615d8b2
Lana Bates <lana.bates@techork.com>
2022-07-27 21:00:37 +0300

    TMS-472 feat:stripHtmlTags method moved to helper class

---
+++ src/helpers/text/StringHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace src\helpers\text;
+
+class StringHelper extends \yii\helpers\StringHelper
+{
+    /**
+     * @param $text
+     * @return mixed
+     */
+    public static function stripHtmlTags($text)
+    {
+        $text = preg_replace(
+            [
+                // Remove invisible content
+                '@<head[^>]*?>.*?</head>@siu',
+                '@<style[^>]*?>.*?</style>@siu',
+                '@<script[^>]*?.*?</script>@siu',
+                '@<object[^>]*?.*?</object>@siu',
+                '@<embed[^>]*?.*?</embed>@siu',
+                '@<applet[^>]*?.*?</applet>@siu',
+                '@<noframes[^>]*?.*?</noframes>@siu',
+                '@<noscript[^>]*?.*?</noscript>@siu',
+                '@<noembed[^>]*?.*?</noembed>@siu',
+                // Add line breaks before and after blocks
+                '@</?((address)|(blockquote)|(center)|(del))@iu',
+                '@</?((div)|(h[1-9])|(ins)|(isindex)|(p)|(pre))@iu',
+                '@</?((dir)|(dl)|(dt)|(dd)|(li)|(menu)|(ol)|(ul))@iu',
+                '@</?((table)|(th)|(td)|(caption))@iu',
+                '@</?((form)|(button)|(fieldset)|(legend)|(input))@iu',
+                '@</?((label)|(select)|(optgroup)|(option)|(textarea))@iu',
+                '@</?((frameset)|(frame)|(iframe))@iu',
+            ],
+            [
+                ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+                "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0",
+                "\n\$0", "\n\$0",
+            ],
+            $text
+        );
+
+        $text = strip_tags($text);
+        $text = preg_replace('!\s+!', ' ', $text);
+
+        return $text;
+    }
+}
