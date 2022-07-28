4f345d321b6410582d9cdf921567db9330f67387
skincaid <san.kincaid@techork.com>
2022-07-28 13:39:07 +0600

    feat: added temporary log to BEQ 2(P2BPT-1291)

--- common/components/jobs/CallOutEndedJob.php
+++ common/components/jobs/CallOutEndedJob.php
@@ -203,14 +203,14 @@ class CallOutEndedJob extends BaseJob implements JobInterface
                 if (!$lead->isBusinessType()) {
                     return;
                 }
-                \Yii::info($call->c_call_status . ' Call Status ' . $lead->id . ' Lead Id', 'BEQCal');
+                \Yii::info($call->c_call_status . ' Call Status ' . $lead->id . ' Lead Id', 'info\BEQCal');
                 if ($call->c_call_duration >= SettingHelper::getUserPrickedCallDuration() && $call->isStatusCompleted()) {
                     LeadBusinessExtraQueueService::addLeadBusinessExtraQueueRemoverJob($lead->id, LeadBusinessExtraQueueLogStatus::REASON_CALL);
                 } elseif (($call->isDeclined() || $call->isStatusNoAnswer() || $call->isStatusBusy()) && LeadBusinessExtraQueueLogQuery::isLeadWasInBusinessExtraQueue($lead->id)) {
-                    \Yii::info('All Correct', 'BEQCal');
+                    \Yii::info('All Correct', 'info\BEQCal');
                     LeadBusinessExtraQueueService::addLeadBusinessExtraQueueRemoverJob($lead->id, LeadBusinessExtraQueueLogStatus::REASON_FAILED_CALL);
                 } else {
-                    \Yii::info(LeadBusinessExtraQueueLogQuery::isLeadWasInBusinessExtraQueue($lead->id) . ' Incorrect', 'BEQCal');
+                    \Yii::info(LeadBusinessExtraQueueLogQuery::isLeadWasInBusinessExtraQueue($lead->id) . ' Incorrect', 'info\BEQCal');
                 }
                 return;
             }
