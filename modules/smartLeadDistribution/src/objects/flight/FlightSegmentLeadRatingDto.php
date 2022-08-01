ea8cce9c11fbc3913fa5fc2af56d4dcbfa50807a
Ronson Gordy <ronson.gordy@kiv.dev>
2022-08-01 12:46:40 +0600

    feat: changed key from name to code (P2BPT-1349)

--- modules/smartLeadDistribution/src/objects/flight/FlightSegmentLeadRatingDto.php
+++ modules/smartLeadDistribution/src/objects/flight/FlightSegmentLeadRatingDto.php
@@ -16,14 +16,14 @@ class FlightSegmentLeadRatingDto
         /** @var LeadFlightSegment $ffs */
         $ffs = $lead->firstFlightSegment;
         $airportOrigin = Airports::findByIata($ffs->origin);
-        $airportDestination = Airports::findByIata($ffs->origin);
+        $airportDestination = Airports::findByIata($ffs->destination);
 
         if ($airportOrigin !== null) {
-            $this->origin_country = $airportOrigin->country;
+            $this->origin_country = $airportOrigin->a_country_code;
         }
 
         if ($airportDestination !== null) {
-            $this->destination_country = $airportDestination->country;
+            $this->destination_country = $airportDestination->a_country_code;
         }
     }
 }
