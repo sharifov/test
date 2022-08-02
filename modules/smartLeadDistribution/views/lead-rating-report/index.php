2011f72b803f0a088dceb7471137179a03a90a82
Ronson Gordy <ronson.gordy@kiv.dev>
2022-08-02 11:44:04 +0600

    feat: moved pjax block to up (P2BPT-1350)

--- modules/smartLeadDistribution/views/lead-rating-report/index.php
+++ modules/smartLeadDistribution/views/lead-rating-report/index.php
@@ -20,60 +20,59 @@ $this->params['breadcrumbs'][] = $this->title;
 $total = 0;
 
 ?>
+<?php Pjax::begin(['timeout' => 5000, 'scrollTo' => 0]); ?>
+
 <?= $this->render('partial/_search-summary', ['model' => $searchModel]); ?>
 
 <h1><?=\yii\helpers\Html::encode($this->title)?></h1>
 
 <div class="lead-business-inbox">
-
-    <?php Pjax::begin(['timeout' => 5000, 'scrollTo' => 0]); ?>
-
-        <?php if (!empty($searchModel->ratingCategoryIds) && !empty($searchModel->leadStatusIds)) : ?>
-            <table class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th rowspan="2" class="text-center align-middle">
-                            Category
+    <?php if (!empty($searchModel->ratingCategoryIds) && !empty($searchModel->leadStatusIds)) : ?>
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th rowspan="2" class="text-center align-middle">
+                        Category
+                    </th>
+                    <th colspan="<?= count($searchModel->leadStatusIds) ?>" class="text-center font-weight-bold">
+                        Status
+                    </th>
+                </tr>
+                <tr>
+                    <?php foreach ($searchModel->leadStatusIds as $leadStatusId) : ?>
+                        <th class="text-center">
+                            <?= Lead::STATUS_LIST[$leadStatusId] ?>
                         </th>
-                        <th colspan="<?= count($searchModel->leadStatusIds) ?>" class="text-center font-weight-bold">
-                            Status
-                        </th>
-                    </tr>
+                    <?php endforeach; ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($searchModel->ratingCategoryIds as $category) : ?>
                     <tr>
-                        <?php foreach ($searchModel->leadStatusIds as $leadStatusId) : ?>
-                            <th class="text-center">
-                                <?= Lead::STATUS_LIST[$leadStatusId] ?>
-                            </th>
+                        <td><?= SmartLeadDistribution::CATEGORY_LIST[$category] ?></td>
+                        <?php foreach ($searchModel->leadStatusIds as $status) : ?>
+                            <td class="text-center">
+                                <?php $leads = 0 ?>
+                                <?php /** @var array{status: int, leads_amount: int, category: int} $reportData */ ?>
+                                <?php foreach ($reportDataList as $reportData) : ?>
+                                    <?php if ((int)$reportData['category'] === (int)$category && (int)$reportData['status'] === (int)$status) : ?>
+                                        <?php $leads = $reportData['leads_amount'] ?>
+                                        <?php $total += $leads; ?>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                                <?= $leads ?>
+                            </td>
                         <?php endforeach; ?>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($searchModel->ratingCategoryIds as $category) : ?>
-                        <tr>
-                            <td><?= SmartLeadDistribution::CATEGORY_LIST[$category] ?></td>
-                            <?php foreach ($searchModel->leadStatusIds as $status) : ?>
-                                <td class="text-center">
-                                    <?php $leads = 0 ?>
-                                    <?php /** @var array{status: int, leads_amount: int, category: int} $reportData */ ?>
-                                    <?php foreach ($reportDataList as $reportData) : ?>
-                                        <?php if ((int)$reportData['category'] === (int)$category && (int)$reportData['status'] === (int)$status) : ?>
-                                            <?php $leads = $reportData['leads_amount'] ?>
-                                            <?php $total += $leads; ?>
-                                        <?php endif; ?>
-                                    <?php endforeach; ?>
-                                    <?= $leads ?>
-                                </td>
-                            <?php endforeach; ?>
-                        </tr>
-                    <?php endforeach; ?>
-                    <tr>
-                        <td colspan="<?= count($searchModel->leadStatusIds) ?>" class="text-right font-weight-bold">
-                            Total
-                        </td>
-                        <td class="font-weight-bold text-center"><?= $total ?></td>
-                    </tr>
-                </tbody>
-            </table>
-        <?php endif; ?>
-    <?php Pjax::end(); ?>
+                <?php endforeach; ?>
+                <tr>
+                    <td colspan="<?= count($searchModel->leadStatusIds) ?>" class="text-right font-weight-bold">
+                        Total
+                    </td>
+                    <td class="font-weight-bold text-center"><?= $total ?></td>
+                </tr>
+            </tbody>
+        </table>
+    <?php endif; ?>
 </div>
+<?php Pjax::end(); ?>
