f5d8d4a9a249aede180b0937b55033f3748349d5
Serge Renshou <serge.renshou@techork.com>
2022-08-03 16:28:21 +0300

    refactored rds template for dev env

--- devops/terraform/dev/pgsql-rds.tf
+++ devops/terraform/dev/pgsql-rds.tf
@@ -1,41 +1,52 @@
 # PostgreSQL RDS Instance
 module "pgsql" {
-  source     = "terraform-aws-modules/rds/aws"
-  version    = "~> 2.0"
-  identifier = "pgsql-${var.PROJECT}-${var.ENV}"
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 2.0"
 
-  engine            = "postgres"
-  engine_version    = "12.8"
-  instance_class    = var.PGSQL_RDS_INSTANCE_TYPE
-  allocated_storage = var.PGSQL_RDS_VOLUME_SIZE
-  storage_encrypted = true
-  multi_az          = false
+  identifier            = "pgsql-${var.PROJECT}-${var.ENV}"
+  engine                = "postgres"
+  engine_version        = "12.8"
+  instance_class        = var.PGSQL_RDS_INSTANCE_TYPE
+  allocated_storage     = var.PGSQL_RDS_VOLUME_SIZE
+  max_allocated_storage = var.PGSQL_RDS_VOLUME_MAX
+  storage_encrypted     = true
+  port                  = "5432"
 
-  name     = var.PGSQL_RDS_DATABASE
-  username = var.PGSQL_RDS_USERNAME
-  password = var.PGSQL_RDS_PASSWORD
-  port     = 5432
+  name                   = var.PGSQL_RDS_DATABASE
+  username               = var.PGSQL_RDS_USERNAME
+  password               = var.PGSQL_RDS_PASSWORD
+  create_random_password = false
 
-  vpc_security_group_ids  = [aws_security_group.pgsql.id]
-  maintenance_window      = "Mon:00:00-Mon:03:00"
-  backup_window           = "03:00-06:00"
-  backup_retention_period = 0
+  multi_az                            = false
+  vpc_security_group_ids              = [aws_security_group.pgsql.id]
+  iam_database_authentication_enabled = true
+
+  maintenance_window     = "Mon:00:00-Mon:03:00"
+  backup_window          = "03:00-06:00"
+  monitoring_interval    = "30"
+  monitoring_role_name   = "pgsql-${var.PROJECT}-${var.ENV}"
+  create_monitoring_role = true
+
+  create_db_subnet_group  = true
+  subnet_ids              = var.PRIVATE_SUBNETS
+  family                  = "postgres12"
+  major_engine_version    = "12"
+  deletion_protection     = false
+  skip_final_snapshot     = true
+  backup_retention_period = var.IS_PRODUCTION ? 14 : 2
+
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  #create_cloudwatch_log_group           = true
+  #performance_insights_enabled          = true
+  performance_insights_retention_period = 7
 
   tags = {
-    Terraform   = "true"
-    Environment = var.ENV
     Project     = var.PROJECT
+    Environment = var.ENV
     Ns          = var.NAMESPACE
     Domain      = var.DOMAIN
   }
 
-  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
-  subnet_ids                      = var.PRIVATE_SUBNETS
-  family                          = "postgres12"
-  major_engine_version            = "12"
-  final_snapshot_identifier       = "pgsql-${var.PROJECT}-${var.ENV}"
-  deletion_protection             = false
-  skip_final_snapshot             = true
 }
 
 
