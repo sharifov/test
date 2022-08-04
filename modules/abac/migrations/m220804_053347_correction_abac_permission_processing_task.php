5c75af5ac9d96b37686385bdbeafe95bb96c970c
andrew.snake <andrew.snake@techork.com>
2022-08-04 11:14:18 +0300

    feat: added correction migration (P2BPT-1381)

--- modules/abac/migrations/m220804_053347_correction_abac_permission_processing_task.php
+++ modules/abac/migrations/m220804_053347_correction_abac_permission_processing_task.php
@@ -2,18 +2,45 @@
 
 namespace modules\abac\migrations;
 
+use modules\abac\src\entities\AbacPolicy;
+use src\helpers\app\AppHelper;
 use yii\db\Migration;
+use yii\db\Query;
 
 /**
  * Class m220804_053347_correction_abac_permission_processing_task
  */
 class m220804_053347_correction_abac_permission_processing_task extends Migration
 {
+    private const AP_OBJECT = 'lead/lead/task_list/processing_task';
+    private const AP_ACTION = '(access)';
+
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
+        try {
+            $abacPolicies = AbacPolicy::find()
+                ->where(['ap_object' => self::AP_OBJECT])
+                ->andWhere(['ap_action' => self::AP_ACTION])
+                ->andWhere(['ap_enabled' => 1])
+                ->orderBy(['ap_id' => SORT_ASC])
+                ->all()
+            ;
+
+            if (count($abacPolicies) > 1) {
+                array_pop($abacPolicies);
+                foreach ($abacPolicies as $abacPolicy) {
+                    AbacPolicy::updateAll(
+                        ['ap_enabled' => 0],
+                        ['ap_id' => $abacPolicy->ap_id]
+                    );
+                }
+            }
+        } catch (\Throwable $throwable) {
+            \Yii::error(AppHelper::throwableLog($throwable), 'm220804_053347_correction_abac_permission_processing_task:safeUp:Throwable');
+        }
     }
 
     /**
@@ -22,7 +49,5 @@ class m220804_053347_correction_abac_permission_processing_task extends Migratio
     public function safeDown()
     {
         echo "m220804_053347_correction_abac_permission_processing_task cannot be reverted.\n";
-
-        return false;
     }
 }
