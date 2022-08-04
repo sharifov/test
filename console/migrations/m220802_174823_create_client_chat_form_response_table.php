bb74ae6d5c0008e776c3faed233aa70e74ed2ac4
Deni Iverson <deni.iverson@kiv.dev>
2022-08-04 16:11:19 +0600

    feature/CLCH-1435 remove [[]] from migration

--- console/migrations/m220802_174823_create_client_chat_form_response_table.php
+++ console/migrations/m220802_174823_create_client_chat_form_response_table.php
@@ -18,17 +18,17 @@ class m220802_174823_create_client_chat_form_response_table extends Migration
         }
 
         $this->createTable('{{%client_chat_form_response}}', [
-            '[[ccfr_id]]' => $this->primaryKey(),
-            '[[ccfr_uid]]' => $this->string(64)->notNull(),
-            '[[ccfr_form_id]]' => $this->integer()->notNull(),
-            '[[ccfr_client_chat_id]]' => $this->integer()->notNull(),
-            '[[ccfr_value]]' => $this->string(),
-            '[[ccfr_rc_created_dt]]' => $this->timestamp(),
-            '[[ccfr_created_dt]]' => $this->timestamp()
+            'ccfr_id' => $this->primaryKey(),
+            'ccfr_uid' => $this->string(64)->notNull(),
+            'ccfr_form_id' => $this->integer()->notNull(),
+            'ccfr_client_chat_id' => $this->integer()->notNull(),
+            'ccfr_value' => $this->string(),
+            'ccfr_rc_created_dt' => $this->timestamp(),
+            'ccfr_created_dt' => $this->timestamp()
         ], $tableOptions);
 
-        $this->addForeignKey('FK-client_chat_form_response-ccfr_client_chat_id', '{{%client_chat_form_response}}', '[[ccfr_client_chat_id]]', '{{%client_chat}}', '[[cch_id]]');
-        $this->addForeignKey('FK-client_chat_form_response-ccfr_form_id', '{{%client_chat_form_response}}', '[[ccfr_form_id]]', '{{%client_chat_form}}', '[[ccf_id]]');
+        $this->addForeignKey('FK-client_chat_form_response-ccfr_client_chat_id', '{{%client_chat_form_response}}', 'ccfr_client_chat_id', '{{%client_chat}}', 'cch_id');
+        $this->addForeignKey('FK-client_chat_form_response-ccfr_form_id', '{{%client_chat_form_response}}', 'ccfr_form_id', '{{%client_chat_form}}', 'ccf_id');
     }
 
     /**
