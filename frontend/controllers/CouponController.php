0dad437b422c0c8ad534381fd75099e98d870824
Den Dexo <den.dexo@techork.com>
2022-08-05 17:19:36 +0300

    AOP-983 bugfix: removed variable does not exist "$attachments"

--- frontend/controllers/CouponController.php
+++ frontend/controllers/CouponController.php
@@ -264,7 +264,7 @@ class CouponController extends FController
 
                     $mail = $this->emailService->createFromCase($previewEmailForm, $case);
                     $previewEmailForm->is_send = true;
-                    $this->emailService->sendMail($mail, $attachments);
+                    $this->emailService->sendMail($mail);
                     Yii::$app->session->setFlash('send-success', 'Success: <strong>Email Message</strong> is sent to <strong>' . $mail->getEmailTo() . '</strong>');
 
                     $selectedCoupons = json_decode($previewEmailForm->coupon_list, true);
