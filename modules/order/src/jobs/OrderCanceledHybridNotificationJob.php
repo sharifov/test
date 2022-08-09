3776b41184013577de396c8dbe228b6a5515f66d
Ronson Gordy <ronson.gordy@kiv.dev>
2022-08-09 15:12:37 +0600

    feat: added retry methods for hybrid notification job (P2BPT-1430)

--- modules/order/src/jobs/OrderCanceledHybridNotificationJob.php
+++ modules/order/src/jobs/OrderCanceledHybridNotificationJob.php
@@ -12,7 +12,7 @@ use yii\queue\RetryableJobInterface;
  *
  * @property $orderId
  */
-class OrderCanceledHybridNotificationJob implements JobInterface
+class OrderCanceledHybridNotificationJob implements JobInterface, RetryableJobInterface
 {
     public $orderId;
 
@@ -40,22 +40,25 @@ class OrderCanceledHybridNotificationJob implements JobInterface
                 'status' => OrderStatus::CANCELED,
                 'error' => $e->getMessage(),
             ], 'OrderCanceledHybridNotificationJob');
+
+            throw new \Exception('Retry job');
         }
     }
 
-//    public function getTtr(): int
-//    {
-//        return 1 * 60;
-//    }
-//
-//    public function canRetry($attempt, $error): bool
-//    {
-//        \Yii::error([
-//            'attempt' => $attempt,
-//            'message' => 'Order canceled hybrid notification error',
-//            'error' => $error->getMessage(),
-//            'orderId' => $this->orderId,
-//        ], 'OrderCanceledHybridNotificationJob');
-//        return !($attempt > 5);
-//    }
+    public function getTtr(): int
+    {
+        return 2 * 60;
+    }
+
+    public function canRetry($attempt, $error): bool
+    {
+        \Yii::error([
+            'attempt' => $attempt,
+            'message' => 'Order canceled hybrid notification error',
+            'error' => $error->getMessage(),
+            'orderId' => $this->orderId,
+        ], 'OrderCanceledHybridNotificationJob');
+
+        return $attempt <= 15;
+    }
 }
