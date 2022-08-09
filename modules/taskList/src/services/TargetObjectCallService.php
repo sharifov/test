44fbe49fb76dc7c3258b37aa1ec715679b90246d
Esto Boston <esto.boston@techork.com>
2022-08-09 17:47:51 +0600

    feat: callLog in rules (P2BPT-1411)

--- modules/taskList/src/services/TargetObjectCallService.php
+++ modules/taskList/src/services/TargetObjectCallService.php
@@ -3,6 +3,10 @@
 namespace modules\taskList\src\services;
 
 use common\models\Call;
+use src\model\callLog\entity\callLog\CallLog;
+use src\model\callLog\entity\callLog\CallLogStatus;
+use src\model\callLog\entity\callLog\CallLogType;
+use src\model\callLog\entity\callLogLead\CallLogLead;
 
 class TargetObjectCallService
 {
@@ -16,11 +20,11 @@ class TargetObjectCallService
     private int $targetObjectCallCompleted = 0;
 
     private array $leadCallAttemptsStatuses = [
-        Call::STATUS_NO_ANSWER,
-        Call::STATUS_BUSY,
-        Call::STATUS_FAILED,
-        Call::STATUS_DECLINED,
-        Call::STATUS_CANCELED,
+        CallLogStatus::NOT_ANSWERED,
+        CallLogStatus::BUSY,
+        CallLogStatus::FAILED,
+        CallLogStatus::DECLINED,
+        CallLogStatus::CANCELED,
     ];
 
     public function __construct(Call $call, ?string $userTaskStartDT = null, ?string $userTaskEndDT = null)
@@ -43,18 +47,22 @@ class TargetObjectCallService
 
     private function countLeadCallProcessing(int $leadId): int
     {
-        $query = Call::find()
-            ->where(['c_lead_id' => $leadId])
-            ->andWhere(['c_call_type_id' => Call::CALL_TYPE_OUT])
-            ->andWhere(['c_status_id' => Call::STATUS_COMPLETED])
-            ->andWhere(['>=', 'c_call_duration', $this->getCallCompletedDuration()])
+        $query = CallLogLead::find()
+            ->innerJoin(
+                CallLog::tableName(),
+                CallLog::tableName() . '.cl_id = ' . CallLogLead::tableName() . '.cll_cl_id'
+            )
+            ->where(['cll_lead_id' => $leadId])
+            ->andWhere(['cl_type_id' =>  CallLogType::OUT])
+            ->andWhere(['cl_status_id' => CallLogStatus::COMPLETE])
+            ->andWhere(['>=', 'cl_duration', $this->getCallCompletedDuration()])
         ;
 
         if ($this->userTaskStartDT) {
-            $query->andWhere(['>=', 'c_created_dt', $this->userTaskStartDT]);
+            $query->andWhere(['>=', 'cl_call_created_dt', $this->userTaskStartDT]);
         }
         if ($this->userTaskEndDT) {
-            $query->andWhere(['<=', 'c_created_dt', $this->userTaskEndDT]);
+            $query->andWhere(['<=', 'cl_call_created_dt', $this->userTaskEndDT]);
         }
 
         return (int) $query->count();
@@ -62,17 +70,21 @@ class TargetObjectCallService
 
     private function countLeadCallAttempts(int $leadId): int
     {
-        $query = Call::find()
-            ->where(['c_lead_id' => $leadId])
-            ->andWhere(['c_call_type_id' => Call::CALL_TYPE_OUT])
-            ->andWhere(['IN', 'c_status_id', $this->getLeadCallAttemptsStatuses()])
+        $query = CallLogLead::find()
+            ->innerJoin(
+                CallLog::tableName(),
+                CallLog::tableName() . '.cl_id = ' . CallLogLead::tableName() . '.cll_cl_id'
+            )
+            ->where(['cll_lead_id' => $leadId])
+            ->andWhere(['cl_type_id' =>  CallLogType::OUT])
+            ->andWhere(['IN', 'cl_status_id' , $this->getLeadCallAttemptsStatuses()])
         ;
 
         if ($this->userTaskStartDT) {
-            $query->andWhere(['>=', 'c_created_dt', $this->userTaskStartDT]);
+            $query->andWhere(['>=', 'cl_call_created_dt', $this->userTaskStartDT]);
         }
         if ($this->userTaskEndDT) {
-            $query->andWhere(['<=', 'c_created_dt', $this->userTaskEndDT]);
+            $query->andWhere(['<=', 'cl_call_created_dt', $this->userTaskEndDT]);
         }
 
         return (int) $query->count();
