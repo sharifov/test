cf7224839fb5b3d90129d9f272dc7958ed9f40d4
Esto Boston <esto.boston@techork.com>
2022-08-09 15:17:57 +0600

    feat: change canceled status list (P2BPT-1422)

--- src/listeners/lead/LeadUserTaskCanceledListener.php
+++ src/listeners/lead/LeadUserTaskCanceledListener.php
@@ -9,7 +9,21 @@ use src\events\lead\LeadStatusChangedEvent;
 class LeadUserTaskCanceledListener
 {
     private const CANCELED_LEAD_STATUS = [
-        Lead::STATUS_TRASH
+        Lead::STATUS_PENDING,
+        Lead::STATUS_PROCESSING,
+        Lead::STATUS_REJECT,
+        Lead::STATUS_FOLLOW_UP,
+        Lead::STATUS_ON_HOLD,
+        Lead::STATUS_SOLD,
+        Lead::STATUS_TRASH,
+        Lead::STATUS_BOOKED,
+        Lead::STATUS_SNOOZE,
+        Lead::STATUS_BOOK_FAILED,
+        Lead::STATUS_ALTERNATIVE,
+        Lead::STATUS_NEW,
+        Lead::STATUS_EXTRA_QUEUE,
+        Lead::STATUS_CLOSED,
+        Lead::STATUS_BUSINESS_EXTRA_QUEUE,
     ];
 
     public function handle(LeadStatusChangedEvent $event)
