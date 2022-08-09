9a3470caad7425b283e6f5d129c40b3b784946be
Deni Iverson <deni.iverson@kiv.dev>
2022-08-09 18:11:04 +0600

    feature/CLCH-1443 add crud pages for client chat form response table

---
+++ frontend/views/client-chat-form-response-crud/view.php
@@ -0,0 +1,49 @@
+<?php
+
+use src\model\clientChatFormResponse\entity\ClientChatFormResponse;
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model src\model\clientChatFormResponse\entity\clientChatFormResponse */
+
+$this->title = $model->ccfr_id;
+$this->params['breadcrumbs'][] = ['label' => 'Client Chat Form Response', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="client-chat-form-response-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->ccfr_id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->ccfr_id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <div class="col-md-4">
+        <?= DetailView::widget([
+            'model' => $model,
+            'attributes' => [
+                'ccfr_id',
+                'ccfr_uid',
+                'ccfr_client_chat_id',
+                [
+                    'attribute' => 'ccfr_form_id',
+                    'value' => static function (ClientChatFormResponse $model) {
+                        return $model->clientChatForm ? $model->clientChatForm->ccf_name : '-';
+                    },
+                ],
+                'ccfr_value:ntext',
+                'ccfr_rc_created_dt:byUserDateTime',
+                'ccfr_created_dt:byUserDateTime',
+            ],
+        ]) ?>
+    </div>
+</div>
---
+++ src/model/clientChatFormResponse/entity/ClientChatFormResponseSearch.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace src\model\clientChatFormResponse\entity;
+
+use common\models\Employee;
+use yii\data\ActiveDataProvider;
+
+/**
+ * ClientChatFormResponseSearch represents the model behind the search form of `src\model\clientChatFormResponse\entity\ClientChatFormResponse`.
+ */
+class ClientChatFormResponseSearch extends ClientChatFormResponse
+{
+    public function rules(): array
+    {
+        return [
+            [['ccfr_id', 'ccfr_client_chat_id', 'ccfr_form_id'], 'integer'],
+            [['ccfr_value', 'ccfr_uid'], 'string'],
+            [['ccfr_created_dt', 'ccfr_rc_created_dt'], 'date', 'format' => 'php:Y-m-d'],
+        ];
+    }
+
+    public function search($params): ActiveDataProvider
+    {
+        $query = ClientChatFormResponse::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort' => ['defaultOrder' => ['ccfr_id' => SORT_DESC]],
+            'pagination' => [
+                'pageSize' => 30,
+            ],
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'ccfr_id' => $this->ccfr_id,
+            'ccfr_client_chat_id' => $this->ccfr_client_chat_id,
+            'ccfr_form_id' => $this->ccfr_form_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'ccfr_value', $this->ccfr_value]);
+        $query->andFilterWhere(['like', 'ccfr_uid', $this->ccfr_uid]);
+
+        if ($this->ccfr_rc_created_dt) {
+            $query->andFilterWhere(['>=', 'ccfr_rc_created_dt', Employee::convertTimeFromUserDtToUTC(strtotime($this->ccfr_rc_created_dt))])
+                ->andFilterWhere(['<=', 'ccfr_rc_created_dt', Employee::convertTimeFromUserDtToUTC(strtotime($this->ccfr_rc_created_dt) + 3600 * 24)]);
+        }
+        if ($this->ccfr_created_dt) {
+            $query->andFilterWhere(['>=', 'ccfr_created_dt', Employee::convertTimeFromUserDtToUTC(strtotime($this->ccfr_created_dt))])
+                ->andFilterWhere(['<=', 'ccfr_created_dt', Employee::convertTimeFromUserDtToUTC(strtotime($this->ccfr_created_dt) + 3600 * 24)]);
+        }
+
+        return $dataProvider;
+    }
+}
