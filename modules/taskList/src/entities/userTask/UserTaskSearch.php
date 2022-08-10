6f7b77fab7fc308edddfe7349d371a3241d7141c
Esto Boston <esto.boston@techork.com>
2022-08-10 18:23:51 +0600

    feat: my task list filter date (P2BPT-1448)

--- modules/taskList/src/entities/userTask/UserTaskSearch.php
+++ modules/taskList/src/entities/userTask/UserTaskSearch.php
@@ -162,15 +162,15 @@ class UserTaskSearch extends UserTask
      *
      * @param array $params
      * @param int $userId
-     * @param string|null $startDate
-     * @param string|null $endDate
+     * @param \DateTime|null $startDate
+     * @param \DateTime|null $endDate
      * @return ActiveDataProvider
      */
     public function searchByUserId(
         array $params,
         int $userId,
-        ?string $startDate = null,
-        ?string $endDate = null
+        ?\DateTime $startDate = null,
+        ?\DateTime $endDate = null
     ): ActiveDataProvider {
         $query = UserTask::find();
 
@@ -191,11 +191,11 @@ class UserTaskSearch extends UserTask
         $query->andWhere(['ut_user_id' => $userId]);
 
         if (!empty($startDate) && !empty($endDate)) {
-            $this->clientStartDate = $startDate;
-            $this->clientEndDate = $endDate;
+            $this->clientStartDate = $startDate->format('Y-m-d');
+            $this->clientEndDate = $endDate->format('Y-m-d');
 
-            $startDateTime = Employee::convertTimeFromUserDtToUTC(strtotime($startDate));
-            $endDateTime = Employee::convertTimeFromUserDtToUTC((new \DateTime($endDate))->setTime(23, 59, 59)->getTimestamp());
+            $startDateTime = Employee::convertTimeFromUserDtToUTC($startDate->getTimestamp());
+            $endDateTime = Employee::convertTimeFromUserDtToUTC($endDate->getTimestamp());
             $query->andWhere([
                 'OR',
                 ['between', 'ut_start_dt', $startDateTime, $endDateTime],
