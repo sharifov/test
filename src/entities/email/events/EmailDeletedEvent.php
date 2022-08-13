3d576cf93ee18385ef757899ce667c7ae545572c
Lana Bates <lana.bates@techork.com>
2022-08-13 19:09:02 +0300

    TMS-876 feat:EmailDeletedEvent add property emailId

--- src/entities/email/events/EmailDeletedEvent.php
+++ src/entities/email/events/EmailDeletedEvent.php
@@ -8,16 +8,19 @@ use src\entities\email\EmailBody;
  * Class EmailDeletedEvent
  *
  * @property EmailBody $emailBody
+ * @property int $emailId
  */
 class EmailDeletedEvent
 {
     public $emailBody;
+    public $emailId;
 
     /**
      * @property EmailBody $emailBody
      */
-    public function __construct(EmailBody $emailBody)
+    public function __construct(EmailBody $emailBody, int $emailId)
     {
         $this->emailBody = $emailBody;
+        $this->emailId = $emailId;
     }
 }
