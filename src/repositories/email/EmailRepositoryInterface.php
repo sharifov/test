b8d801baaa2bf01bf971fac8b99e5bea4f1c5dcb
Lana Bates <lana.bates@techork.com>
2022-08-16 22:33:53 +0300

    TMS-862 ref:changeStatus removed from EmailRepositoryInterface

--- src/repositories/email/EmailRepositoryInterface.php
+++ src/repositories/email/EmailRepositoryInterface.php
@@ -40,8 +40,6 @@ interface EmailRepositoryInterface
 
     public function getTableName(): string;
 
-    public function changeStatus(Email $email, int $statusId): void;
-
     public function getRawSqlCountGroupedByLead(): string;
 
     public function getRawSqlCountGroupedByCase(): string;
