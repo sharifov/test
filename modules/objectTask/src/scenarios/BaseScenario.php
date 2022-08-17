2db259841338c199a13d351ee58ef9dc9e5bfc80
Ronson Gordy <ronson.gordy@kiv.dev>
2022-08-17 19:10:46 +0600

    feat: added base classes (P2BPT-1260)

---
+++ modules/objectTask/src/scenarios/BaseScenario.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace modules\objectTask\src\scenarios;
+
+use modules\objectTask\src\entities\ObjectTaskScenario;
+
+abstract class BaseScenario
+{
+    protected ?ObjectTaskScenario $objectTaskScenario = null;
+
+    public function __construct()
+    {
+        $this->objectTaskScenario = ObjectTaskScenario::find()
+            ->where([
+                'ots_key' => static::KEY,
+            ])
+            ->limit(1)
+            ->one();
+    }
+
+    abstract public function process(): void;
+
+    public function getConfig(): array
+    {
+        return $this->objectTaskScenario->ots_data_json;
+    }
+
+    public function getConfigParameter(string $key, mixed $defaultValue = null): mixed
+    {
+        return \yii\helpers\ArrayHelper::getValue(
+            $this->getConfig(),
+            $key,
+            $defaultValue
+        );
+    }
+}
