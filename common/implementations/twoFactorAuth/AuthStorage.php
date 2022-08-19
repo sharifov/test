d39e36b4f9efcb50ef8809d35b6d3b71ee333a11
shakarim <safi.shain@techork.com>
2022-08-19 15:13:56 +0600

    feat: add new data storage implementation for 2fa module. We are use `redis` instead of file cache (TMS-867)

---
+++ common/implementations/twoFactorAuth/AuthStorage.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace common\implementations\twoFactorAuth;
+
+use kivork\TwoFactorAuth\Storage\StorageBaseInterface;
+
+/**
+ * Class AuthStorage
+ * @package common\implementations\twoFactorAuth
+ */
+class AuthStorage implements StorageBaseInterface
+{
+    /** @var array $options */
+    public $options = [];
+
+    /**
+     * @param $key
+     * @param $value
+     * @param $duration
+     * @return mixed
+     */
+    public function set($key, $value, $duration)
+    {
+        return \Yii::$app->redis->set($key, serialize($value));
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        $result = \Yii::$app->redis->get($key);
+        return is_null($result) ? null : unserialize($result);
+    }
+}
