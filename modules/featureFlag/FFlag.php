80a6c674172cff64adb38eb5e9e2f09e4040d060
Esto Boston <esto.boston@techork.com>
2022-08-22 16:46:37 +0600

    feat: userConversion exclude alternative lead (P2BPT-1479)

--- modules/featureFlag/FFlag.php
+++ modules/featureFlag/FFlag.php
@@ -57,6 +57,7 @@ class FFlag implements FeatureFlagObjectModelInterface
     public const FF_KEY_CHANGE_QUERY_GET_SEND_QUOTE = 'changeQueryGetSendQuote';
     public const FF_KEY_REMOVE_LIMITATION_SUPERVISION_IN_LEAD_SEARCH = 'removeLimitationSupervisionInLeadSearch';
     public const FF_KEY_NO_ANSWER_PROTOCOL_ENABLE = 'noAnswerProtocolEnable';
+    public const FF_KEY_USER_CONVERSION_EXCLUDE_ALTERNATIVE_LEAD_ENABLE = 'userConversionExcludeAlternativeLeadEnable';
 
     public const FF_KEY_LIST = [
         self::FF_KEY_LPP_ENABLE,
@@ -104,6 +105,7 @@ class FFlag implements FeatureFlagObjectModelInterface
         self::FF_KEY_CHANGE_QUERY_GET_SEND_QUOTE,
         self::FF_KEY_REMOVE_LIMITATION_SUPERVISION_IN_LEAD_SEARCH,
         self::FF_KEY_NO_ANSWER_PROTOCOL_ENABLE,
+        self::FF_KEY_USER_CONVERSION_EXCLUDE_ALTERNATIVE_LEAD_ENABLE,
     ];
 
     public const FF_OBJECT_LIST = [
