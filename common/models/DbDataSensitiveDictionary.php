d4a7d1a35eff33e53ec5ee3c37921f1ac4bd2fb3
Ady Pryce <ady.pryce@kiv.dev>
2022-08-31 11:13:31 +0300

    - Move source data from migration file to new class DbDataSensitiveDictionary.

---
+++ common/models/DbDataSensitiveDictionary.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace common\models;
+
+class DbDataSensitiveDictionary
+{
+    public const SOURCE = [
+        'contact_phone_list' => ['cpl_phone_number'],
+        'phone_blacklist' => ['pbl_phone'],
+        'email_list' => ['el_email'],
+        'phone_list' => ['pl_phone_number'],
+        'email' => [
+            'e_email_from',
+            'e_email_to',
+            'e_email_cc',
+            'e_email_bc',
+            'e_email_body_text',
+            'e_attach',
+            'e_email_from_name',
+            'e_email_to_name',
+            'e_message_id',
+            'e_ref_message_id',
+        ],
+        'leads' => [
+            'l_client_first_name',
+            'l_client_last_name',
+            'l_client_phone',
+            'l_client_email',
+            'additional_information',
+        ],
+        'sms' => [
+            's_phone_from',
+            's_phone_to',
+            's_sms_text',
+            's_sms_data',
+        ],
+        'sms_distribution_list' => [
+            'sdl_phone_from',
+            'sdl_phone_to',
+        ],
+        'invoice' => [
+            'inv_description',
+        ],
+        'flight_quote' => [
+            'fq_json_booking',
+            'fq_ticket_json',
+        ],
+        'api_user' => [
+            'au_api_username',
+            'au_api_password',
+            'au_email',
+        ],
+        'attraction_pax' => [
+            'atnp_first_name',
+            'atnp_last_name',
+        ],
+        'hotel_room_pax' => [
+            'hrp_first_name',
+            'hrp_last_name',
+        ],
+        'hotel_quote' => [
+            'hq_json_booking',
+        ],
+        'cruise_quote' => ['crq_data_json'],
+        'credit_card' => [
+            'cc_number',
+            'cc_display_number',
+            'cc_holder_name',
+            'cc_expiration_month',
+            'cc_expiration_year',
+            'cc_cvv',
+            'cc_security_hash',
+        ],
+        'clients' => [
+            'first_name',
+            'middle_name',
+            'last_name',
+        ],
+        'client_phone' => ['phone'],
+        'client_email' => ['email'],
+        'client_account' => [
+            'ca_username',
+            'ca_first_name',
+            'ca_middle_name',
+            'ca_last_name',
+            'ca_phone',
+            'ca_email',
+        ],
+        //'client_account_social' => ['cas_identity'],
+        'billing_info' => [
+            'bi_first_name',
+            'bi_last_name',
+            'bi_middle_name',
+            'bi_address_line1',
+            'bi_address_line2',
+            'bi_contact_phone',
+            'bi_contact_email',
+            'bi_contact_name',
+        ],
+        'call_log_record' => ['clr_record_sid'],
+        'case_sale' => [
+            'css_sale_data',
+            'css_sale_data_updated',
+        ],
+        'conference' => [
+            'cf_recording_url',
+            'cf_recording_sid',
+        ],
+        'email_unsubscribe' => ['eu_email'],
+        'employee_contact_info' => ['email_pass'],
+        'lead_qcall' => ['lqc_call_from'],
+        'projects' => ['api_key'],
+        'call_log' => [
+            'cl_phone_from',
+            'cl_phone_to',
+        ],
+        'sale_ticket' => [
+            'st_client_name',
+        ],
+        'call' => [
+            'c_from',
+            'c_to',
+            'c_forwarded_from',
+            'c_caller_name',
+            'c_recording_url',
+        ],
+        'product_holder' => [
+            'ph_first_name',
+            'ph_last_name',
+            'ph_middle_name',
+            'ph_email',
+            'ph_phone_number',
+        ],
+        'coupon' => [
+            'c_code',
+        ],
+        'phone_blacklist_log' => ['pbll_phone'],
+        'hotel_quote_room_pax' => ['hqrp_first_name', 'hqrp_last_name'],
+        'order_contact' => [
+            'oc_first_name',
+            'oc_last_name',
+            'oc_middle_name',
+            'oc_email',
+            'oc_phone_number',
+        ],
+    ];
+}
