7e4c272f2b505d7233a1a2623eb355424a1f3219
Ady Pryce <ady.pryce@kiv.dev>
2022-08-31 13:01:17 +0300

    - Add virtual cron job for regeneration Views.

---
+++ console/migrations/m220831_094807_add_cron_job_regenerate_default_sensitive_views.php
@@ -0,0 +1,58 @@
+<?php
+
+use yii\db\Migration;
+use kivork\VirtualCron\Services\Hasher;
+use kivork\VirtualCron\Models\CronScheduler;
+
+/**
+ * Class m220831_094807_add_cron_job_regenerate_default_sensitive_views
+ */
+class m220831_094807_add_cron_job_regenerate_default_sensitive_views extends Migration
+{
+    public const COMMAND = 'db/regenerate-default-sensitive-views';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        try {
+            $cronScheduler = new CronScheduler();
+            $cronScheduler->cs_cron_expression = '* 2 * * *';
+            $cronScheduler->cs_cron_command = self::COMMAND;
+            $cronScheduler->cs_description = 'Regenerate views from db_data_sensitive (default record)';
+            $cronScheduler->cs_enabled = false;
+            $cronScheduler->cs_hash = Hasher::generate(
+                $cronScheduler->cs_cron_expression,
+                $cronScheduler->cs_cron_command,
+                $cronScheduler->cs_cron_params
+            );
+
+            if (!$cronScheduler->save()) {
+                throw new \RuntimeException(\src\helpers\ErrorsToStringHelper::extractFromModel($cronScheduler, ' '));
+            }
+            echo ' - Hash: ' . $cronScheduler->cs_hash . PHP_EOL;
+        } catch (Throwable $throwable) {
+            Yii::error($throwable, 'm220831_094807_add_cron_job_regenerate_default_sensitive_views:safeUp:Throwable');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        try {
+            $cs = CronScheduler::find()->where(['cs_cron_command' => self::COMMAND])->one();
+            if ($cs) {
+                if ($cs->delete()) {
+                    echo ' - Delete: OK' . PHP_EOL;
+                } else {
+                    Yii::error($cs->errors, 'm220831_094807_add_cron_job_regenerate_default_sensitive_views:safeDown');
+                }
+            }
+        } catch (Throwable $throwable) {
+            Yii::error($throwable, 'm220831_094807_add_cron_job_regenerate_default_sensitive_views:safeDown:Throwable');
+        }
+    }
+}
