d2981e3a41b9f19f3dc148a5626a2b69a3601130
Lana Bates <lana.bates@techork.com>
2022-09-05 14:32:45 +0300

    feat:isEmpty and isEmptyVal methods in FormAttributesTrait (TMS-930)

--- src/entities/email/form/FormAttributesTrait.php
+++ src/entities/email/form/FormAttributesTrait.php
@@ -13,10 +13,28 @@ trait FormAttributesTrait
         $result = [];
         foreach ($this->fields() as $index => $name) {
             $key = is_int($index) ? $name : $index;
-            if (!$skipEmpty || !empty($this->$name)) {
+            if (!$skipEmpty || !$this->isEmptyVal($this->$name)) {
                 $result[$key] = $this->$name;
             }
         }
         return $result;
+    }
+
+    public function isEmptyVal($val): bool
+    {
+        return  !isset($val) ||
+            $val == null ||
+            (is_array($val) && count($val) == 0) ||
+            (is_string($val) && strlen($val) == 0);
+    }
+
+    public function isEmpty(): bool
+    {
+        foreach ($this->attributes() as $attribute) {
+            if (!empty($this->$attribute)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
