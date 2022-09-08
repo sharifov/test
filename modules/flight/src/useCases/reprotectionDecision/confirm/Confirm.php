6ff6d0402a82683b906313d3070b0656beea0e68
mikhael.celso <mikhael.celso@techork.com>
2022-09-08 09:17:06 +0300

    fix: "Wrong case got the Awaiting status". New method to find case by status and product quote. New services (AOP-1118)

--- modules/flight/src/useCases/reprotectionDecision/confirm/Confirm.php
+++ modules/flight/src/useCases/reprotectionDecision/confirm/Confirm.php
@@ -3,6 +3,7 @@
 namespace modules\flight\src\useCases\reprotectionDecision\confirm;
 
 use modules\flight\src\useCases\reprotectionDecision\CancelOtherReprotectionQuotes;
+use modules\flight\src\useCases\services\cases\CaseService;
 use modules\product\src\entities\productQuote\ProductQuote;
 use modules\product\src\entities\productQuote\ProductQuoteStatus;
 use modules\product\src\entities\productQuoteChange\ProductQuoteChange;
@@ -12,6 +13,7 @@ use modules\product\src\entities\productQuoteChange\ProductQuoteChangeStatus;
 use modules\product\src\entities\productQuoteData\ProductQuoteData;
 use modules\product\src\entities\productQuoteData\ProductQuoteDataRepository;
 use src\entities\cases\CaseEventLog;
+use src\entities\cases\Cases;
 use src\helpers\setting\SettingHelper;
 use src\repositories\product\ProductQuoteRepository;
 use src\services\TransactionManager;
@@ -23,6 +25,7 @@ use src\services\TransactionManager;
  * @property TransactionManager $transactionManager
  * @property ProductQuoteChangeRepository $productQuoteChangeRepository
  * @property CancelOtherReprotectionQuotes $cancelOtherReprotectionQuotes
+ * @property CaseService $caseService
  */
 class Confirm
 {
@@ -31,19 +34,22 @@ class Confirm
     private ProductQuoteChangeRepository $productQuoteChangeRepository;
     private ProductQuoteDataRepository $productQuoteDataRepository;
     private CancelOtherReprotectionQuotes $cancelOtherReprotectionQuotes;
+    private CaseService $caseService;
 
     public function __construct(
         ProductQuoteRepository $productQuoteRepository,
         TransactionManager $transactionManager,
         ProductQuoteChangeRepository $productQuoteChangeRepository,
         CancelOtherReprotectionQuotes $cancelOtherReprotectionQuotes,
-        ProductQuoteDataRepository $productQuoteDataRepository
+        ProductQuoteDataRepository $productQuoteDataRepository,
+        CaseService $caseService
     ) {
         $this->productQuoteRepository = $productQuoteRepository;
         $this->transactionManager = $transactionManager;
         $this->productQuoteChangeRepository = $productQuoteChangeRepository;
         $this->cancelOtherReprotectionQuotes = $cancelOtherReprotectionQuotes;
         $this->productQuoteDataRepository = $productQuoteDataRepository;
+        $this->caseService = $caseService;
     }
 
     public function handle(string $reprotectionQuoteGid, ?int $userId): void
@@ -65,15 +71,27 @@ class Confirm
             throw new \DomainException('Product Quote Change status is not in one of allowable statuses(' . implode(',', $processingListAllowable) . ') or is not Schedule Change. Current status "' . ProductQuoteChangeStatus::getName($productQuoteChange->pqc_status_id) . '"; Current Type: "' . $productQuoteChange->getTypeName() . '"', 101);
         }
 
-        $this->transactionManager->wrap(function () use ($reprotectionQuote, $productQuoteChange, $userId) {
-            $this->confirmProductQuoteChange($productQuoteChange);
-            $this->inProgressProductQuoteChange($productQuoteChange);
+        $case = $this->caseService->getCase($productQuoteChange, $reprotectionQuote);
+        if (!$case) {
+            \Yii::warning([
+                'message' => 'Case not found by Product Quote Change (' . $productQuoteChange->pqc_id . ') or by Product Quote (' . $reprotectionQuote->pq_id . ')',
+                'reprotectionQuoteId' => $reprotectionQuote->pq_id,
+                'reprotectionQuoteGid' => $reprotectionQuote->pq_gid,
+                'productQuoteChangeId' => $productQuoteChange->pqc_id,
+                'productQuoteChangeGid' => $productQuoteChange->pqc_gid,
+            ], 'FlightController:reprotectionDecision:confirm:DomainException');
+            throw new \DomainException('Case not found by Product Quote Change (' . $productQuoteChange->pqc_id . ') or by Product Quote (' . $reprotectionQuote->pq_id . ')');
+        }
+
+        $this->transactionManager->wrap(function () use ($reprotectionQuote, $productQuoteChange, $userId, $case) {
+            $this->confirmProductQuoteChange($productQuoteChange, $case);
+            $this->inProgressProductQuoteChange($productQuoteChange, $case);
             $this->markQuoteToApplied($reprotectionQuote);
             $this->cancelOtherReprotectionQuotes->cancelByQuoteChange($productQuoteChange, $reprotectionQuote, $userId);
-            $this->processingProductQuoteChange($productQuoteChange, $userId, $reprotectionQuote);
+            $this->processingProductQuoteChange($productQuoteChange, $userId, $reprotectionQuote, $case);
         });
 
-        $this->createBoRequestJob($reprotectionQuote, $userId);
+        $this->createBoRequestJob($reprotectionQuote, $userId, $case);
 
         if (!empty($reprotectionQuote->pq_id)) {
             $productQuoteData = ProductQuoteData::createConfirmed($reprotectionQuote->pq_id);
@@ -81,23 +99,23 @@ class Confirm
         }
     }
 
-    private function processingProductQuoteChange(ProductQuoteChange $change, ?int $userId, $reprotectionQuote): void
+    private function processingProductQuoteChange(ProductQuoteChange $change, ?int $userId, $reprotectionQuote, Cases $case): void
     {
         $change->customerDecisionConfirm($userId, new \DateTimeImmutable());
         $this->productQuoteChangeRepository->save($change);
-        CaseEventLog::add($change->pqc_case_id, CaseEventLog::REPROTECTION_DECISION, 'Flight reprotection decided: ' . ProductQuoteChangeDecisionType::LIST[ProductQuoteChangeDecisionType::CONFIRM], [
-                'productQuoteChangeId' => $change->pqc_id,
-                'confirmedProductQuoteId' => $reprotectionQuote->pq_id,
-                'originalProductQuoteId' => $reprotectionQuote->pqRelation->originProductQuote->pq_id,
-            ]);
+        CaseEventLog::add($case->cs_id, CaseEventLog::REPROTECTION_DECISION, 'Flight reprotection decided: ' . ProductQuoteChangeDecisionType::LIST[ProductQuoteChangeDecisionType::CONFIRM], [
+            'productQuoteChangeId' => $change->pqc_id,
+            'confirmedProductQuoteId' => $reprotectionQuote->pq_id,
+            'originalProductQuoteId' => $reprotectionQuote->pqRelation->originProductQuote->pq_id,
+        ]);
     }
 
-    private function confirmProductQuoteChange(ProductQuoteChange $change): void
+    private function confirmProductQuoteChange(ProductQuoteChange $change, Cases $case): void
     {
         $fromStatus = $change->getSystemStatusName();
         $change->statusToComplete();
         $this->productQuoteChangeRepository->save($change);
-        CaseEventLog::add($change->pqc_case_id, CaseEventLog::REPROTECTION_DECISION, 'Product Quote Change updated status', [
+        CaseEventLog::add($case->cs_id, CaseEventLog::REPROTECTION_DECISION, 'Product Quote Change updated status', [
             'gid' => $change->pqc_gid,
             'fromStatus' => $fromStatus,
             'toStatus' => $change->getSystemStatusName(),
@@ -105,12 +123,12 @@ class Confirm
         ], CaseEventLog::CATEGORY_DEBUG);
     }
 
-    private function inProgressProductQuoteChange(ProductQuoteChange $change): void
+    private function inProgressProductQuoteChange(ProductQuoteChange $change, Cases $case): void
     {
         $fromStatus = $change->getSystemStatusName();
         $change->inProgress();
         $this->productQuoteChangeRepository->save($change);
-        CaseEventLog::add($change->pqc_case_id, CaseEventLog::REPROTECTION_DECISION, 'Product Quote Change updated status', [
+        CaseEventLog::add($case->cs_id, CaseEventLog::REPROTECTION_DECISION, 'Product Quote Change updated status', [
             'gid' => $change->pqc_gid,
             'fromStatus' => $fromStatus,
             'toStatus' => $change->getSystemStatusName(),
@@ -118,11 +136,12 @@ class Confirm
         ], CaseEventLog::CATEGORY_DEBUG);
     }
 
-    private function createBoRequestJob(ProductQuote $quote, ?int $userId): void
+    private function createBoRequestJob(ProductQuote $quote, ?int $userId, Cases $case): void
     {
         $boJob = new BoRequestJob();
         $boJob->quoteGid = $quote->pq_gid;
         $boJob->userId = $userId;
+        $boJob->caseId = $case->cs_id;
         $jobId = \Yii::$app->queue_job->push($boJob);
         if (!$jobId) {
             \Yii::error([
--- modules/flight/src/useCases/reprotectionDecision/modify/Modify.php
+++ modules/flight/src/useCases/reprotectionDecision/modify/Modify.php
@@ -2,9 +2,10 @@
 
 namespace modules\flight\src\useCases\reprotectionDecision\modify;
 
-use modules\flight\models\FlightQuoteFlight;
 use modules\flight\src\useCases\flightQuote\FlightQuoteManageService;
 use modules\flight\src\useCases\reprotectionDecision\CancelOtherReprotectionQuotes;
+use modules\flight\src\useCases\services\cases\CaseService;
+use modules\flight\src\useCases\services\productQuote\ProductQuoteService;
 use modules\product\src\entities\productQuote\ProductQuote;
 use modules\product\src\entities\productQuoteChange\ProductQuoteChange;
 use modules\product\src\entities\productQuoteChange\ProductQuoteChangeDecisionType;
@@ -16,6 +17,7 @@ use modules\product\src\entities\productQuoteData\service\ProductQuoteDataManage
 use modules\product\src\entities\productQuoteRelation\ProductQuoteRelation;
 use modules\product\src\repositories\ProductQuoteRelationRepository;
 use src\entities\cases\CaseEventLog;
+use src\entities\cases\Cases;
 use src\repositories\product\ProductQuoteRepository;
 use src\services\TransactionManager;
 
@@ -29,6 +31,8 @@ use src\services\TransactionManager;
  * @property FlightQuoteManageService $flightQuoteManageService
  * @property ProductQuoteRelationRepository $productQuoteRelationRepository
  * @property ProductQuoteDataManageService $productQuoteDataManageService
+ * @property CaseService $caseService
+ * @property ProductQuoteService $productQuoteService
  */
 class Modify
 {
@@ -39,6 +43,8 @@ class Modify
     private FlightQuoteManageService $flightQuoteManageService;
     private ProductQuoteRelationRepository $productQuoteRelationRepository;
     private ProductQuoteDataManageService $productQuoteDataManageService;
+    private CaseService $caseService;
+    private ProductQuoteService $productQuoteService;
 
     public function __construct(
         ProductQuoteRepository $productQuoteRepository,
@@ -47,7 +53,9 @@ class Modify
         CancelOtherReprotectionQuotes $cancelOtherReprotectionQuotes,
         FlightQuoteManageService $flightQuoteManageService,
         ProductQuoteRelationRepository $productQuoteRelationRepository,
-        ProductQuoteDataManageService $productQuoteDataManageService
+        ProductQuoteDataManageService $productQuoteDataManageService,
+        CaseService $caseService,
+        ProductQuoteService $productQuoteService
     ) {
         $this->productQuoteRepository = $productQuoteRepository;
         $this->productQuoteChangeRepository = $productQuoteChangeRepository;
@@ -56,24 +64,38 @@ class Modify
         $this->flightQuoteManageService = $flightQuoteManageService;
         $this->productQuoteRelationRepository = $productQuoteRelationRepository;
         $this->productQuoteDataManageService = $productQuoteDataManageService;
+        $this->caseService = $caseService;
+        $this->productQuoteService = $productQuoteService;
     }
 
     public function handle(string $bookingId, array $newQuote, ?int $userId): void
     {
-        $originalProductQuote = $this->getProductQuote($bookingId);
+        $originalProductQuote = $this->productQuoteService->getProductQuote($bookingId);
 
         $productQuoteChange = $this->productQuoteChangeRepository->findByProductIdAndType($originalProductQuote->pq_id, ProductQuoteChange::TYPE_RE_PROTECTION);
         if (!$productQuoteChange->isPending() || !$productQuoteChange->isTypeReProtection()) {
             throw new \DomainException('Product Quote Change status is not in "pending" or is not Schedule Change. Current status "' . ProductQuoteChangeStatus::getName($productQuoteChange->pqc_status_id) . '"; Current Type: "' . $productQuoteChange->getTypeName() . '"', 101);
         }
 
-        $quote = $this->transactionManager->wrap(function () use ($originalProductQuote, $newQuote, $productQuoteChange, $userId) {
+        $case = $this->caseService->getCase($productQuoteChange, $originalProductQuote);
+        if (!$case) {
+            \Yii::warning([
+                'message' => 'Case not found by Product Quote Change (' . $productQuoteChange->pqc_id . ') or by Product Quote (' . $originalProductQuote->pq_id . ')',
+                'originalProductQuote' => $originalProductQuote->pq_id,
+                'originalProductQuoteGid' => $originalProductQuote->pq_gid,
+                'productQuoteChangeId' => $productQuoteChange->pqc_id,
+                'productQuoteChangeGid' => $productQuoteChange->pqc_gid,
+            ], 'FlightController:reprotectionDecision:modify:DomainException');
+            throw new \DomainException('Case not found by Product Quote Change (' . $productQuoteChange->pqc_id . ') or by Product Quote (' . $originalProductQuote->pq_id . ')');
+        }
+
+        $quote = $this->transactionManager->wrap(function () use ($originalProductQuote, $newQuote, $productQuoteChange, $userId, $case) {
             $quote = $this->createNewReprotectionQuote($originalProductQuote, $newQuote, $userId);
             $this->markQuoteToApplied($quote);
             $this->cancelOtherReprotectionQuotes->cancelByQuoteChange($productQuoteChange, $quote, $userId);
-            $this->confirmProductQuoteChange($productQuoteChange);
-            $this->inProgressProductQuoteChange($productQuoteChange);
-            $this->modifyProductQuoteChange($productQuoteChange, $userId);
+            $this->confirmProductQuoteChange($productQuoteChange, $case);
+            $this->inProgressProductQuoteChange($productQuoteChange, $case);
+            $this->modifyProductQuoteChange($productQuoteChange, $userId, $case);
 
             $productQuoteChangeRelation = ProductQuoteChangeRelation::create(
                 $productQuoteChange->pqc_id,
@@ -85,35 +107,22 @@ class Modify
 
         $this->productQuoteDataManageService->updateRecommendedChangeQuote($originalProductQuote->pq_id, $quote->pq_id);
 
-        $this->createBoRequestJob($quote, $userId);
+        $this->createBoRequestJob($quote, $userId, $case);
     }
 
-    private function getProductQuote(string $bookingId): ProductQuote
-    {
-        $flight = FlightQuoteFlight::find()->andWhere(['fqf_booking_id' => $bookingId])->orderBy(['fqf_id' => SORT_DESC])->one();
-        if (!$flight) {
-            throw new \DomainException('Not found Flight Quote Flight. BookingId: ' . $bookingId);
-        }
-        $productQuote = $flight->fqfFq->fqProductQuote ?? null;
-        if ($productQuote) {
-            return $productQuote;
-        }
-        throw new \DomainException('Not found Product Quote. BookingId: ' . $bookingId);
-    }
-
-    private function modifyProductQuoteChange(ProductQuoteChange $change, ?int $userId): void
+    private function modifyProductQuoteChange(ProductQuoteChange $change, ?int $userId, Cases $case): void
     {
         $change->customerDecisionModify($userId, new \DateTimeImmutable());
         $this->productQuoteChangeRepository->save($change);
-        CaseEventLog::add($change->pqc_case_id, CaseEventLog::REPROTECTION_DECISION, 'Flight reprotection decided: ' . ProductQuoteChangeDecisionType::LIST[ProductQuoteChangeDecisionType::MODIFY]);
+        CaseEventLog::add($case->cs_id, CaseEventLog::REPROTECTION_DECISION, 'Flight reprotection decided: ' . ProductQuoteChangeDecisionType::LIST[ProductQuoteChangeDecisionType::MODIFY]);
     }
 
-    private function confirmProductQuoteChange(ProductQuoteChange $change): void
+    private function confirmProductQuoteChange(ProductQuoteChange $change, Cases $case): void
     {
         $fromStatus = $change->getSystemStatusName();
         $change->statusToComplete();
         $this->productQuoteChangeRepository->save($change);
-        CaseEventLog::add($change->pqc_case_id, CaseEventLog::REPROTECTION_DECISION, 'Product Quote Change updated status', [
+        CaseEventLog::add($case->cs_id, CaseEventLog::REPROTECTION_DECISION, 'Product Quote Change updated status', [
             'gid' => $change->pqc_gid,
             'fromStatus' => $fromStatus,
             'toStatus' => $change->getSystemStatusName(),
@@ -121,12 +130,12 @@ class Modify
         ], CaseEventLog::CATEGORY_DEBUG);
     }
 
-    private function inProgressProductQuoteChange(ProductQuoteChange $change): void
+    private function inProgressProductQuoteChange(ProductQuoteChange $change, Cases $case): void
     {
         $fromStatus = $change->getSystemStatusName();
         $change->inProgress();
         $this->productQuoteChangeRepository->save($change);
-        CaseEventLog::add($change->pqc_case_id, CaseEventLog::REPROTECTION_DECISION, 'Product Quote Change updated status', [
+        CaseEventLog::add($case->cs_id, CaseEventLog::REPROTECTION_DECISION, 'Product Quote Change updated status', [
             'gid' => $change->pqc_gid,
             'fromStatus' => $fromStatus,
             'toStatus' => $change->getSystemStatusName(),
@@ -134,11 +143,12 @@ class Modify
         ], CaseEventLog::CATEGORY_DEBUG);
     }
 
-    private function createBoRequestJob(ProductQuote $quote, ?int $userId): void
+    private function createBoRequestJob(ProductQuote $quote, ?int $userId, Cases $case): void
     {
         $boJob = new BoRequestJob();
         $boJob->quoteGid = $quote->pq_gid;
         $boJob->userId = $userId;
+        $boJob->caseId = $case->cs_id;
         $jobId = \Yii::$app->queue_job->push($boJob);
         if (!$jobId) {
             \Yii::error([
