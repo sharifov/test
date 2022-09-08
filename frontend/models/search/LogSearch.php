654f69b9292831ccca9660e7fa1a53c25c894826
Ronson Gordy <ronson.gordy@kiv.dev>
2022-09-08 17:26:14 +0600

    feat: added improvements for search block in system logs (P2BPT-1650)

--- frontend/models/search/LogSearch.php
+++ frontend/models/search/LogSearch.php
@@ -2,6 +2,8 @@
 
 namespace frontend\models\search;
 
+use frontend\models\LogQuery;
+use src\helpers\DateHelper;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
@@ -14,28 +16,21 @@ use frontend\models\Log;
 class LogSearch extends Log
 {
     public $days;
-
     public $excludedCategories;
+    public $createdDateTimeRange;
 
     /**
      * @inheritdoc
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             [['id', 'level'], 'integer'],
             [['category', 'prefix', 'message', 'log_time', 'excludedCategories'], 'safe'],
             [['days'], 'integer', 'min' => 0, 'max' => 365],
-            ['excludedCategories', 'each', 'rule' => ['string']]
+            ['excludedCategories', 'each', 'rule' => ['string']],
+            [['createdDateTimeRange'], 'match', 'pattern' => '/^.+\s\-\s.+$/'],
         ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function scenarios()
-    {
-        return Model::scenarios();
     }
 
     /**
@@ -45,32 +40,114 @@ class LogSearch extends Log
      *
      * @return ActiveDataProvider
      */
-    public function search($params)
+    public function search(array $params): ActiveDataProvider
     {
         $query = Log::find();
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
+
         $this->load($params);
+
         if (!$this->validate()) {
             return $dataProvider;
         }
+
+        $this->searchQuery($query);
+        $query->orderBy(['id' => SORT_DESC]);
+
+        return $dataProvider;
+    }
+
+    protected function searchQuery(LogQuery $query, bool $ignoreCategoryFilter = false)
+    {
         $query->andFilterWhere([
             'id' => $this->id,
             'level' => $this->level,
         ]);
+
+        if ($this->createdDateTimeRange) {
+            $dates = DateHelper::getDatesFromDateRangeString($this->createdDateTimeRange);
+            $query->andFilterWhere([
+                'BETWEEN',
+                'log_time',
+                strtotime(Employee::convertTimeFromUserDtToUTC(strtotime($dates['start']))),
+                strtotime(Employee::convertTimeFromUserDtToUTC(strtotime($dates['end']))),
+            ]);
+        }
+
         if (empty($this->log_time) === false) {
             $from = Employee::convertTimeFromUserDtToUTC(strtotime($this->log_time));
             $from = strtotime($from);
             $to = strtotime(Employee::convertTimeFromUserDtToUTC(strtotime($this->log_time) + 3600 * 24));
             $query->andOnCondition('log_time >= :from AND log_time <= :to', array(':from' => $from, ':to' => $to));
         }
-        $query->andFilterWhere(['like', 'category', $this->category])
-            ->andFilterWhere(['like', 'prefix', $this->prefix])
+
+        if ($ignoreCategoryFilter === false) {
+            /** @fflag FFlag::FF_KEY_SYSTEM_LOG_SEARCH_BLOCK_IMPROVEMENTS_ENABLE, Enable improvements in system log search block */
+            if (\Yii::$app->featureFlag->isEnable(\modules\featureFlag\FFlag::FF_KEY_SYSTEM_LOG_SEARCH_BLOCK_IMPROVEMENTS_ENABLE) === true) {
+                $query->andFilterWhere(['category' => $this->category]);
+            } else {
+                $query->andFilterWhere(['LIKE', 'category', $this->category]);
+            }
+        }
+
+        $query->andFilterWhere(['like', 'prefix', $this->prefix])
             ->andFilterWhere(['like', 'message', $this->message])
             ->andFilterWhere(['NOT IN', 'category', $this->excludedCategories]);
+    }
 
-        $query->orderBy(['id' => SORT_DESC]);
-        return $dataProvider;
+    public function getCategoriesFilter(bool $isPjax = true): array
+    {
+        /** @fflag FFlag::FF_KEY_SYSTEM_LOG_SEARCH_BLOCK_IMPROVEMENTS_ENABLE, Enable improvements in system log search block */
+        if (\Yii::$app->featureFlag->isEnable(\modules\featureFlag\FFlag::FF_KEY_SYSTEM_LOG_SEARCH_BLOCK_IMPROVEMENTS_ENABLE) === true) {
+            $categories = $this->getCategories(true);
+        } else {
+            $categories = \frontend\models\Log::getCategoryFilter(is_numeric($this->level) ? $this->level : null, $isPjax);
+        }
+
+        return $categories;
+    }
+
+    public function getCategories(bool $countGroup = false): array
+    {
+        $arr = [];
+
+        if ($countGroup) {
+            $query = self::find();
+            $this->searchQuery($query, true);
+
+            $query->select(['COUNT(*) AS cnt', 'category'])
+                ->andWhere('category IS NOT NULL')
+                ->groupBy(['category'])
+                ->orderBy('cnt DESC')
+                ->cache(-10)
+                ->asArray();
+
+            $data = $query->all();
+
+            if ($data) {
+                foreach ($data as $v) {
+                    $arr[$v['category']] = $v['category'] . ' - [' . $v['cnt'] . ']';
+                }
+            }
+        } else {
+            $query = self::find();
+            $this->searchQuery($query, true);
+            $query->select('DISTINCT(category) AS category')
+                ->cache(-10)
+                ->orderBy('category')
+                ->asArray();
+
+            $data = $query->all();
+
+            if ($data) {
+                foreach ($data as $v) {
+                    $arr[$v['category']] = $v['category'];
+                }
+            }
+        }
+
+        return $arr;
     }
 }
