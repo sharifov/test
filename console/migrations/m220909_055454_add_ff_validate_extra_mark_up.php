42589536a6457eb378175217627473241a3c99ce
andrew.snake <andrew.snake@techork.com>
2022-09-09 09:46:06 +0300

    feat: added Feature Flag (P2BPT-1651)

--- console/migrations/m220909_055454_add_ff_validate_extra_mark_up.php
+++ console/migrations/m220909_055454_add_ff_validate_extra_mark_up.php
@@ -1,5 +1,6 @@
 <?php
 
+use src\helpers\app\AppHelper;
 use yii\db\Migration;
 
 /**
@@ -7,12 +8,36 @@ use yii\db\Migration;
  */
 class m220909_055454_add_ff_validate_extra_mark_up extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
     public function safeUp()
     {
-        /* TODO:: */
+        try {
+            if (!class_exists('\kivork\FeatureFlag\Models\FeatureFlag')) {
+                throw new \RuntimeException('Class (FeatureFlag) not found');
+            }
+            if (\kivork\FeatureFlag\Models\FeatureFlag::find()->where(['ff_key' => \modules\featureFlag\FFlag::FF_KEY_VALIDATE_CHANGE_EXTRA_MARK_UP])->exists()) {
+                throw new \RuntimeException('FeatureFlag (' . \modules\featureFlag\FFlag::FF_KEY_VALIDATE_CHANGE_EXTRA_MARK_UP . ') already exit');
+            }
+
+            $featureFlagService = new \kivork\FeatureFlag\Services\FeatureFlagService();
+            $featureFlagService::add(
+                \modules\featureFlag\FFlag::FF_KEY_VALIDATE_CHANGE_EXTRA_MARK_UP,
+                'Enable validate change Extra Mark Up',
+                \kivork\FeatureFlag\Models\FeatureFlag::TYPE_BOOL,
+                false,
+                \kivork\FeatureFlag\Models\FeatureFlag::ET_DISABLED,
+                [
+                    'ff_category' => \modules\featureFlag\FFlag::FF_CATEGORY_LEAD,
+                    'ff_description' => 'Enable validate change Extra Mark Up in lead/view page',
+                ]
+            );
+
+            \Yii::$app->featureFlag->invalidateCache();
+        } catch (\Throwable $throwable) {
+            \Yii::error(
+                AppHelper::throwableLog($throwable),
+                'm220909_055454_add_ff_validate_extra_mark_up:safeUp:Throwable'
+            );
+        }
     }
 
     /**
@@ -20,8 +45,20 @@ class m220909_055454_add_ff_validate_extra_mark_up extends Migration
      */
     public function safeDown()
     {
-        echo "m220909_055454_add_ff_validate_extra_mark_up cannot be reverted.\n";
+        try {
+            if (!class_exists('\kivork\FeatureFlag\Models\FeatureFlag')) {
+                throw new \RuntimeException('Class (FeatureFlag) not found');
+            }
 
-        return false;
+            $featureFlagService = new \kivork\FeatureFlag\Services\FeatureFlagService();
+            $featureFlagService::delete(\modules\featureFlag\FFlag::FF_KEY_VALIDATE_CHANGE_EXTRA_MARK_UP);
+
+            \Yii::$app->featureFlag->invalidateCache();
+        } catch (\Throwable $throwable) {
+            \Yii::error(
+                AppHelper::throwableLog($throwable),
+                'm220909_055454_add_ff_validate_extra_mark_up:safeDown:Throwable'
+            );
+        }
     }
 }
--- modules/featureFlag/FFlag.php
+++ modules/featureFlag/FFlag.php
@@ -66,6 +66,7 @@ class FFlag implements FeatureFlagObjectModelInterface
     public const FF_KEY_NO_ANSWER_PROTOCOL_SOLD_AUTO_REPLACE_AFK_EMPLOYEE_ENABLE = 'noAnswerProtocolSoldAutoReplaceAfkEmployee';
     public const FF_KEY_SYSTEM_LOG_SEARCH_BLOCK_IMPROVEMENTS_ENABLE = 'systemLogSearchBlockImprovementsEnable';
     public const FF_KEY_BO_API_RBAC_AUTH = 'boApiRbacAuth';
+    public const FF_KEY_VALIDATE_CHANGE_EXTRA_MARK_UP = 'validateChangeExtraMarkUp';
 
     public const FF_KEY_LIST = [
         self::FF_KEY_LPP_ENABLE,
