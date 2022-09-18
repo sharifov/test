b75ecb724181dc49799f0afd039b281ecfcba336
Ady Pryce <ady.pryce@kiv.dev>
2022-09-18 10:47:52 +0300

    feat: add FF autoRefreshLeadTaskListEnable. (P2BPT-1692)

--- modules/taskList/src/notifications/LeadTasksListSavedNotification.php
+++ modules/taskList/src/notifications/LeadTasksListSavedNotification.php
@@ -4,6 +4,7 @@ namespace modules\taskList\src\notifications;
 
 use common\models\Lead;
 use common\models\Notifications;
+use modules\featureFlag\FFlag;
 
 /**
  * @property Lead $lead
@@ -26,12 +27,17 @@ class LeadTasksListSavedNotification
      */
     public function send(bool $isSuccessSaved = true): bool
     {
-        return Notifications::pub(['lead-' . $this->lead->id], 'refreshTaskList', [
-            'data' => [
-                'gid' => $this->lead->gid,
-                'leadId' => $this->lead->id,
-                'isSuccessSaved' => $isSuccessSaved,
-            ],
-        ]);
+        /** @fflag FFlag::FF_KEY_AUTO_REFRESH_LEAD_TASK_LIST_ENABLE, Auto refresh lead task list enabled */
+        if (\Yii::$app->featureFlag->isEnable(FFlag::FF_KEY_AUTO_REFRESH_LEAD_TASK_LIST_ENABLE)) {
+            return Notifications::pub(['lead-' . $this->lead->id], 'refreshTaskList', [
+                'data' => [
+                    'gid' => $this->lead->gid,
+                    'leadId' => $this->lead->id,
+                    'isSuccessSaved' => $isSuccessSaved,
+                ],
+            ]);
+        }
+
+        return false;
     }
 }
