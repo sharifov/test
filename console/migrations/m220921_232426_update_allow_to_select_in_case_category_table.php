92d4d4a28b4b557c03c0be3352df5a873ca9f4f0
paulo.nicco <paulo.nicco@kiv.dev>
2022-09-22 09:21:20 +0300

    feat: add nested category select widget to case create page, add migration to set allow_to_select 1 by default (CCW-45)

---
+++ console/migrations/m220921_232426_update_allow_to_select_in_case_category_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220921_232426_update_allow_to_select_in_case_category_table
+ */
+class m220921_232426_update_allow_to_select_in_case_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->update('{{%case_category}}', [
+            'cc_allow_to_select' => 1,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220921_232426_update_allow_to_select_in_case_category_table cannot be reverted.\n";
+
+        return false;
+    }
+}
