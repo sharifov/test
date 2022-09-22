84563479d9d45f74588137a38d4663b94d75ff70
alex-elliot <alex.elliot@kiv.dev>
2022-09-22 15:17:51 +0300

    feat: disabling page-loader for debugging purposes (TMS-1000)

--- modules/fileStorage/src/validator/FileValidator.php
+++ modules/fileStorage/src/validator/FileValidator.php
@@ -10,6 +10,7 @@ class FileValidator extends \yii\validators\FileValidator
     {
         $this->maxSize = FileStorageSettings::getUploadMaxSize();
 //        $this->mimeTypes = array_keys(FileStorageSettings::getMimeTypes());
+//        TODO: add MIME type validation in short time
         $this->extensions = array_keys(FileStorageSettings::getAllowExt());
         $this->checkExtensionByMimeType = false;
         parent::init();
